bcfbd7203c490204c0782dc913f5314d051a5c48
Tomi Virkki <virkki@vaadin.com>
2015-08-27 09:32:15 +0300

    Did some technical drafting

--- packages/vaadin-combo-box/README.md
+++ packages/vaadin-combo-box/README.md
@@ -1,2 +1,11 @@
 vaadin-combo-box
 ============
+To run the demo:
+```shell
+$ npm install && gulp demo
+```
+
+To run the tests:
+```shell
+$ wct
+```
---
+++ packages/vaadin-combo-box/gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('demo', function () {
+  browserSync({
+    port: 5000,
+
+    server: {
+      baseDir: ['.','demo'],
+      routes: {
+        '': 'bower_components'
+      }
+    }
+  });
+
+  gulp.watch(['./*'], reload);
+  gulp.watch(['./demo/*'], reload);
+});
