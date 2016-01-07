c6d7d5c8b021f8b090ebf5de40682a8f59f7c85b
Sauli Tähkäpää <sauli@vaadin.com>
2016-01-07 15:26:48 +0200

    Remove async waits from tests.

--- packages/vaadin-combo-box/test/common.js
+++ packages/vaadin-combo-box/test/common.js
@@ -22,47 +22,6 @@ var describeIf = function(bool, title, callback) {
   describeSkipIf(!bool, title, callback);
 };
 
-var asyncDone = function(cb, done, timeout) {
-  Polymer.Base.async(function() {
-    try {
-      cb();
-      if (done) {
-        done();
-      }
-    } catch (err) {
-      if (done) {
-        done(err);
-      } else {
-        throw (err);
-      }
-    }
-  }, timeout || 1);
-};
-
-function _waitUntilOpened(open) {
-  if (!window.Promise) {
-    window.Promise = MakePromise(Polymer.Base.async);
-  }
-
-  return new Promise(function(resolve, reject) {
-    var handle = setInterval(function() {
-      var combobox = document.querySelector('vaadin-combo-box');
-      if (combobox.opened == open) {
-        clearInterval(handle);
-        resolve();
-      }
-    }, 10);
-  });
-}
-
-var waitUntilOpen = function() {
-  return _waitUntilOpened(true);
-};
-
-var waitUntilClosed = function() {
-  return _waitUntilOpened(false);
-};
-
 var getItemArray = function(length) {
   return new Array(length).join().split(',')
     .map(function(item, index) {
