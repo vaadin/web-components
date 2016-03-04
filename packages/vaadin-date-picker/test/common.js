d888231ebc2ec6ce5e49eed5cad63bb9a131665a
Manolo Carrasco <manolo@vaadin.com>
2016-03-04 12:41:20 +0100

    Implement localization via i18n object.
    - user can customise messages using i18n object
    - remove moment dependency
    - fix infinite scroller to notify parent changes
    - added more tests for i18n and mutations passed to children

--- packages/vaadin-date-picker/test/common.js
+++ packages/vaadin-date-picker/test/common.js
@@ -7,14 +7,6 @@ function tap(element) {
   });
 }
 
-function waitUntilLocaleAvailable(locale, callback) {
-  if (moment.localeData(locale)) {
-    callback();
-  } else {
-    setTimeout(waitUntilLocaleAvailable, 10, locale, callback);
-  }
-}
-
 function monthsEqual(date1, date2) {
   return date1.getFullYear() === date2.getFullYear() && date1.getMonth() === date2.getMonth();
 }
