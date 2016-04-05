902ee4c43effa7e4f98afcab47945afae71644cc
Teemu Pöntelin <teemu@vaadin.com>
2016-04-05 14:53:51 +0300

    Extend the DefaultValueAccessor in VaadinDatePicker directive

--- packages/vaadin-date-picker/directives/vaadin-date-picker.ts
+++ packages/vaadin-date-picker/directives/vaadin-date-picker.ts
@@ -1,17 +1,31 @@
-import {Directive, ElementRef, Output, HostListener, EventEmitter} from 'angular2/core';
+import {Directive, ElementRef, Output, HostListener, EventEmitter, Provider, forwardRef, Renderer} from 'angular2/core';
+import {NG_VALUE_ACCESSOR} from 'angular2/src/common/forms/directives/control_value_accessor';
+import {DefaultValueAccessor} from 'angular2/src/common/forms/directives/default_value_accessor';
+import {CONST_EXPR} from 'angular2/src/facade/lang';
 declare var Polymer;
 
-@Directive({selector: 'vaadin-date-picker'})
-export class VaadinDatePicker {
+const VAADIN_DATE_PICKER_CONTROL_VALUE_ACCESSOR = CONST_EXPR(new Provider(
+    NG_VALUE_ACCESSOR, {
+      useExisting: forwardRef(() => VaadinDatePicker),
+      multi: true
+    }));
+
+@Directive({
+  selector: 'vaadin-date-picker',
+  providers: [VAADIN_DATE_PICKER_CONTROL_VALUE_ACCESSOR]
+})
+export class VaadinDatePicker extends DefaultValueAccessor {
 
   private element;
 
   @Output() valueChange: EventEmitter<any> = new EventEmitter(false);
   @HostListener('value-changed', ['$event.detail.value'])
   valuechanged(value) {
+    this.valueChange.emit(value);
     if (value) {
-      this.valueChange.emit(value);
-      this.element.fire('input');
+      // Assuming that the registered onChange function is only used
+      // for the pristine/dirty status here.
+      this.onChange(value);
     }
   }
 
@@ -24,15 +38,15 @@ export class VaadinDatePicker {
   onImport(e) {
     this.element.$$('paper-input-container').addEventListener('blur', () => {
       if (!this.element.opened && !this.element._opened) {
-        this.element.fire('blur');
+        this.onTouched();
       }
     });
   }
 
-  constructor(el: ElementRef) {
+  constructor(renderer: Renderer, el: ElementRef) {
+    super(renderer, el);
     this.element = el.nativeElement;
-    if (!Polymer.isInstance(el.nativeElement)) {
-      Polymer.Base.importHref('bower_components/vaadin-date-picker/vaadin-date-picker.html', this.onImport.bind(this));
-    }
+    Polymer.Base.importHref('bower_components/vaadin-date-picker/vaadin-date-picker.html', this.onImport.bind(this));
   }
+
 }
