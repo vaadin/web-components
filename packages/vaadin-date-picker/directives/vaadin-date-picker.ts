40c2c48e135a58a5e9f2d17b2121e04059eb2a85
Teemu Pöntelin <teemu@vaadin.com>
2016-04-05 11:16:21 +0300

    Update Angular2 directive with ngControl support

--- packages/vaadin-date-picker/directives/vaadin-date-picker.ts
+++ packages/vaadin-date-picker/directives/vaadin-date-picker.ts
@@ -1,30 +1,38 @@
 import {Directive, ElementRef, Output, HostListener, EventEmitter} from 'angular2/core';
+declare var Polymer;
 
 @Directive({selector: 'vaadin-date-picker'})
-
 export class VaadinDatePicker {
-  
-  @Output() _selectedDateChange: EventEmitter<any> = new EventEmitter(false);
-  @HostListener('selected-date-changed', ['$event.detail.value'])
-  _selectedDatechanged(value) {
-    this._selectedDateChange.emit(value);
-  }
-  
+
+  private element;
+
   @Output() valueChange: EventEmitter<any> = new EventEmitter(false);
   @HostListener('value-changed', ['$event.detail.value'])
   valuechanged(value) {
-    this.valueChange.emit(value);
+    if (value) {
+      this.valueChange.emit(value);
+      this.element.fire('input');
+    }
   }
-  
+
   @Output() invalidChange: EventEmitter<any> = new EventEmitter(false);
   @HostListener('invalid-changed', ['$event.detail.value'])
   invalidchanged(value) {
     this.invalidChange.emit(value);
   }
-  
+
+  onImport(e) {
+    this.element.$$('paper-input-container').addEventListener('blur', () => {
+      if (!this.element.opened && !this.element._opened) {
+        this.element.fire('blur');
+      }
+    });
+  }
+
   constructor(el: ElementRef) {
+    this.element = el.nativeElement;
     if (!Polymer.isInstance(el.nativeElement)) {
-      Polymer.Base.importHref('bower_components/vaadin-date-picker/vaadin-date-picker.html')
+      Polymer.Base.importHref('bower_components/vaadin-date-picker/vaadin-date-picker.html', this.onImport.bind(this));
     }
   }
 }
