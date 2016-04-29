fe1598e8a5f17eb173f3eef19067adc7d4a102e4
Tomi Virkki <virkki@vaadin.com>
2016-04-29 10:36:40 +0300

    Added Angular 2 tests and related updates

---
+++ packages/vaadin-combo-box/test/angular2/main.ts
@@ -0,0 +1,40 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {Component, ElementRef, ChangeDetectorRef} from 'angular2/core';
+import {VaadinComboBox} from '../../directives/vaadin-combo-box';
+
+@Component({
+  selector: 'test-app',
+  template: `
+    <vaadin-combo-box [(value)]="selection" required class="bound"></vaadin-combo-box>
+
+    <form #testForm="ngForm">
+      <vaadin-combo-box [items]="items" ngControl="selection" [value]="selection" required [_form]="testForm"></vaadin-combo-box>
+    </form>
+    `,
+  directives: [VaadinComboBox]
+})
+export class TestApp {
+
+  private _host;
+  private _ref;
+  private items = ['foo', 'bar', 'baz'];
+  private selection = 'foo';
+
+  constructor(e: ElementRef, ref: ChangeDetectorRef) {
+    this._host = e.nativeElement;
+    this._ref = ref;
+  }
+
+  ngAfterViewInit() {
+    var event = new CustomEvent('readyForTests', {detail: this});
+    this._host.dispatchEvent(event);
+  }
+
+  public detectChanges() {
+    this._ref.detectChanges();
+  }
+}
+
+document.body.addEventListener('bootstrap', () => {
+  bootstrap(TestApp);
+});
