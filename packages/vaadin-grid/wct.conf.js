898d86c63bf62128cf08683a34d8450a47faf052
Sauli Tähkäpää <sauli@vaadin.com>
2016-08-25 08:59:03 +0300

    Merge remote-tracking branch 'origin/master' into 2.0

---
+++ packages/vaadin-grid/wct.conf.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // See https://github.com/Polymer/web-component-tester/blob/master/runner/config.js#L47-54
+  activeBrowsers: [],
+  plugins: {
+    local: true
+  },
+  root: '.'
+};
