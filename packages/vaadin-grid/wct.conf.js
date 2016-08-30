dbe7f7f5a88664aabd0c31c558f12ac85e9d2d60
Sauli Tähkäpää <sauli@vaadin.com>
2016-08-30 15:12:11 +0300

    Add coverage

--- packages/vaadin-grid/wct.conf.js
+++ packages/vaadin-grid/wct.conf.js
@@ -2,7 +2,29 @@ module.exports = {
   // See https://github.com/Polymer/web-component-tester/blob/master/runner/config.js#L47-54
   activeBrowsers: [],
   plugins: {
-    local: true
+    local: true,
+    "istanbul": {
+      "dir": "./coverage",
+      "reporters": ["text-summary", "lcov"],
+      "include": [
+        "/iron-list-behavior.html",
+        "/vaadin-grid-cell.html",
+        "/vaadin-grid-column.html",
+        "/vaadin-grid-data-source-behavior.html",
+        "/vaadin-grid-edge-behavior.html",
+        "/vaadin-grid-footer.html",
+        "/vaadin-grid-header.html",
+        "/vaadin-grid-outer-scroller.html",
+        "/vaadin-grid-row-details-behavior.html",
+        "/vaadin-grid-row.html",
+        "/vaadin-grid-scroll-behavior.html",
+        "/vaadin-grid-scroller.html",
+        // "/vaadin-grid-selection-behavior.html",
+        "/vaadin-grid-sizer.html",
+        "/vaadin-grid.html"
+      ],
+      "exclude": []
+    }
   },
   root: '.'
 };
