5533f5a08bb331f16d0e6c84ba0402489d201998
Limon Monte <limon.monte@gmail.com>
2016-12-30 09:07:54 +0200

    Run Saucelabs tests for internal PRs except if branch contains 'quick/'

--- packages/vaadin-combo-box/wct.conf.js
+++ packages/vaadin-combo-box/wct.conf.js
@@ -3,8 +3,25 @@ var args = require('yargs').argv;
 module.exports = {
   extraScripts: args.dom === 'shadow' ? ['test/enable-shadow-dom.js'] : [],
   registerHooks: function(context) {
-    if (process.env.SAUCE_BROWSERS) {
-      context.options.plugins.sauce.browsers = JSON.parse(process.env.SAUCE_BROWSERS);
+    // run Saucelabs tests for
+    //  - internal PRs, except cases when branch contains 'quick/'
+    //  - daily builds, triggered by cron
+    if (
+      (process.env.TRAVIS_EVENT_TYPE === 'push' && process.env.TRAVIS_BRANCH.indexOf('quick/') === -1) ||
+      process.env.TRAVIS_EVENT_TYPE === 'cron'
+    ) {
+      context.options.plugins.sauce.browsers = [
+        // desktop
+        'Windows 10/chrome@54',
+        'Windows 10/firefox@50',
+        'Windows 10/microsoftedge@13',
+        'Windows 10/internet explorer@11',
+        'OS X 10.11/safari@9.0',
+        // mobile
+        'OS X 10.11/iphone@9.2',
+        'OS X 10.11/ipad@9.2',
+        'Linux/android@5.1'
+      ];
     }
   }
 };
