d1e0b38126cf283c821c529023a507d58530d61a
Limon Monte <limon.monte@gmail.com>
2017-01-12 15:38:31 +0200

    Add stylelint and lint:css Gulp task

--- packages/vaadin-upload/gulpfile.js
+++ packages/vaadin-upload/gulpfile.js
@@ -5,8 +5,9 @@ var htmlExtract = require('gulp-html-extract');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var typings = require('gulp-typings');
+var stylelint = require('gulp-stylelint');
 
-gulp.task('lint', ['lint:js', 'lint:html']);
+gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
 
 gulp.task('lint:js', function() {
   return gulp.src([
@@ -30,6 +31,22 @@ gulp.task('lint:html', function() {
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError('fail'));
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src([
+    '*.html',
+    'demo/*.html',
+    'test/*.html'
+  ])
+  .pipe(htmlExtract({
+    sel: 'style'
+  }))
+  .pipe(stylelint({
+    reporters: [
+      {formatter: 'string', console: true}
+    ]
+  }));
 });
 
 gulp.task('typings', function() {
