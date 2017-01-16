8dee712f4d0ff60fd3c8c22d656934576fec7ebb
Limon Monte <limonte@users.noreply.github.com>
2017-01-16 17:52:08 +0200

    Remove Linux/android@5.1 from automated tests

--- packages/vaadin-context-menu/wct.conf.js
+++ packages/vaadin-context-menu/wct.conf.js
@@ -13,7 +13,6 @@ module.exports = {
     var otherPlatforms = [
       'OS X 10.11/iphone@9.3',
       'OS X 10.11/ipad@9.3',
-      'Linux/android@5.1',
       'Windows 10/microsoftedge@13',
       'Windows 10/internet explorer@11',
       'OS X 10.11/safari@10.0'
