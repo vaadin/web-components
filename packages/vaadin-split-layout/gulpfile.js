f6e9f5a52b620dc16327266a98ed9282498e30bb
Limon Monte <limon.monte@gmail.com>
2017-01-16 11:39:27 +0200

    Add stylelint and lint:css Gulp task

--- packages/vaadin-split-layout/gulpfile.js
+++ packages/vaadin-split-layout/gulpfile.js
@@ -3,9 +3,10 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var htmlExtract = require('gulp-html-extract');
+var stylelint = require('gulp-stylelint');
 var wct = require('web-component-tester').test;
 
-gulp.task('lint', ['lint:js', 'lint:html']);
+gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
 
 gulp.task('lint:js', function() {
   return gulp.src([
@@ -30,6 +31,22 @@ gulp.task('lint:html', function() {
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src([
+    '*.html',
+    'demo/*.html',
+    'test/*.html'
+  ])
+  .pipe(htmlExtract({
+    sel: 'style'
+  }))
+  .pipe(stylelint({
+    reporters: [
+      {formatter: 'string', console: true}
+    ]
+  }));
 });
 
 gulp.task('test', ['lint'], function(done) {
