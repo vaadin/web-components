edf96c66b764cde47a1dabf802777e230f0a027b
Limon Monte <limon.monte@gmail.com>
2017-01-18 15:23:43 +0200

    Add stylelint and lint:css Gulp task

--- packages/vaadin-combo-box/gulpfile.js
+++ packages/vaadin-combo-box/gulpfile.js
@@ -4,10 +4,11 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var htmlExtract = require('gulp-html-extract');
 var sourcemaps = require('gulp-sourcemaps');
+var stylelint = require('gulp-stylelint');
 var ts = require('gulp-typescript');
 var typings = require('gulp-typings');
 
-gulp.task('lint', ['lint:js', 'lint:html']);
+gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
 
 gulp.task('lint:js', function() {
   return gulp.src([
@@ -32,6 +33,22 @@ gulp.task('lint:html', function() {
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
+});
+
+gulp.task('lint:css', function() {
+  return gulp.src([
+    '*.html',
+    'demo/*.html',
+    'test/*.html'
+  ])
+  .pipe(htmlExtract({
+    sel: 'style'
+  }))
+  .pipe(stylelint({
+    reporters: [
+      {formatter: 'string', console: true}
+    ]
+  }));
 });
 
 gulp.task('typings', function() {
