9a89a1de8b0a8e1f427f1f411d6b6f820dda1459
Manolo Carrasco <manolo@vaadin.com>
2017-01-19 09:58:49 +0100

    First Commit, just the skeleton

---
+++ packages/vaadin-text-field/wct.conf.js
@@ -0,0 +1,31 @@
+var args = require('yargs').argv;
+
+module.exports = {
+  extraScripts: args.dom === 'shadow' ? ['test/enable-shadow-dom.js'] : [],
+  registerHooks: function(context) {
+    // The Good
+    var crossPlatforms = [
+      'Windows 10/chrome@55',
+      'Windows 10/firefox@50'
+    ];
+
+    // The Bad
+    var otherPlatforms = [
+      'OS X 10.11/iphone@9.3',
+      'OS X 10.11/ipad@9.3',
+      'Windows 10/microsoftedge@13',
+      'Windows 10/internet explorer@11',
+      'OS X 10.11/safari@10.0'
+    ];
+
+    // run SauceLabs tests for pushes, except cases when branch contains 'quick/'
+    if (process.env.TRAVIS_EVENT_TYPE === 'push' && process.env.TRAVIS_BRANCH.indexOf('quick/') === -1) {
+      // crossPlatforms are not tested here, but in Selenium WebDriver (see .travis.yml)
+      context.options.plugins.sauce.browsers = otherPlatforms;
+
+    // Run SauceLabs for daily builds, triggered by cron
+    } else if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
+      context.options.plugins.sauce.browsers = crossPlatforms.concat(otherPlatforms);
+    }
+  }
+};
