519ed4aebdf9f3276c5de9fbb4becdca094d6f02
Limon Monte <limon.monte@gmail.com>
2017-01-23 20:24:22 +0200

    Improve build (Skeleton PR#13 sync)

--- packages/vaadin-upload/wct.conf.js
+++ packages/vaadin-upload/wct.conf.js
@@ -1,27 +1,26 @@
-var args = require('yargs').argv;
-
 module.exports = {
-  extraScripts: args.dom === 'shadow' ? ['test/enable-shadow-dom.js'] : [],
   registerHooks: function(context) {
-    // run Saucelabs tests for
-    //  - internal PRs, except cases when branch contains 'quick/'
-    //  - daily builds, triggered by cron
-    if (
-      (process.env.TRAVIS_EVENT_TYPE === 'push' && process.env.TRAVIS_BRANCH.indexOf('quick/') === -1) ||
-      process.env.TRAVIS_EVENT_TYPE === 'cron'
-    ) {
-      context.options.plugins.sauce.browsers = [
-        // desktop
-        'Windows 10/chrome@54',
-        'Windows 10/firefox@50',
-        'Windows 10/microsoftedge@13',
-        'Windows 10/internet explorer@11',
-        'OS X 10.11/safari@9.0',
-        // mobile
-        'OS X 10.11/iphone@9.2',
-        'OS X 10.11/ipad@9.2',
-        'Linux/android@5.1'
-      ];
+    var crossPlatforms = [
+      'Windows 10/chrome@55',
+      'Windows 10/firefox@50'
+    ];
+
+    var otherPlatforms = [
+      'OS X 10.11/iphone@9.3',
+      'OS X 10.11/ipad@9.3',
+      'Windows 10/microsoftedge@13',
+      'Windows 10/internet explorer@11',
+      'OS X 10.11/safari@10.0'
+    ];
+
+    // run SauceLabs tests for pushes, except cases when branch contains 'quick/'
+    if (process.env.TRAVIS_EVENT_TYPE === 'push' && process.env.TRAVIS_BRANCH.indexOf('quick/') === -1) {
+      // crossPlatforms are not tested here, but in Selenium WebDriver (see .travis.yml)
+      context.options.plugins.sauce.browsers = otherPlatforms;
+
+    // Run SauceLabs for daily builds, triggered by cron
+    } else if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
+      context.options.plugins.sauce.browsers = crossPlatforms.concat(otherPlatforms);
     }
   }
 };
