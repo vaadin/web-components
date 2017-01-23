6a2d735686d024f64653bc91420bb365ac21fd4e
Limon Monte <limon.monte@gmail.com>
2017-01-23 12:16:10 +0200

    Improve build (Skeleton PR#13 sync)

--- packages/vaadin-context-menu/wct.conf.js
+++ packages/vaadin-context-menu/wct.conf.js
@@ -1,15 +1,10 @@
-var args = require('yargs').argv;
-
 module.exports = {
-  extraScripts: args.dom === 'shadow' ? ['test/enable-shadow-dom.js'] : [],
   registerHooks: function(context) {
-    // The Good
     var crossPlatforms = [
       'Windows 10/chrome@55',
       'Windows 10/firefox@50'
     ];
 
-    // The Bad
     var otherPlatforms = [
       'OS X 10.11/iphone@9.3',
       'OS X 10.11/ipad@9.3',
