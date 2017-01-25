370d5f83539adab94f7d686aa0ede3bf0d892889
Manolo Carrasco <manolo@vaadin.com>
2017-01-25 06:57:10 +0100

    Reviewers suggestions

--- packages/vaadin-text-field/wct.conf.js
+++ packages/vaadin-text-field/wct.conf.js
@@ -2,11 +2,12 @@
 module.exports = {
   registerHooks: function(context) {
 
-    context.options.plugins.sauce.browsers = [
+    context.options.plugins.sauce.browserss = [
       'Windows 10/chrome@54',
       'Windows 10/firefox@50',
       'Windows 10/microsoftedge@14',
       'OS X 10.11/safari@10.0',
+      'OSX 10.11/iphone@10.0'
     ];
 
   }
