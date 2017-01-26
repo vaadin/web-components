8d56494648e581aa7e452944af84bb8cda9917eb
rogozinds <rogozinds@gmail.com>
2017-01-26 06:00:48 -0800

    Add test that WC has something in local DOM, closes DASH-10. vaadin/vaadin-board#3
    * Add test that WC has something in local DOM, closes DASH-10.
    
    Add tests for IPad and Safari10.
    Exclude IE11 and Safari9 from tests, because it's not supporting ES6.

--- packages/vaadin-board/wct.conf.js
+++ packages/vaadin-board/wct.conf.js
@@ -1,16 +1,42 @@
+//To add more device use Platform generator from here:
+//https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+// and check that the configuration is available at https://saucelabs.com/beta/manual
 module.exports = {
     verbose: false,
     testTimeout: 6 * 60 * 1000,
-    suites: ['test'],
+    suites: ["test"],
     plugins: {
         sauce: {
             disabled: true,
-            browsers: [
-                "Windows 10/microsoftedge",
-                "Windows 10/internet explorer",
-                "Windows 10/chrome",
-                "Windows 10/firefox",
-                "OS X 10.10/safari"
+            "browsers": [
+                {
+                    "browserName": "MicrosoftEdge",
+                    "platform": "Windows 10",
+                    "version": "14.14393"
+                },
+                {
+                    "browserName": "Chrome",
+                    "platform": "Windows 10",
+                    "version": "55.0"
+                },
+                {
+                    "browserName": "Firefox",
+                    "platform": "Windows 10",
+                    "version": "50.0"
+                },
+                {
+                    "browserName": "safari",
+                    "platform": "OS X 10.11",
+                    "version": "10.0"
+                },
+                {
+                    "browserName": "safari",
+                    "appiumVersion": "1.6.3",
+                    "deviceName": "iPad Simulator",
+                    "deviceOrientation": "portrait",
+                    "platformName": "iOS",
+                    "platformVersion": "10.0"
+                }
             ]
         }
     }
