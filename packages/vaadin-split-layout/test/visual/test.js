9baa4fd0d7680aeeccd56b504675dc6d112fea0e
Limon Monte <limon.monte@gmail.com>
2017-02-01 13:44:55 +0200

    Fix visual tests for IE/Edge

--- packages/vaadin-split-layout/test/visual/test.js
+++ packages/vaadin-split-layout/test/visual/test.js
@@ -5,13 +5,17 @@ gemini.suite('vaadin-split-layout', function(rootSuite) {
   gemini.suite('default', function(suite) {
     suite
       .setCaptureElements('#default')
-      .capture('default');
+      .capture('default', {}, function(actions, find) {
+        actions.wait(3000); // hack for IE/Edge
+      });
   });
 
   gemini.suite('customized', function(suite) {
     suite
       .setCaptureElements('#customized')
-      .capture('customized');
+      .capture('customized', {}, function(actions, find) {
+        actions.wait(3000); // hack for IE/Edge
+      });
   });
 
 });
