5aa6d6ac4495625a0d949a224b0de61a91a9de78
Tomi Virkki <virkki@vaadin.com>
2017-02-02 11:34:43 +0200

    Only run coverage tests locally
    The istanbul wct plugin makes all remote tests timeout.

--- packages/vaadin-grid/wct.conf.js
+++ packages/vaadin-grid/wct.conf.js
@@ -20,43 +20,45 @@ module.exports = {
     // Run SauceLabs for daily builds, triggered by cron
     } else if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
       context.options.plugins.sauce.browsers = crossPlatforms.concat(otherPlatforms);
+
+    // Add coverage for local tests only
+    } else {
+      context.options.plugins.istanbul = {
+        'dir': './coverage',
+        'reporters': ['text-summary', 'lcov'],
+        'include': [
+          '/iron-list-behavior.html',
+          '/vaadin-grid-active-item-behavior.html',
+          '/vaadin-grid-array-data-source-behavior.html',
+          '/vaadin-grid-cell-click-behavior.html',
+          '/vaadin-grid-column.html',
+          '/vaadin-grid-data-source-behavior.html',
+          '/vaadin-grid-dynamic-columns-behavior.html',
+          // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
+          // '/vaadin-grid-filter-behavior.html',
+          '/vaadin-grid-focusable-cell-container-behavior.html',
+          '/vaadin-grid-keyboard-navigation-behavior.html',
+          '/vaadin-grid-row-details-behavior.html',
+          '/vaadin-grid-selection-behavior.html',
+          '/vaadin-grid-selection-column.html',
+          '/vaadin-grid-sizer.html',
+          // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
+          // '/vaadin-grid-sort-behavior.html',
+          '/vaadin-grid-table-cell.html',
+          '/vaadin-grid-table-header-footer.html',
+          '/vaadin-grid-table-outer-scroller.html',
+          '/vaadin-grid-table-row.html',
+          '/vaadin-grid-table-scroll-behavior.html',
+          '/vaadin-grid-table.html',
+          '/vaadin-grid-templatizer.html',
+          '/vaadin-grid.html'
+        ],
+        'exclude': []
+      };
     }
   },
+
   plugins: {
-    'local': true,
-    'random-output': true,
-    'istanbul': {
-      'dir': './coverage',
-      'reporters': ['text-summary', 'lcov'],
-      'include': [
-        '/iron-list-behavior.html',
-        '/vaadin-grid-active-item-behavior.html',
-        '/vaadin-grid-array-data-source-behavior.html',
-        '/vaadin-grid-cell-click-behavior.html',
-        '/vaadin-grid-column.html',
-        '/vaadin-grid-data-source-behavior.html',
-        '/vaadin-grid-dynamic-columns-behavior.html',
-        // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
-        // '/vaadin-grid-filter-behavior.html',
-        '/vaadin-grid-focusable-cell-container-behavior.html',
-        '/vaadin-grid-keyboard-navigation-behavior.html',
-        '/vaadin-grid-row-details-behavior.html',
-        '/vaadin-grid-selection-behavior.html',
-        '/vaadin-grid-selection-column.html',
-        '/vaadin-grid-sizer.html',
-        // TODO: @limonte, revisit this in future, currently a weird istanbul bug here
-        // '/vaadin-grid-sort-behavior.html',
-        '/vaadin-grid-table-cell.html',
-        '/vaadin-grid-table-header-footer.html',
-        '/vaadin-grid-table-outer-scroller.html',
-        '/vaadin-grid-table-row.html',
-        '/vaadin-grid-table-scroll-behavior.html',
-        '/vaadin-grid-table.html',
-        '/vaadin-grid-templatizer.html',
-        '/vaadin-grid.html'
-      ],
-      'exclude': []
-    }
-  },
-  root: '.'
+    'random-output': true
+  }
 };
