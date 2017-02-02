d7ec8ce5559ae0c0265991cc2dadfaf6a7c29686
Limon Monte <limon.monte@gmail.com>
2017-02-02 16:37:35 +0200

    Remove Angular2 tests

--- packages/vaadin-upload/gulpfile.js
+++ packages/vaadin-upload/gulpfile.js
@@ -2,9 +2,6 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var htmlExtract = require('gulp-html-extract');
-var sourcemaps = require('gulp-sourcemaps');
-var ts = require('gulp-typescript');
-var typings = require('gulp-typings');
 var stylelint = require('gulp-stylelint');
 
 gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
@@ -47,23 +44,4 @@ gulp.task('lint:css', function() {
       {formatter: 'string', console: true}
     ]
   }));
-});
-
-gulp.task('typings', function() {
-  return gulp.src('test/angular2/typings.json')
-    .pipe(typings());
-});
-
-gulp.task('ng2', ['typings'], function() {
-  ['test/angular2'].forEach(function(dir) {
-    gulp.src([dir + '/*.ts', 'test/angular2/typings/main/**/*.d.ts'])
-      .pipe(sourcemaps.init())
-      .pipe(ts(ts.createProject('test/angular2/tsconfig.json')))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(dir));
-  });
-});
-
-gulp.task('ng2:watch', function() {
-  gulp.watch('test/angular2/*.ts', ['ng2']);
 });
