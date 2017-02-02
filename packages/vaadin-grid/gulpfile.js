3837616cd57daab4b6ad01def666310eabd1c50e
Tomi Virkki <virkki@vaadin.com>
2017-02-02 08:52:29 +0200

    Run tests for all supported browsers

--- packages/vaadin-grid/gulpfile.js
+++ packages/vaadin-grid/gulpfile.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var eslint = require('gulp-eslint');
 var htmlExtract = require('gulp-html-extract');
-var ts = require('gulp-typescript');
-var typings = require('gulp-typings');
 var stylelint = require('gulp-stylelint');
 
 gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
@@ -49,28 +46,4 @@ gulp.task('lint:css', function() {
       {formatter: 'string', console: true}
     ]
   }));
-});
-
-gulp.task('gwt', ['gwt:copy']);
-
-gulp.task('test', ['gwt:validate', 'test:local']);
-
-gulp.task('typings', function() {
-  return gulp.src('directives/typings.json')
-    .pipe(typings());
-});
-
-gulp.task('ng2', ['typings'], function() {
-  ['directives', 'test/angular2'].forEach(function(dir) {
-    gulp.src([dir + '/*.ts', 'directives/typings/main/**/*.d.ts'])
-      .pipe(sourcemaps.init())
-      .pipe(ts(ts.createProject('directives/tsconfig.json')))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(dir));
-  });
-});
-
-gulp.task('ng2:watch', function() {
-  gulp.watch('directives/*.ts', ['ng2']);
-  gulp.watch('test/angular2/*.ts', ['ng2']);
 });
