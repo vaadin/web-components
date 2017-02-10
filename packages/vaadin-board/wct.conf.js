734147f3375470b57cd090d6970714b147e8d7d4
rogozinds <rogozinds@gmail.com>
2017-02-10 06:20:07 -0800

    Add eslint and gulp tasks for it. vaadin/vaadin-board#12

--- packages/vaadin-board/wct.conf.js
+++ packages/vaadin-board/wct.conf.js
@@ -1,43 +1,43 @@
-//To add more device use Platform generator from here:
-//https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+// To add more device use Platform generator from here:
+// https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
 // and check that the configuration is available at https://saucelabs.com/beta/manual
 module.exports = {
-    verbose: false,
-    testTimeout: 6 * 60 * 1000,
-    suites: ["test"],
-    plugins: {
-        sauce: {
-            disabled: true,
-            "browsers": [
-                {
-                    "browserName": "MicrosoftEdge",
-                    "platform": "Windows 10",
-                    "version": "14.14393"
-                },
-                {
-                    "browserName": "Chrome",
-                    "platform": "Windows 10",
-                    "version": "55.0"
-                },
-                {
-                    "browserName": "Firefox",
-                    "platform": "Windows 10",
-                    "version": "50.0"
-                },
-                {
-                    "browserName": "safari",
-                    "platform": "OS X 10.11",
-                    "version": "10.0"
-                },
-                {
-                    "browserName": "safari",
-                    "appiumVersion": "1.6.3",
-                    "deviceName": "iPad Simulator",
-                    "deviceOrientation": "portrait",
-                    "platformName": "iOS",
-                    "platformVersion": "10.0"
-                }
-            ]
+  verbose: false,
+  testTimeout: 6 * 60 * 1000,
+  suites: ['test'],
+  plugins: {
+    sauce: {
+      'disabled': true,
+      'browsers': [
+        {
+          'browserName': 'MicrosoftEdge',
+          'platform': 'Windows 10',
+          'version': '14.14393'
+        },
+        {
+          'browserName': 'Chrome',
+          'platform': 'Windows 10',
+          'version': '55.0'
+        },
+        {
+          'browserName': 'Firefox',
+          'platform': 'Windows 10',
+          'version': '50.0'
+        },
+        {
+          'browserName': 'safari',
+          'platform': 'OS X 10.11',
+          'version': '10.0'
+        },
+        {
+          'browserName': 'safari',
+          'appiumVersion': '1.6.3',
+          'deviceName': 'iPad Simulator',
+          'deviceOrientation': 'portrait',
+          'platformName': 'iOS',
+          'platformVersion': '10.0'
         }
+      ]
     }
+  }
 };
