bbc11f4d590e81d67dd2f9cd7b6c2e5cf84587fc
Limon Monte <limon.monte@gmail.com>
2017-04-06 16:54:35 +0300

    Implement focus-ring

--- packages/vaadin-text-field/test/visual/test.js
+++ packages/vaadin-text-field/test/visual/test.js
@@ -1,10 +1,4 @@
-
-
 gemini.suite('vaadin-text-field', function(rootSuite) {
-  // Hack needed for
-  // - Edge (https://github.com/vaadin/vaadin-text-field/issues/10)
-  // - Making sure that animations have finished
-  // - FF has async focused the input.
   function wait(actions, find) {
     actions.wait(5000);
   }
@@ -13,7 +7,17 @@ gemini.suite('vaadin-text-field', function(rootSuite) {
     suite
       .setUrl('/default.html')
       .setCaptureElements('#default-tests')
-      .capture('screenshots', {}, wait);
+      .capture('default', {}, wait);
+  });
+
+  gemini.suite('control-state', function(suite) {
+    suite
+      .setUrl('/control-state.html')
+      .setCaptureElements('#focus-ring')
+      .capture('control-state-focused', {}, wait)
+      .capture('control-state-focus-ring', function(actions, find) {
+        actions.sendKeys(gemini.TAB);
+      });
   });
 
 });
