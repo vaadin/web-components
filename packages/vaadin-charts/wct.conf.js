0bf7f311a4d099723ab3910669faaa9eb5317619
rogozinds <rogozinds@gmail.com>
2017-04-12 06:03:13 -0700

    Fix tests after migrating to Polymer 2.

--- packages/vaadin-charts/wct.conf.js
+++ packages/vaadin-charts/wct.conf.js
@@ -3,14 +3,39 @@ module.exports = {
     testTimeout: 6 * 60 * 1000,
     plugins: {
         sauce: {
-            disabled: true,
-            browsers: [
-                "Windows 10/microsoftedge",
-                "Windows 10/internet explorer",
-                "Windows 10/chrome",
-                "Windows 10/firefox",
-                "OS X 10.10/safari"
+            'disabled': true,
+            'browsers': [
+                {
+                    'browserName': 'MicrosoftEdge',
+                    'platform': 'Windows 10',
+                    'version': '14.14393'
+                },
+                {
+                    'browserName': 'Internet Explorer',
+                    'platform': 'Windows 7',
+                    'version': '11'
+                },
+                {
+                    'browserName': 'Chrome',
+                    'platform': 'Windows 10',
+                    'version': '56.0'
+                },
+                {
+                    'browserName': 'Firefox',
+                    'platform': 'Windows 10',
+                    'version': '50.0'
+                },
+                {
+                    'browserName': 'safari',
+                    'platform': 'OS X 10.11',
+                    'version': '10.0'
+                },
+                {
+                    'browserName': 'safari',
+                    'platform': 'OS X 10.11',
+                    'version': '9.0'
+                }
             ]
         }
     }
-};
+}
