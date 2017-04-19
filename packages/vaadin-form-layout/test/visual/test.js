da90c3ffa86cd06fb3a6002115981d0eb07ec676
Anton Platonov <platosha@gmail.com>
2017-04-19 12:58:24 +0300

    Add label support with <vaadin-form-item> element
    Fixes: vaadin/vaadin-form-layout#9

--- packages/vaadin-form-layout/test/visual/test.js
+++ packages/vaadin-form-layout/test/visual/test.js
@@ -1,44 +1,49 @@
 gemini.suite('vaadin-form-layout', (rootSuite) => {
+  function waitAndBlurAll(actions, find) {
+    return actions
+      .waitForJSCondition(function(window) {
+        return window.webComponentsAreReady;
+      }, 60000)
+      .executeJS(function(window) {
+        // Ensure nothing is focused to prevent blinking cursor
+        var input = window.document.createElement('input');
+        window.document.body.appendChild(input);
+        input.focus();
+        window.document.body.removeChild(input);
+      });
+  }
 
-  var demos = [
-    {name: 'index', snippets: [
+  function setBodyWidth(bodyWidth) {
+    // Can’t use context references in page JS callbacks. Have to use string
+    // substitution for the provided bodyWidth.
+    const resizeBodyFn = new Function('window', `
+      window.document.body.style.width = '${bodyWidth}';
+      window.dispatchEvent(new window.CustomEvent('resize'));
+    `);
+    return function(actions, find) {
+      return actions.executeJS(resizeBodyFn);
+    };
+  }
+
+  gemini.suite('form-layout', suite => {
+    suite
+      .setUrl(`/form-layout.html`)
+      .before(waitAndBlurAll);
+
+    [
       'basic',
-      'single-column',
+      'singleColumn',
       'columns',
       'colspan',
       'br',
-      'column-gap',
-      'paper-input'
-    ]}
-  ];
-
-  demos.forEach(function(demo) {
-
-    gemini.suite(demo.name, (demoSuite) => {
-
-      demo.snippets.forEach((snippet, snippetIndex) => {
-        gemini.suite(snippet, (snippetSuite) => {
-          snippetSuite
-            .setUrl('/../../demo/' + demo.name + '.html')
-            .setCaptureElements('demo-snippet:nth-of-type(' + (snippetIndex + 1) + ') vaadin-form-layout')
-            .capture('default', {}, (actions, find) => {
-              actions
-                .waitForJSCondition(function(window) {
-                  return window.webComponentsAreReady;
-                }, 60000)
-                .executeJS(function(window) {
-                  // Ensure nothing is focused to prevent blinking cursor
-                  var input = window.document.createElement('input');
-                  window.document.body.appendChild(input);
-                  input.focus();
-                  window.document.body.removeChild(input);
-                });
-            });
-        });
+      'cssProperties'
+    ].forEach(elementId => {
+      gemini.suite(elementId, suite => {
+        suite.setCaptureElements('#' + elementId)
+          .capture('default')
+          .capture('20em', setBodyWidth('20em'))
+          .capture('10em', setBodyWidth('10em'));
       });
-
     });
-
   });
-
 });
