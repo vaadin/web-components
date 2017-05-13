d2f5d9ffa1353452b4d3063faf125b4ef40650a8
Manolo Carrasco <manolo@vaadin.com>
2017-05-13 11:51:42 +0300

    Configure travis for variants & WCT for sauce

--- packages/vaadin-date-picker/wct.conf.js
+++ packages/vaadin-date-picker/wct.conf.js
@@ -1,28 +1,30 @@
-var argv = require('yargs').argv;
-
 module.exports = {
-  plugins: {
-    'random-output': true
-  },
   registerHooks: function(context) {
-    var cronPlatforms = [
+    var crossPlatforms = [
       'Windows 10/chrome@55',
       'Windows 10/firefox@50'
     ];
 
-    var saucelabsPlatforms = [
+    var otherPlatforms = [
       'OS X 10.11/iphone@10.2',
       'OS X 10.11/ipad@10.2',
       'Windows 10/microsoftedge@14',
       'Windows 10/internet explorer@11',
-      'OS X 10.12/safari@10.0'
+      'OS X 10.11/safari@10.0'
     ];
 
-    if (argv.env === 'saucelabs') {
-      context.options.plugins.sauce.browsers = saucelabsPlatforms;
+    // run SauceLabs tests for pushes, except cases when branch contains 'quick/'
+    if (process.env.TRAVIS_EVENT_TYPE === 'push' && process.env.TRAVIS_BRANCH.indexOf('quick/') === -1) {
+      // crossPlatforms are not tested here, but in Selenium WebDriver (see .travis.yml)
+      context.options.plugins.sauce.browsers = otherPlatforms;
 
-    } else if (argv.env === 'saucelabs-cron') {
-      context.options.plugins.sauce.browsers = cronPlatforms;
+    // Run SauceLabs for daily builds, triggered by cron
+    } else if (process.env.TRAVIS_EVENT_TYPE === 'cron') {
+      context.options.plugins.sauce.browsers = crossPlatforms.concat(otherPlatforms);
     }
+  },
+
+  plugins: {
+    'random-output': true
   }
 };
