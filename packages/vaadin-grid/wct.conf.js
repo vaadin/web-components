fc80ff9c4e26085ea0d2a413933770505648f140
Anton Platonov <platosha@gmail.com>
2017-05-15 17:07:54 +0300

    Don’t run tests in Travis

--- packages/vaadin-grid/wct.conf.js
+++ packages/vaadin-grid/wct.conf.js
@@ -3,16 +3,18 @@ var argv = require('yargs').argv;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'OS X 10.11/iphone@10.0',
-      'OS X 10.11/ipad@10.0',
+      'OS X 10.12/iphone@10.2',
+      'OS X 10.12/ipad@10.2',
       'Windows 10/microsoftedge@14',
       'Windows 10/internet explorer@11',
-      'OS X 10.11/safari@10.0'
+      'OS X 10.12/safari@10.0',
+      'Windows 10/chrome@58',
+      'Windows 10/firefox@53'
     ];
 
     var cronPlatforms = [
-      'Windows 10/chrome@55',
-      'Windows 10/firefox@50'
+      'Windows 10/chrome@58',
+      'Windows 10/firefox@53'
     ];
 
     if (argv.env === 'saucelabs') {
