1a436110a4e4d3104f6870e9035b685369ccf2af
Tomi Virkki <virkki@vaadin.com>
2017-05-16 11:18:28 +0300

    Test fixes

--- packages/vaadin-grid/test/visual/test.js
+++ packages/vaadin-grid/test/visual/test.js
@@ -53,8 +53,10 @@ gemini.suite('vaadin-grid', (rootSuite) => {
         actions.wait(3000);
       })
       .capture('row-details-visible', {}, (actions, find) => {
-        const row = find('vaadin-grid-table-row');
-        actions.click(row);
+        actions.executeJS(window => {
+          const grid = window.document.querySelector('vaadin-grid');
+          grid.expandItem(grid.items[0]);
+        });
       });
   });
 
