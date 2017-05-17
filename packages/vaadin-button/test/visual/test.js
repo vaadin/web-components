fa6237ce4111d828067e0c215ed37f11a0720896
Limon Monte <limon.monte@gmail.com>
2017-05-17 16:29:11 +0300

    Visual tests DRY refactor

--- packages/vaadin-button/test/visual/test.js
+++ packages/vaadin-button/test/visual/test.js
@@ -1,25 +1,26 @@
 gemini.suite('vaadin-button', function(rootSuite) {
-  // Hack needed for
-  // - Edge (https://github.com/vaadin/vaadin-text-field/issues/10)
-  // - Making sure that animations have finished
-  // - FF has async focused the input.
   function wait(actions, find) {
     actions.wait(5000);
   }
+
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
 
   gemini.suite('default-tests', function(suite) {
     suite
       .setUrl('/default.html')
       .setCaptureElements('#default-tests')
-      .capture('default', {}, wait)
+      .capture('default')
       .capture('focus-tabindex', function(actions) {
         actions.sendKeys(gemini.TAB);
-      })
-      .after(function(actions, find) {
-        // Firefox stops responding on socket after a test, workaround:
-        return actions.executeJS(function(window) {
-          window.location.href = 'about:blank'; // just go away, please!
-        });
       });
   });
 
@@ -27,13 +28,7 @@ gemini.suite('vaadin-button', function(rootSuite) {
     suite
       .setUrl('styling.html')
       .setCaptureElements('vaadin-button')
-      .capture('default', {}, wait)
-      .after(function(actions, find) {
-        // Firefox stops responding on socket after a test, workaround:
-        return actions.executeJS(function(window) {
-          window.location.href = 'about:blank'; // just go away, please!
-        });
-      });
+      .capture('default');
   });
 
 });
