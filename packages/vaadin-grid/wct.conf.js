fcd6f61d7b28ab389c3e1f070ea0fb4f7e1a88e4
Manolo Carrasco <manolo@vaadin.com>
2017-08-31 08:52:53 +0200

    Run Chrome only in Sauce

--- packages/vaadin-grid/wct.conf.js
+++ packages/vaadin-grid/wct.conf.js
@@ -7,14 +7,14 @@ module.exports = {
       // TODO: Excluded iOS tests from the hybrid version. Re-enable for P2 version.
       // 'OS X 10.12/iphone@10.2',
       // 'OS X 10.12/ipad@10.2',
-      'Windows 10/microsoftedge@14',
-      'Windows 10/internet explorer@11',
-      'OS X 10.12/safari@10.0'
+      // 'Windows 10/microsoftedge@14',
+      // 'Windows 10/internet explorer@11',
+      // 'OS X 10.12/safari@10.0'
     ];
 
     var cronPlatforms = [
       'Windows 10/chrome@55',
-      'Windows 10/firefox@50'
+      // 'Windows 10/firefox@50'
     ];
 
     if (argv.env === 'saucelabs') {
