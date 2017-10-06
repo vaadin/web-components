32c5dd4f6f3c6964ea5acb9ecdfaea98d0f0982a
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2017-10-06 12:32:29 +0300

    Add lighthouse

--- packages/vaadin-upload/test/visual/test.js
+++ packages/vaadin-upload/test/visual/test.js
@@ -17,7 +17,7 @@ gemini.suite('vaadin-upload', function(rootSuite) {
   gemini.suite('default-tests', function(suite) {
     suite
       .setUrl('/default.html')
-      .setCaptureElements('#default-tests')
+      .setCaptureElements('body')
       .capture('vaadin-upload');
   });
 
