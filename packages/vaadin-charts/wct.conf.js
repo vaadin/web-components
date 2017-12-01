a654c43aa11ad8d5a7b681be281c91a8def58188
Guille <alvarezguille@users.noreply.github.com>
2017-12-01 12:52:42 +0200

    Add test against iOS 11 vaadin/vaadin-charts#236
    iOS11 is supported by SauceLabs now. Updated one of the iOS devices to use it

--- packages/vaadin-charts/wct.conf.js
+++ packages/vaadin-charts/wct.conf.js
@@ -4,7 +4,7 @@ module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
+      'macOS 10.12/ipad@11.0',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
       'macOS 10.12/safari@11.0'
