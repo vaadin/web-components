8b2c8c04ae48d132262b0f8e7476844963458175
web-padawan <serguey.kulikov@gmail.com>
2018-01-03 15:45:55 +0200

    Align with skeleton: add separate file for test suites

---
+++ packages/vaadin-date-picker/test/test-suites.js
@@ -0,0 +1,13 @@
+window.VaadinDatePickerSuites = [
+  'basic.html',
+  'dropdown.html',
+  'overlay.html',
+  'month-calendar.html',
+  'scroller.html',
+  'form-input.html',
+  'custom-input.html',
+  'keyboard-navigation.html',
+  'keyboard-input.html',
+  'late-upgrade.html',
+  'wai-aria.html'
+];
