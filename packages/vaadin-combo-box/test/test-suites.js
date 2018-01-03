9a71f0380f282bcd595e256501816a783a685064
web-padawan <serguey.kulikov@gmail.com>
2018-01-03 15:00:17 +0200

    Align with skeleton: add separate file for test suites

---
+++ packages/vaadin-combo-box/test/test-suites.js
@@ -0,0 +1,17 @@
+window.VaadinComboBoxSuites = [
+  'vaadin-combo-box.html',
+  'toggling-dropdown.html',
+  'overlay-position.html',
+  'selecting-items.html',
+  'form-input.html',
+  'filtering.html',
+  'keyboard.html',
+  'scrolling.html',
+  'aria.html',
+  'using-object-values.html',
+  'data-binding.html',
+  'late-upgrade.html',
+  'vaadin-combo-box-light.html',
+  'item-template.html',
+  'vaadin-combo-box-dropdown.html'
+];
