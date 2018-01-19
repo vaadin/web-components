97665e888ba71269748aeb2a4efcded8f2f7fc6e
web-padawan <serguey.kulikov@gmail.com>
2018-01-19 15:33:05 +0200

    Align with skeleton: update eslint and stylelint. Fix indentation

--- packages/vaadin-form-layout/gulpfile.js
+++ packages/vaadin-form-layout/gulpfile.js
@@ -18,9 +18,9 @@ gulp.task('lint:js', function() {
     '*.js',
     'test/**/*.js'
   ])
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError('fail'));
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError('fail'));
 });
 
 gulp.task('lint:html', function() {
@@ -29,12 +29,13 @@ gulp.task('lint:html', function() {
     'demo/**/*.html',
     'test/**/*.html'
   ])
-  .pipe(htmlExtract({
-    sel: 'script:not([type="text/html"]), code-example code'
-  }))
-  .pipe(eslint())
-  .pipe(eslint.format())
-  .pipe(eslint.failAfterError('fail'));
+    .pipe(htmlExtract({
+      sel: 'script:not([type="text/html"]), code-example code',
+      strip: true
+    }))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError('fail'));
 });
 
 gulp.task('lint:css', function() {
@@ -43,14 +44,14 @@ gulp.task('lint:css', function() {
     'demo/**/*.html',
     'test/**/*.html'
   ])
-  .pipe(htmlExtract({
-    sel: 'style'
-  }))
-  .pipe(stylelint({
-    reporters: [
-      {formatter: 'string', console: true}
-    ]
-  }));
+    .pipe(htmlExtract({
+      sel: 'style'
+    }))
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
 });
 
 gulp.task('version:check', function() {
