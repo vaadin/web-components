76e8f263a21163921366b2b9af64fa75cca831f6
web-padawan <serguey.kulikov@gmail.com>
2018-02-05 11:41:30 +0200

    Update eslint and stylelint to check ./src and ./theme

--- packages/vaadin-grid/gulpfile.js
+++ packages/vaadin-grid/gulpfile.js
@@ -26,10 +26,11 @@ gulp.task('lint:js', function() {
 gulp.task('lint:html', function() {
   return gulp.src([
     '*.html',
+    'src/**/*.html',
     'demo/**/*.html',
     'test/**/*.html',
     '!demo/x-data-provider.html',
-    '!iron-list.html'
+    '!src/iron-list.html'
   ])
     .pipe(htmlExtract({
       sel: 'script, code-example code',
@@ -43,7 +44,9 @@ gulp.task('lint:html', function() {
 gulp.task('lint:css', function() {
   return gulp.src([
     '*.html',
+    'src/**/*.html',
     'demo/**/*.html',
+    'theme/**/*.html',
     'test/**/*.html'
   ])
     .pipe(htmlExtract({
