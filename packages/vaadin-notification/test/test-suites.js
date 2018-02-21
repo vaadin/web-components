fb0dee762ce6f1395ba08728b9077069602e12b1
web-padawan <serguey.kulikov@gmail.com>
2018-02-21 08:33:54 +0200

    Support data-binding in notification template

--- packages/vaadin-notification/test/test-suites.js
+++ packages/vaadin-notification/test/test-suites.js
@@ -1,5 +1,6 @@
 window.VaadinNotificationSuites = [
   'vaadin-notification-test.html',
+  'notification-template-test.html',
   'multiple-notification-test.html',
   'animate-notification-test.html',
 ];
