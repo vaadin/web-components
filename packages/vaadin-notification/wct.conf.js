8b29cf1527111fd3a68d01097c5bdf8121ddf710
web-padawan <serguey.kulikov@gmail.com>
2018-02-22 10:16:22 +0200

    Add test against iOS 11 for iPhone

--- packages/vaadin-notification/wct.conf.js
+++ packages/vaadin-notification/wct.conf.js
@@ -3,7 +3,7 @@ var argv = require('yargs').argv;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'macOS 10.12/iphone@10.3',
+      'macOS 10.12/iphone@11.0',
       'macOS 10.12/ipad@10.3',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
