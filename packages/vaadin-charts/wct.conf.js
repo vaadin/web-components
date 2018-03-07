4debc3f8284f2cecf626e0fb1bfb2286f170c3f1
Sayo Oladeji <oladejioluwasayo@gmail.com>
2018-03-07 15:39:31 +0200

    Pin quick tests to Chrome 64 on Windows 10 only vaadin/vaadin-charts#300
    * Testing Sauce addon
    
    * Quick test in Chrom 64 only
    
    * Sort sauce platforms, add Firefox 58 to PR builds.

--- packages/vaadin-charts/wct.conf.js
+++ packages/vaadin-charts/wct.conf.js
@@ -3,24 +3,30 @@ var argv = require('yargs').argv;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'macOS 10.12/iphone@10.3',
       'macOS 10.12/ipad@11.0',
-      'Windows 10/microsoftedge@15',
+      'macOS 10.12/iphone@10.3',
+      'macOS 10.12/safari@11.0',
+      'Windows 10/firefox@58',
       'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0'
+      'Windows 10/microsoftedge@15'
     ];
 
     var cronPlatforms = [
       'Android/chrome',
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
+      'Windows 10/chrome@64',
+      'Windows 10/firefox@58'
+    ];
+
+    var quickPlatforms = [
+      'Windows 10/chrome@64'
     ];
 
     if (argv.env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
     } else if (argv.env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
+    } else {
+      context.options.plugins.sauce.browsers = quickPlatforms;
     }
   }
 };
