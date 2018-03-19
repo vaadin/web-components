c1286f8cf4a101db6acc84e42c18c8d53f5032e0
Tomi Virkki <virkki@vaadin.com>
2018-03-19 12:27:36 +0200

    Run tests with modulized P3 version

--- packages/vaadin-themable-mixin/wct.conf.js
+++ packages/vaadin-themable-mixin/wct.conf.js
@@ -10,6 +10,13 @@ module.exports = {
       'macOS 10.12/safari@11.0'
     ];
 
+    var saucelabsPlatformsP3 = [
+      'macOS 10.12/iphone@10.3',
+      'macOS 10.12/ipad@10.3',
+      'Windows 10/chrome@59',
+      'macOS 10.12/safari@11.0'
+    ];
+
     var cronPlatforms = [
       'Windows 10/chrome@59',
       'Windows 10/firefox@54'
@@ -17,7 +24,8 @@ module.exports = {
 
     if (argv.env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
+    } else if (argv.env === 'saucelabs-p3') {
+      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
     } else if (argv.env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
