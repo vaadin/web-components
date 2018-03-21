f6d572b6447b0e833032de31dc56e5b0f1e0761c
Tomi Virkki <virkki@vaadin.com>
2018-03-21 18:02:25 +0200

    Fix Travis tests script and prepare for npm packaging

--- packages/vaadin-themable-mixin/wct.conf.js
+++ packages/vaadin-themable-mixin/wct.conf.js
@@ -1,4 +1,5 @@
-var argv = require('yargs').argv;
+var envIndex = process.argv.indexOf('--env') + 1;
+var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
   registerHooks: function(context) {
@@ -7,13 +8,14 @@ module.exports = {
       'macOS 10.12/ipad@10.3',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0'
+      'macOS 10.12/safari@11.0',
+      'Windows 10/chrome@59'
     ];
 
     var saucelabsPlatformsP3 = [
-      'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
-      'Windows 10/chrome@59',
+      'macOS 10.12/iphone@11.2',
+      'macOS 10.12/ipad@11.2',
+      'Windows 10/chrome@63',
       'macOS 10.12/safari@11.0'
     ];
 
@@ -22,11 +24,11 @@ module.exports = {
       'Windows 10/firefox@54'
     ];
 
-    if (argv.env === 'saucelabs') {
+    if (env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-    } else if (argv.env === 'saucelabs-p3') {
+    } else if (env === 'saucelabs-p3') {
       context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
-    } else if (argv.env === 'saucelabs-cron') {
+    } else if (env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
   }
