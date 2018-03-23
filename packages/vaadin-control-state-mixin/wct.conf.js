9d015f269b5380bd464ce2fe211be0ac7cd2fa82
web-padawan <serguey.kulikov@gmail.com>
2018-03-23 09:25:15 +0200

    Run tests with modulized P3 version

--- packages/vaadin-control-state-mixin/wct.conf.js
+++ packages/vaadin-control-state-mixin/wct.conf.js
@@ -1,53 +1,35 @@
-var argv = require('yargs').argv;
+var envIndex = process.argv.indexOf('--env') + 1;
+var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
-  testTimeout: 180 * 1000,
-
   registerHooks: function(context) {
-    const saucelabsPlatformsMobile = [
-      'macOS 10.12/iphone@11.0',
-      'macOS 10.12/ipad@11.0',
-    ];
-
-    const saucelabsPlatformsPolyfilled = [
+    var saucelabsPlatforms = [
+      'macOS 10.12/iphone@10.3',
+      'macOS 10.12/ipad@10.3',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
+      'macOS 10.12/safari@11.0',
+      'Windows 10/chrome@65'
     ];
 
-    const saucelabsPlatformsDesktop = [
+    var saucelabsPlatformsP3 = [
+      'macOS 10.12/iphone@11.2',
+      'macOS 10.12/ipad@11.2',
+      'Windows 10/chrome@65',
       'macOS 10.12/safari@11.0'
     ];
 
-    const cronPlatforms = [
-      'Android/chrome',
-      'Windows 10/chrome@60',
-      'Windows 10/firefox@54'
+    var cronPlatforms = [
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59'
     ];
 
-    switch (argv.env) {
-      case 'saucelabs:mobile':
-        context.options.plugins.sauce.browsers = saucelabsPlatformsMobile;
-        break;
-      case 'saucelabs:polyfilled':
-        context.options.plugins.sauce.browsers = saucelabsPlatformsPolyfilled;
-        break;
-      case 'saucelabs:desktop':
-        context.options.plugins.sauce.browsers = saucelabsPlatformsDesktop;
-        break;
-      case 'saucelabs-cron':
-        context.options.plugins.sauce.browsers = cronPlatforms;
-        break;
-      case 'saucelabs':
-        context.options.plugins.sauce.browsers = cronPlatforms.concat(
-          saucelabsPlatformsDesktop,
-          saucelabsPlatformsMobile,
-          saucelabsPlatformsPolyfilled
-        );
-        break;
+    if (env === 'saucelabs') {
+      context.options.plugins.sauce.browsers = saucelabsPlatforms;
+    } else if (env === 'saucelabs-p3') {
+      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
+    } else if (env === 'saucelabs-cron') {
+      context.options.plugins.sauce.browsers = cronPlatforms;
     }
-  },
-
-  plugins: {
-    'random-output': true
   }
 };
