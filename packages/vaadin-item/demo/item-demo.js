86048ebfb4924ff1cbc852d2cfc5e4f795c7e823
web-padawan <serguey.kulikov@gmail.com>
2018-03-27 14:29:59 +0300

    Prepare for the P3 conversion

---
+++ packages/vaadin-item/demo/item-demo.js
@@ -0,0 +1,15 @@
+/**
+ * @polymerMixin
+ */
+window.ItemDemo = superClass => {
+  return class extends superClass {
+    static get properties() {
+      return {
+      };
+    }
+  };
+};
+
+window.addEventListener('WebComponentsReady', () => {
+  document.body.removeAttribute('unresolved');
+});
