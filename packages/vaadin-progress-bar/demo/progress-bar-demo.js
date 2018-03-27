bf403d10d9cff60f5b8be0bc530f0d52dab828e8
Tomi Virkki <virkki@vaadin.com>
2018-03-27 10:30:37 +0300

    Prepare for P3

---
+++ packages/vaadin-progress-bar/demo/progress-bar-demo.js
@@ -0,0 +1,11 @@
+window.ProgressBarDemo = superClass => {
+  return class extends superClass {
+    static get properties() {
+      return {
+      };
+    }
+  };
+};
+window.addEventListener('WebComponentsReady', () => {
+  document.body.removeAttribute('unresolved');
+});
--- packages/vaadin-progress-bar/wct.conf.js
+++ packages/vaadin-progress-bar/wct.conf.js
@@ -1,14 +1,22 @@
-var argv = require('yargs').argv;
+var envIndex = process.argv.indexOf('--env') + 1;
+var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
+      'macOS 10.12/ipad@11.0',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
       'macOS 10.12/safari@11.0',
       'macOS 9.3.2/iphone@9.3'
+    ];
+
+    var saucelabsPlatformsP3 = [
+      'macOS 10.12/iphone@11.2',
+      'macOS 10.12/ipad@11.2',
+      'Windows 10/chrome@63',
+      'macOS 10.12/safari@11.0'
     ];
 
     var cronPlatforms = [
@@ -17,10 +25,11 @@ module.exports = {
       'Windows 10/firefox@54'
     ];
 
-    if (argv.env === 'saucelabs') {
+    if (env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
-    } else if (argv.env === 'saucelabs-cron') {
+    } else if (env === 'saucelabs-p3') {
+      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
+    } else if (env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
   }
