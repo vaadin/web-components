1a5c2e06bcf9ccf887e3cb59fad92753ee1b6ff4
Anton Platonov <platosha@gmail.com>
2018-03-27 16:32:55 +0300

    Prepare for P3
    Fixes: vaadin/vaadin-button#74

--- packages/vaadin-button/wct.conf.js
+++ packages/vaadin-button/wct.conf.js
@@ -1,4 +1,5 @@
-var argv = require('yargs').argv;
+var envIndex = process.argv.indexOf('--env') + 1;
+var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
   registerHooks: function(context) {
@@ -8,18 +9,26 @@ module.exports = {
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
       'macOS 10.12/safari@11.0',
-      'macOS 9.3.2/iphone@9.3'
+      'Windows 10/chrome@65'
+    ];
+
+    var saucelabsPlatformsP3 = [
+      'macOS 10.12/iphone@11.2',
+      'macOS 10.12/ipad@11.2',
+      'Windows 10/chrome@65',
+      'macOS 10.12/safari@11.0'
     ];
 
     var cronPlatforms = [
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59'
     ];
 
-    if (argv.env === 'saucelabs') {
+    if (env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-
-    } else if (argv.env === 'saucelabs-cron') {
+    } else if (env === 'saucelabs-p3') {
+      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
+    } else if (env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
   }
