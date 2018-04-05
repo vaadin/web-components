0e046f84c2e6961d642dc6e44003116aa6a22753
web-padawan <serguey.kulikov@gmail.com>
2018-04-05 11:21:09 +0300

    Alin with skeleton: update travis config and browsers

--- packages/vaadin-text-field/wct.conf.js
+++ packages/vaadin-text-field/wct.conf.js
@@ -4,17 +4,14 @@ var env = envIndex ? process.argv[envIndex] : undefined;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59',
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
+      'macOS 10.12/ipad@11.0',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
       'macOS 10.12/safari@11.0',
       'macOS 9.3.2/iphone@9.3'
-    ];
-
-    var cronPlatforms = [
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
     ];
 
     var saucelabsPlatformsP3 = [
@@ -24,16 +21,18 @@ module.exports = {
       'macOS 10.12/safari@11.0'
     ];
 
-    switch (env) {
-      case 'saucelabs':
-        context.options.plugins.sauce.browsers = saucelabsPlatforms;
-        break;
-      case 'saucelabs-cron':
-        context.options.plugins.sauce.browsers = cronPlatforms;
-        break;
-      case 'saucelabs-p3':
-        context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
-        break;
+    var cronPlatforms = [
+      'Android/chrome',
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59'
+    ];
+
+    if (env === 'saucelabs') {
+      context.options.plugins.sauce.browsers = saucelabsPlatforms;
+    } else if (env === 'saucelabs-p3') {
+      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
+    } else if (env === 'saucelabs-cron') {
+      context.options.plugins.sauce.browsers = cronPlatforms;
     }
   }
 };
