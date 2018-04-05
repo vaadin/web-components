9b6ddcd3a5db6dc2d99385e416cf7daa752f0edf
web-padawan <serguey.kulikov@gmail.com>
2018-04-05 11:27:51 +0300

    Alin with skeleton: update travis config and browsers

--- packages/vaadin-themable-mixin/wct.conf.js
+++ packages/vaadin-themable-mixin/wct.conf.js
@@ -4,24 +4,27 @@ var env = envIndex ? process.argv[envIndex] : undefined;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59',
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@10.3',
+      'macOS 10.12/ipad@11.0',
       'Windows 10/microsoftedge@15',
       'Windows 10/internet explorer@11',
       'macOS 10.12/safari@11.0',
-      'Windows 10/chrome@59'
+      'macOS 9.3.2/iphone@9.3'
     ];
 
     var saucelabsPlatformsP3 = [
       'macOS 10.12/iphone@11.2',
       'macOS 10.12/ipad@11.2',
-      'Windows 10/chrome@63',
+      'Windows 10/chrome@65',
       'macOS 10.12/safari@11.0'
     ];
 
     var cronPlatforms = [
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
+      'Android/chrome',
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59'
     ];
 
     if (env === 'saucelabs') {
