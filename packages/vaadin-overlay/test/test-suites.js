97dcfef406c71cc1de6e0f9816ce3e2e17f55d74
Anton Platonov <platosha@gmail.com>
2018-04-12 18:04:39 +0300

    Add tests for template stamping

--- packages/vaadin-overlay/test/test-suites.js
+++ packages/vaadin-overlay/test/test-suites.js
@@ -2,5 +2,7 @@ window.VaadinOverlaySuites = [
   'vaadin-overlay.html',
   'vaadin-overlay.html?wc-shadydom=true',
   'styling.html',
-  'styling.html?wc-shadydom=true'
+  'styling.html?wc-shadydom=true',
+  'templating.html',
+  'templating.html?wc-shadydom=true'
 ];
