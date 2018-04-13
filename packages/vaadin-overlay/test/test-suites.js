f8f4aa321ebb3e6ab16841cdf0f9972423e80eda
Anton Platonov <platosha@gmail.com>
2018-04-13 16:24:10 +0300

    Extract focus-trap tests to separate suite, run them for in both scope variants

--- packages/vaadin-overlay/test/test-suites.js
+++ packages/vaadin-overlay/test/test-suites.js
@@ -1,6 +1,8 @@
 window.VaadinOverlaySuites = [
   'vaadin-overlay.html',
   'vaadin-overlay.html?wc-shadydom=true',
+  'focus-trap.html',
+  'focus-trap.html?wc-shadydom=true',
   'styling.html',
   'styling.html?wc-shadydom=true',
   'templating.html',
