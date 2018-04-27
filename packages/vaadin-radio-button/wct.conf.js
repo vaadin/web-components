e834cfbd515a886ce21ccf1e2a5cd2b7d2540f88
web-padawan <serguey.kulikov@gmail.com>
2018-04-27 09:15:34 +0300

    Run P3 tests in all browsers. Bump node to 8.11

--- packages/vaadin-radio-button/wct.conf.js
+++ packages/vaadin-radio-button/wct.conf.js
@@ -2,28 +2,33 @@ var envIndex = process.argv.indexOf('--env') + 1;
 var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
+  testTimeout: 180 * 1000,
+  verbose: true,
   registerHooks: function(context) {
-    var saucelabsPlatforms = [
-      'Windows 10/chrome@65',
-      'Windows 10/firefox@59',
+    const saucelabsPlatformsMobile = [
       'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@11.0',
-      'Windows 10/microsoftedge@15',
-      'Windows 10/internet explorer@11',
-      'macOS 10.12/safari@11.0',
+      'macOS 10.12/ipad@11.2',
       'macOS 9.3.2/iphone@9.3'
     ];
 
-    var saucelabsPlatformsP3 = [
-      'macOS 10.12/iphone@11.2',
-      'macOS 10.12/ipad@11.2',
-      'Windows 10/chrome@65',
-      'macOS 10.12/safari@11.0',
-      'Windows 10/firefox@59',
-      'Windows 10/microsoftedge@16'
+    const saucelabsPlatformsMicrosoft = [
+      'Windows 10/microsoftedge@16',
+      'Windows 10/internet explorer@11'
     ];
 
-    var cronPlatforms = [
+    const saucelabsPlatformsDesktop = [
+      'Windows 10/chrome@65',
+      'Windows 10/firefox@59',
+      'macOS 10.12/safari@11.0'
+    ];
+
+    const saucelabsPlatforms = [
+      ...saucelabsPlatformsMobile,
+      ...saucelabsPlatformsMicrosoft,
+      ...saucelabsPlatformsDesktop
+    ];
+
+    const cronPlatforms = [
       'Android/chrome',
       'Windows 10/chrome@65',
       'Windows 10/firefox@59'
@@ -31,8 +36,6 @@ module.exports = {
 
     if (env === 'saucelabs') {
       context.options.plugins.sauce.browsers = saucelabsPlatforms;
-    } else if (env === 'saucelabs-p3') {
-      context.options.plugins.sauce.browsers = saucelabsPlatformsP3;
     } else if (env === 'saucelabs-cron') {
       context.options.plugins.sauce.browsers = cronPlatforms;
     }
