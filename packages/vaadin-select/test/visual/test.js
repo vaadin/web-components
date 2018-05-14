443c92cdc120b1cb27454b2e2d6e644f226f6da6
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2018-05-14 20:05:23 +0300

    Enable visual tests with Lumo

--- packages/vaadin-select/test/visual/test.js
+++ packages/vaadin-select/test/visual/test.js
@@ -1,15 +1,23 @@
 gemini.suite('vaadin-dropdown-menu', function(rootSuite) {
+  function wait(actions, find) {
+    actions.wait(5000);
+  }
 
-  gemini.suite('dropdown-menu', function(suite) {
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
+
+  gemini.suite('default-tests', function(suite) {
     suite
-      .setUrl('/dropdown-menu.html')
+      .setUrl('dropdown-menu.html')
       .setCaptureElements('#dropdown-menu')
-      .before(actions => {
-        actions
-          .waitForJSCondition(function(window) {
-            return window.webComponentsAreReady;
-          }, 120000);
-      })
       .capture('dropdown-menu');
   });
 
