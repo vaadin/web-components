58d53aedc82c75a1d0a927f041cb6e4910efb876
Sayo Oladeji <oladejioluwasayo@gmail.com>
2018-05-30 17:16:51 +0300

    Move IE11 and iOS to cron build vaadin/vaadin-charts#358

--- packages/vaadin-charts/wct.conf.js
+++ packages/vaadin-charts/wct.conf.js
@@ -3,18 +3,18 @@ var argv = require('yargs').argv;
 module.exports = {
   registerHooks: function(context) {
     var saucelabsPlatforms = [
-      'macOS 10.12/ipad@11.0',
-      'macOS 10.12/iphone@10.3',
       'macOS 10.12/safari@11.0',
       'Windows 10/firefox@58',
-      'Windows 10/internet explorer@11',
       'Windows 10/microsoftedge@16'
     ];
 
     var cronPlatforms = [
       'Android/chrome',
       'Windows 10/chrome@64',
-      'Windows 10/firefox@58'
+      'Windows 10/firefox@58',
+      'Windows 10/internet explorer@11',
+      'macOS 10.12/ipad@11.0',
+      'macOS 10.12/iphone@10.3'
     ];
 
     var quickPlatforms = [
