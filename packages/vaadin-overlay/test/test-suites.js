3ac318ea076e6061122e71ad16fcf408f5d13410
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-07-12 14:24:14 +0300

    Add renderer tests

--- packages/vaadin-overlay/test/test-suites.js
+++ packages/vaadin-overlay/test/test-suites.js
@@ -1,6 +1,8 @@
 window.VaadinOverlaySuites = [
   'vaadin-overlay.html',
   'vaadin-overlay.html?wc-shadydom=true',
+  'renderer.html',
+  'renderer.html?wc-shadydom=true',
   'focus-trap.html',
   'focus-trap.html?wc-shadydom=true',
   'styling.html',
