de2b068db4ca7b49afb1a9bb7f9827492584063f
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2018-07-19 16:32:29 +0300

    Add renderer vaadin/vaadin-notification#77
    * Add renderer tests
    
    * Add renderer implementation
    
    * Add renderer demo
    
    * Update docs and owner arg of renderer function
    
    * Update the renderer args order and test for it
    
    * Update demo wording
    
    * Update docs and demo
    
    * Bump analysis.json
    
    * Fix coveralls for PRs
    
    * Update renderer args and bump analysis.json
    
    * Remove public template, clean up model, bump analysis.json

--- packages/vaadin-notification/test/test-suites.js
+++ packages/vaadin-notification/test/test-suites.js
@@ -1,6 +1,7 @@
 window.VaadinNotificationSuites = [
   'vaadin-notification-test.html',
   'notification-styling-test.html',
+  'notification-renderer-test.html',
   'notification-template-test.html',
   'multiple-notification-test.html',
   'animate-notification-test.html',
