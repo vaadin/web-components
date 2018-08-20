720b75f90f79277eaf17b76d85cc26012a6fe5dd
Sohrab Taee <sohrab.taee@gmail.com>
2018-08-20 13:01:09 +0300

    Added visual tests for Material theme.

--- packages/vaadin-checkbox/test/visual/test.js
+++ packages/vaadin-checkbox/test/visual/test.js
@@ -13,7 +13,7 @@ gemini.suite('vaadin-checkbox', function(rootSuite) {
   rootSuite
     .before(wait)
     .after(goToAboutBlank);
-
+  // Lumo theme
   gemini.suite('default-tests', function(suite) {
     suite
       .setUrl('default.html')
@@ -41,6 +41,38 @@ gemini.suite('vaadin-checkbox', function(rootSuite) {
   gemini.suite('validation-tests', function(suite) {
     suite
       .setUrl('default.html')
+      .setCaptureElements('#validation-tests')
+      .capture('error');
+  });
+
+  // Material theme
+  gemini.suite('material-default-tests', function(suite) {
+    suite
+      .setUrl('material.html')
+      .setCaptureElements('#default-tests')
+      .capture('default')
+      .capture('focus-ring', function(actions) {
+        actions.executeJS(function(window) {
+          window.document.querySelector('vaadin-checkbox').setAttribute('focus-ring', '');
+        });
+      })
+      .capture('checked', function(actions) {
+        actions.executeJS(function(window) {
+          window.document.querySelector('vaadin-checkbox').checked = true;
+        });
+      });
+  });
+
+  gemini.suite('material-group-tests', (suite) => {
+    suite
+      .setUrl('material.html')
+      .setCaptureElements('#group-tests')
+      .capture('default');
+  });
+
+  gemini.suite('material-validation-tests', function(suite) {
+    suite
+      .setUrl('material.html')
       .setCaptureElements('#validation-tests')
       .capture('error');
   });
