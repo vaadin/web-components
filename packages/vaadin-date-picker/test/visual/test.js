ddc2ca3b92f0c74756a366128c4350892b1757e4
Sohrab Taee <sohrab.taee@gmail.com>
2018-08-20 14:47:02 +0300

    Added visual tests for Material theme.

--- packages/vaadin-date-picker/test/visual/test.js
+++ packages/vaadin-date-picker/test/visual/test.js
@@ -14,41 +14,43 @@ gemini.suite('vaadin-date-picker', function(rootSuite) {
     .before(wait)
     .after(goToAboutBlank);
 
-  gemini.suite('default-tests', function(suite) {
-    suite
-      .setUrl('default.html')
-      .setCaptureElements('#default-tests')
-      .capture('default');
-  });
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`default-tests-${theme}`, function(suite) {
+      suite
+        .setUrl(`default.html?theme=${theme}`)
+        .setCaptureElements('#default-tests')
+        .capture('default');
+    });
 
-  gemini.suite('dropdown', function(suite) {
-    suite
-      .setUrl('dropdown.html')
-      .setCaptureElements('body')
-      .capture('default', function(actions) {
-        actions.executeJS(function(window) {
-          window.closeDatePickers();
-          window.document.querySelector('#plain').open();
+    gemini.suite(`dropdown-${theme}`, function(suite) {
+      suite
+        .setUrl(`dropdown.html?theme=${theme}`)
+        .setCaptureElements('body')
+        .capture('default', function(actions) {
+          actions.executeJS(function(window) {
+            window.closeDatePickers();
+            window.document.querySelector('#plain').open();
+          });
+        })
+        .capture('selected-value', function(actions) {
+          actions.executeJS(function(window) {
+            window.closeDatePickers();
+            window.document.querySelector('#selected-value').open();
+          });
+        })
+        .capture('date-limit', function(actions) {
+          actions.executeJS(function(window) {
+            window.closeDatePickers();
+            window.document.querySelector('#date-limit').open();
+          });
+        })
+        .capture('week-numbers', function(actions) {
+          actions.executeJS(function(window) {
+            window.closeDatePickers();
+            window.document.querySelector('#weeks').open();
+          });
         });
-      })
-      .capture('selected-value', function(actions) {
-        actions.executeJS(function(window) {
-          window.closeDatePickers();
-          window.document.querySelector('#selected-value').open();
-        });
-      })
-      .capture('date-limit', function(actions) {
-        actions.executeJS(function(window) {
-          window.closeDatePickers();
-          window.document.querySelector('#date-limit').open();
-        });
-      })
-      .capture('week-numbers', function(actions) {
-        actions.executeJS(function(window) {
-          window.closeDatePickers();
-          window.document.querySelector('#weeks').open();
-        });
-      });
+    });
   });
 
 });
