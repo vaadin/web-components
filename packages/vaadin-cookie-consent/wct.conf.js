7e14a5e2aeccee0fb115a0c1a8d3cf4512194399
alexberazouski <alex@vaadin.com>
2018-08-24 14:10:15 +0300

    Update dependencies vaadin/vaadin-cookie-consent#49
    Sync travis and wct settings with skeleton

--- packages/vaadin-cookie-consent/wct.conf.js
+++ packages/vaadin-cookie-consent/wct.conf.js
@@ -2,20 +2,55 @@ var envIndex = process.argv.indexOf('--env') + 1;
 var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
+  testTimeout: 180 * 1000,
+  verbose: false,
+  plugins: {
+    local: {
+      browserOptions: {
+        chrome: [
+          'headless',
+          'disable-gpu',
+          'no-sandbox'
+        ],
+        firefox: [
+          '-headless'
+        ]
+      }
+    }
+  },
+
   registerHooks: function(context) {
-    var saucelabsPlatforms = [
-      'macOS 10.12/iphone@10.3',
-      'macOS 10.12/ipad@11.0',
-      'Windows 10/microsoftedge@15',
-      'Windows 10/internet explorer@11',
-      'macOS 10.13/safari@11.1',
-      'macOS 9.3.2/iphone@9.3'
+    const saucelabsPlatformsMobile = [
+      'iOS Simulator/iphone@11.3',
+      'iOS Simulator/iphone@9.3'
     ];
 
-    var cronPlatforms = [
-      'Android/chrome',
-      'Windows 10/chrome@59',
-      'Windows 10/firefox@54'
+    const saucelabsPlatformsMicrosoft = [
+      'Windows 10/microsoftedge@17',
+      'Windows 10/internet explorer@11'
+    ];
+
+    const saucelabsPlatformsDesktop = [
+      'macOS 10.13/safari@11.1'
+    ];
+
+    const saucelabsPlatforms = [
+      ...saucelabsPlatformsMobile,
+      ...saucelabsPlatformsMicrosoft,
+      ...saucelabsPlatformsDesktop
+    ];
+
+    const cronPlatforms = [
+      {
+        deviceName: 'Android GoogleAPI Emulator',
+        platformName: 'Android',
+        platformVersion: '7.1',
+        browserName: 'chrome'
+      },
+      'iOS Simulator/ipad@11.3',
+      'iOS Simulator/iphone@10.3',
+      'Windows 10/chrome@latest',
+      'Windows 10/firefox@latest'
     ];
 
     if (env === 'saucelabs') {
