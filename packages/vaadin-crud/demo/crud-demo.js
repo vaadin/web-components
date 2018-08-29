1572cbb8cf8324bd0a150f7db515b957992b4823
Manolo Carrasco <manolo@apache.org>
2018-08-29 17:21:58 +0200

    Rename demos to be aligned with other elements

---
+++ packages/vaadin-crud/demo/crud-demo.js
@@ -0,0 +1,12 @@
+window.CrudDemo = superClass => {
+  return class extends superClass {
+    static get properties() {
+      return {
+      };
+    }
+  };
+};
+
+window.addEventListener('WebComponentsReady', () => {
+  document.body.removeAttribute('unresolved');
+});
