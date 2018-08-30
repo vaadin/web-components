edc6346bdaf2a2b2ff87b517c05bdae90c71bcf8
Sohrab Taee <sohrab.taee@gmail.com>
2018-08-30 13:15:47 +0300

    Added visual tests for Material theme. vaadin/vaadin-text-field#251

--- packages/vaadin-text-field/test/visual/test.js
+++ packages/vaadin-text-field/test/visual/test.js
@@ -1,9 +1,6 @@
 gemini.suite('vaadin-text-field', function(rootSuite) {
   function wait(actions, find) {
-    return actions
-      .waitForJSCondition(function(window) {
-        return window.WebComponents && window.WebComponents.ready;
-      }, 60000);
+    actions.wait(5000);
   }
 
   function goToAboutBlank(actions, find) {
@@ -16,81 +13,83 @@ gemini.suite('vaadin-text-field', function(rootSuite) {
   rootSuite
     .before(wait)
     .after(goToAboutBlank);
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`text-field-1-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-field/text-field-1.html?theme=${theme}`)
+        .setCaptureElements('#text-field')
+        .capture('text-field-1');
+    });
 
-  gemini.suite('text-field-1', function(suite) {
-    suite
-      .setUrl('vaadin-text-field/text-field-1.html')
-      .setCaptureElements('#text-field')
-      .capture('text-field-1');
+    gemini.suite(`text-field-2-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-field/text-field-2.html?theme=${theme}`)
+        .setCaptureElements('#text-field')
+        .capture('text-field-2');
+    });
+
+    gemini.suite(`text-field-styling-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-field/styling.html?theme=${theme}`)
+        .setCaptureElements('#text-field')
+        .capture('styling');
+    });
+
+    gemini.suite(`text-field-rtl-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-field/rtl.html?theme=${theme}`)
+        .setCaptureElements('#rtl')
+        .capture('rtl');
+    });
+
+    gemini.suite(`password-field-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-password-field/password-field.html?theme=${theme}`)
+        .setCaptureElements('#password-field')
+        .capture('password-field');
+    });
+
+    gemini.suite(`password-field-rtl-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-password-field/rtl.html?theme=${theme}`)
+        .setCaptureElements('#rtl')
+        .capture('rtl');
+    });
+
+    gemini.suite(`text-area-1-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-area/text-area-1.html?theme=${theme}`)
+        .setCaptureElements('#text-area')
+        .capture('text-area-1');
+    });
+
+    gemini.suite(`text-area-2-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-area/text-area-2.html?theme=${theme}`)
+        .setCaptureElements('#text-area')
+        .capture('text-area-2');
+    });
+
+    gemini.suite(`text-area-3-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-area/text-area-3.html?theme=${theme}`)
+        .setCaptureElements('#text-area')
+        .capture('text-area-3');
+    });
+
+    gemini.suite(`text-area-styling-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-area/styling.html?theme=${theme}`)
+        .setCaptureElements('#text-area')
+        .capture('styling');
+    });
+
+    gemini.suite(`text-area-rtl-${theme}`, function(suite) {
+      suite
+        .setUrl(`vaadin-text-area/rtl.html?theme=${theme}`)
+        .setCaptureElements('#rtl')
+        .capture('rtl');
+    });
   });
 
-  gemini.suite('text-field-2', function(suite) {
-    suite
-      .setUrl('vaadin-text-field/text-field-2.html')
-      .setCaptureElements('#text-field')
-      .capture('text-field-2');
-  });
-
-  gemini.suite('text-field-styling', function(suite) {
-    suite
-      .setUrl('vaadin-text-field/styling.html')
-      .setCaptureElements('#text-field')
-      .capture('styling');
-  });
-
-  gemini.suite('text-field-rtl', function(suite) {
-    suite
-      .setUrl('vaadin-text-field/rtl.html')
-      .setCaptureElements('#rtl')
-      .capture('rtl');
-  });
-
-  gemini.suite('password-field', function(suite) {
-    suite
-      .setUrl('vaadin-password-field/password-field.html')
-      .setCaptureElements('#password-field')
-      .capture('password-field');
-  });
-
-  gemini.suite('password-field-rtl', function(suite) {
-    suite
-      .setUrl('vaadin-password-field/rtl.html')
-      .setCaptureElements('#rtl')
-      .capture('rtl');
-  });
-
-  gemini.suite('text-area-1', function(suite) {
-    suite
-      .setUrl('vaadin-text-area/text-area-1.html')
-      .setCaptureElements('#text-area')
-      .capture('text-area-1');
-  });
-
-  gemini.suite('text-area-2', function(suite) {
-    suite
-      .setUrl('vaadin-text-area/text-area-2.html')
-      .setCaptureElements('#text-area')
-      .capture('text-area-2');
-  });
-
-  gemini.suite('text-area-3', function(suite) {
-    suite
-      .setUrl('vaadin-text-area/text-area-3.html')
-      .setCaptureElements('#text-area')
-      .capture('text-area-3');
-  });
-
-  gemini.suite('text-area-styling', function(suite) {
-    suite
-      .setUrl('vaadin-text-area/styling.html')
-      .setCaptureElements('#text-area')
-      .capture('styling');
-  });
-
-  gemini.suite('text-area-rtl', function(suite) {
-    suite
-      .setUrl('vaadin-text-area/rtl.html')
-      .setCaptureElements('#rtl')
-      .capture('rtl');
-  });
 });
