7b7e0cf5976c7e8fa8ee983fac6496c630a6d1de
Sami Suo-Heikki <suoheikki.sami@gmail.com>
2018-09-04 10:35:09 +0300

    Add visual tests vaadin/vaadin-overlay#121

---
+++ packages/vaadin-overlay/test/visual/test.js
@@ -0,0 +1,25 @@
+gemini.suite('vaadin-overlay', function(rootSuite) {
+  function wait(actions, find) {
+    actions.wait(5000);
+  }
+
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
+
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`default-tests-${theme}`, function(suite) {
+      suite
+        .setUrl(`default.html?theme=${theme}`)
+        .setCaptureElements('#overlay')
+        .capture('default');
+    });
+  });
+});
