6ea35a4836650ab8b2f6255e14d202c198325552
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-01-15 10:04:16 +0200

    feat: import details from vaadin-accordion repo vaadin/vaadin-details#2

---
+++ packages/vaadin-details/test/test-suites.js
@@ -0,0 +1,3 @@
+window.VaadinDetailsSuites = [
+  'details.html'
+];
---
+++ packages/vaadin-details/test/visual/test.js
@@ -0,0 +1,35 @@
+gemini.suite('vaadin-details', function(rootSuite) {
+  function wait(actions, find) {
+    return actions
+      .waitForJSCondition(function(window) {
+        return window.webComponentsAreReady;
+      }, 80000);
+  }
+
+  function goToAboutBlank(actions, find) {
+    // Firefox stops responding on socket after a test, workaround:
+    return actions.executeJS(function(window) {
+      window.location.href = 'about:blank'; // just go away, please!
+    });
+  }
+
+  rootSuite
+    .before(wait)
+    .after(goToAboutBlank);
+
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`details-${theme}`, function(suite) {
+      suite
+        .setUrl(`/details.html?theme=${theme}`)
+        .setCaptureElements('#details-tests')
+        .capture(`vaadin-details`);
+    });
+  });
+
+  gemini.suite(`lumo-theme`, function(suite) {
+    suite
+      .setUrl(`/lumo-theme.html`)
+      .setCaptureElements('#details-tests')
+      .capture(`vaadin-details`);
+  });
+});
