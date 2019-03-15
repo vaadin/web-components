1e331d72335aa5159da783a15dfa5d77776a9c1f
Kari Söderholm <haprog@vaadin.com>
2019-03-15 13:20:23 +0200

    chore: remove lint tasks from gulpfile
    Remove lint tasks from gulpfile in preparation for https://github.com/vaadin/vaadin-element-skeleton/issues/199

--- packages/vaadin-icons/gulpfile.js
+++ packages/vaadin-icons/gulpfile.js
@@ -2,60 +2,9 @@
 'use strict';
 
 const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const htmlExtract = require('gulp-html-extract');
-const lec = require('gulp-line-ending-corrector');
-const stylelint = require('gulp-stylelint');
 const modify = require('gulp-modify');
 const cheerio = require('cheerio');
 const concat = require('gulp-concat');
-
-gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
-
-gulp.task('lint:js', function() {
-  return gulp.src([
-    '*.js',
-    'test/**/*.js'
-  ])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError('fail'));
-});
-
-gulp.task('lint:html', function() {
-  return gulp.src([
-    '*.html',
-    'src/**/*.html',
-    'demo/**/*.html',
-    'test/**/*.html'
-  ])
-    .pipe(htmlExtract({
-      sel: 'script, code-example code',
-      strip: true
-    }))
-    .pipe(lec())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError('fail'));
-});
-
-gulp.task('lint:css', function() {
-  return gulp.src([
-    '*.html',
-    'src/**/*.html',
-    'demo/**/*.html',
-    'theme/**/*.html',
-    'test/**/*.html'
-  ])
-    .pipe(htmlExtract({
-      sel: 'style'
-    }))
-    .pipe(stylelint({
-      reporters: [
-        {formatter: 'string', console: true}
-      ]
-    }));
-});
 
 gulp.task('default', function() {
   return gulp.src(['assets/svg/*.svg'], {base: '.'})
