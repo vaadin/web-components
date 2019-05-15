45ed804f58e50f4f09d50e6f143ce173b75089df
Kari Söderholm <haprog@vaadin.com>
2019-05-15 13:58:45 +0300

    test: update browser versions used in tests
    Related to:
    https://github.com/vaadin/components-team-tasks/issues/450
    https://github.com/vaadin/vaadin-element-skeleton/pull/204

--- packages/vaadin-board/wct.conf.js
+++ packages/vaadin-board/wct.conf.js
@@ -3,20 +3,37 @@ var env = envIndex ? process.argv[envIndex] : undefined;
 
 module.exports = {
   registerHooks: function(context) {
-    var saucelabsPlatforms = [
-      'macOS 10.12/ipad@10.3',
-      'macOS 10.12/ipad@11.2',
-      'Windows 10/microsoftedge@16',
-      'Windows 10/internet explorer@11',
-      'macOS 10.13/safari@11.1',
-      'Windows 10/firefox@59',
-      'Windows 10/chrome@65'
+    const saucelabsPlatformsMobile = [
+      'iOS Simulator/iphone@12.2',
+      'iOS Simulator/iphone@10.3'
     ];
 
-    var cronPlatforms = [
-      'Android/chrome',
-      'Windows 10/chrome@65',
-      'Windows 10/firefox@59'
+    const saucelabsPlatformsMicrosoft = [
+      'Windows 10/microsoftedge@18',
+      'Windows 10/internet explorer@11'
+    ];
+
+    const saucelabsPlatformsDesktop = [
+      'macOS 10.13/safari@latest'
+    ];
+
+    const saucelabsPlatforms = [
+      ...saucelabsPlatformsMobile,
+      ...saucelabsPlatformsMicrosoft,
+      ...saucelabsPlatformsDesktop
+    ];
+
+    const cronPlatforms = [
+      {
+        deviceName: 'Android GoogleAPI Emulator',
+        platformName: 'Android',
+        platformVersion: '8.1',
+        browserName: 'chrome'
+      },
+      'iOS Simulator/ipad@12.2',
+      'iOS Simulator/iphone@10.3',
+      'Windows 10/chrome@latest',
+      'Windows 10/firefox@latest'
     ];
 
     if (env === 'saucelabs') {
