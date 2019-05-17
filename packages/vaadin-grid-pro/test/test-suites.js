ba040069c3b1a6962f9f032776117c6ffe66a282
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-05-17 14:01:48 +0300

    test: ensure custom editor component with overlay works

--- packages/vaadin-grid-pro/test/test-suites.js
+++ packages/vaadin-grid-pro/test/test-suites.js
@@ -3,5 +3,6 @@ window.GridProElementSuites = [
   'edit-column.html',
   'edit-column-renderer.html',
   'edit-column-template.html',
-  'edit-column-type.html'
+  'edit-column-type.html',
+  'edit-column-overlay.html'
 ];
