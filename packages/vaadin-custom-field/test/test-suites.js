3c089977de2aab052ebc6f9bd771f90760866533
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2019-06-03 09:55:27 +0300

    Internal tab vaadin/vaadin-custom-field#32
    * Fire internal-tabbing on Tab, focus first input, add tests
    
    * Add event documentation
    
    * Rename to internal-tab
    
    * Fire an event in tests

--- packages/vaadin-custom-field/test/test-suites.js
+++ packages/vaadin-custom-field/test/test-suites.js
@@ -2,5 +2,6 @@ window.CustomFieldElementSuites = [
   'custom-field-basic-test.html',
   'custom-field-slot-test.html',
   'custom-field-validation-test.html',
-  'custom-field-functions-test.html'
+  'custom-field-functions-test.html',
+  'custom-field-keyboard-test.html'
 ];
