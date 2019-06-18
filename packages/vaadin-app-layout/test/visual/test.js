868c14c7bb0684c35c4083a1b9a4d01fffa854e4
web-padawan <serguey.kulikov@gmail.com>
2019-06-18 19:44:01 +0300

    test: add visual tests for app-layout with drawer

--- packages/vaadin-app-layout/test/visual/test.js
+++ packages/vaadin-app-layout/test/visual/test.js
@@ -1,6 +1,9 @@
-gemini.suite('vaadin-element', function(rootSuite) {
+gemini.suite('vaadin-app-layout', function(rootSuite) {
   function wait(actions, find) {
-    actions.wait(5000);
+    return actions
+      .waitForJSCondition(function(window) {
+        return window.webComponentsAreReady;
+      }, 80000);
   }
 
   function goToAboutBlank(actions, find) {
@@ -14,11 +17,26 @@ gemini.suite('vaadin-element', function(rootSuite) {
     .before(wait)
     .after(goToAboutBlank);
 
-  gemini.suite('default-tests', function(suite) {
-    suite
-      .setUrl('/default.html')
-      .setCaptureElements('#default-tests')
-      .capture('normal-button');
-  });
+  ['lumo', 'material'].forEach(theme => {
+    gemini.suite(`drawer-${theme}`, function(suite) {
+      suite
+        .setUrl(`/drawer.html?theme=${theme}`)
+        .setCaptureElements('#drawer-tests')
+        .capture('default');
+    });
 
+    gemini.suite(`primary-drawer-${theme}`, function(suite) {
+      suite
+        .setUrl(`/primary-drawer.html?theme=${theme}`)
+        .setCaptureElements('#primary-drawer-tests')
+        .capture('default');
+    });
+
+    gemini.suite(`tabs-${theme}`, function(suite) {
+      suite
+        .setUrl(`/tabs.html?theme=${theme}`)
+        .setCaptureElements('#tabs-tests')
+        .capture('default');
+    });
+  });
 });
