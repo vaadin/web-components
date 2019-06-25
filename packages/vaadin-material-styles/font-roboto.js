f3d77cc55dad0d7b94a517566f5020cd01f86f93
Serhii Kulykov <serguey.kulikov@gmail.com>
2019-06-25 16:06:42 +0300

    feat: opt-out loading Roboto from Google Fonts vaadin/vaadin-material-styles#91

--- packages/vaadin-material-styles/font-roboto.js
+++ packages/vaadin-material-styles/font-roboto.js
@@ -1,7 +1,9 @@
-const font = 'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|Roboto:400,300,300italic,400italic,500,500italic,700,700italic';
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.crossOrigin = 'anonymous';
-link.href = font;
-document.head.appendChild(link);
+if (!window.polymerSkipLoadingFontRoboto) {
+  const font = 'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|Roboto:400,300,300italic,400italic,500,500italic,700,700italic';
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  link.crossOrigin = 'anonymous';
+  link.href = font;
+  document.head.appendChild(link);
+}
