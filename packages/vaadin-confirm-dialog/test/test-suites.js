7d6205f9517571da9210d9bef05a864c550cbcfa
Tulio Garcia <tulio@vaadin.com>
2019-07-09 17:30:56 +0300

    Create internal API for changing width and height vaadin/vaadin-confirm-dialog#84
    Component now has internal `_setWidth` and `_setHeight` API that flow integration can use to set dimensions for the content

--- packages/vaadin-confirm-dialog/test/test-suites.js
+++ packages/vaadin-confirm-dialog/test/test-suites.js
@@ -2,5 +2,6 @@ window.VaadinConfirmDialogSuites = [
   'alert-api-test.html',
   'cancel-api-test.html',
   'confirmation-api-test.html',
-  'confirm-reject-api-test.html'
+  'confirm-reject-api-test.html',
+  'resize-api-test.html'
 ];
