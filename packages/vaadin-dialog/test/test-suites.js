94936fda6506b89de2e255b26227e72a38714dfc
Nii Yeboah <nii@vaadin.com>
2020-01-14 02:51:34 -0800

    Feature: modeless, draggable, resizable dialogs vaadin/vaadin-dialog#139

--- packages/vaadin-dialog/test/test-suites.js
+++ packages/vaadin-dialog/test/test-suites.js
@@ -1,4 +1,5 @@
 window.VaadinDialogSuites = [
   'vaadin-dialog_test.html',
-  'vaadin-dialog_renderer-test.html'
+  'vaadin-dialog_renderer-test.html',
+  'vaadin-dialog_draggable-resizable-test.html'
 ];
