27d049134a25155e7e5fab8ff3f57b6292ba6ee5
Yuriy Yevstihnyeyev <yuriy@vaadin.com>
2020-04-08 09:17:05 +0300

    Add rtl support vaadin/vaadin-grid#1706
    * Initial rtl support
    
    * Fix the styles, update frozen, add rtl visual tests
    
    * Optimize frozen cells calculations
    
    * Add test for frozen columns
    
    * Import dir mixin in tree toggle
    
    * Fix FF issues with normalizedScrollLeft
    
    * Fix sorter order
    
    * Update sorting visual tests and fix tree toggle rtl material
    
    * Add missing sorting styles
    
    * Move toggle margin to not(rtl)
    
    * Bump dependencies
    
    * Move _updateScrollerMeasurements to vaadin-grid-scroll-mixin
    
    * Remove extra count styles
    
    * Update helper
    
    * Bump version

--- packages/vaadin-grid/test/visual/test.js
+++ packages/vaadin-grid/test/visual/test.js
@@ -16,63 +16,65 @@ gemini.suite('vaadin-grid', (rootSuite) => {
     .after(goToAboutBlank);
 
   ['lumo', 'material'].forEach(theme => {
-    gemini.suite(`header-footer-${theme}`, (suite) => {
-      suite
-        .setUrl(`header-footer.html?theme=${theme}`)
-        .setCaptureElements('#header-footer')
-        .capture('header-footer', {}, (actions, find) => {
-          actions.wait(6000);
-        });
-    });
-
-    gemini.suite(`column-groups-${theme}`, (suite) => {
-      suite
-        .setUrl(`column-groups.html?theme=${theme}`)
-        .setCaptureElements('#column-groups')
-        .capture('column-groups', {}, (actions, find) => {
-          actions.wait(6000);
-        });
-    });
-
-    gemini.suite(`sorting-${theme}`, (suite) => {
-      suite
-        .setUrl(`sorting.html?theme=${theme}`)
-        .setCaptureElements('#sorting')
-        .capture('sorting-initial', {}, (actions, find) => {
-          actions.wait(6000);
-        })
-        .before((actions, find) => {
-          this.firstNameSorter = find('#first-name-sorter');
-          this.lastNameSorter = find('#last-name-sorter');
-        })
-        .capture('single-column-asc', {}, (actions, find) => {
-          actions.click(this.firstNameSorter);
-        })
-        .capture('multiple-columns-asc-asc', {}, (actions, find) => {
-          actions.click(this.lastNameSorter);
-        })
-        .capture('multiple-columns-asc-desc', {}, (actions, find) => {
-          actions.click(this.lastNameSorter);
-        })
-        .capture('single-column-desc', {}, (actions, find) => {
-          actions.click(this.lastNameSorter);
-          actions.click(this.firstNameSorter);
-        });
-    });
-
-    gemini.suite(`row-details-${theme}`, (suite) => {
-      suite
-        .setUrl(`row-details.html?theme=${theme}`)
-        .setCaptureElements('#row-details')
-        .capture('row-details-initial', {}, (actions, find) => {
-          actions.wait(6000);
-        })
-        .capture('row-details-visible', {}, (actions, find) => {
-          actions.executeJS(function(window) {
-            var grid = window.document.querySelector('vaadin-grid');
-            grid.openItemDetails(grid.items[0]);
+    ['ltr', 'rtl'].forEach(direction => {
+      gemini.suite(`header-footer-${theme}-${direction}`, (suite) => {
+        suite
+          .setUrl(`header-footer.html?theme=${theme}&dir=${direction}`)
+          .setCaptureElements('#header-footer')
+          .capture('header-footer', {}, (actions, find) => {
+            actions.wait(6000);
           });
-        });
+      });
+
+      gemini.suite(`column-groups-${theme}-${direction}`, (suite) => {
+        suite
+          .setUrl(`column-groups.html?theme=${theme}&dir=${direction}`)
+          .setCaptureElements('#column-groups')
+          .capture('column-groups', {}, (actions, find) => {
+            actions.wait(6000);
+          });
+      });
+
+      gemini.suite(`row-details-${theme}-${direction}`, (suite) => {
+        suite
+          .setUrl(`row-details.html?theme=${theme}`)
+          .setCaptureElements('#row-details')
+          .capture('row-details-initial', {}, (actions, find) => {
+            actions.wait(6000);
+          })
+          .capture('row-details-visible', {}, (actions, find) => {
+            actions.executeJS(function(window) {
+              var grid = window.document.querySelector('vaadin-grid');
+              grid.openItemDetails(grid.items[0]);
+            });
+          });
+      });
+
+      gemini.suite(`sorting-${theme}-${direction}`, (suite) => {
+        suite
+          .setUrl(`sorting.html?theme=${theme}&dir=${direction}`)
+          .setCaptureElements('#sorting')
+          .capture('sorting-initial', {}, (actions, find) => {
+            actions.wait(6000);
+          })
+          .before((actions, find) => {
+            this.firstNameSorter = find('#first-name-sorter');
+            this.lastNameSorter = find('#last-name-sorter');
+          })
+          .capture('single-column-asc', {}, (actions, find) => {
+            actions.click(this.firstNameSorter);
+          })
+          .capture('multiple-columns-asc-asc', {}, (actions, find) => {
+            actions.click(this.lastNameSorter);
+          })
+          .capture('multiple-columns-asc-desc', {}, (actions, find) => {
+            actions.click(this.lastNameSorter);
+          })
+          .capture('single-column-desc', {}, (actions, find) => {
+            actions.click(this.lastNameSorter);
+            actions.click(this.firstNameSorter);
+          });
+      });
     });
 
     gemini.suite(`drag-and-drop-${theme}`, (suite) => {
