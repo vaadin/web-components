3d2dc213191f8797229d0799d8b741929f028238
Serhii Kulykov <iamkulykov@gmail.com>
2020-04-29 13:55:33 +0300

    feat: prepare for TS defs generation vaadin/vaadin-button#148

---
+++ packages/vaadin-button/magi-p3-post.js
@@ -0,0 +1,11 @@
+module.exports = {
+  files: [
+    'vaadin-button.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
