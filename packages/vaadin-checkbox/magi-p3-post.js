574c996111e590c7b3510ce7095aad7dd7620d4f
Serhii Kulykov <iamkulykov@gmail.com>
2020-05-07 10:47:14 +0300

    feat: prepare for TS defs generation vaadin/vaadin-checkbox#173

---
+++ packages/vaadin-checkbox/magi-p3-post.js
@@ -0,0 +1,12 @@
+module.exports = {
+  files: [
+    'vaadin-checkbox.js',
+    'vaadin-checkbox-group.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
