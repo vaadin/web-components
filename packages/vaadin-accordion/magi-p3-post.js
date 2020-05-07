a1d8eefd8ea3926175e9c2b43b7773da2717071d
Serhii Kulykov <iamkulykov@gmail.com>
2020-05-07 10:31:39 +0300

    feat: prepare for TS defs generation vaadin/vaadin-accordion#41

---
+++ packages/vaadin-accordion/magi-p3-post.js
@@ -0,0 +1,11 @@
+module.exports = {
+  files: [
+    'vaadin-accordion.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
