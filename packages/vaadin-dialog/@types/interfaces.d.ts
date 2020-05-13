f175417c52ba11591a359933c9df2b9310a244b2
Kari Söderholm <haprog@vaadin.com>
2020-05-13 18:02:20 +0300

    feat: prepare for TS defs generation vaadin/vaadin-dialog#160

---
+++ packages/vaadin-dialog/@types/interfaces.d.ts
@@ -0,0 +1,22 @@
+export type DialogResizableDirection = 'n' | 'e' | 's' | 'w' | 'nw' | 'ne' | 'se' | 'sw';
+
+export type DialogResizeDimensions = {
+  width: string;
+  height: string;
+  contentWidth: string;
+  contentHeight: string;
+};
+
+export type DialogOverlayBounds = {
+  top: number;
+  left: number;
+  width: number;
+  height: number;
+};
+
+export type DialogOverlayBoundsParam = DialogOverlayBounds | {
+  top?: string | number;
+  left?: string | number;
+  width?: string | number;
+  height?: string | number;
+};
