d9d1e7b9aba72e82ee995230e4b3d642079fd438
Pekka Maanpää <pekkamaa@vaadin.com>
2020-05-14 15:51:43 +0300

    feat: prepare for TS defs generation vaadin/vaadin-progress-bar#72
    resolvesvaadin/vaadin-progress-bar#71

---
+++ packages/vaadin-progress-bar/magi-p3-post.js
@@ -0,0 +1,11 @@
+module.exports = {
+  files: [
+    'vaadin-progress-bar.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
