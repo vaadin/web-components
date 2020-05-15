6bdbd5f0f68e452c6fdfcf94cc528a41b8f06172
Kari Söderholm <haprog@vaadin.com>
2020-05-15 13:25:03 +0300

    feat: prepare for TS defs generation vaadin/vaadin-notification#110

---
+++ packages/vaadin-notification/@types/interfaces.d.ts
@@ -0,0 +1,10 @@
+import { NotificationElement } from '../src/vaadin-notification.js';
+
+export type NotificationPosition =
+  'top-stretch' | 'top-start' | 'top-center' | 'top-end' | 'middle' | 'bottom-start' | 'bottom-center' | 'bottom-end' |
+  'bottom-stretch';
+
+export type NotificationRenderer = (
+  root: HTMLElement,
+  notification: NotificationElement
+) => void;
