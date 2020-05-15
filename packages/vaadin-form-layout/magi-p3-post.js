28ce3880bef40c4026d7d53ea1ec1cca25c3e864
Kari Söderholm <haprog@vaadin.com>
2020-05-15 15:20:28 +0300

    feat: prepare for TS defs generation

---
+++ packages/vaadin-form-layout/magi-p3-post.js
@@ -0,0 +1,12 @@
+module.exports = {
+  files: [
+    'vaadin-form-item.js',
+    'vaadin-form-layout.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
