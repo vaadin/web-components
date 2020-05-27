7c416a7b95d2ea7d52066330851f7575f470d492
Serhii Kulykov <iamkulykov@gmail.com>
2020-05-27 10:12:07 +0300

    feat: prepare for TS defs generation vaadin/vaadin-date-picker#727

---
+++ packages/vaadin-date-picker/@types/interfaces.d.ts
@@ -0,0 +1,20 @@
+export interface DatePickerDate {
+  day: number;
+  month: number;
+  year: number;
+}
+
+export interface DatePickerI18n {
+  monthNames: string[];
+  weekdays: string[];
+  weekdaysShort: string[];
+  firstDayOfWeek: number;
+  week: string;
+  calendar: string;
+  clear: string;
+  today: string;
+  cancel: string;
+  parseDate: (date: string) => DatePickerDate | undefined;
+  formatDate: (date: DatePickerDate) => string;
+  formatTitle: (monthName: string, fullYear: number) => string;
+}
---
+++ packages/vaadin-date-picker/magi-p3-post.js
@@ -0,0 +1,12 @@
+module.exports = {
+  files: [
+    'vaadin-date-picker.js',
+    'vaadin-date-picker-light.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
