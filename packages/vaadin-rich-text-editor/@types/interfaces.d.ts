190760a9e07d5436aa3dab45c441444887d50f17
Serhii Kulykov <iamkulykov@gmail.com>
2020-05-28 15:13:54 +0300

    feat: prepare for TS defs generation vaadin/vaadin-rich-text-editor#139

---
+++ packages/vaadin-rich-text-editor/@types/interfaces.d.ts
@@ -0,0 +1,27 @@
+export interface RichTextEditorI18n {
+  undo: string;
+  redo: string;
+  bold: string;
+  italic: string;
+  underline: string;
+  strike: string;
+  h1: string;
+  h2: string;
+  h3: string;
+  subscript: string;
+  superscript: string;
+  listOrdered: string;
+  listBullet: string;
+  alignLeft: string;
+  alignCenter: string;
+  alignRight: string;
+  image: string;
+  link: string;
+  blockquote: string;
+  codeBlock: string;
+  clean: string;
+  linkDialogTitle: string;
+  ok: string;
+  cancel: string;
+  remove: string;
+}
---
+++ packages/vaadin-rich-text-editor/magi-p3-post.js
@@ -0,0 +1,11 @@
+module.exports = {
+  files: [
+    'vaadin-rich-text-editor.js'
+  ],
+  from: [
+    /import '\.\/theme\/lumo\/vaadin-(.+)\.js';/
+  ],
+  to: [
+    `import './theme/lumo/vaadin-$1.js';\nexport * from './src/vaadin-$1.js';`
+  ]
+};
