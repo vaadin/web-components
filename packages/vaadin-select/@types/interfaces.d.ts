62658c459606b8a349442d8ef9c159920c0167f9
Serhii Kulykov <iamkulykov@gmail.com>
2020-06-17 12:45:39 +0300

    fix: make renderer owner optional [skip ci] vaadin/vaadin-select#230

--- packages/vaadin-select/@types/interfaces.d.ts
+++ packages/vaadin-select/@types/interfaces.d.ts
@@ -1,3 +1,3 @@
 import { SelectElement } from '../src/vaadin-select.js';
 
-export type SelectRenderer = (root: HTMLElement, select: SelectElement) => void;
+export type SelectRenderer = (root: HTMLElement, select?: SelectElement) => void;
