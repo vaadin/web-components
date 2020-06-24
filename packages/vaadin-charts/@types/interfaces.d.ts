2a20b3771e77cfbde4412b41166befbb138ae460
Guille <alvarezguille@users.noreply.github.com>
2020-06-24 13:32:23 +0300

    style: change nbsp vaadin/vaadin-charts#483

--- packages/vaadin-charts/@types/interfaces.d.ts
+++ packages/vaadin-charts/@types/interfaces.d.ts
@@ -1,18 +1,18 @@
-import { PointOptionsObject, SeriesOptionsType } from 'highcharts';
+import { PointOptionsObject, SeriesOptionsType } from 'highcharts';
 
-export type ChartCategories = Array<string> | { [key: number]: string };
+export type ChartCategories = Array<string> | { [key: number]: string };
 
-export type ChartCategoryPosition = 'left' | 'right' | 'top' | 'bottom';
+export type ChartCategoryPosition = 'left' | 'right' | 'top' | 'bottom';
 
-export type ChartSeriesMarkers = 'shown' | 'hidden' | 'auto';
+export type ChartSeriesMarkers = 'shown' | 'hidden' | 'auto';
 
 export interface ChartSeriesConfig {
   data?: ChartSeriesValues;
-  marker?: { enabled: boolean | null }
+  marker?: { enabled: boolean | null }
   name?: string;
-  neckWidth?: number | string;
-  neckHeight?: number | string;
-  stack?: number | string;
+  neckWidth?: number | string;
+  neckHeight?: number | string;
+  stack?: number | string;
   type?: string;
   yAxis?: string;
   yAxisValueMin?: number;
@@ -23,4 +23,4 @@ export type ChartSeriesOptions = ChartSeriesConfig & SeriesOptionsType;
 
 export type ChartSeriesValues = Array<number|Array<number>|PointOptionsObject>;
 
-export type ChartStacking = 'normal' | 'percent' | null;
+export type ChartStacking = 'normal' | 'percent' | null;
