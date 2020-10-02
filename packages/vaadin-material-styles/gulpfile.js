cb5a0e793ce7eda889d33cc9fb5c7c32f56d1054
web-padawan <iamkulykov@gmail.com>
2020-10-02 13:35:00 +0300

    chore: fix gulp-icons to work with Node 12

--- packages/vaadin-material-styles/gulpfile.js
+++ packages/vaadin-material-styles/gulpfile.js
@@ -11,19 +11,20 @@ var grepContents = require('gulp-grep-contents');
 var clip = require('gulp-clip-empty-files');
 var git = require('gulp-git');
 
-gulp.task('lint', ['lint:js', 'lint:html', 'lint:css']);
-
-gulp.task('lint:js', function() {
+gulp.task('lint:js', async function() {
   return gulp.src([
     '*.js',
     'test/*.js'
   ])
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError('fail'));
+    .pipe(eslint.failAfterError('fail'))
+    .on('error', function(err) {
+      // return console.error(err);
+    });
 });
 
-gulp.task('lint:html', function() {
+gulp.task('lint:html', async function() {
   return gulp.src([
     '*.html',
     'demo/**/*.html',
@@ -35,10 +36,13 @@ gulp.task('lint:html', function() {
     }))
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError('fail'));
+    .pipe(eslint.failAfterError('fail'))
+    .on('error', function(err) {
+      // return console.error(err);
+    });
 });
 
-gulp.task('lint:css', function() {
+gulp.task('lint:css', async function() {
   return gulp.src([
     '*.html',
     'demo/**/*.html',
@@ -51,10 +55,13 @@ gulp.task('lint:css', function() {
       reporters: [
         {formatter: 'string', console: true}
       ]
-    }));
+    }))
+    .on('error', function(err) {
+      // return console.error(err);
+    });
 });
 
-gulp.task('version:check', function() {
+gulp.task('version:check', async function() {
   const expectedVersion = new RegExp('^' + require('./package.json').version + '$');
   return gulp.src(['*.html'])
     .pipe(htmlExtract({sel: 'script'}))
@@ -65,7 +72,7 @@ gulp.task('version:check', function() {
     .pipe(expect({reportUnexpected: true}, []));
 });
 
-gulp.task('version:update', function() {
+gulp.task('version:update', async function() {
   // Should be run from 'preversion'
   // Assumes that the old version is in package.json and the new version in the `npm_package_version` environment variable
   const oldversion = require('./package.json').version;
@@ -85,12 +92,27 @@ gulp.task('version:update', function() {
 /* Generate font icons from source SVGs */
 
 var iconfont = require('gulp-iconfont');
-var exec = require('child_process').exec;
 var fs = require('fs');
-var svgpath = require('svgpath');
 var svgmin = require('gulp-svgmin');
+var sort = require('gulp-sort');
 
-gulp.task('icons', function() {
+/**
+ * Normalize file sort order across platforms (OS X vs Linux, maybe others).
+ *
+ * Before: `[..., 'eye-disabled', 'eye', ...]`
+ * After:  `[..., 'eye', 'eye-disabled', ...]`
+ *
+ * Order of appearance impacts assigned Unicode codepoints, and sometimes build diffs.
+ *
+ * @see https://github.com/nfroidure/svgicons2svgfont/pull/82
+ * @see https://github.com/nfroidure/svgicons2svgfont/blob/master/src/filesorter.js
+ * @see http://support.ecisolutions.com/doc-ddms/help/reportsmenu/ascii_sort_order_chart.htm
+ */
+function sortIconFilesNormalized(file1, file2) {
+  return file1.replace(/-/g, '~').localeCompare(file2.replace(/-/g, '~'), 'en-US');
+}
+
+gulp.task('icons', async function() {
   var folder = 'icons/svg/';
   var glyphs;
 
@@ -113,12 +135,18 @@ gulp.task('icons', function() {
     .on('finish', function(args) {
       // icon font
       gulp.src(folder + '*.svg')
+        .pipe(sort({
+          comparator: function(file1, file2) {
+            return sortIconFilesNormalized(file1.relative, file2.relative);
+          }
+        }))
         .pipe(iconfont({
           fontName: 'material-icons',
           formats: ['woff'],
           fontHeight: 2400,
           descent: 400,
           normalize: true,
+          timestamp: 1, // Truthy!
         }))
         .on('glyphs', function(glyphData, options) {
           // Store for later use
@@ -127,9 +155,9 @@ gulp.task('icons', function() {
         .pipe(gulp.dest('.'))
         .on('finish', function(args) {
           // Generate base64 version of the font
-          exec('base64 material-icons.woff', function(err, stdout, stderr) {
-            // Write the output to font-icons.html
-            var output = `<!-- NOTICE: Generated with 'gulp icons' -->
+          const materialIconsWoff = fs.readFileSync('material-icons.woff');
+          // Write the output to font-icons.html
+          let output = `<!-- NOTICE: Generated with 'gulp icons' -->
 <link rel="import" href="../polymer/lib/elements/custom-style.html">
 <link rel="import" href="version.html">
 
@@ -137,31 +165,36 @@ gulp.task('icons', function() {
   <style>
     @font-face {
       font-family: 'material-icons';
-      src: url(data:application/font-woff;charset=utf-8;base64,${stdout.trim()}) format('woff');
+      src: url(data:application/font-woff;charset=utf-8;base64,${materialIconsWoff.toString('base64')}) format('woff');
       font-weight: normal;
       font-style: normal;
     }
 
     html {
 `;
-            glyphs.forEach(g => {
-              var name = g.name.replace(/\s/g, '-').toLowerCase();
-              var unicode = '\\' + g.unicode[0].charCodeAt(0).toString(16);
-              output += `      --material-icons-${name}: "${unicode}";\n`;
-            });
-            output += `    }
+          glyphs.forEach(g => {
+            var name = g.name.replace(/\s/g, '-').toLowerCase();
+            var unicode = '\\' + g.unicode[0].charCodeAt(0).toString(16);
+            output += `      --material-icons-${name}: "${unicode}";\n`;
+          });
+          output += `    }
   </style>
 </custom-style>
 `;
-            fs.writeFile('font-icons.html', output, function(err) {
-              if (err) {
-                return console.error(err);
-              }
-            });
+          fs.writeFile('font-icons.html', output, function(err) {
+            if (err) {
+              return console.error(err);
+            }
+          });
 
-            // Cleanup
-            fs.unlink('material-icons.woff');
+          // Cleanup
+          fs.unlink('material-icons.woff', function(err) {
+            if (err) {
+              return console.error(err);
+            }
           });
         });
     });
 });
+
+gulp.task('lint', gulp.parallel('lint:js', 'lint:html', 'lint:css'));
