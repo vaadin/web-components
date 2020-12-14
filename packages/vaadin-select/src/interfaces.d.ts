269dd3e47a5ba74136d8cf4e95f8ca360b819a5b
web-padawan <iamkulykov@gmail.com>
2020-12-14 13:10:16 +0200

    chore: cleanup events typings [skip ci]

--- packages/vaadin-select/src/interfaces.d.ts
+++ packages/vaadin-select/src/interfaces.d.ts
@@ -13,17 +13,17 @@ export type SelectRenderer = (root: HTMLElement, select?: SelectElement) => void
 /**
  * Fired when the `opened` property changes.
  */
-export type SelectOpenedChanged = CustomEvent<{ value: boolean; path: 'opened' }>;
+export type SelectOpenedChanged = CustomEvent<{ value: boolean }>;
 
 /**
  * Fired when the `invalid` property changes.
  */
-export type SelectInvalidChanged = CustomEvent<{ value: boolean; path: 'invalid' }>;
+export type SelectInvalidChanged = CustomEvent<{ value: boolean }>;
 
 /**
  * Fired when the `value` property changes.
  */
-export type SelectValueChanged = CustomEvent<{ value: string; path: 'value' }>;
+export type SelectValueChanged = CustomEvent<{ value: string }>;
 
 export interface SelectElementEventMap {
   'opened-changed': SelectOpenedChanged;
