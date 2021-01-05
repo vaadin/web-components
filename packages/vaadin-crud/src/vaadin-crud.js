a742ef2ea27ed177f810367fe5434bc9a92edbc7
web-padawan <iamkulykov@gmail.com>
2021-01-05 09:23:54 +0200

    2.0.0-alpha2

--- packages/vaadin-crud/src/vaadin-crud.js
+++ packages/vaadin-crud/src/vaadin-crud.js
@@ -252,7 +252,7 @@ class CrudElement extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '2.0.0-alpha1';
+    return '2.0.0-alpha2';
   }
 
   static get properties() {
