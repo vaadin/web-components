d06fbe38cfc3205309bfb161cfa7fe044cf3b292
Serhii Kulykov <iamkulykov@gmail.com>
2021-01-08 09:35:43 +0200

    chore: align JSDoc, add change event [skip ci] vaadin/vaadin-checkbox#200

--- packages/vaadin-checkbox/src/vaadin-checkbox-group.js
+++ packages/vaadin-checkbox/src/vaadin-checkbox-group.js
@@ -45,8 +45,8 @@ import { CheckboxElement } from './vaadin-checkbox.js';
  *
  * See [ThemableMixin – how to apply styles for shadow parts](https://github.com/vaadin/vaadin-themable-mixin/wiki)
  *
- * @fires {CustomEvent<boolean>} invalid-changed
- * @fires {CustomEvent<Array<string>>} value-changed
+ * @fires {CustomEvent} invalid-changed - Fired when the `invalid` property changes.
+ * @fires {CustomEvent} value-changed - Fired when the `value` property changes.
  *
  * @extends HTMLElement
  * @mixes ThemableMixin
