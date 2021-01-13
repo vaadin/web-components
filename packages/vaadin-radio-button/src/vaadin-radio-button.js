466d5cbe888ee171187d0263949139e0db6c684b
web-padawan <iamkulykov@gmail.com>
2021-01-13 11:08:42 +0200

    2.0.0-alpha2

--- packages/vaadin-radio-button/src/vaadin-radio-button.js
+++ packages/vaadin-radio-button/src/vaadin-radio-button.js
@@ -108,7 +108,7 @@ class RadioButtonElement extends ElementMixin(ControlStateMixin(ThemableMixin(Ge
   }
 
   static get version() {
-    return '1.5.1';
+    return '2.0.0-alpha2';
   }
 
   static get properties() {
