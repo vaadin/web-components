562169d6cebf2bcdadb506bd43f19a84f86895c8
web-padawan <iamkulykov@gmail.com>
2021-01-28 09:19:24 +0200

    3.0.0-alpha1

--- packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
+++ packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
@@ -130,7 +130,7 @@ class ConfirmDialogElement extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '2.0.0';
+    return '3.0.0-alpha1';
   }
 
   static get properties() {
