083c9fef4f14fb8a729dd9b46676d3c7385abc87
Manolo Carrasco <manolo@vaadin.com>
2021-03-02 09:11:29 +0100

    chore: rewrite links in commit messages pointing to original repos

--- scripts/cloneRepos.sh
+++ scripts/cloneRepos.sh
@@ -7,6 +7,9 @@
 #   ./scripts/update.sh
 #
 
+# skip renaming links in commig messages #nnn -> vaadin-xxx#nnn
+[ "$1" = "--skipLinks" ] && skipLinks=true
+
 set -e
 list='repos.txt'
 repos=`grep 'vaadin' $list`
@@ -16,6 +19,21 @@ rm -rf migrate
 mkdir migrate
 cd migrate
 
+renameLinks() {
+  repo=$1
+  echo "Filtering $repo"
+  FILTER_BRANCH_SQUELCH_WARNING=1 git filter-branch -f \
+     --prune-empty \
+     --msg-filter '
+        vrepo=vaadin/'$repo'
+        msg=`cat -`
+        head=`echo "$msg" | head -1 | perl -pe "s, *\((#\d+)\) *$,\n$vrepo\\$1,g" | perl -pe "s,(.* +)(#\d+)(.*),\\$1\\$2\\$3\n$vrepo\\$1,"`
+        body=`echo "$msg" | tail +2 | perl -pe "s,(fixes|fix):? *(#\d+),Fixes: $vrepo\\$2,ig" | perl -pe "s,[ \():](#\d+),$vrepo\\$1,g"`
+        echo "$head\n$body\n" | perl -p0e "s,\n\n\n+,\n\n,g"
+        true
+     '
+}
+
 checkoutProject() {
   repo=$1
   echo cloning $repo
@@ -23,6 +41,8 @@ checkoutProject() {
   cd $repo
   # See https://github.com/newren/git-filter-repo
   git filter-repo --to-subdirectory-filter "packages/$repo"
+
+  [ -z "$skipLinks" ] && renameLinks $repo
   cd ../
 }
 
