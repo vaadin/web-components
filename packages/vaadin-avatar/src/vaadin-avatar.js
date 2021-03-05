0a6d22125c4b72b3001375a8f1ab1747f96cc29c
web-padawan <iamkulykov@gmail.com>
2021-03-05 11:17:15 +0200

    2.0.1

--- packages/vaadin-avatar/src/vaadin-avatar.js
+++ packages/vaadin-avatar/src/vaadin-avatar.js
@@ -147,7 +147,7 @@ class AvatarElement extends ElementMixin(ThemableMixin(PolymerElement)) {
   }
 
   static get version() {
-    return '2.0.0';
+    return '2.0.1';
   }
 
   static get properties() {
