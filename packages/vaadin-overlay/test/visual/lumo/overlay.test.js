b0ec03958715c99f2520231418892c83dd4051fa
web-padawan <iamkulykov@gmail.com>
2021-03-18 09:24:44 +0200

    test: overlay visual tests

---
+++ packages/vaadin-overlay/test/visual/lumo/overlay.test.js
@@ -0,0 +1,34 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/lumo/vaadin-overlay.js';
+
+describe('overlay', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.height = '100%';
+
+    element = fixtureSync(
+      `
+        <vaadin-overlay>
+          <template>
+            <div>Simple overlay with text</div>
+          </template>
+        </vaadin-overlay>
+      `,
+      div
+    );
+  });
+
+  it('basic', async () => {
+    element.opened = true;
+    await visualDiff(div, 'overlay:basic');
+  });
+
+  it('with-backdrop', async () => {
+    element.withBackdrop = true;
+    element.opened = true;
+    await visualDiff(div, 'overlay:with-backdrop');
+  });
+});
