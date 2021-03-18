c031067d3dabfdc2130c44a6ea3120997abd429e
web-padawan <iamkulykov@gmail.com>
2021-03-18 09:25:08 +0200

    test: progress-bar visual tests

---
+++ packages/vaadin-progress-bar/test/visual/lumo/progress-bar.test.js
@@ -0,0 +1,38 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/lumo/vaadin-progress-bar.js';
+
+describe('progress-bar', () => {
+  let div, element;
+
+  beforeEach(async () => {
+    div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.style.width = '200px';
+    element = fixtureSync('<vaadin-progress-bar value="0.1"></vaadin-progress-bar>', div);
+  });
+
+  it('basic', async () => {
+    await visualDiff(div, 'progress-bar:basic');
+  });
+
+  it('RTL', async () => {
+    element.setAttribute('dir', 'rtl');
+    await visualDiff(div, 'progress-bar:rtl-basic');
+  });
+
+  it('contrast', async () => {
+    element.setAttribute('theme', 'contrast');
+    await visualDiff(div, 'progress-bar:theme-contrast');
+  });
+
+  it('success', async () => {
+    element.setAttribute('theme', 'success');
+    await visualDiff(div, 'progress-bar:theme-success');
+  });
+
+  it('error', async () => {
+    element.setAttribute('theme', 'error');
+    await visualDiff(div, 'progress-bar:theme-error');
+  });
+});
