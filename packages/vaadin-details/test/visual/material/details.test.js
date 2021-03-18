51e6158b9ce8624bb415de4bdb8898ace7ce1728
web-padawan <iamkulykov@gmail.com>
2021-03-18 09:16:49 +0200

    test: details visual tests

---
+++ packages/vaadin-details/test/visual/material/details.test.js
@@ -0,0 +1,68 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/material/vaadin-details.js';
+
+describe('details', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.padding = '10px';
+
+    element = fixtureSync(
+      `
+      <vaadin-details>
+        <div slot="summary">Summary</div>
+        <span>Content</span>
+      </vaadin-details>
+      `,
+      div
+    );
+  });
+
+  describe('default', () => {
+    it('basic', async () => {
+      await visualDiff(div, 'details:basic');
+    });
+
+    it('opened', async () => {
+      element.opened = true;
+      await visualDiff(div, 'details:opened');
+    });
+
+    it('focus-ring', async () => {
+      element.setAttribute('focus-ring', '');
+      await visualDiff(div, 'details:focus-ring');
+    });
+
+    it('disabled', async () => {
+      element.disabled = true;
+      await visualDiff(div, 'details:disabled');
+    });
+
+    it('disabled opened', async () => {
+      element.opened = true;
+      element.disabled = true;
+      await visualDiff(div, 'details:disabled-opened');
+    });
+  });
+
+  describe('RTL', () => {
+    before(() => {
+      document.documentElement.setAttribute('dir', 'rtl');
+    });
+
+    after(() => {
+      document.documentElement.removeAttribute('dir');
+    });
+
+    it('RTL basic', async () => {
+      await visualDiff(div, 'details:rtl-basic');
+    });
+
+    it('RTL opened', async () => {
+      element.opened = true;
+      await visualDiff(div, 'details:rtl-opened');
+    });
+  });
+});
