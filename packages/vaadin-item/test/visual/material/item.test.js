6b8887772c6d7a1c74dd75dee87ab02be0ccbc55
web-padawan <iamkulykov@gmail.com>
2021-03-18 09:18:23 +0200

    test: item visual tests

---
+++ packages/vaadin-item/test/visual/material/item.test.js
@@ -0,0 +1,48 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/material/vaadin-item.js';
+
+describe('item', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.style.setProperty('--_material-item-selected-icon-display', 'block');
+    element = fixtureSync('<vaadin-item>Basic item</vaadin-item>', div);
+  });
+
+  ['ltr', 'rtl'].forEach((dir) => {
+    describe(dir, () => {
+      before(() => {
+        document.documentElement.setAttribute('dir', dir);
+      });
+
+      after(() => {
+        document.documentElement.removeAttribute('dir');
+      });
+
+      it('basic', async () => {
+        await visualDiff(div, `item:${dir}-basic`);
+      });
+
+      it('focusable', async () => {
+        element.setAttribute('tabindex', '0');
+        await visualDiff(div, `item:${dir}-focusable`);
+      });
+
+      it('selected', async () => {
+        element.setAttribute('tabindex', '0');
+        element.setAttribute('selected', '');
+        await visualDiff(div, `item:${dir}-selected`);
+      });
+
+      it('multi line', async () => {
+        const second = document.createElement('div');
+        second.textContent = 'Second line';
+        element.appendChild(second);
+        await visualDiff(div, `item:${dir}-multi-line`);
+      });
+    });
+  });
+});
