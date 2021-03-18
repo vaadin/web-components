8864e675c0e1f629dfc98576095d3586d8008330
web-padawan <iamkulykov@gmail.com>
2021-03-18 09:08:37 +0200

    test: charts visual tests

---
+++ packages/vaadin-charts/test/visual/lumo/chart.test.js
@@ -0,0 +1,15 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../vaadin-chart.js';
+
+describe('chart', () => {
+  let element;
+
+  beforeEach(() => {
+    element = fixtureSync('<vaadin-chart title="The chart title"></vaadin-chart>');
+  });
+
+  it('empty with title', async () => {
+    await visualDiff(element, 'chart:empty-title');
+  });
+});
