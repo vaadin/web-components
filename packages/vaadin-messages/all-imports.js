4229eb4f6c9363fe446000a2dbb34e5e23866981
Jens Jansson <peppe@vaadin.com>
2021-03-23 11:56:13 +0200

    chore: vaadin-messages: add all-imports.js as default import

---
+++ packages/vaadin-messages/all-imports.js
@@ -0,0 +1,6 @@
+import './vaadin-message.js';
+import './vaadin-message-list.js';
+import './vaadin-message-input.js';
+export * from './src/vaadin-message.js';
+export * from './src/vaadin-message-list.js';
+export * from './src/vaadin-message-input.js';
