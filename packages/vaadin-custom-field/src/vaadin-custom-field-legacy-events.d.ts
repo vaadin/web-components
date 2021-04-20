9664d83a359389f0839394d06880f12a81fd87a3
vursen <mr.vursen@gmail.com>
2021-04-20 14:55:38 +0300

    feat: add Event suffix to custom-field event typings

---
+++ packages/vaadin-custom-field/src/vaadin-custom-field-legacy-events.d.ts
@@ -0,0 +1,11 @@
+import { CustomFieldInvalidChangedEvent, CustomFieldValueChangedEvent } from './interfaces.js';
+
+/**
+ * @deprecated Please use `CustomFieldInvalidChangedEvent` instead.
+ */
+export type CustomFieldInvalidChanged = CustomFieldInvalidChangedEvent;
+
+/**
+ * @deprecated Please use `CustomFieldValueChangedEvent` instead.
+ */
+export type CustomFieldValueChanged = CustomFieldValueChangedEvent;
--- packages/vaadin-custom-field/vaadin-custom-field.d.ts
+++ packages/vaadin-custom-field/vaadin-custom-field.d.ts
@@ -1,2 +1,5 @@
 export * from './src/vaadin-custom-field.js';
 export * from './src/interfaces';
+
+// TODO: remove for Vaadin 21
+export * from './src/vaadin-custom-field-legacy-events.js';
