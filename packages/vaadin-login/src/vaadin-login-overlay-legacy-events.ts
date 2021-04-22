a7a16d46446b65737e90495662cee4e2390d0454
vursen <mr.vursen@gmail.com>
2021-04-22 08:32:44 +0300

    feat: add Event suffix to vaadin-login event typings

---
+++ packages/vaadin-login/src/vaadin-login-overlay-legacy-events.ts
@@ -0,0 +1,6 @@
+import { LoginEvent } from './interfaces.js';
+
+/**
+ * @deprecated Please use `LoginEvent` instead.
+ */
+export type LoginSubmit = LoginEvent;
--- packages/vaadin-login/test/typings/login-legacy-events.types.ts
+++ packages/vaadin-login/test/typings/login-legacy-events.types.ts
@@ -1,21 +1,17 @@
 import '../../vaadin-login-form.js';
 
-import { LoginEvent } from '../../vaadin-login-overlay.js';
+import { LoginSubmit } from '../../vaadin-login-overlay.js';
 
 const assertType = <TExpected>(actual: TExpected) => actual;
 
 const overlay = document.createElement('vaadin-login-overlay');
 
 overlay.addEventListener('login', (event) => {
-  assertType<LoginEvent>(event);
-  assertType<string>(event.detail.username);
-  assertType<string>(event.detail.password);
+  assertType<LoginSubmit>(event);
 });
 
 const form = document.createElement('vaadin-login-form');
 
 form.addEventListener('login', (event) => {
-  assertType<LoginEvent>(event);
-  assertType<string>(event.detail.username);
-  assertType<string>(event.detail.password);
+  assertType<LoginSubmit>(event);
 });
