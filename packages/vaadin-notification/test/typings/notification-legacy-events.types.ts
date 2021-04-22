f3e77f09648901c36bef9cdbe4a0b368846366db
vursen <mr.vursen@gmail.com>
2021-04-22 08:59:18 +0300

    feat: add Event suffix to vaadin-notification event typings

--- packages/vaadin-notification/test/typings/notification-legacy-events.types.ts
+++ packages/vaadin-notification/test/typings/notification-legacy-events.types.ts
@@ -1,12 +1,12 @@
 import '../../vaadin-notification.js';
 
-import { NotificationOpenedChangedEvent } from '../../vaadin-notification.js';
+import { NotificationOpenedChanged } from '../../vaadin-notification.js';
 
 const assertType = <TExpected>(value: TExpected) => value;
 
 const notification = document.createElement('vaadin-notification');
 
 notification.addEventListener('opened-changed', (event) => {
-  assertType<NotificationOpenedChangedEvent>(event);
+  assertType<NotificationOpenedChanged>(event);
   assertType<boolean>(event.detail.value);
 });
--- packages/vaadin-notification/vaadin-notification.d.ts
+++ packages/vaadin-notification/vaadin-notification.d.ts
@@ -1,2 +1,5 @@
 export * from './src/vaadin-notification.js';
 export * from './src/interfaces';
+
+// TODO: remove from Vaadin 21
+export * from './src/vaadin-notification-legacy-events.js';
