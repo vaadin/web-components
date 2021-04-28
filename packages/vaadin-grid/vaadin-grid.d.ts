b616a81b5677910eb32191d714388e67eaf06d7d
web-padawan <iamkulykov@gmail.com>
2021-04-28 13:02:28 +0300

    fix: use d.ts and not .ts for legacy typings

--- packages/vaadin-grid/vaadin-grid.d.ts
+++ packages/vaadin-grid/vaadin-grid.d.ts
@@ -2,4 +2,4 @@ export * from './src/vaadin-grid.js';
 export * from './src/interfaces';
 
 // TODO: remove from Vaadin 21
-export * from './src/vaadin-grid-legacy-events.js';
+export * from './src/vaadin-grid-legacy-events';
