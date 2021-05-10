4ac0615405f71811cb2287883d30ea2a78eb3739
web-padawan <iamkulykov@gmail.com>
2021-05-10 13:42:46 +0300

    fix: use correct string type literal

--- packages/vaadin-themable-mixin/register-styles.d.ts
+++ packages/vaadin-themable-mixin/register-styles.d.ts
@@ -6,6 +6,6 @@ export { css, unsafeCSS } from 'lit';
  * Registers CSS styles for a component type. Make sure to register the styles before
  * the first instance of a component of the type is attached to DOM.
  */
-declare function registerStyles(themeFor: String | null, styles: CSSResultGroup, options?: object | null): void;
+declare function registerStyles(themeFor: string | null, styles: CSSResultGroup, options?: object | null): void;
 
 export { registerStyles };
