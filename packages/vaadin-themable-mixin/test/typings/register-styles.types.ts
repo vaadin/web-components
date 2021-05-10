1db3ee20c127aa84e4aabd05217d467f854e0e5e
Serhii Kulykov <iamkulykov@gmail.com>
2021-05-10 14:09:23 +0300

    fix: make registerStyles work with Lit 2 (#360)

---
+++ packages/vaadin-themable-mixin/test/typings/register-styles.types.ts
@@ -0,0 +1,11 @@
+import { registerStyles, css } from '../../register-styles.js';
+
+registerStyles(
+  'vaadin-grid',
+  css`
+    [part~='cell'].dark {
+      color: var(--lumo-contrast-color);
+      background-color: var(--lumo-base-color);
+    }
+  `
+);
