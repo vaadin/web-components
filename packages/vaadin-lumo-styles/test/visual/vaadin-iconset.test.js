d395ee620478adae7cc4809605a2d32890c1728f
Serhii Kulykov <iamkulykov@gmail.com>
2021-05-27 10:13:04 +0300

    test: add Lumo and Material visual tests (#1984)

---
+++ packages/vaadin-lumo-styles/test/visual/vaadin-iconset.test.js
@@ -0,0 +1,25 @@
+import { fixtureSync, nextFrame } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/vaadin-icon/vaadin-icon.js';
+import '../../vaadin-iconset.js';
+
+describe('vaadin-iconset', () => {
+  let wrapper;
+
+  before(async () => {
+    const { origin, pathname } = new URL(import.meta.url);
+    const file = pathname.replace(/visual\/.+/, 'glyphs.json');
+    const data = await fetch(`${origin}${file}`);
+    const icons = await data.json();
+    wrapper = fixtureSync(`
+      <div style="display: flex; flex-wrap: wrap; width: 600px">
+        ${icons.map((icon) => `<vaadin-icon icon="lumo:${icon}"></vaadin-icon>`).join('\n')}
+      </div>
+    `);
+    await nextFrame();
+  });
+
+  it('basic', async () => {
+    await visualDiff(wrapper, `${import.meta.url}_vaadin-iconset`);
+  });
+});
