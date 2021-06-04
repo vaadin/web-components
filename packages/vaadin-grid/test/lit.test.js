84ac8291211966a9c5de1f8536636d4800c30475
Tomi Virkki <tomivirkki@users.noreply.github.com>
2021-06-04 09:52:21 +0300

    fix: don't hide rows in the grid logic (#2082)

---
+++ packages/vaadin-grid/test/lit.test.js
@@ -0,0 +1,39 @@
+import { expect } from '@esm-bundle/chai';
+import { aTimeout, fixtureSync } from '@vaadin/testing-helpers';
+import { render, html } from 'lit';
+import '../vaadin-grid.js';
+import { getPhysicalItems } from './helpers.js';
+
+describe('rendering with lit', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = fixtureSync(`<div></div>`);
+  });
+
+  it('should render items after dynamically adding more columns', async () => {
+    function renderGrid(columnPaths, items) {
+      render(
+        html`
+          <vaadin-grid .items=${items}>
+            ${columnPaths.map((columnPath) => {
+              return html`<vaadin-grid-column path=${columnPath}></vaadin-grid-column>`;
+            })}
+          </vaadin-grid>
+        `,
+        wrapper
+      );
+    }
+
+    // First render with just one column and 0 items
+    renderGrid(['first'], []);
+
+    await aTimeout(0);
+    // Then render with more than one column and more than 0 items
+    renderGrid(['first', 'second'], [{ first: 'foo', second: 'bar' }]);
+
+    await aTimeout(0);
+    const grid = wrapper.firstElementChild;
+    expect(getPhysicalItems(grid).length).to.equal(1);
+  });
+});
