95aa34a46e9b0384165e163ef1e69aee082461e9
Sergey Vinogradov <mr.vursen@gmail.com>
2021-06-07 16:01:30 +0300

    fix: address issues that came up after rebase

--- packages/vaadin-crud/src/vaadin-crud-edit-column.js
+++ packages/vaadin-crud/src/vaadin-crud-edit-column.js
@@ -54,7 +54,7 @@ class CrudEditColumnElement extends GridColumnElement {
   }
 
   static get observers() {
-    return ['_onBodyTemplateOrRendererOrBindingChanged(_bodyTemplate, _renderer, _cells, _cells.*, path, ariaLabel)'];
+    return ['_onRendererOrBindingChanged(_renderer, _cells, _cells.*, path, ariaLabel)'];
   }
 
   /**
