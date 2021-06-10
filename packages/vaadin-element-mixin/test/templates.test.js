90b13558b7e53b94c53e97b525d88fc487213aaf
Sergey Vinogradov <mr.vursen@gmail.com>
2021-06-10 16:57:35 +0300

    feat: add template helpers to element mixin

---
+++ packages/vaadin-element-mixin/test/templates.test.js
@@ -0,0 +1,69 @@
+import sinon from 'sinon';
+import { fixtureSync } from '@vaadin/testing-helpers';
+import { expect } from '@esm-bundle/chai';
+import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+import { processTemplates } from '../templates.js';
+
+class ComponentElement extends PolymerElement {
+  static get is() {
+    return 'component-element';
+  }
+}
+
+customElements.define(ComponentElement.is, ComponentElement);
+
+describe('process templates', () => {
+  beforeEach(() => {
+    sinon.stub(console, 'warn');
+  });
+
+  afterEach(() => {
+    console.warn.restore();
+  });
+
+  describe('with template renderer', () => {
+    beforeEach(() => {
+      window.Vaadin = window.Vaadin || {};
+      window.Vaadin.templateRendererCallback = sinon.spy();
+    });
+
+    afterEach(() => {
+      window.Vaadin.templateRendererCallback = undefined;
+    });
+
+    it('should invoke the template renderer callback', () => {
+      const component = fixtureSync(`
+        <component-element>
+          <template></template>
+        </component-element>
+      `);
+
+      processTemplates(component);
+
+      expect(window.Vaadin.templateRendererCallback.calledOnce).to.be.true;
+      expect(window.Vaadin.templateRendererCallback.args[0][0]).to.equal(component);
+    });
+
+    it('should not show the deprecation warning', () => {
+      expect(console.warn.called).to.be.false;
+    });
+  });
+
+  describe('without template renderer', () => {
+    it('should show the deprecation warning', () => {
+      const component = fixtureSync(`
+        <component-element>
+          <template></template>
+        </component-element>
+      `);
+
+      processTemplates(component);
+
+      expect(console.warn.calledOnce).to.be.true;
+      expect(console.warn.args[0][0]).to.equal(
+        `WARNING: <template> inside <component-element> is no longer supported. Import @vaadin/vaadin-template-renderer to enable compatibility (see https://vaad.in/template-renderer)`
+      );
+    });
+  });
+});
