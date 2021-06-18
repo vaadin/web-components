05f0528736ac7f6656e6eec5a6b113488e0166f4
Tomi Virkki <virkki@vaadin.com>
2021-06-18 12:41:58 +0300

    feat: allow passing specific item type to virtual list types

--- packages/vaadin-virtual-list/test/typings/virtual-list.types.ts
+++ packages/vaadin-virtual-list/test/typings/virtual-list.types.ts
@@ -1,28 +1,36 @@
 import '../../vaadin-virtual-list.js';
 
-import {
-  VirtualListElement,
-  VirtualListItem,
-  VirtualListItemModel,
-  VirtualListRenderer
-} from '../../vaadin-virtual-list.js';
+import { VirtualListElement, VirtualListItemModel, VirtualListRenderer } from '../../vaadin-virtual-list.js';
 
 const assertType = <TExpected>(actual: TExpected) => actual;
 
-const virtualList = document.createElement('vaadin-virtual-list');
+const genericVirtualList = document.createElement('vaadin-virtual-list');
 
-assertType<VirtualListElement>(virtualList);
+assertType<VirtualListElement>(genericVirtualList);
 
-assertType<VirtualListItem>(1);
-virtualList.items = [1, 2, 3];
+genericVirtualList.items = [1, 2, 3];
+
+interface TestVirtualListItem {
+  testProperty: string;
+}
+
+const virtualList = genericVirtualList as VirtualListElement<TestVirtualListItem>;
+
+assertType<VirtualListElement<TestVirtualListItem>>(virtualList);
+
+virtualList.items = [{ testProperty: '1' }, { testProperty: '2' }, { testProperty: '3' }];
+
+assertType<Array<TestVirtualListItem>>(virtualList.items);
 
 virtualList.renderer = (root, virtualList, model) => {
   assertType<HTMLElement>(root);
   assertType<VirtualListElement>(virtualList);
-  assertType<VirtualListItemModel>(model);
+  assertType<VirtualListItemModel<TestVirtualListItem>>(model);
+  assertType<number>(model.index);
+  assertType<TestVirtualListItem>(model.item);
 };
 
-assertType<VirtualListRenderer>(virtualList.renderer);
+assertType<VirtualListRenderer<TestVirtualListItem>>(virtualList.renderer);
 
 assertType<void>(virtualList.scrollToIndex(3));
 
