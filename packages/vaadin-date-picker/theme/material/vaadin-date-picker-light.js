a3c9aa8226dc49d18150f970fcac92dbab2a104b
Bram Kragten <mail@bramkragten.nl>
2021-07-08 14:23:29 +0200

    fix: correct material theme imports of `vaadin-date-picker-light` (#2221)

--- packages/vaadin-date-picker/theme/material/vaadin-date-picker-light.js
+++ packages/vaadin-date-picker/theme/material/vaadin-date-picker-light.js
@@ -1,4 +1,4 @@
-import './vaadin-date-picker-overlay.js';
-import './vaadin-date-picker-overlay-content.js';
-import './vaadin-month-calendar.js';
+import './vaadin-date-picker-overlay-styles.js';
+import './vaadin-date-picker-overlay-content-styles.js';
+import './vaadin-month-calendar-styles.js';
 import '../../src/vaadin-date-picker-light.js';
