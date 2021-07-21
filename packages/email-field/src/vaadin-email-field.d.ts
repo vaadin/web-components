d2ab12f872b90913d5a04c6d534688d32f0a59a6
web-padawan <iamkulykov@gmail.com>
2021-07-21 16:22:12 +0300

    feat: add EmailField base class

---
+++ packages/email-field/src/vaadin-email-field.d.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { TextField } from '@vaadin/text-field/src/vaadin-text-field.js';
+
+declare class EmailField extends TextField {}
+
+export { EmailField };
---
+++ packages/email-field/vaadin-email-field.js
@@ -0,0 +1,1 @@
+export { EmailField } from './src/vaadin-email-field.js';
