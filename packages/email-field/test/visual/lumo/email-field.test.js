f98c18833a4544ff1618b2d088a45564b33e0be3
web-padawan <iamkulykov@gmail.com>
2021-07-26 17:31:03 +0300

    feat: add Lumo and Material for email-field

---
+++ packages/email-field/test/visual/lumo/email-field.test.js
@@ -0,0 +1,47 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/lumo/vaadin-email-field.js';
+import { EmailField } from '../../../src/vaadin-email-field.js';
+
+customElements.define('vaadin-email-field', EmailField);
+
+describe('email-field', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.style.padding = '10px';
+    element = fixtureSync('<vaadin-email-field></vaadin-email-field>', div);
+  });
+
+  it('placeholder', async () => {
+    element.placeholder = 'Placeholder';
+    await visualDiff(div, `${import.meta.url}_placeholder`);
+  });
+
+  it('value', async () => {
+    element.value = 'serguey.kulikov@gmail.com';
+    await visualDiff(div, `${import.meta.url}_value`);
+  });
+
+  describe('RTL', () => {
+    before(() => {
+      document.documentElement.setAttribute('dir', 'rtl');
+    });
+
+    after(() => {
+      document.documentElement.removeAttribute('dir');
+    });
+
+    it('RTL value', async () => {
+      element.value = 'serguey.kulikov@gmail.com';
+      await visualDiff(div, `${import.meta.url}_rtl-value`);
+    });
+
+    it('RTL placeholder', async () => {
+      element.placeholder = 'Placeholder';
+      await visualDiff(div, `${import.meta.url}_rtl-placeholder`);
+    });
+  });
+});
--- packages/email-field/vaadin-email-field.js
+++ packages/email-field/vaadin-email-field.js
@@ -1,1 +1,3 @@
+import './theme/lumo/vaadin-email-field.js';
+
 export { EmailField } from './src/vaadin-email-field.js';
