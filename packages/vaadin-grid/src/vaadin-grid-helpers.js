7f2408b174c0517b1044b69c28f69b86d37cbb60
Hadi Amiri <84700249+hdamr@users.noreply.github.com>
2021-08-05 14:54:52 +0300

    fix: set columns order correctly with more than 10 columns (#2297)

---
+++ packages/vaadin-grid/src/vaadin-grid-helpers.js
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
+ * @param {Array<Object>} columns array of columns to be modified
+ * @param {number} scope multiplier added to base order for each column
+ * @param {number} baseOrder base number used for order
+ */
+export function updateColumnOrders(columns, scope, baseOrder) {
+  let c = 1;
+  columns.forEach((column) => {
+    // avoid multiples of 10 because they introduce and extra zero and
+    // causes the underlying calculations for child order goes wrong
+    if (c % 10 === 0) {
+      c++;
+    }
+    column._order = baseOrder + c * scope;
+    c++;
+  });
+}
