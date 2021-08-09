a62fae83be4d585b127925915abfcf4c9f5ff801
Jouni Koivuviita <jouni@vaadin.com>
2021-08-09 15:20:56 +0300

    feat: Indicate editable and read-only cells visually
    Add a default hover style for editable cells, and opt-in theme variants for indicating editable or read-only cells.

--- packages/grid-pro/theme/lumo/vaadin-grid-pro-styles.js
+++ packages/grid-pro/theme/lumo/vaadin-grid-pro-styles.js
@@ -14,6 +14,36 @@ registerStyles(
       pointer-events: none;
       box-shadow: inset 0 0 0 2px var(--lumo-primary-color-50pct);
     }
+
+    [part~='editable-cell']:hover,
+    [part~='editable-cell']:focus {
+      background-color: var(--lumo-contrast-5pct);
+      background-clip: padding-box;
+    }
+
+    /* Indicate editable cells */
+
+    :host([theme~='highlight-editable-cells']) [part~='editable-cell'] {
+      background-color: var(--lumo-contrast-5pct);
+      background-clip: border-box;
+    }
+
+    :host([theme~='highlight-editable-cells']) [part~='editable-cell']:hover,
+    :host([theme~='highlight-editable-cells']) [part~='editable-cell']:focus {
+      background-color: var(--lumo-contrast-10pct);
+    }
+
+    /* Indicate read-only cells */
+
+    :host([theme~='highlight-read-only-cells']) [part~='body-cell']:not([part~='editable-cell']) {
+      background-image: repeating-linear-gradient(
+        135deg,
+        transparent,
+        transparent 6px,
+        var(--lumo-contrast-5pct) 6px,
+        var(--lumo-contrast-5pct) 14px
+      );
+    }
   `,
   { moduleId: 'lumo-grid-pro' }
 );
