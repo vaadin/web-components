e604abaed37a2c37df6d6dae5760c2c60b689169
web-padawan <iamkulykov@gmail.com>
2021-08-12 09:48:00 +0300

    chore: add TODOs to temporary files

--- packages/text-field/test/visual/lumo/vaadin-text-field.js
+++ packages/text-field/test/visual/lumo/vaadin-text-field.js
@@ -1,3 +1,4 @@
+// TODO: remove this file in https://github.com/vaadin/web-components/issues/2220
 import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '../../../theme/lumo/vaadin-text-field.js';
 import '../../../theme/lumo/vaadin-input-field-shared-styles.js';
--- packages/text-field/test/visual/material/vaadin-text-field.js
+++ packages/text-field/test/visual/material/vaadin-text-field.js
@@ -1,3 +1,4 @@
+// TODO: remove this file in https://github.com/vaadin/web-components/issues/2220
 import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import '../../../theme/material/vaadin-text-field.js';
 import '../../../theme/material/vaadin-input-field-shared-styles.js';
