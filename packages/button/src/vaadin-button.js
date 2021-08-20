e343c4b40cbcddb675c6332c5943b31d01e81742
Sergey Vinogradov <mr.vursen@gmail.com>
2021-08-20 18:19:13 +0300

    fix: set focus-ring attribute on new button

--- packages/button/src/vaadin-button.js
+++ packages/button/src/vaadin-button.js
@@ -7,10 +7,9 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin/vaadin-element-mixin.js';
 import { ActiveMixin } from '@vaadin/field-base/src/active-mixin.js';
-import { DisabledMixin } from '@vaadin/field-base/src/disabled-mixin.js';
-import { DelegateFocusMixin } from '@vaadin/field-base/src/delegate-focus-mixin.js';
+import { ControlStateMixin } from '@vaadin/vaadin-control-state-mixin/vaadin-control-state-mixin.js';
 
-class Button extends ActiveMixin(DelegateFocusMixin(DisabledMixin(ElementMixin(ThemableMixin(PolymerElement))))) {
+class Button extends ControlStateMixin(ActiveMixin(ElementMixin(ThemableMixin(PolymerElement)))) {
   static get is() {
     return 'vaadin-button';
   }
@@ -81,7 +80,7 @@ class Button extends ActiveMixin(DelegateFocusMixin(DisabledMixin(ElementMixin(T
   }
 
   /**
-   * A getter that returns the native button as a focusable element for DelegateFocusMixin.
+   * A getter that returns the native button as a focusable element for ControlStateMixin.
    *
    * @protected
    * @override
