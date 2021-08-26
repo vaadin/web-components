b6d7d62d54f3afd1539347592645b8dca91087f1
web-padawan <iamkulykov@gmail.com>
2021-08-26 22:43:00 +0300

    test: cleanup item unit tests

--- packages/vaadin-item/test/item.test.js
+++ packages/vaadin-item/test/item.test.js
@@ -21,23 +21,4 @@ describe('vaadin-item', () => {
   it('should extend item-mixin', () => {
     expect(item._hasVaadinItemMixin).to.be.true;
   });
-
-  it('should have an unnamed slot for label', () => {
-    const slot = item.shadowRoot.querySelector('slot:not([name])');
-    const content = slot.assignedNodes()[0];
-    expect(content.nodeType).to.be.equal(3);
-    expect(content.textContent.trim()).to.be.equal('label');
-  });
-
-  it('should have a content part wrapping slot', () => {
-    const slot = item.shadowRoot.querySelector('slot');
-    const content = item.shadowRoot.querySelector('[part="content"]');
-    expect(content).to.be.instanceof(Element);
-    expect(slot.parentElement).to.equal(content);
-  });
-
-  it('should have a block context for content part', () => {
-    const content = item.shadowRoot.querySelector('[part="content"]');
-    expect(getComputedStyle(content).display).to.equal('block');
-  });
 });
