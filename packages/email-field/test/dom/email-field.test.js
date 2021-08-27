41715edd3c67378deea56a33c1c19cba8795ece2
web-padawan <iamkulykov@gmail.com>
2021-08-27 12:40:12 +0300

    test: add email field snapshot tests

---
+++ packages/email-field/test/dom/email-field.test.js
@@ -0,0 +1,46 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync } from '@vaadin/testing-helpers';
+import '../../src/vaadin-email-field.js';
+
+describe('vaadin-email-field', () => {
+  let field;
+
+  // Ignore generated attributes to prevent failures
+  // when running snapshot tests in a different order
+  // Also ignore pattern because of escape characters
+  const SNAPSHOT_CONFIG = {
+    ignoreAttributes: ['id', 'aria-describedby', 'aria-labelledby', 'for', 'pattern']
+  };
+
+  beforeEach(() => {
+    field = fixtureSync('<vaadin-email-field></vaadin-email-field>');
+  });
+
+  it('default', async () => {
+    await expect(field).shadowDom.to.equalSnapshot();
+  });
+
+  it('disabled', async () => {
+    field.disabled = true;
+    await expect(field).shadowDom.to.equalSnapshot();
+  });
+
+  it('readonly', async () => {
+    field.readonly = true;
+    await expect(field).shadowDom.to.equalSnapshot();
+  });
+
+  it('invalid', async () => {
+    field.invalid = true;
+    await expect(field).shadowDom.to.equalSnapshot();
+  });
+
+  it('theme', async () => {
+    field.setAttribute('theme', 'align-right');
+    await expect(field).shadowDom.to.equalSnapshot();
+  });
+
+  it('slots', async () => {
+    await expect(field).lightDom.to.equalSnapshot(SNAPSHOT_CONFIG);
+  });
+});
