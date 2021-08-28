391d2c5c5f07ef3fe3befd0920c7526e78d007bc
web-padawan <iamkulykov@gmail.com>
2021-08-28 21:29:36 +0300

    test: add messages snapshot tests

---
+++ packages/vaadin-messages/test/dom/__snapshots__/message-input.test.snap.js
@@ -0,0 +1,67 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-message-input default"] = 
+`<vaadin-message-input-text-area>
+  <textarea
+    aria-label="Message"
+    placeholder="Message"
+    rows="1"
+    slot="textarea"
+  >
+  </textarea>
+  <label slot="label">
+  </label>
+  <div
+    aria-live="assertive"
+    slot="error-message"
+  >
+  </div>
+  <div slot="helper">
+  </div>
+</vaadin-message-input-text-area>
+<vaadin-message-input-button
+  role="button"
+  tabindex="0"
+  theme="primary contained"
+>
+  Send
+</vaadin-message-input-button>
+`;
+/* end snapshot vaadin-message-input default */
+
+snapshots["vaadin-message-input disabled"] = 
+`<vaadin-message-input-text-area
+  aria-disabled="true"
+  disabled=""
+>
+  <textarea
+    aria-label="Message"
+    disabled=""
+    placeholder="Message"
+    rows="1"
+    slot="textarea"
+  >
+  </textarea>
+  <label slot="label">
+  </label>
+  <div
+    aria-live="assertive"
+    slot="error-message"
+  >
+  </div>
+  <div slot="helper">
+  </div>
+</vaadin-message-input-text-area>
+<vaadin-message-input-button
+  aria-disabled="true"
+  disabled=""
+  role="button"
+  tabindex="-1"
+  theme="primary contained"
+>
+  Send
+</vaadin-message-input-button>
+`;
+/* end snapshot vaadin-message-input disabled */
+
