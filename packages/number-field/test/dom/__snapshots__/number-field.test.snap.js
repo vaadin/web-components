54b7579594f3f1b0544e232146dc7b7eb4f38426
web-padawan <iamkulykov@gmail.com>
2021-08-28 15:51:32 +0300

    test: add number field snapshot tests

---
+++ packages/number-field/test/dom/__snapshots__/number-field.test.snap.js
@@ -0,0 +1,363 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-number-field default"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container part="input-field">
+    <div
+      hidden=""
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      hidden=""
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field default */
+
+snapshots["vaadin-number-field controls"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container part="input-field">
+    <div
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field controls */
+
+snapshots["vaadin-number-field disabled"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    disabled=""
+    part="input-field"
+  >
+    <div
+      hidden=""
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      hidden=""
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field disabled */
+
+snapshots["vaadin-number-field readonly"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    readonly=""
+  >
+    <div
+      hidden=""
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      hidden=""
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field readonly */
+
+snapshots["vaadin-number-field invalid"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    invalid=""
+    part="input-field"
+  >
+    <div
+      hidden=""
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      hidden=""
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field invalid */
+
+snapshots["vaadin-number-field theme"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    theme="align-right"
+  >
+    <div
+      hidden=""
+      part="decrease-button"
+      slot="prefix"
+    >
+    </div>
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      hidden=""
+      part="increase-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-number-field theme */
+
+snapshots["vaadin-number-field slots"] = 
+`<input
+  max="undefined"
+  min="undefined"
+  slot="input"
+  step="any"
+  type="number"
+>
+<label slot="label">
+</label>
+<div
+  aria-live="assertive"
+  slot="error-message"
+>
+</div>
+<div slot="helper">
+</div>
+`;
+/* end snapshot vaadin-number-field slots */
+
