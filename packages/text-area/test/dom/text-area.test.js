87fe72bb9d80bc79bb85ff4e58dc1460dab8ffb0
web-padawan <iamkulykov@gmail.com>
2021-08-28 16:16:20 +0300

    test: add text-area snapshot tests

---
+++ packages/text-area/test/dom/text-area.test.js
@@ -0,0 +1,45 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync } from '@vaadin/testing-helpers';
+import '../../src/vaadin-text-area.js';
+
+describe('vaadin-text-area', () => {
+  let textArea;
+
+  // Ignore generated attributes to prevent failures
+  // when running snapshot tests in a different order
+  const SNAPSHOT_CONFIG = {
+    ignoreAttributes: ['id', 'aria-describedby', 'aria-labelledby', 'for']
+  };
+
+  beforeEach(() => {
+    textArea = fixtureSync('<vaadin-text-area></vaadin-text-area>');
+  });
+
+  it('default', async () => {
+    await expect(textArea).shadowDom.to.equalSnapshot();
+  });
+
+  it('disabled', async () => {
+    textArea.disabled = true;
+    await expect(textArea).shadowDom.to.equalSnapshot();
+  });
+
+  it('readonly', async () => {
+    textArea.readonly = true;
+    await expect(textArea).shadowDom.to.equalSnapshot();
+  });
+
+  it('invalid', async () => {
+    textArea.invalid = true;
+    await expect(textArea).shadowDom.to.equalSnapshot();
+  });
+
+  it('theme', async () => {
+    textArea.setAttribute('theme', 'align-right');
+    await expect(textArea).shadowDom.to.equalSnapshot();
+  });
+
+  it('slots', async () => {
+    await expect(textArea).lightDom.to.equalSnapshot(SNAPSHOT_CONFIG);
+  });
+});
