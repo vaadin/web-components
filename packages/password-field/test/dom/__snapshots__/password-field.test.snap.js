beca552b567fd512809d672feac289883fdec56d
web-padawan <iamkulykov@gmail.com>
2021-08-28 16:00:45 +0300

    test: add password-field snapshot tests

---
+++ packages/password-field/test/dom/__snapshots__/password-field.test.snap.js
@@ -0,0 +1,291 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-password-field default"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container part="input-field">
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="reveal-button"
+      slot="suffix"
+    >
+      <slot name="reveal">
+      </slot>
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-password-field default */
+
+snapshots["vaadin-password-field disabled"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    disabled=""
+    part="input-field"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="reveal-button"
+      slot="suffix"
+    >
+      <slot name="reveal">
+      </slot>
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-password-field disabled */
+
+snapshots["vaadin-password-field readonly"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    readonly=""
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="reveal-button"
+      slot="suffix"
+    >
+      <slot name="reveal">
+      </slot>
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-password-field readonly */
+
+snapshots["vaadin-password-field invalid"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    invalid=""
+    part="input-field"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="reveal-button"
+      slot="suffix"
+    >
+      <slot name="reveal">
+      </slot>
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-password-field invalid */
+
+snapshots["vaadin-password-field theme"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    theme="align-right"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <slot name="input">
+    </slot>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+    <div
+      part="reveal-button"
+      slot="suffix"
+    >
+      <slot name="reveal">
+      </slot>
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-password-field theme */
+
+snapshots["vaadin-password-field slots"] = 
+`<input
+  autocapitalize="off"
+  slot="input"
+  type="password"
+>
+<div
+  aria-live="assertive"
+  slot="error-message"
+>
+</div>
+<label slot="label">
+</label>
+<div slot="helper">
+</div>
+<button
+  aria-label="Show password"
+  aria-pressed="false"
+  slot="reveal"
+  tabindex="0"
+  type="button"
+>
+</button>
+`;
+/* end snapshot vaadin-password-field slots */
+
