d80cd8146a28db6385391b379b2295b8a7cc9518
web-padawan <iamkulykov@gmail.com>
2021-08-28 16:16:20 +0300

    test: add text-area snapshot tests

---
+++ packages/text-area/test/dom/__snapshots__/text-area.test.snap.js
@@ -0,0 +1,270 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-text-area default"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container part="input-field">
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <div
+      class="textarea-wrapper"
+      data-replicated-value=""
+    >
+      <slot name="textarea">
+      </slot>
+    </div>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-text-area default */
+
+snapshots["vaadin-text-area disabled"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    disabled=""
+    part="input-field"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <div
+      class="textarea-wrapper"
+      data-replicated-value=""
+    >
+      <slot name="textarea">
+      </slot>
+    </div>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-text-area disabled */
+
+snapshots["vaadin-text-area readonly"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    readonly=""
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <div
+      class="textarea-wrapper"
+      data-replicated-value=""
+    >
+      <slot name="textarea">
+      </slot>
+    </div>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-text-area readonly */
+
+snapshots["vaadin-text-area invalid"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    invalid=""
+    part="input-field"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <div
+      class="textarea-wrapper"
+      data-replicated-value=""
+    >
+      <slot name="textarea">
+      </slot>
+    </div>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-text-area invalid */
+
+snapshots["vaadin-text-area theme"] = 
+`<div part="container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="indicator"
+    >
+    </span>
+  </div>
+  <vaadin-input-container
+    part="input-field"
+    theme="align-right"
+  >
+    <slot
+      name="prefix"
+      slot="prefix"
+    >
+    </slot>
+    <div
+      class="textarea-wrapper"
+      data-replicated-value=""
+    >
+      <slot name="textarea">
+      </slot>
+    </div>
+    <slot
+      name="suffix"
+      slot="suffix"
+    >
+    </slot>
+    <div
+      id="clearButton"
+      part="clear-button"
+      slot="suffix"
+    >
+    </div>
+  </vaadin-input-container>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+`;
+/* end snapshot vaadin-text-area theme */
+
+snapshots["vaadin-text-area slots"] = 
+`<textarea slot="textarea">
+</textarea>
+<label slot="label">
+</label>
+<div
+  aria-live="assertive"
+  slot="error-message"
+>
+</div>
+<div slot="helper">
+</div>
+`;
+/* end snapshot vaadin-text-area slots */
+
