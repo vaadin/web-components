26a4903859705ee75aa280a5b29d4d52af23402d
Sergey Vinogradov <mr.vursen@gmail.com>
2021-08-29 13:21:50 +0300

    fix: update vaadin-confirm-dialog to use new button

--- packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
+++ packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
@@ -7,7 +7,7 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { ElementMixin } from '@vaadin/vaadin-element-mixin/vaadin-element-mixin.js';
 import '@vaadin/vaadin-license-checker/vaadin-license-checker.js';
-import '@vaadin/vaadin-button/src/vaadin-button.js';
+import '@vaadin/button/src/vaadin-button.js';
 import '@vaadin/vaadin-dialog/src/vaadin-dialog.js';
 
 /**
