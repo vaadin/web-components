b360fc43d9dfdc553183b677f8df379e475e1c28
Sergey Vinogradov <mr.vursen@gmail.com>
2021-08-29 13:26:09 +0300

    fix: update vaadin-login to use new button

--- packages/vaadin-login/theme/lumo/vaadin-login-overlay.js
+++ packages/vaadin-login/theme/lumo/vaadin-login-overlay.js
@@ -1,6 +1,6 @@
 import '@vaadin/vaadin-text-field/theme/lumo/vaadin-text-field.js';
 import '@vaadin/vaadin-text-field/theme/lumo/vaadin-password-field.js';
-import '@vaadin/vaadin-button/theme/lumo/vaadin-button.js';
+import '@vaadin/button/theme/lumo/vaadin-button.js';
 import '@vaadin/vaadin-overlay/theme/lumo/vaadin-overlay.js';
 import './vaadin-login-form.js';
 import './vaadin-login-overlay-styles.js';
