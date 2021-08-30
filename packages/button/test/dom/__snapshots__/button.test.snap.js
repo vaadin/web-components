c90c0d628dd11113305e5d865c02fea4de5ffdcc
web-padawan <iamkulykov@gmail.com>
2021-08-30 18:50:33 +0300

    test: add button snapshot tests

---
+++ packages/button/test/dom/__snapshots__/button.test.snap.js
@@ -0,0 +1,21 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-button default"] = 
+`<div class="vaadin-button-container">
+  <span part="prefix">
+    <slot name="prefix">
+    </slot>
+  </span>
+  <span part="label">
+    <slot>
+    </slot>
+  </span>
+  <span part="suffix">
+    <slot name="suffix">
+    </slot>
+  </span>
+</div>
+`;
+/* end snapshot vaadin-button default */
+
