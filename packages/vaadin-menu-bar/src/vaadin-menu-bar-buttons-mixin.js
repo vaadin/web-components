0faeb86f09946b5dc6090c555f6a9f8a3f796b36
web-padawan <iamkulykov@gmail.com>
2021-08-31 14:19:50 +0300

    chore: cleanup no longer needed logic and test

--- packages/vaadin-menu-bar/src/vaadin-menu-bar-buttons-mixin.js
+++ packages/vaadin-menu-bar/src/vaadin-menu-bar-buttons-mixin.js
@@ -153,13 +153,6 @@ export const ButtonsMixin = (superClass) =>
     /** @private */
     __detectOverflow() {
       const overflow = this._overflow;
-
-      // Overflow button is not yet initialized during first observer run.
-      // Call the method again in `connectedCallback` to handle this case.
-      if (!overflow) {
-        return;
-      }
-
       const buttons = this._buttons.filter((btn) => btn !== overflow);
       const oldOverflowCount = this.__getOverflowCount(overflow);
 
