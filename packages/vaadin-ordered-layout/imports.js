6ce151b7b703a81dd470ceaff2217b95397383c6
David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
2021-09-09 14:01:42 +0300

    refactor!: re-create vaadin-ordered-layout for backwards compatibility (#2491)

---
+++ packages/vaadin-ordered-layout/imports.js
@@ -0,0 +1,6 @@
+import './vaadin-horizontal-layout.js';
+import './vaadin-vertical-layout.js';
+import './vaadin-scroller.js';
+export * from './src/vaadin-horizontal-layout.js';
+export * from './src/vaadin-vertical-layout.js';
+export * from './src/vaadin-scroller.js';
---
+++ packages/vaadin-ordered-layout/vaadin-vertical-layout.js
@@ -0,0 +1,2 @@
+import './theme/lumo/vaadin-vertical-layout.js';
+export * from './src/vaadin-vertical-layout.js';
