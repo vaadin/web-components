697419b97c96e35513e0ba4b01db50c44f1d925c
web-padawan <iamkulykov@gmail.com>
2021-09-10 11:04:48 +0300

    test: add avatar snapshot tests

---
+++ packages/avatar-group/test/dom/avatar-group.test.js
@@ -0,0 +1,28 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync, nextFrame } from '@vaadin/testing-helpers';
+import '../../src/vaadin-avatar-group.js';
+
+describe('vaadin-avatar-group', () => {
+  let group;
+
+  beforeEach(() => {
+    group = fixtureSync('<vaadin-avatar-group></vaadin-avatar-group>');
+  });
+
+  it('default', async () => {
+    await expect(group).shadowDom.to.equalSnapshot();
+  });
+
+  it('items', async () => {
+    group.items = [{ abbr: 'YY' }, { name: 'Tomi Virkki' }];
+    await nextFrame();
+    await expect(group).shadowDom.to.equalSnapshot();
+  });
+
+  it('theme', async () => {
+    group.items = [{ abbr: 'YY' }, { name: 'Tomi Virkki' }];
+    await nextFrame();
+    group.setAttribute('theme', 'small');
+    await expect(group).shadowDom.to.equalSnapshot();
+  });
+});
---
+++ packages/avatar/test/dom/__snapshots__/avatar.test.snap.js
@@ -0,0 +1,19 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-avatar default"] = 
+`<img
+  aria-hidden="true"
+  hidden=""
+>
+`;
+/* end snapshot vaadin-avatar default */
+
+snapshots["vaadin-avatar img"] = 
+`<img
+  aria-hidden="true"
+  src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
+>
+`;
+/* end snapshot vaadin-avatar img */
+
