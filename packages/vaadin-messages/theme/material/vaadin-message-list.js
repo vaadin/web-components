59be46217c27ba312ef2070cf58b1f5a1fc89fad
David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
2021-09-10 13:39:43 +0300

    refactor: re-create vaadin-messages for backwards compatibility (#2500)

---
+++ packages/vaadin-messages/theme/material/vaadin-message-list.js
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import '@vaadin/message-list/theme/material/vaadin-message-list.js';
