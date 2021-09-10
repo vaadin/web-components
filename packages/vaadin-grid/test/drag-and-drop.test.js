11daf0137b1d26900fb3f1322a653eb7eac4bc2b
web-padawan <iamkulykov@gmail.com>
2021-09-10 14:25:53 +0300

    chore: fill array properly

--- packages/vaadin-grid/test/drag-and-drop.test.js
+++ packages/vaadin-grid/test/drag-and-drop.test.js
@@ -753,7 +753,7 @@ describe('drag and drop', () => {
   describe('auto scroll', () => {
     beforeEach(async () => {
       grid.dropMode = 'between';
-      grid.items = new Array(100).fill({});
+      grid.items = new Array(100).fill().map((_, idx) => ({ value: idx }));
 
       grid.$.table.scrollTop = 0;
       flushGrid(grid);
