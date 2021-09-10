47ee2e7cf2ead5ba571e03ba37034907f88a92c0
web-padawan <iamkulykov@gmail.com>
2021-09-10 12:57:54 +0300

    test: cleanup messages unit tests

--- packages/message-list/test/message.test.js
+++ packages/message-list/test/message.test.js
@@ -3,69 +3,18 @@ import { fixtureSync } from '@vaadin/testing-helpers';
 import '../vaadin-message.js';
 
 describe('message', () => {
-  let message, avatar, content, time, name;
+  let message, tagName;
 
   beforeEach(() => {
-    let root = document.documentElement;
-    root.style.setProperty('--vaadin-user-color-2', 'blue');
-
     message = fixtureSync('<vaadin-message>Hello</vaadin-message>');
-    avatar = message.shadowRoot.querySelector('vaadin-message-avatar');
-    name = message.shadowRoot.querySelector('[part="name"]');
-    content = message.shadowRoot.querySelector('[part="content"]');
-    time = message.shadowRoot.querySelector('[part="time"]');
+    tagName = message.tagName.toLowerCase();
   });
 
-  it('avatar should be initially visible but without data', () => {
-    expect(avatar.getAttribute('name')).to.be.null;
-    expect(avatar.getAttribute('abbr')).to.be.null;
-    expect(avatar.getAttribute('img')).to.be.null;
-    expect(avatar.getAttribute('title')).to.be.equal('anonymous');
-    expect(avatar.getAttribute('has-color-index')).to.be.null;
+  it('should be defined in custom element registry', () => {
+    expect(customElements.get(tagName)).to.be.ok;
   });
 
-  it('name should be initially empty', () => {
-    expect(name.textContent.trim()).to.be.equal('');
-  });
-
-  it('time should be initially empty', () => {
-    expect(time.textContent.trim()).to.be.equal('');
-  });
-
-  it('avatar should be set with provided user name', () => {
-    message.userName = 'Joan Doe';
-    expect(avatar.name).to.be.equal('Joan Doe');
-  });
-
-  it('avatar should be set with provided user abbreviation', () => {
-    message.userAbbr = 'JD';
-    expect(avatar.abbr).to.be.equal('JD');
-  });
-
-  it('avatar should be set with provided user image', () => {
-    message.userImg = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
-    expect(avatar.img).to.be.equal('data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=');
-  });
-
-  it('avatar should be set with provided user color', () => {
-    message.userColorIndex = 2;
-    expect(avatar.getAttribute('has-color-index')).to.be.not.null;
-    expect(avatar.colorIndex).to.be.equal(2);
-  });
-
-  it('name should be set with provided user', () => {
-    message.userName = 'Joan Doe';
-    expect(name.textContent).to.be.equal('Joan Doe');
-  });
-
-  it('time should be set', () => {
-    message.time = 'long ago';
-    expect(time.textContent).to.be.equal('long ago');
-  });
-
-  it('content should be set', () => {
-    const slot = content.querySelector('slot');
-    const nodes = slot.assignedNodes({ flatten: true });
-    expect(nodes[0].textContent).to.be.equal('Hello');
+  it('should have a valid static "is" getter', () => {
+    expect(customElements.get(tagName).is).to.equal(tagName);
   });
 });
