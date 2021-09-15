ec440541860c3154efed260892d315ce562010dd
David Sosa <sosa@vaadin.com>
2021-09-15 11:34:53 +0300

    fix: change exports to imports on theme files

--- packages/vaadin-accordion/theme/lumo/vaadin-accordion-panel.js
+++ packages/vaadin-accordion/theme/lumo/vaadin-accordion-panel.js
@@ -3,4 +3,4 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-export * from '@vaadin/accordion/theme/lumo/vaadin-accordion-panel.js';
+import '@vaadin/accordion/theme/lumo/vaadin-accordion-panel.js';
--- packages/vaadin-accordion/theme/material/vaadin-accordion-panel.js
+++ packages/vaadin-accordion/theme/material/vaadin-accordion-panel.js
@@ -3,4 +3,4 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-export * from '@vaadin/accordion/theme/material/vaadin-accordion-panel.js';
+import '@vaadin/accordion/theme/material/vaadin-accordion-panel.js';
