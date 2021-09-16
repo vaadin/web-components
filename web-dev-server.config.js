905bc906345d07bdc6643fc6d6bc36bbfe33dfdd
Tomi Virkki <tomivirkki@users.noreply.github.com>
2021-09-16 11:58:27 +0300

    chore: open dev page listing by default (#2545)

---
+++ web-dev-server.config.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+const fs = require('fs');
+
+module.exports = {
+  plugins: [
+    {
+      name: 'dev-page-listing',
+      transform(context) {
+        if (context.path === '/dev/index.html') {
+          const listing = `
+            <ul id="listing">
+              ${fs
+                .readdirSync('./dev')
+                .map((file) => `<li><a href="/dev/${file}">${file}</a></li>`)
+                .join('')}
+            </ul>`;
+
+          return { body: context.body.replace(/<ul id="listing">.*<\/ul>/, listing) };
+        }
+      }
+    }
+  ]
+};
