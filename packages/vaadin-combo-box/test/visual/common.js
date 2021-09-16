16855708c8767ff2c5a9c6413ff8f0df6415118b
web-padawan <iamkulykov@gmail.com>
2021-09-16 15:41:47 +0300

    test: hide caret for opened combo-box

--- packages/vaadin-combo-box/test/visual/common.js
+++ packages/vaadin-combo-box/test/visual/common.js
@@ -4,7 +4,8 @@ import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styl
 registerStyles(
   'vaadin-combo-box',
   css`
-    :host([focus-ring]) ::slotted(input) {
+    :host([focus-ring]) ::slotted(input),
+    :host([opened]) ::slotted(input) {
       font-size: 0 !important;
     }
   `
