1b5b11416878ab4604d244f3bcf6cf8ab41e8256
Tomi Virkki <virkki@vaadin.com>
2021-09-16 10:36:12 +0300

    chore: open dev page listing by default

---
+++ web-dev-server.config.mjs
@@ -0,0 +1,22 @@
+import fs from 'fs';
+
+export default {
+  plugins: [
+    {
+      name: 'dev-page-listing',
+      transform(context) {
+        if (context.path === '/dev/index.html') {
+          const listing = `
+            <ul id="listing">
+              ${fs
+                .readdirSync('./dev')
+                .map((file) => `<li><a href="/dev/${file}">${file}</a></li>`)
+                .join('')}
+            </ul>`;
+
+          return { body: context.body.replace(/<ul id="listing">.*<\/ul>/, listing) };
+        }
+      }
+    }
+  ]
+};
