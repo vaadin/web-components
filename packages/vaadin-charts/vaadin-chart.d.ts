d54968ad90b48e76d3435e16770e30427ac02a83
David Sosa <sosa@vaadin.com>
2021-09-17 10:23:57 +0300

    refactor: re-create vaadin-charts for backwards compatibility

---
+++ packages/vaadin-charts/vaadin-chart.d.ts
@@ -0,0 +1,2 @@
+export * from './src/vaadin-chart.js';
+export * from './src/interfaces';
---
+++ packages/vaadin-charts/vaadin-chart.js
@@ -0,0 +1,3 @@
+import './vaadin-chart-default-theme.js';
+import './src/vaadin-chart.js';
+export * from './src/vaadin-chart.js';
