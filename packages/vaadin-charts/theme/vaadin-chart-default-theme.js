a7452a4b75c83338e4f10c23950306641b563e0b
David Sosa <sosa@vaadin.com>
2021-09-17 10:27:59 +0300

    fix: remove license comment

--- packages/vaadin-charts/theme/vaadin-chart-default-theme.js
+++ packages/vaadin-charts/theme/vaadin-chart-default-theme.js
@@ -3,12 +3,4 @@
  * Copyright (c) 2015 - 2021 Vaadin Ltd
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
-
-/**
- * @license Highcharts
- *
- * (c) 2009-2016 Torstein Honsi
- *
- * License: www.highcharts.com/license
- */
 import '@vaadin/charts/theme/vaadin-chart-default-theme.js';
