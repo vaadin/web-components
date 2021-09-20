bd6c3466675d0d214e83e65b9e13f8b965647a1f
web-padawan <iamkulykov@gmail.com>
2021-09-20 10:32:24 +0300

    fix: add missing RTL styles and visual tests

---
+++ packages/vaadin-date-picker/src/vaadin-date-picker-styles.js
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
+
+export const datePickerStyles = css`
+  :host([dir='rtl']) [part='input-field'] {
+    direction: ltr;
+  }
+
+  :host([dir='rtl']) [part='value']::placeholder {
+    direction: rtl;
+    text-align: left;
+  }
+
+  :host([dir='rtl']) [part='input-field'] ::slotted(input)::placeholder {
+    direction: rtl;
+    text-align: left;
+  }
+`;
