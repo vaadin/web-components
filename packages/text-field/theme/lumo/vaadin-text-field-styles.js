8a824ff063f0365e6851224a084e4742ffd057f6
web-padawan <iamkulykov@gmail.com>
2021-09-24 09:12:08 +0300

    refactor!: move text-field to new base class

--- packages/text-field/theme/lumo/vaadin-text-field-styles.js
+++ packages/text-field/theme/lumo/vaadin-text-field-styles.js
@@ -3,12 +3,9 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-
-/* TODO: uncomment in https://github.com/vaadin/web-components/issues/2220
-import { registerStyles, css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
+import { registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import { inputFieldShared } from '@vaadin/vaadin-lumo-styles/mixins/input-field-shared.js';
 
 registerStyles('vaadin-text-field', inputFieldShared, {
   moduleId: 'lumo-text-field-styles'
 });
-*/
