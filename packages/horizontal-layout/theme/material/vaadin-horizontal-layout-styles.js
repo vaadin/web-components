f5f898f2fcf432eed6f632ee8527b8fc47caf60d
Jouni Koivuviita <jouni@vaadin.com>
2021-09-28 18:38:39 +0300

    refactor: use CSS gap property for ordered layout spacing

--- packages/horizontal-layout/theme/material/vaadin-horizontal-layout-styles.js
+++ packages/horizontal-layout/theme/material/vaadin-horizontal-layout-styles.js
@@ -9,95 +9,24 @@ const horizontalLayout = css`
     padding: 16px;
   }
 
-  :host([theme~='spacing-xs']:not([dir='rtl'])) ::slotted(*) {
-    margin-left: 4px;
+  :host([theme~='spacing-xs']) {
+    gap: 4px;
   }
 
-  :host([theme~='spacing-s']:not([dir='rtl'])) ::slotted(*) {
-    margin-left: 8px;
+  :host([theme~='spacing-s']) {
+    gap: 8px;
   }
 
-  :host([theme~='spacing']:not([dir='rtl'])) ::slotted(*) {
-    margin-left: 16px;
+  :host([theme~='spacing']) {
+    gap: 16px;
   }
 
-  :host([theme~='spacing-l']:not([dir='rtl'])) ::slotted(*) {
-    margin-left: 24px;
+  :host([theme~='spacing-l']) {
+    gap: 24px;
   }
 
-  :host([theme~='spacing-xl']:not([dir='rtl'])) ::slotted(*) {
-    margin-left: 40px;
-  }
-
-  /* Compensate for the first item margin, so that there is no gap around the layout itself. */
-  :host([theme~='spacing-xs'])::before,
-  :host([theme~='spacing-s'])::before,
-  :host([theme~='spacing'])::before,
-  :host([theme~='spacing-l'])::before,
-  :host([theme~='spacing-xl'])::before {
-    content: '';
-  }
-
-  :host([theme~='spacing-xs']:not([dir='rtl']))::before {
-    margin-left: -4px;
-  }
-
-  :host([theme~='spacing-s']:not([dir='rtl']))::before {
-    margin-left: -8px;
-  }
-
-  :host([theme~='spacing']:not([dir='rtl']))::before {
-    margin-left: -16px;
-  }
-
-  :host([theme~='spacing-l']:not([dir='rtl']))::before {
-    margin-left: -24px;
-  }
-
-  :host([theme~='spacing-xl']:not([dir='rtl']))::before {
-    margin-left: -40px;
-  }
-
-  /* RTL styles */
-  :host([dir='rtl'][theme~='spacing-xs']) ::slotted(*) {
-    margin-right: 4px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-s']) ::slotted(*) {
-    margin-right: 8px;
-  }
-
-  :host([dir='rtl'][theme~='spacing']) ::slotted(*) {
-    margin-right: 16px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-l']) ::slotted(*) {
-    margin-right: 24px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-xl']) ::slotted(*) {
-    margin-right: 40px;
-  }
-
-  /* Compensate for the first item margin, so that there is no gap around the layout itself. */
-  :host([dir='rtl'][theme~='spacing-xs'])::before {
-    margin-right: -4px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-s'])::before {
-    margin-right: -8px;
-  }
-
-  :host([dir='rtl'][theme~='spacing'])::before {
-    margin-right: -16px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-l'])::before {
-    margin-right: -24px;
-  }
-
-  :host([dir='rtl'][theme~='spacing-xl'])::before {
-    margin-right: -40px;
+  :host([theme~='spacing-xl']) {
+    gap: 40px;
   }
 `;
 
--- packages/vertical-layout/theme/material/vaadin-vertical-layout-styles.js
+++ packages/vertical-layout/theme/material/vaadin-vertical-layout-styles.js
@@ -9,50 +9,24 @@ const verticalLayout = css`
     padding: 16px;
   }
 
-  :host([theme~='spacing-xs']) ::slotted(*) {
-    margin-top: 4px;
+  :host([theme~='spacing-xs']) {
+    gap: 4px;
   }
 
-  :host([theme~='spacing-s']) ::slotted(*) {
-    margin-top: 8px;
+  :host([theme~='spacing-s']) {
+    gap: 8px;
   }
 
-  :host([theme~='spacing']) ::slotted(*) {
-    margin-top: 16px;
+  :host([theme~='spacing']) {
+    gap: 16px;
   }
 
-  :host([theme~='spacing-l']) ::slotted(*) {
-    margin-top: 32px;
+  :host([theme~='spacing-l']) {
+    gap: 24px;
   }
 
-  :host([theme~='spacing-xl']) ::slotted(*) {
-    margin-top: 64px;
-  }
-
-  /* Compensate for the first item margin, so that there is no gap around the layout itself. */
-  :host([theme~='spacing-xs'])::before {
-    content: '';
-    margin-top: -4px;
-  }
-
-  :host([theme~='spacing-s'])::before {
-    content: '';
-    margin-top: -8px;
-  }
-
-  :host([theme~='spacing'])::before {
-    content: '';
-    margin-top: -16px;
-  }
-
-  :host([theme~='spacing-l'])::before {
-    content: '';
-    margin-top: -32px;
-  }
-
-  :host([theme~='spacing-xl'])::before {
-    content: '';
-    margin-top: -64px;
+  :host([theme~='spacing-xl']) {
+    gap: 40px;
   }
 `;
 
