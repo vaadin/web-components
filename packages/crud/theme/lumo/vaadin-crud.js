6c66dde3169ad5f1530dc617923b59cf621cf9e8
web-padawan <iamkulykov@gmail.com>
2021-09-30 15:08:09 +0300

    chore: sort imports alphabetically

--- packages/crud/theme/lumo/vaadin-crud.js
+++ packages/crud/theme/lumo/vaadin-crud.js
@@ -3,7 +3,7 @@ import '@vaadin/confirm-dialog/theme/lumo/vaadin-confirm-dialog.js';
 import '@vaadin/dialog/theme/lumo/vaadin-dialog.js';
 import '@vaadin/form-layout/theme/lumo/vaadin-form-layout.js';
 import '@vaadin/grid/theme/lumo/vaadin-grid.js';
-import '@vaadin/text-field/theme/lumo/vaadin-text-field.js';
 import '@vaadin/grid/theme/lumo/vaadin-grid-sorter.js';
+import '@vaadin/text-field/theme/lumo/vaadin-text-field.js';
 import './vaadin-crud-styles.js';
 import '../../src/vaadin-crud.js';
--- packages/crud/theme/material/vaadin-crud.js
+++ packages/crud/theme/material/vaadin-crud.js
@@ -3,7 +3,7 @@ import '@vaadin/confirm-dialog/theme/material/vaadin-confirm-dialog.js';
 import '@vaadin/dialog/theme/material/vaadin-dialog.js';
 import '@vaadin/form-layout/theme/material/vaadin-form-layout.js';
 import '@vaadin/grid/theme/material/vaadin-grid.js';
-import '@vaadin/text-field/theme/material/vaadin-text-field.js';
 import '@vaadin/grid/theme/material/vaadin-grid-sorter.js';
+import '@vaadin/text-field/theme/material/vaadin-text-field.js';
 import './vaadin-crud-styles.js';
 import '../../src/vaadin-crud.js';
