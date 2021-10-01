df08ff5bcf7b68a7de8fcb0006d2a11424974310
David Sosa <76832183+sosa-vaadin@users.noreply.github.com>
2021-10-01 10:08:07 +0300

    refactor: re-create vaadin-icon for backwards compatibility (#2700)

---
+++ packages/vaadin-icon/src/vaadin-icon-svg.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+export * from '@vaadin/icon/src/vaadin-icon-svg.js';
