3dacee5c83cbf3dc78f39bd2efae3e3a64e34e34
Sergey Vinogradov <mr.vursen@gmail.com>
2021-10-06 16:21:01 +0300

    feat: deprecate CheckboxGroupElement

--- packages/vaadin-checkbox/src/vaadin-checkbox-group.js
+++ packages/vaadin-checkbox/src/vaadin-checkbox-group.js
@@ -3,4 +3,11 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import { CheckboxGroup } from '@vaadin/checkbox-group/src/vaadin-checkbox-group.js';
+
+/**
+ * @deprecated Import `CheckboxGroup` from `@vaadin/checkbox-group` instead.
+ */
+export const CheckboxGroupElement = CheckboxGroup;
+
 export * from '@vaadin/checkbox-group/src/vaadin-checkbox-group.js';
