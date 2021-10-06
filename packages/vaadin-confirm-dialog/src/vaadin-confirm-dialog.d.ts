a74b6991f7f0207b71e0c0a0c164be7b8f0d1937
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-10-06 16:35:26 +0200

    rename confirm dialog element

--- packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.d.ts
+++ packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.d.ts
@@ -3,4 +3,16 @@
  * Copyright (c) 2018 - 2021 Vaadin Ltd
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
+import { ConfirmDialog } from '@vaadin/confirm-dialog/src/vaadin-confirm-dialog.js';
+
+/**
+ * @deprecated Import `ConfirmDialog` from `@vaadin/confirm-dialog` instead.
+ */
+export type ConfirmDialogElement = ConfirmDialog;
+
+/**
+ * @deprecated Import `ConfirmDialog` from `@vaadin/confirm-dialog` instead.
+ */
+export const ConfirmDialogElement: typeof ConfirmDialog;
+
 export * from '@vaadin/confirm-dialog/src/vaadin-confirm-dialog.js';
--- packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
+++ packages/vaadin-confirm-dialog/src/vaadin-confirm-dialog.js
@@ -3,4 +3,11 @@
  * Copyright (c) 2018 - 2021 Vaadin Ltd
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
+import { ConfirmDialog } from '@vaadin/confirm-dialog/src/vaadin-confirm-dialog.js';
+
+/**
+ * @deprecated Import `ConfirmDialog` from `@vaadin/confirm-dialog` instead.
+ */
+export const ConfirmDialogElement = ConfirmDialog;
+
 export * from '@vaadin/confirm-dialog/src/vaadin-confirm-dialog.js';
