1ea4cecfb75823b1c7d5b517d42ff754cac1a957
Serhii Kulykov <iamkulykov@gmail.com>
2021-10-06 14:47:47 +0300

    Apply suggestions from code review

--- packages/vaadin-combo-box/src/vaadin-combo-box-light.d.ts
+++ packages/vaadin-combo-box/src/vaadin-combo-box-light.d.ts
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { ComboBoxLight } from './vaadin-combo-box-light';
+import { ComboBoxLight } from '@vaadin/combo-box/src/vaadin-combo-box-light.js';
 
 /**
  * @deprecated Import `ComboBoxLight` from `@vaadin/combo-box` instead.
