258b19d1a8a99ba86916ce239326df180d020e80
David Sosa <sosa@vaadin.com>
2021-10-07 17:13:29 +0300

    refactor: re-create vaadin-rich-text-editor for backwards compatibility

---
+++ packages/vaadin-rich-text-editor/src/vaadin-rich-text-editor.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @license
+ * Copyright (c) 2020 Vaadin Ltd
+ * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
+ */
+export * from '@vaadin/rich-text-editor/src/vaadin-rich-text-editor.js';
---
+++ packages/vaadin-rich-text-editor/theme/lumo/vaadin-rich-text-editor.js
@@ -0,0 +1,1 @@
+import '@vaadin/rich-text-editor/theme/lumo/vaadin-rich-text-editor.js';
