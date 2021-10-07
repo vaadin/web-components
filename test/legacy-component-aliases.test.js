ba2b423a48cac1d93465119b7d0215892cac305d
David Sosa <sosa@vaadin.com>
2021-10-07 10:38:41 +0300

    refactor!: move vaadin-tabs to tabs

--- test/legacy-component-aliases.test.js
+++ test/legacy-component-aliases.test.js
@@ -67,8 +67,8 @@ import { RadioGroupElement } from '@vaadin/vaadin-radio-button/vaadin-radio-grou
 import { RichTextEditorElement } from '@vaadin/vaadin-rich-text-editor';
 import { SelectElement } from '@vaadin/vaadin-select';
 import { SplitLayoutElement } from '@vaadin/vaadin-split-layout';
-import { TabsElement } from '@vaadin/vaadin-tabs';
-import { TabElement } from '@vaadin/vaadin-tabs/vaadin-tab';
+// import { TabsElement } from '@vaadin/vaadin-tabs';
+// import { TabElement } from '@vaadin/vaadin-tabs/vaadin-tab';
 import { TextFieldElement } from '@vaadin/vaadin-text-field';
 import { EmailFieldElement } from '@vaadin/vaadin-text-field/vaadin-email-field';
 import { IntegerFieldElement } from '@vaadin/vaadin-text-field/vaadin-integer-field';
@@ -137,8 +137,8 @@ describe('legacy-exports', () => {
     expect(new RichTextEditorElement()).to.be.instanceof(HTMLElement);
     expect(new SelectElement()).to.be.instanceof(HTMLElement);
     expect(new SplitLayoutElement()).to.be.instanceof(HTMLElement);
-    expect(new TabsElement()).to.be.instanceof(HTMLElement);
-    expect(new TabElement()).to.be.instanceof(HTMLElement);
+    // expect(new TabsElement()).to.be.instanceof(HTMLElement);
+    // expect(new TabElement()).to.be.instanceof(HTMLElement);
     expect(new TextFieldElement()).to.be.instanceof(HTMLElement);
     expect(new EmailFieldElement()).to.be.instanceof(HTMLElement);
     expect(new IntegerFieldElement()).to.be.instanceof(HTMLElement);
