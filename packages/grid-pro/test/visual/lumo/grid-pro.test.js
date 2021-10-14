e51ff48728e9a26676a0e05f927487fc6bf63c19
web-padawan <iamkulykov@gmail.com>
2021-10-14 12:11:11 +0300

    test: add visual regression tests

--- packages/grid-pro/test/visual/lumo/grid-pro.test.js
+++ packages/grid-pro/test/visual/lumo/grid-pro.test.js
@@ -1,5 +1,6 @@
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import { sendKeys } from '@web/test-runner-commands';
 import { getContainerCell } from '../../helpers.js';
 import { users } from '../users.js';
 import '../../../theme/lumo/vaadin-grid-pro.js';
@@ -13,6 +14,70 @@ describe('grid-pro', () => {
     div = document.createElement('div');
     div.style.display = 'inline-block';
     div.style.width = '200px';
+  });
+
+  describe('editable-cell', () => {
+    beforeEach(async () => {
+      element = fixtureSync(
+        `
+          <vaadin-grid-pro style="height: 100px;">
+            <vaadin-grid-pro-edit-column path="name.first"></vaadin-grid-pro-edit-column>
+            <vaadin-grid-column path="name.last"></vaadin-grid-column>
+          </vaadin-grid-pro>
+        `,
+        div
+      );
+      element.items = users;
+      await nextRender(element);
+      div.style.width = '300px';
+      element.focus();
+    });
+
+    describe('default', () => {
+      beforeEach(async () => {
+        // Focus the header cell
+        await sendKeys({ press: 'Tab' });
+      });
+
+      it('editable-cell focus', async () => {
+        // Focus the editable cell
+        await sendKeys({ press: 'Tab' });
+
+        await visualDiff(div, 'editable-cell-focus');
+      });
+
+      it('read-only-cell focus', async () => {
+        // Focus the editable cell
+        await sendKeys({ press: 'Tab' });
+
+        // Focus the read-only cell
+        await sendKeys({ press: 'ArrowRight' });
+
+        await visualDiff(div, 'read-only-cell-focus');
+      });
+    });
+
+    ['highlight-editable-cells', 'highlight-read-only-cells'].forEach((theme) => {
+      describe(theme, () => {
+        beforeEach(() => {
+          element.setAttribute('theme', theme);
+        });
+
+        it('default', async () => {
+          await visualDiff(div, `theme-${theme}`);
+        });
+
+        it('focus', async () => {
+          // Focus the header cell
+          await sendKeys({ press: 'Tab' });
+
+          // Focus the editable cell
+          await sendKeys({ press: 'Tab' });
+
+          await visualDiff(div, `theme-${theme}-focus`);
+        });
+      });
+    });
   });
 
   describe('edit-column-text', () => {
@@ -29,7 +94,7 @@ describe('grid-pro', () => {
       await nextRender(element);
     });
 
-    it('select', async () => {
+    it('text', async () => {
       const cell = getContainerCell(element.$.items, 0, 0);
       cell._content.dispatchEvent(new CustomEvent('dblclick', { bubbles: true }));
       await visualDiff(div, 'edit-column-text');
