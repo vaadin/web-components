1551f998e35432ddc090ecb36c2e2a87d7a5297e
Sergey Vinogradov <mr.vursen@gmail.com>
2021-11-05 14:33:37 +0300

    refactor: sort import statements in visual tests

--- packages/context-menu/test/visual/material/context-menu.test.js
+++ packages/context-menu/test/visual/material/context-menu.test.js
@@ -1,9 +1,9 @@
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
 import '@vaadin/polymer-legacy-adapter/template-renderer.js';
-import { openSubMenus } from '../common.js';
 import '../../../theme/material/vaadin-context-menu.js';
 import '../../not-animated-styles.js';
+import { openSubMenus } from '../common.js';
 
 describe('context-menu', () => {
   let element;
--- packages/text-field/test/visual/material/text-field.test.js
+++ packages/text-field/test/visual/material/text-field.test.js
@@ -1,6 +1,6 @@
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
-import { visualDiff } from '@web/test-runner-visual-regression';
 import { sendKeys } from '@web/test-runner-commands';
+import { visualDiff } from '@web/test-runner-visual-regression';
 import '../common.js';
 import '../../../theme/material/vaadin-text-field.js';
 
