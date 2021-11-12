92bf03eabf2a9d03e41982c6dde69a48d43256f8
diegocardoso <diego@vaadin.com>
2021-11-12 15:15:57 +0200

    refactor: rename default theme on legacy package

--- packages/vaadin-charts/theme/vaadin-chart-base-theme.js
+++ packages/vaadin-charts/theme/vaadin-chart-base-theme.js
@@ -3,4 +3,4 @@
  * Copyright (c) 2015 - 2021 Vaadin Ltd
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
-import '@vaadin/charts/theme/vaadin-chart-default-theme.js';
+import '@vaadin/charts/theme/vaadin-chart-base-theme.js';
