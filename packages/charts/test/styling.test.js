5f00162e50d8128533fdf01bb8c027589a2211d8
diegocardoso <diego@vaadin.com>
2021-11-12 16:05:50 +0200

    fix: update imports

--- packages/charts/test/styling.test.js
+++ packages/charts/test/styling.test.js
@@ -2,10 +2,10 @@ import { expect } from '@esm-bundle/chai';
 import { fixtureSync, oneEvent } from '@vaadin/testing-helpers';
 import '../vaadin-chart.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import { chartDefaultTheme } from '../theme/vaadin-chart-default-theme.js';
+import { chartBaseTheme } from '../theme/vaadin-chart-base-theme.js';
 
 registerStyles('vaadin-chart', [
-  chartDefaultTheme,
+  chartBaseTheme,
   css`
     :host([theme='custom']) .highcharts-column-series rect.highcharts-point {
       stroke: rgb(255, 0, 0);
