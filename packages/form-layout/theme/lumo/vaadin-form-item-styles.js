68f002031eac85f6565937954eec9bf8bd121898
Tomi Virkki <virkki@vaadin.com>
2021-11-16 14:59:07 +0200

    test: add visual tests, style updates

--- packages/form-layout/theme/lumo/vaadin-form-item-styles.js
+++ packages/form-layout/theme/lumo/vaadin-form-item-styles.js
@@ -2,30 +2,45 @@ import '@vaadin/vaadin-lumo-styles/color.js';
 import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
 import '@vaadin/vaadin-lumo-styles/typography.js';
-import { requiredField } from '@vaadin/vaadin-lumo-styles/mixins/required-field.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-const formItem = css`
-  :host {
-    --vaadin-form-item-row-spacing: 0;
-  }
+registerStyles(
+  'vaadin-form-item',
+  css`
+    :host {
+      --vaadin-form-item-row-spacing: 0;
+    }
 
-  /* font-weight, margin-bottom, transition and line-height same as for part label in text-field */
-  [part='label'] {
-    color: var(--lumo-secondary-text-color);
-    font-family: var(--lumo-font-family);
-    font-size: var(--lumo-font-size-s);
-    font-weight: 500;
-    margin-top: var(--lumo-space-m);
-    margin-left: calc(var(--lumo-border-radius-m) / 4);
-    margin-bottom: var(--lumo-space-xs);
-    transition: color 0.4s;
-    line-height: 1.333;
-  }
+    /* font-weight, margin-bottom, transition and line-height same as for part label in text-field */
+    [part='label'] {
+      color: var(--lumo-secondary-text-color);
+      font-family: var(--lumo-font-family);
+      font-size: var(--lumo-font-size-s);
+      font-weight: 500;
+      margin-top: var(--lumo-space-m);
+      margin-left: calc(var(--lumo-border-radius-m) / 4);
+      margin-bottom: var(--lumo-space-xs);
+      transition: color 0.4s;
+      line-height: 1.333;
+    }
 
-  [part='required-indicator']::after {
-    position: relative;
-  }
-`;
+    [part='required-indicator']::after {
+      content: var(--lumo-required-field-indicator, '•');
+      transition: opacity 0.2s;
+      opacity: 0;
+      color: var(--lumo-required-field-indicator-color, var(--lumo-primary-text-color));
+      position: relative;
+      width: 1em;
+      text-align: center;
+    }
 
-registerStyles('vaadin-form-item', [requiredField, formItem], { moduleId: 'lumo-form-item' });
+    :host([required]) [part='required-indicator']::after {
+      opacity: 1;
+    }
+
+    :host([invalid]) [part='required-indicator']::after {
+      color: var(--lumo-required-field-indicator-color, var(--lumo-error-text-color));
+    }
+  `,
+  { moduleId: 'lumo-form-item' }
+);
--- packages/form-layout/theme/material/vaadin-form-item-styles.js
+++ packages/form-layout/theme/material/vaadin-form-item-styles.js
@@ -1,20 +1,28 @@
 import '@vaadin/vaadin-material-styles/color.js';
 import '@vaadin/vaadin-material-styles/typography.js';
-import { requiredField } from '@vaadin/vaadin-material-styles/mixins/required-field.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-const formItem = css`
-  [part='label'] {
-    position: relative;
-    transform: none;
-    font-family: var(--material-font-family);
-    font-size: var(--material-small-font-size);
-    color: var(--material-secondary-text-color);
-    line-height: 16px;
-    font-weight: 400;
-    margin-top: 16px;
-    margin-bottom: 8px;
-  }
-`;
+registerStyles(
+  'vaadin-form-item',
+  css`
+    [part='label'] {
+      font-family: var(--material-font-family);
+      font-size: var(--material-small-font-size);
+      color: var(--material-secondary-text-color);
+      line-height: 16px;
+      font-weight: 400;
+      margin-top: 16px;
+      margin-bottom: 8px;
+    }
 
-registerStyles('vaadin-form-item', [requiredField, formItem], { moduleId: 'material-form-item' });
+    :host([required]) [part='required-indicator']::after {
+      content: ' *';
+      color: inherit;
+    }
+
+    :host([invalid]) [part='label'] {
+      color: var(--material-error-text-color);
+    }
+  `,
+  { moduleId: 'material-form-item' }
+);
