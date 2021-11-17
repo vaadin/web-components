59f238a40271e1817591a6a2bc5301dd28083ece
Tomi Virkki <tomivirkki@users.noreply.github.com>
2021-11-17 15:36:31 +0200

    fix: set tabindex on the vaadin-upload-file element (#3068)

--- packages/upload/test/a11y.test.js
+++ packages/upload/test/a11y.test.js
@@ -1,32 +1,13 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { fixtureSync } from '@vaadin/testing-helpers';
 import '../vaadin-upload.js';
 import { createFile } from './common.js';
 
 const FAKE_FILE = createFile(100000, 'application/uknown');
 
 describe('a11y', () => {
-  describe('<vaadin-upload>', () => {
-    let uploadElement, fileList;
-
-    beforeEach(async () => {
-      uploadElement = fixtureSync(`<vaadin-upload></vaadin-upload>`);
-      uploadElement.files = [FAKE_FILE];
-
-      await nextRender();
-
-      fileList = uploadElement.shadowRoot.querySelector('[part=file-list]');
-    });
-
-    describe('file list', () => {
-      it('should be a <ul> element', () => {
-        expect(fileList).to.be.an.instanceOf(HTMLUListElement);
-      });
-    });
-  });
-
   describe('<vaadin-upload-file>', () => {
-    let uploadFileElement, i18n, button, name, row;
+    let uploadFileElement, i18n, button, name;
 
     beforeEach(() => {
       uploadFileElement = fixtureSync(`<vaadin-upload-file></vaadin-upload-file>`);
@@ -40,23 +21,12 @@ describe('a11y', () => {
       uploadFileElement.i18n = i18n;
       uploadFileElement.file = FAKE_FILE;
 
-      row = uploadFileElement.shadowRoot.querySelector('[part=row]');
       name = uploadFileElement.shadowRoot.querySelector('[part=name]');
-    });
-
-    describe('row', () => {
-      it('should be a <li> element', () => {
-        expect(row).to.be.an.instanceOf(HTMLLIElement);
-      });
     });
 
     describe('start button', () => {
       beforeEach(() => {
         button = uploadFileElement.shadowRoot.querySelector('[part=start-button]');
-      });
-
-      it('should be a <button> element', () => {
-        expect(button).to.be.an.instanceOf(HTMLButtonElement);
       });
 
       it('should have aria-describedby attribute', () => {
@@ -73,10 +43,6 @@ describe('a11y', () => {
         button = uploadFileElement.shadowRoot.querySelector('[part=retry-button]');
       });
 
-      it('should be a <button> element', () => {
-        expect(button).to.be.an.instanceOf(HTMLButtonElement);
-      });
-
       it('should have aria-describedby attribute', () => {
         expect(button.getAttribute('aria-describedby')).to.equal(name.id);
       });
@@ -89,10 +55,6 @@ describe('a11y', () => {
     describe('remove button', () => {
       beforeEach(() => {
         button = uploadFileElement.shadowRoot.querySelector('[part=remove-button]');
-      });
-
-      it('should be a <button> element', () => {
-        expect(button).to.be.an.instanceOf(HTMLButtonElement);
       });
 
       it('should have aria-describedby attribute', () => {
