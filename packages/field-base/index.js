83e5fd3e2ff327dfa6d88b0b45dc5be254f8985d
web-padawan <iamkulykov@gmail.com>
2021-11-17 21:32:45 +0200

    refactor: sort exports alphabetically

--- packages/field-base/index.js
+++ packages/field-base/index.js
@@ -1,4 +1,3 @@
-export { LabelledInputController } from './src/labelled-input-controller.js';
 export { CheckedMixin } from './src/checked-mixin.js';
 export { DelegateFocusMixin } from './src/delegate-focus-mixin.js';
 export { DelegateStateMixin } from './src/delegate-state-mixin.js';
@@ -8,6 +7,7 @@ export { InputController } from './src/input-controller.js';
 export { InputControlMixin } from './src/input-control-mixin.js';
 export { InputFieldMixin } from './src/input-field-mixin.js';
 export { InputMixin } from './src/input-mixin.js';
+export { LabelledInputController } from './src/labelled-input-controller.js';
 export { LabelMixin } from './src/label-mixin.js';
 export { PatternMixin } from './src/pattern-mixin.js';
 export { ShadowFocusMixin } from './src/shadow-focus-mixin.js';
