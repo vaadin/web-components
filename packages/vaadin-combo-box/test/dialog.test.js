6747d51d4d271f440202a57f64fce0799bc2e375
Sascha Ißbrücker <sissbruecker@vaadin.com>
2021-11-25 12:57:13 +0100

    fix: prevent combo box closing other overlays

--- packages/vaadin-combo-box/test/dialog.test.js
+++ packages/vaadin-combo-box/test/dialog.test.js
@@ -7,33 +7,65 @@ import '../src/vaadin-combo-box.js';
 describe('dialog', () => {
   let dialog, comboBox;
 
-  beforeEach(async () => {
-    dialog = fixtureSync(`
+  describe('modeless', () => {
+    beforeEach(async () => {
+      dialog = fixtureSync(`
       <vaadin-dialog modeless>
         <template>
           <vaadin-combo-box items="[1,2,3]"></vaadin-combo-box>
         </template>
       </vaadin-dialog>
     `);
-    dialog.opened = true;
-    await nextFrame();
-    comboBox = dialog.$.overlay.querySelector('vaadin-combo-box');
+      dialog.opened = true;
+      await nextFrame();
+      comboBox = dialog.$.overlay.querySelector('vaadin-combo-box');
+    });
+
+    it('should not end up behind the dialog overlay on mousedown', async () => {
+      comboBox.open();
+      mousedown(comboBox);
+      await nextFrame();
+      expect(comboBox.$.overlay.$.dropdown.$.overlay._last).to.be.true;
+    });
+
+    it('should not end up behind the dialog overlay on touchstart', async () => {
+      comboBox.open();
+
+      const { left: x, top: y } = comboBox.getBoundingClientRect();
+      touchstart(comboBox, { x, y });
+
+      await nextFrame();
+      expect(comboBox.$.overlay.$.dropdown.$.overlay._last).to.be.true;
+    });
   });
 
-  it('should not end up behind the dialog overlay on mousedown', async () => {
-    comboBox.open();
-    mousedown(comboBox);
-    await nextFrame();
-    expect(comboBox.$.overlay.$.dropdown.$.overlay._last).to.be.true;
-  });
+  describe('modal', () => {
+    let backdrop;
 
-  it('should not end up behind the dialog overlay on touchstart', async () => {
-    comboBox.open();
+    beforeEach(async () => {
+      dialog = fixtureSync(`
+      <vaadin-dialog>
+        <template>
+          <div id="content">
+            <vaadin-combo-box items="[1,2,3]"></vaadin-combo-box>
+          </div>
+        </template>
+      </vaadin-dialog>
+    `);
+      dialog.opened = true;
+      await nextFrame();
+      comboBox = dialog.$.overlay.querySelector('vaadin-combo-box');
+      backdrop = dialog.$.overlay.$.backdrop;
+    });
 
-    const { left: x, top: y } = comboBox.getBoundingClientRect();
-    touchstart(comboBox, { x, y });
+    it('should not close the dialog on outside click', async () => {
+      comboBox.open();
+      await nextFrame();
 
-    await nextFrame();
-    expect(comboBox.$.overlay.$.dropdown.$.overlay._last).to.be.true;
+      // Clicking on backdrop should close the combo box overlay, but not the dialog overlay
+      backdrop.click();
+      expect(comboBox.opened).to.be.false;
+      expect(dialog.opened).to.be.true;
+    });
   });
 });
