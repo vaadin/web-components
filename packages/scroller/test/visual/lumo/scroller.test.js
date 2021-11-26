5b7d879d1c8e55278c07c94e4adf23d82b810d9d
Tomi Virkki <tomivirkki@users.noreply.github.com>
2021-11-26 15:26:53 +0200

    feat: make scroller focusable by default (#3107)

---
+++ packages/scroller/test/visual/lumo/scroller.test.js
@@ -0,0 +1,31 @@
+import { fixtureSync } from '@vaadin/testing-helpers';
+import { sendKeys } from '@web/test-runner-commands';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../theme/lumo/vaadin-scroller.js';
+
+describe('scroller', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.padding = '10px';
+
+    element = fixtureSync(
+      `
+      <vaadin-scroller style="width: 200px; height: 200px;">
+        <div id="content" style="width: 400px;"></div>
+      </vaadin-scroller>`,
+      div
+    );
+    element.firstElementChild.textContent = new Array(1000).fill('content').join(' ');
+  });
+
+  it('default', async () => {
+    await visualDiff(div, 'default');
+  });
+
+  it('focus-ring', async () => {
+    await sendKeys({ press: 'Tab' });
+    await visualDiff(div, 'focus-ring');
+  });
+});
