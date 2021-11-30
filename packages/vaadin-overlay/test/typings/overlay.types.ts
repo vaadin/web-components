2f318191ce7b672062901c5d2a8a5c4d23d901e9
Tomi Virkki <tomivirkki@users.noreply.github.com>
2021-11-30 11:27:40 +0200

    docs: document missing overlay event types (#3132)

--- packages/vaadin-overlay/test/typings/overlay.types.ts
+++ packages/vaadin-overlay/test/typings/overlay.types.ts
@@ -1,5 +1,11 @@
 import '../../vaadin-overlay.js';
-import { OverlayOpenedChangedEvent } from '../../vaadin-overlay.js';
+import {
+  OverlayCloseEvent,
+  OverlayEscapePressEvent,
+  OverlayOpenedChangedEvent,
+  OverlayOpenEvent,
+  OverlayOutsideClickEvent
+} from '../../vaadin-overlay.js';
 
 const assertType = <TExpected>(actual: TExpected) => actual;
 
@@ -8,4 +14,22 @@ const overlay = document.createElement('vaadin-overlay');
 overlay.addEventListener('opened-changed', (event) => {
   assertType<OverlayOpenedChangedEvent>(event);
   assertType<boolean>(event.detail.value);
+});
+
+overlay.addEventListener('vaadin-overlay-open', (event) => {
+  assertType<OverlayOpenEvent>(event);
+});
+
+overlay.addEventListener('vaadin-overlay-close', (event) => {
+  assertType<OverlayCloseEvent>(event);
+});
+
+overlay.addEventListener('vaadin-overlay-escape-press', (event) => {
+  assertType<OverlayEscapePressEvent>(event);
+  assertType<KeyboardEvent>(event.detail.sourceEvent);
+});
+
+overlay.addEventListener('vaadin-overlay-outside-click', (event) => {
+  assertType<OverlayOutsideClickEvent>(event);
+  assertType<MouseEvent>(event.detail.sourceEvent);
 });
