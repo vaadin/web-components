8f6058b96677bb9b33d786de7b8a49db6e74f57f
Sergey Vinogradov <mr.vursen@gmail.com>
2021-12-07 17:49:02 +0300

    Update packages/vaadin-overlay/test/focus-trap.test.js
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- packages/vaadin-overlay/test/focus-trap.test.js
+++ packages/vaadin-overlay/test/focus-trap.test.js
@@ -209,8 +209,9 @@ describe('focus-trap', function () {
             i++;
           }
 
-          expect(getFocusedElementIndex()).to.eql(i);
-          tabKeyDown(focusableElements[getFocusedElementIndex()]);
+          const focusedIndex = getFocusedElementIndex();
+          expect(focusedIndex).to.equal(i);
+          tabKeyDown(focusableElements[focusedIndex]);
         }
         expect(getFocusedElementIndex()).to.eql(0);
 
