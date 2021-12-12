e295bde3c44d84d779ba5f6fe3485ff8604abe7a
web-padawan <iamkulykov@gmail.com>
2021-12-12 15:31:03 +0200

    refactor: move SlotController to component-base

--- packages/field-base/src/text-area-controller.js
+++ packages/field-base/src/text-area-controller.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { SlotController } from './slot-controller.js';
+import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
 
 /**
  * A controller to create and initialize slotted `<textarea>` element.
