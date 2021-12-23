eacb6c4f632fd765daec25aecdf4ab57609f00a8
web-padawan <iamkulykov@gmail.com>
2021-12-23 11:05:41 +0200

    chore: init multi-select-combo-box package

---
+++ packages/multi-select-combo-box/theme/material/vaadin-multi-select-combo-box.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2021 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import '@vaadin/combo-box/theme/material/vaadin-combo-box-light.js';
+import '@vaadin/input-container/theme/material/vaadin-input-container.js';
+import '../../src/vaadin-multi-select-combo-box.js';
