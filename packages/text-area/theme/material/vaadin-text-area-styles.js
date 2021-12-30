8244f26ec5771806efcd345fd8b4dc75d6967d1b
Tatu Lund <tatu@vaadin.com>
2021-12-30 12:33:38 +0200

    Refactor vaadin-text-area Material styles

--- packages/text-area/theme/material/vaadin-text-area-styles.js
+++ packages/text-area/theme/material/vaadin-text-area-styles.js
@@ -21,6 +21,53 @@ const textArea = css`
     white-space: pre-wrap; /* override "nowrap" from <vaadin-text-field> */
     align-self: stretch; /* override "baseline" from <vaadin-text-field> */
   }
+
+  /* Do not show inherited underline from vaadin-text-field */
+  /* See: https://github.com/vaadin/web-components/issues/1333 */
+  [part="input-field"]::before,
+  [part="input-field"]::after {
+    display: none;
+  }
+
+  /* Set the pseudoelement in vaadin-text-area-container instead */
+  /* NOTE: If DOM ever changed, this will break */
+  .vaadin-text-area-container::before,
+  .vaadin-text-area-container::after {
+    order: 3;
+    content: "";
+    display: block;
+    height: 1px;
+    background-color: var(--_material-text-field-input-line-background-color, #000);
+    opacity: var(--_material-text-field-input-line-opacity, 0.42);
+  }
+
+  .vaadin-text-area-container::after {
+    margin-top: -1px;
+    background-color: var(--material-primary-color);
+    opacity: 0;
+    height: 2px;
+    transform: scaleX(0);
+    transition: opacity 0.175s;
+  }
+
+  :host([focused]) .vaadin-text-area-container::after,
+  :host([invalid]) .vaadin-text-area-container::after {
+    opacity: 1;
+    transform: none;
+    transition: transform 0.175s, opacity 0.175s;
+  }
+
+  [part="label"] {
+    order: 1;
+  }
+
+  [part="input-field"] {
+    order: 2;
+  }
+
+  [part="error-message"] {
+    order: 4;
+  }  
 `;
 
 registerStyles('vaadin-text-area', [inputFieldShared, textArea], { moduleId: 'material-text-area' });
