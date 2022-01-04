0c3b553479dc0f4b0af28a672833f1be8835608b
web-padawan <iamkulykov@gmail.com>
2022-01-04 12:20:40 +0200

    chore: update copyright header to 2022

--- packages/vaadin-login/src/vaadin-login-overlay.js
+++ packages/vaadin-login/src/vaadin-login-overlay.js
@@ -1,7 +1,6 @@
 /**
  * @license
- * Vaadin Login
- * Copyright (C) 2020 Vaadin Ltd
+ * Copyright (c) 2018 - 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { LoginOverlay } from '@vaadin/login/src/vaadin-login-overlay.js';
--- packages/vaadin-rich-text-editor/src/vaadin-rich-text-editor.d.ts
+++ packages/vaadin-rich-text-editor/src/vaadin-rich-text-editor.d.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2020 Vaadin Ltd
+ * Copyright (c) 2018 - 2022 Vaadin Ltd.
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
 import { RichTextEditor } from '@vaadin/rich-text-editor/src/vaadin-rich-text-editor.js';
