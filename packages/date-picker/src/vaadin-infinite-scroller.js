c4f99805f0b572a165f844a1be6f211992765fc1
Sergey Vinogradov <mr.vursen@gmail.com>
2022-01-07 14:32:06 +0300

    fix: ensure focused month is visible when receiving focus (#3219)

--- packages/date-picker/src/vaadin-infinite-scroller.js
+++ packages/date-picker/src/vaadin-infinite-scroller.js
@@ -186,9 +186,9 @@ class InfiniteScroller extends PolymerElement {
     }
 
     // Check if we scrolled enough to translate the buffer positions.
-    const bufferOffset = this.root.querySelector('.buffer').offsetTop;
-    const upperThresholdReached = scrollTop > this._buffers[1].translateY + this.itemHeight + bufferOffset;
-    const lowerThresholdReached = scrollTop < this._buffers[0].translateY + this.itemHeight + bufferOffset;
+    const offset = this.itemHeight + this.bufferOffset;
+    const upperThresholdReached = scrollTop > this._buffers[1].translateY + offset;
+    const lowerThresholdReached = scrollTop < this._buffers[0].translateY + offset;
 
     if (upperThresholdReached || lowerThresholdReached) {
       this._translateBuffer(lowerThresholdReached);
@@ -210,9 +210,23 @@ class InfiniteScroller extends PolymerElement {
   }
 
   /**
+   * @return {number}
+   */
+  get bufferOffset() {
+    return this._buffers[0].offsetTop;
+  }
+
+  /**
+   * @return {number}
+   */
+  get position() {
+    return (this.$.scroller.scrollTop - this._buffers[0].translateY) / this.itemHeight + this._firstIndex;
+  }
+
+  /**
    * Current scroller position as index. Can be a fractional number.
    *
-   * @type {Number}
+   * @type {number}
    */
   set position(index) {
     this._preventScrollEvent = true;
@@ -239,12 +253,8 @@ class InfiniteScroller extends PolymerElement {
   }
 
   /**
-   * @private
+   * @return {number}
    */
-  get position() {
-    return (this.$.scroller.scrollTop - this._buffers[0].translateY) / this.itemHeight + this._firstIndex;
-  }
-
   get itemHeight() {
     if (!this._itemHeightVal) {
       const itemHeight = getComputedStyle(this).getPropertyValue('--vaadin-infinite-scroller-item-height');
