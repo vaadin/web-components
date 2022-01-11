0262149c3692075375c40a438a79b49a676dae64
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-01-11 10:27:17 +0100

    chore: add map package skeleton

---
+++ packages/map/src/vaadin-map.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2015 - 2022 Vaadin Ltd.
+ * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
+ */
+declare class Map {}
+
+export default Map;
