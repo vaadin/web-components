014e290be2f0a1011f43cd24d6ae5c7e8cfee6c9
web-padawan <iamkulykov@gmail.com>
2022-01-19 14:10:17 +0200

    test: add CSS to hide caret when focused

---
+++ packages/date-time-picker/test/visual/common.js
@@ -0,0 +1,11 @@
+import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+/* hide caret */
+registerStyles(
+  'vaadin-date-picker',
+  css`
+    :host([focused]) ::slotted(input) {
+      caret-color: transparent;
+    }
+  `
+);
