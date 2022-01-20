b3252a300e6a8da47c40a0baaa4a02f97d08cf41
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-01-20 08:53:13 +0100

    chore: remove Flow sync logic

--- packages/map/src/vaadin-map.js
+++ packages/map/src/vaadin-map.js
@@ -7,7 +7,6 @@ import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import OpenLayersMap from 'ol/Map.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import { synchronize } from './synchronization.js';
 
 class Map extends ElementMixin(ThemableMixin(PolymerElement)) {
   static get template() {
@@ -57,45 +56,6 @@ class Map extends ElementMixin(ThemableMixin(PolymerElement)) {
       target: this.$.map
     });
     this.__initialized = true;
-  }
-
-  /**
-   * Synchronize a configuration object into the internal OpenLayers map instance.
-   *
-   * The target parameter can be left unspecified, in which case the OpenLayers
-   * map instance itself is used as synchronization target. This results in a
-   * full sync of the map configuration hierarchy.
-   * Specifying a target instance allows synchronizing only a part of the
-   * configuration. In that case the configuration parameter needs to be for the
-   * specific part that should be synchronized. For example, when passing an
-   * `ol/View` instance as target, then the configuration parameter should
-   * contain a configuration object for a view as well.
-   *
-   * @param configuration the configuration object to synchronize from
-   * @param target the OpenLayers configuration instance to synchronize into, or undefined if the root map instance should be used
-   */
-  synchronize(configuration, target) {
-    if (!this.__initialized) {
-      throw new Error('Component is not initialized yet');
-    }
-    if (!configuration || typeof configuration !== 'object') {
-      throw new Error('Configuration must be an object');
-    }
-    // Assume we want to sync the full map by default
-    if (!target) {
-      target = this.configuration;
-    }
-
-    // We don't want the synchronization to return a new instance (we have no
-    // idea where to store it afterwards), so we adopt the ID from the
-    // configuration object and assume that the types will be the same
-    target.id = configuration.id;
-
-    const context = { synchronize };
-
-    synchronize(target, configuration, context);
-    // TODO: layers don't render on initialization in some cases, needs investigation
-    this.configuration.updateSize();
   }
 }
 
