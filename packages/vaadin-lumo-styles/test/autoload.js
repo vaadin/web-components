2b91ac40617869f0b1a19500c614fc2820f1bf20
Serhii Kulykov <iamkulykov@gmail.com>
2022-02-04 10:53:03 +0200

    test: add autoload helpers to Lumo and Material (#3392)

---
+++ packages/vaadin-lumo-styles/test/autoload.js
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { color, typography } from '../all-imports.js';
+
+const style = document.createElement('style');
+style.innerHTML = `${color.toString()} ${typography.toString()}`;
+document.head.appendChild(style);
+
+export * from '../all-imports.js';
