aeb0931283f44f25f3fa63f6190be88ab46f161b
Tomi Virkki <virkki@vaadin.com>
2022-02-04 11:10:52 +0200

    chore: use lumo autoload helper in dev pages

--- dev/lumo-globals.js
+++
@@ -1,5 +0,0 @@
-import { color, typography } from '@vaadin/vaadin-lumo-styles';
-
-const style = document.createElement('style');
-style.innerHTML = `${color.toString()} ${typography.toString()}`;
-document.head.appendChild(style);
