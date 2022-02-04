b60d06df94a6fd31ee6ad77e6dc4f70b794b45b9
Tomi Virkki <virkki@vaadin.com>
2022-02-04 11:48:08 +0200

    chore: prevent fouc on dev pages

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,24 +1,50 @@
 /* eslint-env node */
 const fs = require('fs');
 
-const indexFile = fs.readFileSync('./dev/index.html', 'utf8');
+const preventFouc = `
+  <style>
+    body:not(.resolved) {
+      opacity: 0;
+    }
+
+    body {
+      transition: opacity 0.2s;
+    }
+  </style>
+
+  <script type="module">
+    // It's important to use type module for the script so the timing is correct
+    document.body.classList.add('resolved');
+  </script>
+`;
 
 module.exports = {
   plugins: [
     {
       name: 'dev-page-listing',
       transform(context) {
-        if (['/dev/index.html', '/dev', '/dev/'].includes(context.path)) {
-          const listing = `
-            <ul id="listing">
-              ${fs
-                .readdirSync('./dev')
-                .filter((file) => file !== 'index.html')
-                .map((file) => `<li><a href="/dev/${file}">${file}</a></li>`)
-                .join('')}
-            </ul>`;
+        if (context.response.is('html')) {
+          let body = context.body;
 
-          return { body: indexFile.replace(/<ul id="listing">.*<\/ul>/, listing) };
+          // Fouc prevention
+          body = body.replace(/<\/body>/, `${preventFouc}\n</body>`);
+
+          // Index page listing
+          if (['/dev/index.html', '/dev', '/dev/'].includes(context.path)) {
+            const listing = `
+              <ul id="listing">
+                ${fs
+                  .readdirSync('./dev')
+                  .filter((file) => file !== 'index.html')
+                  .filter((file) => file.endsWith('.html'))
+                  .map((file) => `<li><a href="/dev/${file}">${file}</a></li>`)
+                  .join('')}
+              </ul>`;
+
+            body = body.replace(/<ul id="listing">.*<\/ul>/, listing);
+          }
+
+          return { body };
         }
       }
     }
