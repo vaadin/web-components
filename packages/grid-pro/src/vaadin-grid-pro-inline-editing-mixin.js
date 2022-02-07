5b476b61ac203548586b6f348ca5481e0e55fc14
J. Diogo Oliveira <jdso@vhelsi.com>
2022-02-07 07:11:14 +0000

    PR comment #2

--- packages/grid-pro/src/vaadin-grid-pro-inline-editing-mixin.js
+++ packages/grid-pro/src/vaadin-grid-pro-inline-editing-mixin.js
@@ -456,6 +456,10 @@ export const InlineEditingMixin = (superClass) =>
     }
 
     /**
+     * Triggers the editor for a given (row,col)
+     * row can be either a number (element index) or an object
+     * with the property 'key' = index+1 (e.g. {key: 1} is equivalent to pass it 0)
+     * column can be either a number (element index) or a string (columnId)
      * @param {number|object} row
      * @param {number|string} col
      * @public
