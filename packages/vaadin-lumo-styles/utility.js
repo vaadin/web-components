291cd7b7c5ea58c33b73f7616746a99708ec9de8
Serhii Kulykov <iamkulykov@gmail.com>
2022-02-14 15:39:25 +0200

    fix: add missing .js extension to utilities (#3438)

--- packages/vaadin-lumo-styles/utility.js
+++ packages/vaadin-lumo-styles/utility.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2017 - 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles';
+import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import { accessibility } from './utilities/accessibility.js';
 import { background } from './utilities/background.js';
 import { border } from './utilities/border.js';
