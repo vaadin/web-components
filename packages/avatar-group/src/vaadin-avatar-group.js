001492c19e531b23fce5761b9e0fdb4a5bc26287
Serhii Kulykov <iamkulykov@gmail.com>
2022-03-03 15:37:28 +0200

    fix: check if item exists before accessing it (#3513)

--- packages/avatar-group/src/vaadin-avatar-group.js
+++ packages/avatar-group/src/vaadin-avatar-group.js
@@ -395,7 +395,9 @@ class AvatarGroup extends ResizeMixin(ElementMixin(ThemableMixin(PolymerElement)
     const result = [];
     for (let i = limit; i < items; i++) {
       const item = this.items[i];
-      result.push(item.name || item.abbr || 'anonymous');
+      if (item) {
+        result.push(item.name || item.abbr || 'anonymous');
+      }
     }
     // override generated title attribute
     this.$.overflow.setAttribute('title', result.join('\n'));
