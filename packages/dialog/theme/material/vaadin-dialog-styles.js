553ef82f3f6f50cee6161e02ca80b5456c158ec9
Jouni Koivuviita <jouni@vaadin.com>
2022-04-06 17:07:41 +0300

    Improved Dialog Material theme

--- packages/dialog/theme/material/vaadin-dialog-styles.js
+++ packages/dialog/theme/material/vaadin-dialog-styles.js
@@ -10,23 +10,88 @@ const dialogOverlay = css`
     max-width: 560px;
     min-width: 280px;
     -webkit-tap-highlight-color: transparent;
+    border-radius: 8px;
   }
 
   [part='content'] {
     padding: 24px;
   }
 
-  :host([has-header]) [part='header'],
-  :host([has-title]) [part='header'] {
-    padding: 9px 24px;
-    font-weight: 500;
+  :host(:is([has-header], [has-title])) [part='header'] {
+    padding: 16px;
+  }
+
+  :host(:is([has-header], [has-title])) [part='header'] + [part='content'] {
+    padding-top: 0;
+  }
+
+  [part='header'],
+  [part='header-content'],
+  [part='footer'] {
     gap: 8px;
-    border-bottom: 1px solid var(--material-divider-color);
+    line-height: 1.2;
+  }
+
+  [part='title'] {
+    font-size: var(--material-h5-font-size);
+    font-weight: 500;
+  }
+
+  [part='title'],
+  [part='header-content'] {
+    margin-inline-start: 8px;
   }
 
   :host([has-footer]) [part='footer'] {
     padding: 8px;
-    border-top: 1px solid var(--material-divider-color);
+  }
+
+  @media (min-height: 320px) {
+    :host(:is([has-header], [has-title])) [part='header'] {
+      box-shadow: 0 1px 0 0 var(--material-divider-color);
+    }
+
+    :host([has-footer]) [part='footer'] {
+      box-shadow: 0 -1px 0 0 var(--material-divider-color);
+    }
+
+    /* "scroll divider" */
+
+    :host([has-footer]) [part='content'] {
+      height: 100%;
+      padding-bottom: 0;
+    }
+
+    :host([has-footer]) [part='content'] slot {
+      display: block;
+      min-height: calc(100% - 26px);
+    }
+
+    :host(:is([has-header], [has-title])) [part='content']::before {
+      content: '';
+      display: block;
+      width: calc(100% + 24px * 2);
+      height: 200px;
+      background: var(--material-background-color);
+      margin-top: -199px;
+      margin-left: -24px;
+      margin-right: -24px;
+      position: relative;
+      z-index: 1;
+    }
+
+    :host([has-footer]) [part='content']::after {
+      content: '';
+      display: block;
+      width: calc(100% + 24px * 2);
+      height: 1px;
+      background: var(--material-background-color);
+      margin-top: 24px;
+      margin-left: -24px;
+      margin-right: -24px;
+      position: relative;
+      z-index: 2;
+    }
   }
 
   /* No padding */
