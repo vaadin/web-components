c4d185819f111091fa8663d3f5398e80f8902078
web-padawan <iamkulykov@gmail.com>
2022-04-08 13:32:01 +0300

    chore: add missing copyright headers

--- packages/component-base/src/polylit-mixin.d.ts
+++ packages/component-base/src/polylit-mixin.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { LitElement } from 'lit';
 
 declare type Constructor<T> = new (...args: any[]) => T;
