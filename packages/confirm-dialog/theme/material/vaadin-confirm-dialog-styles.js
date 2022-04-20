da8ee7a7dad791105473fa29c312ffb1944bbd9a
Serhii Kulykov <iamkulykov@gmail.com>
2022-04-20 16:53:41 +0300

    refactor!: use header and footer in confirm-dialog (#3673)

--- packages/confirm-dialog/theme/material/vaadin-confirm-dialog-styles.js
+++ packages/confirm-dialog/theme/material/vaadin-confirm-dialog-styles.js
@@ -9,64 +9,25 @@ registerStyles(
     }
 
     [part='content'] {
-      padding: 8px 24px;
       min-width: 0;
     }
 
     [part='header'] ::slotted(h3) {
-      margin-top: 0.75em !important;
+      margin-top: 0 !important;
+      margin-bottom: 0 !important;
+      margin-inline-start: 8px;
     }
 
     [part='message'] {
       width: 25em;
       max-width: 100%;
-      margin-right: 24px;
-    }
-
-    [part='footer'] {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: flex-end;
-      margin-top: 28px;
-      margin-right: -16px;
-    }
-
-    /* LTR styles */
-    :host(:not([dir='rtl'])) ::slotted([slot$='button']:not([slot^='confirm'])) {
-      margin-right: 8px;
-    }
-
-    /* RTL styles */
-    :host([dir='rtl']) [part='message'] {
-      margin-left: 24px;
-      margin-right: 0;
-    }
-
-    :host([dir='rtl']) [part='footer'] {
-      margin-left: -16px;
-      margin-right: 0;
-    }
-
-    :host([dir='rtl']) ::slotted([slot$='button']:not([slot^='confirm'])) {
-      margin-left: 8px;
+      margin-inline-end: 24px;
     }
 
     @media (max-width: 360px) {
       [part='footer'] {
         flex-direction: column-reverse;
         align-items: flex-end;
-      }
-
-      ::slotted([slot$='button']:not([slot^='confirm'])) {
-        margin-top: 8px;
-      }
-
-      :host(:not([dir='rtl'])) ::slotted([slot$='button']:not([slot^='confirm'])) {
-        margin-right: 0;
-      }
-
-      :host([dir='rtl']) ::slotted([slot$='button']:not([slot^='confirm'])) {
-        margin-left: 0;
       }
     }
   `,
