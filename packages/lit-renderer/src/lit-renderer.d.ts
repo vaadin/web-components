374e7fef2e0ea3494faf37d0a739d7c7ececcf5c
Sergey Vinogradov <mr.vursen@gmail.com>
2022-04-28 13:41:57 +0300

    add license header, update package description

--- packages/lit-renderer/src/lit-renderer.d.ts
+++ packages/lit-renderer/src/lit-renderer.d.ts
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { RenderOptions } from 'lit';
 import { Directive } from 'lit/directive.js';
 
--- packages/lit-renderer/src/lit-renderer.js
+++ packages/lit-renderer/src/lit-renderer.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { nothing } from 'lit';
 import { AsyncDirective } from 'lit/async-directive.js';
 import { PartType } from 'lit/directive.js';
