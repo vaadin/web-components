1e0eef8564d926add672f3c4a917268e18d8ab4a
Sergey Vinogradov <mr.vursen@gmail.com>
2022-04-29 10:25:13 +0300

    add constructor TS definition

--- packages/lit-renderer/src/lit-renderer.d.ts
+++ packages/lit-renderer/src/lit-renderer.d.ts
@@ -3,12 +3,15 @@
  * Copyright (c) 2016 - 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { ElementPart, nothing, RenderOptions, TemplateResult } from 'lit';
+import { nothing, RenderOptions, TemplateResult } from 'lit';
 import { AsyncDirective } from 'lit/async-directive.js';
+import { PartInfo } from 'lit/directive.js';
 
 export type LitRenderer = (...args: any[]) => TemplateResult;
 
 export abstract class LitRendererDirective<E extends Element, R extends LitRenderer> extends AsyncDirective {
+  constructor(info: PartInfo);
+
   protected host: RenderOptions['host'];
 
   protected element: E;
@@ -18,8 +21,6 @@ export abstract class LitRendererDirective<E extends Element, R extends LitRende
   protected renderRenderer(container: Element, ...args: Parameters<R>): void;
 
   render(_renderer: R, _value?: unknown): typeof nothing;
-
-  update(_part: ElementPart, [R, unknown]): typeof nothing;
 
   /**
    * Adds the renderer callback to the element.
