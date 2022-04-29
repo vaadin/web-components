0a35ee97657d5727804f5dc0aa4222a7e44c3424
Sergey Vinogradov <mr.vursen@gmail.com>
2022-04-29 13:21:40 +0300

    add LitRendererDirectiveFactory type to lit-renderer

--- packages/lit-renderer/src/lit-renderer.d.ts
+++ packages/lit-renderer/src/lit-renderer.d.ts
@@ -5,6 +5,7 @@
  */
 import { ElementPart, nothing, RenderOptions, TemplateResult } from 'lit';
 import { AsyncDirective } from 'lit/async-directive.js';
+import { DirectiveClass, DirectiveResult } from 'lit/directive';
 
 export type LitRenderer = (...args: any[]) => TemplateResult;
 
@@ -36,3 +37,7 @@ export abstract class LitRendererDirective<E extends Element, R extends LitRende
    */
   abstract removeRenderer(): void;
 }
+
+export type LitRendererDirectiveFactory<D extends DirectiveClass> = (
+  ...args: Parameters<InstanceType<D>['render']>
+) => DirectiveResult<D>;
