40971cedd887633c3b4e7724d78997a71d20e888
web-padawan <iamkulykov@gmail.com>
2022-05-04 09:51:45 +0300

    refactor: apply code review suggestion

--- packages/combo-box/src/vaadin-combo-box-mixin.js
+++ packages/combo-box/src/vaadin-combo-box-mixin.js
@@ -972,11 +972,7 @@ export const ComboBoxMixin = (subclass) =>
         }
 
         const inputValue = this._inputElementValue;
-        if (
-          inputValue === undefined ||
-          inputValue === this.value ||
-          inputValue === this._getItemLabel(this.selectedItem)
-        ) {
+        if (inputValue === undefined || inputValue === this._getItemLabel(this.selectedItem)) {
           // When the input element value is the same as the current value or not defined,
           // set the focused index to the item that matches the value.
           this._focusedIndex = this._indexOfValue(this.value, this.filteredItems);
