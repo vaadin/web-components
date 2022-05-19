2deba69ef767992c432ce9778f5fd81fc7fbd8b6
Tomi Virkki <tomivirkki@users.noreply.github.com>
2022-05-19 16:42:33 +0300

    fix: import themed overlay with themed extensions (#3888)

--- packages/avatar-group/theme/material/vaadin-avatar-group.js
+++ packages/avatar-group/theme/material/vaadin-avatar-group.js
@@ -1,5 +1,6 @@
 import '@vaadin/avatar/theme/material/vaadin-avatar.js';
 import '@vaadin/item/theme/material/vaadin-item.js';
 import '@vaadin/list-box/theme/material/vaadin-list-box.js';
+import '@vaadin/vaadin-overlay/theme/material/vaadin-overlay.js';
 import './vaadin-avatar-group-styles.js';
 import '../../src/vaadin-avatar-group.js';
--- packages/date-picker/theme/material/vaadin-date-picker-overlay-styles.js
+++ packages/date-picker/theme/material/vaadin-date-picker-overlay-styles.js
@@ -1,3 +1,4 @@
+import '@vaadin/vaadin-overlay/theme/material/vaadin-overlay.js';
 import { overlay } from '@vaadin/vaadin-material-styles/mixins/overlay.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
