8cf9ded7a0a5962b75c5b4ea8a0e7a85bfbac555
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-05-20 10:00:55 +0200

    chore: fix flaky lazy loading scrolling test (#3897)

--- packages/combo-box/test/lazy-loading.test.js
+++ packages/combo-box/test/lazy-loading.test.js
@@ -439,6 +439,10 @@ describe('lazy loading', () => {
           comboBox.close();
 
           comboBox.opened = true;
+          // Wait for the async data provider to respond
+          await aTimeout(0);
+          // Wait for items to render
+          await nextFrame();
           comboBox.$.dropdown._scrollIntoView(50);
           // Wait for the async data provider to respond
           await aTimeout(0);
