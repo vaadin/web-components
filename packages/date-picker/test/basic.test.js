4fdbd44e5a9af97c6a18f831ba3e73012321483d
web-padawan <iamkulykov@gmail.com>
2022-05-23 14:27:46 +0300

    test: remove test not working in CI

--- packages/date-picker/test/basic.test.js
+++ packages/date-picker/test/basic.test.js
@@ -4,7 +4,7 @@ import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
 import '../src/vaadin-date-picker.js';
 import * as settings from '@polymer/polymer/lib/utils/settings.js';
-import { close, getFocusedCell, getOverlayContent, monthsEqual, open, waitForScrollToFinish } from './common.js';
+import { close, getOverlayContent, monthsEqual, open } from './common.js';
 
 settings.setCancelSyntheticClickEvents(false);
 
@@ -83,15 +83,6 @@ describe('basic features', () => {
     await sendKeys({ press: 'ArrowDown' });
     await sendKeys({ press: 'Escape' });
     expect(datepicker.hasAttribute('focused')).to.be.true;
-  });
-
-  it('should focus date element when focused on fullscreen', async () => {
-    datepicker._fullscreen = true;
-    await open(datepicker);
-    const content = getOverlayContent(datepicker);
-    await waitForScrollToFinish(content);
-    const cell = getFocusedCell(content);
-    expect(isFocused(cell)).to.be.true;
   });
 
   it('should set focused attribute when focused on fullscreen', async () => {
