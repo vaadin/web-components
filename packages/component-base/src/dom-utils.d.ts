5b333281c3bfdf1966ceed11a8c403468b0df91a
Sergey Vinogradov <mr.vursen@gmail.com>
2022-05-26 11:51:19 +0300

    add license header

--- packages/component-base/src/dom-utils.d.ts
+++ packages/component-base/src/dom-utils.d.ts
@@ -1,4 +1,10 @@
 /**
+ * @license
+ * Copyright (c) 2021 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
  * Returns an array of ancestor root nodes for the given node.
  *
  * A root node is either a document node or a document fragment node (Shadow Root).
