3455db68d3a07eed90478cc01b128c71e18e6e0e
web-padawan <iamkulykov@gmail.com>
2022-05-26 14:29:16 +0300

    test: add more tests for combo-box in dialog

--- integration/tests/dialog-combo-box.test.js
+++ integration/tests/dialog-combo-box.test.js
@@ -1,6 +1,6 @@
 import { expect } from '@esm-bundle/chai';
 import { fixtureSync, mousedown, nextFrame, touchstart } from '@vaadin/testing-helpers';
-import '@vaadin/polymer-legacy-adapter/template-renderer.js';
+import { sendKeys } from '@web/test-runner-commands';
 import './not-animated-styles.js';
 import '@vaadin/combo-box';
 import '@vaadin/dialog';
@@ -9,32 +9,80 @@ describe('combo-box in dialog', () => {
   let dialog, comboBox;
 
   beforeEach(async () => {
-    dialog = fixtureSync(`
-      <vaadin-dialog modeless>
-        <template>
-          <vaadin-combo-box items="[1,2,3]"></vaadin-combo-box>
-        </template>
-      </vaadin-dialog>
-    `);
+    dialog = fixtureSync('<vaadin-dialog></vaadin-dialog>');
+    dialog.renderer = (root) => {
+      root.innerHTML = '<vaadin-combo-box></vaadin-combo-box>';
+    };
+    dialog.opened = true;
     dialog.opened = true;
     await nextFrame();
     comboBox = dialog.$.overlay.querySelector('vaadin-combo-box');
+    comboBox.items = ['foo', 'bar'];
+    comboBox.inputElement.focus();
   });
 
-  it('should not end up behind the dialog overlay on mousedown', async () => {
-    comboBox.open();
-    mousedown(comboBox);
-    await nextFrame();
-    expect(comboBox.$.dropdown.$.overlay._last).to.be.true;
+  describe('opened', () => {
+    beforeEach(async () => {
+      comboBox.open();
+      await nextFrame();
+    });
+
+    it('should not close the dialog when closing time-picker on input element Escape', async () => {
+      await sendKeys({ press: 'Escape' });
+
+      expect(comboBox.opened).to.be.false;
+      expect(dialog.opened).to.be.true;
+    });
+
+    it('should close the dialog on subsequent Escape after the time-picker is closed', async () => {
+      await sendKeys({ press: 'Escape' });
+
+      await sendKeys({ press: 'Escape' });
+
+      expect(dialog.opened).to.be.false;
+    });
   });
 
-  it('should not end up behind the dialog overlay on touchstart', async () => {
-    comboBox.open();
+  describe('clear button visible', () => {
+    beforeEach(() => {
+      comboBox.clearButtonVisible = true;
+      comboBox.value = 'foo';
+    });
 
-    const { left: x, top: y } = comboBox.getBoundingClientRect();
-    touchstart(comboBox, { x, y });
+    it('should not close the dialog when clearing on Escape with clear button visible', async () => {
+      await sendKeys({ press: 'Escape' });
 
-    await nextFrame();
-    expect(comboBox.$.dropdown.$.overlay._last).to.be.true;
+      expect(dialog.opened).to.be.true;
+    });
+
+    it('should close the dialog on subsequent Escape after the value is cleared', async () => {
+      await sendKeys({ press: 'Escape' });
+
+      await sendKeys({ press: 'Escape' });
+
+      expect(dialog.opened).to.be.false;
+    });
+  });
+
+  describe('modeless', () => {
+    beforeEach(async () => {
+      dialog.modeless = true;
+      comboBox.open();
+      await nextFrame();
+    });
+
+    it('should not end up behind the dialog overlay on mousedown', async () => {
+      mousedown(comboBox);
+      await nextFrame();
+      expect(comboBox.$.dropdown.$.overlay._last).to.be.true;
+    });
+
+    it('should not end up behind the dialog overlay on touchstart', async () => {
+      const { left: x, top: y } = comboBox.getBoundingClientRect();
+      touchstart(comboBox, { x, y });
+
+      await nextFrame();
+      expect(comboBox.$.dropdown.$.overlay._last).to.be.true;
+    });
   });
 });
