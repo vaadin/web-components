4edaab249227229bcf7733d098339c5f3e8004da
Sergey Vinogradov <mr.vursen@gmail.com>
2022-06-03 17:44:54 +0300

    add license header

--- packages/component-base/src/unique-id-utils.d.ts
+++ packages/component-base/src/unique-id-utils.d.ts
@@ -1,4 +1,10 @@
 /**
+ * @license
+ * Copyright (c) 2021 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
  * Resets the unique id counter.
  */
 export function resetUniqueId(): void;
