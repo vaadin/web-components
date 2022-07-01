86800e4b32cb8acd2b8c830dd6f9c5696ce790bc
Sergey Vinogradov <mr.vursen@gmail.com>
2022-07-01 12:23:08 +0300

    test: replace some unit tests with snapshot tests (#4118)

--- packages/custom-field/test/custom-field.test.js
+++ packages/custom-field/test/custom-field.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { aTimeout, fixtureSync, focusin, focusout } from '@vaadin/testing-helpers';
+import { fixtureSync, focusin, focusout } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import '../src/vaadin-custom-field.js';
 import { dispatchChange } from './common.js';
@@ -69,33 +69,7 @@ describe('custom field', () => {
     });
   });
 
-  describe('label', () => {
-    let labelElement;
-
-    beforeEach(() => {
-      labelElement = customField.querySelector('[slot="label"]');
-    });
-
-    it('should be undefined by default', () => {
-      expect(customField.label).to.be.undefined;
-    });
-
-    it('should be displayed when assigned', () => {
-      customField.label = 'Foo';
-      expect(labelElement.textContent).to.equal('Foo');
-    });
-
-    it('should set has-label attribute on host element when displayed', () => {
-      customField.label = 'Foo';
-      expect(customField.hasAttribute('has-label')).to.be.true;
-    });
-  });
-
   describe('aria-required', () => {
-    it('should not have aria-required attribute by default', () => {
-      expect(customField.hasAttribute('aria-required')).to.be.false;
-    });
-
     it('should toggle aria-required attribute on required property change', () => {
       customField.required = true;
       expect(customField.getAttribute('aria-required')).to.equal('true');
@@ -120,35 +94,6 @@ describe('custom field', () => {
       focusin(customField.inputs[0]);
       focusout(customField.inputs[0], customField.inputs[1]);
       expect(customField.hasAttribute('focused')).to.be.true;
-    });
-  });
-
-  describe('errorMessage', () => {
-    let error;
-
-    beforeEach(() => {
-      customField.invalid = true;
-      error = customField.querySelector('[slot="error-message"]');
-    });
-
-    it('should set error message text content when errorMessage is provided', () => {
-      customField.errorMessage = 'foo';
-      expect(error.textContent).to.equal('foo');
-    });
-
-    it('should set has-error-message attribute when errorMessage is provided', () => {
-      customField.errorMessage = 'foo';
-      expect(customField.hasAttribute('has-error-message')).to.be.true;
-    });
-
-    it('should not set has-error-message when errorMessage is set to empty string', () => {
-      customField.errorMessage = '';
-      expect(customField.hasAttribute('has-error-message')).to.be.false;
-    });
-
-    it('should not set has-error-message when errorMessage is set to null', () => {
-      customField.errorMessage = null;
-      expect(customField.hasAttribute('has-error-message')).to.be.false;
     });
   });
 
@@ -178,40 +123,6 @@ describe('custom field', () => {
       customField.inputs[0].value = '';
       dispatchChange(customField.inputs[0]);
       expect(customField.invalid).to.be.true;
-    });
-  });
-
-  describe('ARIA', () => {
-    let label, helper, error;
-
-    beforeEach(() => {
-      customField.helperText = 'Helper';
-      label = customField.querySelector('[slot="label"]');
-      helper = customField.querySelector('[slot="helper"]');
-      error = customField.querySelector('[slot="error-message"]');
-      customField.label = 'label';
-      customField.helperText = 'helper';
-      customField.errorMessage = 'error';
-    });
-
-    it('should set correct role attribute on the custom field', () => {
-      expect(customField.getAttribute('role')).to.equal('group');
-    });
-
-    it('should add label and helper text to aria-labelledby when valid', () => {
-      const aria = customField.getAttribute('aria-labelledby');
-      expect(aria).to.include(helper.id);
-      expect(aria).to.not.include(error.id);
-      expect(aria).to.include(label.id);
-    });
-
-    it('should add error message to aria-labelledby when field is invalid', async () => {
-      customField.invalid = true;
-      await aTimeout(0);
-      const aria = customField.getAttribute('aria-labelledby');
-      expect(aria).to.include(helper.id);
-      expect(aria).to.include(error.id);
-      expect(aria).to.include(label.id);
     });
   });
 
