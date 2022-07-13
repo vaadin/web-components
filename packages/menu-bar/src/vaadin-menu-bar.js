8569a89a115b7559309f959f3a76dfade714c384
Serhii Kulykov <iamkulykov@gmail.com>
2022-07-13 15:41:56 +0300

    docs: add missing JSDoc annotation for mixin (#4193)

--- packages/menu-bar/src/vaadin-menu-bar.js
+++ packages/menu-bar/src/vaadin-menu-bar.js
@@ -63,6 +63,7 @@ import { InteractionsMixin } from './vaadin-menu-bar-interactions-mixin.js';
  * @extends HTMLElement
  * @mixes ButtonsMixin
  * @mixes InteractionsMixin
+ * @mixes DisabledMixin
  * @mixes ElementMixin
  * @mixes ThemableMixin
  */
