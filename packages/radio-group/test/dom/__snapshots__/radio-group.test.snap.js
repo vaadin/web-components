9199d2f6a972fd63b230b681fc1d000513f2e3d2
Serhii Kulykov <iamkulykov@gmail.com>
2022-07-14 17:20:47 +0300

    test: add more snapshot tests for radio-group (#4202)

--- packages/radio-group/test/dom/__snapshots__/radio-group.test.snap.js
+++ packages/radio-group/test/dom/__snapshots__/radio-group.test.snap.js
@@ -58,6 +58,68 @@ snapshots["vaadin-radio-group host default"] =
 `;
 /* end snapshot vaadin-radio-group host default */
 
+snapshots["vaadin-radio-group host label"] = 
+`<vaadin-radio-group
+  aria-labelledby="label-vaadin-radio-group-0"
+  has-label=""
+  role="radiogroup"
+>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 1"
+    value="1"
+  >
+    <label
+      for="input-vaadin-radio-button-6"
+      id="label-vaadin-radio-button-3"
+      slot="label"
+    >
+      Radio 1
+    </label>
+    <input
+      id="input-vaadin-radio-button-6"
+      slot="input"
+      type="radio"
+      value="1"
+    >
+  </vaadin-radio-button>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 2"
+    value="2"
+  >
+    <label
+      for="input-vaadin-radio-button-7"
+      id="label-vaadin-radio-button-4"
+      slot="label"
+    >
+      Radio 2
+    </label>
+    <input
+      id="input-vaadin-radio-button-7"
+      slot="input"
+      type="radio"
+      value="2"
+    >
+  </vaadin-radio-button>
+  <label
+    id="label-vaadin-radio-group-0"
+    slot="label"
+  >
+    Label
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-radio-group-2"
+    slot="error-message"
+  >
+  </div>
+</vaadin-radio-group>
+`;
+/* end snapshot vaadin-radio-group host label */
+
 snapshots["vaadin-radio-group host disabled"] = 
 `<vaadin-radio-group
   aria-disabled="true"
@@ -126,6 +188,199 @@ snapshots["vaadin-radio-group host disabled"] =
 </vaadin-radio-group>
 `;
 /* end snapshot vaadin-radio-group host disabled */
+
+snapshots["vaadin-radio-group host required"] = 
+`<vaadin-radio-group
+  aria-required="true"
+  required=""
+  role="radiogroup"
+>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 1"
+    value="1"
+  >
+    <label
+      for="input-vaadin-radio-button-6"
+      id="label-vaadin-radio-button-3"
+      slot="label"
+    >
+      Radio 1
+    </label>
+    <input
+      id="input-vaadin-radio-button-6"
+      slot="input"
+      type="radio"
+      value="1"
+    >
+  </vaadin-radio-button>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 2"
+    value="2"
+  >
+    <label
+      for="input-vaadin-radio-button-7"
+      id="label-vaadin-radio-button-4"
+      slot="label"
+    >
+      Radio 2
+    </label>
+    <input
+      id="input-vaadin-radio-button-7"
+      slot="input"
+      type="radio"
+      value="2"
+    >
+  </vaadin-radio-button>
+  <label
+    id="label-vaadin-radio-group-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-radio-group-2"
+    slot="error-message"
+  >
+  </div>
+</vaadin-radio-group>
+`;
+/* end snapshot vaadin-radio-group host required */
+
+snapshots["vaadin-radio-group host helper"] = 
+`<vaadin-radio-group
+  aria-labelledby="helper-vaadin-radio-group-1"
+  has-helper=""
+  role="radiogroup"
+>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 1"
+    value="1"
+  >
+    <label
+      for="input-vaadin-radio-button-6"
+      id="label-vaadin-radio-button-3"
+      slot="label"
+    >
+      Radio 1
+    </label>
+    <input
+      id="input-vaadin-radio-button-6"
+      slot="input"
+      type="radio"
+      value="1"
+    >
+  </vaadin-radio-button>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 2"
+    value="2"
+  >
+    <label
+      for="input-vaadin-radio-button-7"
+      id="label-vaadin-radio-button-4"
+      slot="label"
+    >
+      Radio 2
+    </label>
+    <input
+      id="input-vaadin-radio-button-7"
+      slot="input"
+      type="radio"
+      value="2"
+    >
+  </vaadin-radio-button>
+  <label
+    id="label-vaadin-radio-group-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-radio-group-2"
+    slot="error-message"
+  >
+  </div>
+  <div
+    id="helper-vaadin-radio-group-1"
+    slot="helper"
+  >
+    Helper
+  </div>
+</vaadin-radio-group>
+`;
+/* end snapshot vaadin-radio-group host helper */
+
+snapshots["vaadin-radio-group host error"] = 
+`<vaadin-radio-group
+  aria-labelledby="error-message-vaadin-radio-group-2"
+  has-error-message=""
+  invalid=""
+  role="radiogroup"
+>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 1"
+    value="1"
+  >
+    <label
+      for="input-vaadin-radio-button-6"
+      id="label-vaadin-radio-button-3"
+      slot="label"
+    >
+      Radio 1
+    </label>
+    <input
+      id="input-vaadin-radio-button-6"
+      name="vaadin-radio-group-5"
+      slot="input"
+      type="radio"
+      value="1"
+    >
+  </vaadin-radio-button>
+  <vaadin-radio-button
+    has-label=""
+    has-value=""
+    label="Radio 2"
+    value="2"
+  >
+    <label
+      for="input-vaadin-radio-button-7"
+      id="label-vaadin-radio-button-4"
+      slot="label"
+    >
+      Radio 2
+    </label>
+    <input
+      id="input-vaadin-radio-button-7"
+      name="vaadin-radio-group-5"
+      slot="input"
+      type="radio"
+      value="2"
+    >
+  </vaadin-radio-button>
+  <label
+    id="label-vaadin-radio-group-0"
+    slot="label"
+  >
+  </label>
+  <div
+    id="error-message-vaadin-radio-group-2"
+    role="alert"
+    slot="error-message"
+  >
+    Error
+  </div>
+</vaadin-radio-group>
+`;
+/* end snapshot vaadin-radio-group host error */
 
 snapshots["vaadin-radio-group shadow default"] = 
 `<div class="vaadin-group-field-container">
--- packages/radio-group/test/dom/radio-group.test.js
+++ packages/radio-group/test/dom/radio-group.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync } from '@vaadin/testing-helpers';
+import { aTimeout, fixtureSync } from '@vaadin/testing-helpers';
 import '../../vaadin-radio-group.js';
 
 describe('vaadin-radio-group', () => {
@@ -19,8 +19,30 @@ describe('vaadin-radio-group', () => {
       await expect(group).dom.to.equalSnapshot();
     });
 
+    it('label', async () => {
+      group.label = 'Label';
+      await expect(group).dom.to.equalSnapshot();
+    });
+
     it('disabled', async () => {
       group.disabled = true;
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('required', async () => {
+      group.required = true;
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('helper', async () => {
+      group.helperText = 'Helper';
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('error', async () => {
+      group.errorMessage = 'Error';
+      group.invalid = true;
+      await aTimeout(0);
       await expect(group).dom.to.equalSnapshot();
     });
   });
