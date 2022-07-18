da1ff4946b51e985f76da2ce8993e9dfe5dbbbfa
Serhii Kulykov <iamkulykov@gmail.com>
2022-07-18 23:11:34 +0300

    fix: prevent mousedown event for toggle button (#4240)

--- packages/select/src/vaadin-select.js
+++ packages/select/src/vaadin-select.js
@@ -143,7 +143,7 @@ class Select extends DelegateFocusMixin(FieldMixin(SlotMixin(ElementMixin(Themab
         >
           <slot name="prefix" slot="prefix"></slot>
           <slot name="value"></slot>
-          <div part="toggle-button" slot="suffix" aria-hidden="true"></div>
+          <div part="toggle-button" slot="suffix" aria-hidden="true" on-mousedown="_onToggleMouseDown"></div>
         </vaadin-input-container>
 
         <div part="helper-text">
@@ -427,6 +427,13 @@ class Select extends DelegateFocusMixin(FieldMixin(SlotMixin(ElementMixin(Themab
     event.preventDefault();
 
     this.opened = !this.readonly;
+  }
+
+  /** @private */
+  _onToggleMouseDown(event) {
+    // Prevent mousedown event to avoid blur and preserve focused state
+    // while opening, and to restore focus-ring attribute on closing.
+    event.preventDefault();
   }
 
   /**
