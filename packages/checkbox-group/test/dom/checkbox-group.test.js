6da8556185a4fcabcf28bb01bc0053427b7d679d
Sergey Vinogradov <mr.vursen@gmail.com>
2022-07-24 09:20:14 +0300

    test: replace some unit tests with snapshot tests

--- packages/checkbox-group/test/dom/checkbox-group.test.js
+++ packages/checkbox-group/test/dom/checkbox-group.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync } from '@vaadin/testing-helpers';
+import { aTimeout, fixtureSync } from '@vaadin/testing-helpers';
 import '../../vaadin-checkbox-group.js';
 import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
 
@@ -21,8 +21,30 @@ describe('vaadin-checkbox-group', () => {
       await expect(group).dom.to.equalSnapshot();
     });
 
+    it('label', async () => {
+      group.label = 'Label';
+      await expect(group).dom.to.equalSnapshot();
+    });
+
     it('disabled', async () => {
       group.disabled = true;
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('required', async () => {
+      group.required = true;
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('helper', async () => {
+      group.helperText = 'Helper';
+      await expect(group).dom.to.equalSnapshot();
+    });
+
+    it('error', async () => {
+      group.errorMessage = 'Error';
+      group.invalid = true;
+      await aTimeout(0);
       await expect(group).dom.to.equalSnapshot();
     });
   });
