63910eeb3c28cf45112b3bf8ad54e90bcb675a18
web-padawan <iamkulykov@gmail.com>
2022-08-01 11:21:59 +0300

    feat: add feature flag to control multiSortPriority

---
+++ packages/grid/src/vaadin-grid-multi-sort-priority-append.js
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+window.Vaadin = window.Vaadin || {};
+window.Vaadin.featureFlags = window.Vaadin.featureFlags || {};
+
+/**
+ * Change the default behavior for `multiSortPriority` property.
+ * Import this file to use "append" instead of "prepend".
+ */
+window.Vaadin.featureFlags.multiSortPriorityAppend = true;
