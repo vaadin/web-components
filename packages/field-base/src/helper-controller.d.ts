3da13a6a39789859617058e101ecb9b46ef768d1
web-padawan <iamkulykov@gmail.com>
2022-08-09 14:30:29 +0300

    chore: update year in license headers to 2022

--- packages/field-base/src/helper-controller.d.ts
+++ packages/field-base/src/helper-controller.d.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2021 Vaadin Ltd.
+ * Copyright (c) 2021 - 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
--- packages/vaadin-charts/src/vaadin-chart.d.ts
+++ packages/vaadin-charts/src/vaadin-chart.d.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2015 - 2021 Vaadin Ltd
+ * Copyright (c) 2015 - 2022 Vaadin Ltd.
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
 import type { Chart } from '@vaadin/charts/src/vaadin-chart.js';
