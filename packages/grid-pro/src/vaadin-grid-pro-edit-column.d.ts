b00718d392fa2ef5e277fd33557b1d569006a22c
web-padawan <iamkulykov@gmail.com>
2022-08-09 13:47:37 +0300

    chore: align license header with other components

--- packages/grid-pro/src/vaadin-grid-pro-edit-column.d.ts
+++ packages/grid-pro/src/vaadin-grid-pro-edit-column.d.ts
@@ -1,8 +1,7 @@
 /**
  * @license
  * Copyright (c) 2018 - 2022 Vaadin Ltd.
- * This program is available under Commercial Vaadin Developer License 4.0 (CVDLv4).
- * See <a href="https://vaadin.com/license/cvdl-4.0">the website</a> for the complete license.
+ * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
 import type { GridBodyRenderer, GridDefaultItem, GridItemModel } from '@vaadin/grid/src/vaadin-grid.js';
 import { GridColumn } from '@vaadin/grid/src/vaadin-grid-column.js';
