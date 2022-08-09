6c19ada419897777f72cbce88201c185e4b6dd85
Serhii Kulykov <iamkulykov@gmail.com>
2022-08-09 15:03:00 +0300

    chore: fix Pro components types license headers (#4323)

--- packages/crud/src/vaadin-crud-edit-column.d.ts
+++ packages/crud/src/vaadin-crud-edit-column.d.ts
@@ -1,7 +1,7 @@
 /**
  * @license
  * Copyright (c) 2018 - 2022 Vaadin Ltd.
- * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
 import { GridColumn } from '@vaadin/grid/src/vaadin-grid-column.js';
 
