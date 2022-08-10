937de81ccbe340d0c22d4f1c39c3c48b6588ad42
Serhii Kulykov <iamkulykov@gmail.com>
2022-08-10 11:49:30 +0300

    chore: update year in license headers to 2022 (#4330)

--- packages/vaadin-charts/theme/lumo/vaadin-chart.js
+++ packages/vaadin-charts/theme/lumo/vaadin-chart.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2015 - 2021 Vaadin Ltd
+ * Copyright (c) 2015 - 2022 Vaadin Ltd.
  * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
  */
 import '@vaadin/charts/theme/lumo/vaadin-chart.js';
