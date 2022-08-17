d86b175a2260232acd3dd0f4fd8b7c7f497472d2
Sergey Vinogradov <mr.vursen@gmail.com>
2022-08-17 22:11:02 +0300

    add tests for initial validation

--- packages/multi-select-combo-box/test/validation.test.js
+++ packages/multi-select-combo-box/test/validation.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync } from '@vaadin/testing-helpers';
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import './not-animated-styles.js';
 import '../vaadin-multi-select-combo-box.js';
@@ -7,10 +7,43 @@ import '../vaadin-multi-select-combo-box.js';
 describe('validation', () => {
   let comboBox, validateSpy;
 
+  describe('initial', () => {
+    beforeEach(() => {
+      comboBox = document.createElement('vaadin-multi-select-combo-box');
+      comboBox.items = ['apple', 'banana'];
+      validateSpy = sinon.spy(comboBox, 'validate');
+    });
+
+    afterEach(() => {
+      comboBox.remove();
+    });
+
+    it('should not validate by default', async () => {
+      document.body.appendChild(comboBox);
+      await nextRender();
+      expect(validateSpy.called).to.be.false;
+    });
+
+    it('should not validate when the field has an initial value', async () => {
+      comboBox.selectedItems = ['apple'];
+      document.body.appendChild(comboBox);
+      await nextRender();
+      expect(validateSpy.called).to.be.false;
+    });
+
+    it('should not validate when the field has an initial value and invalid', async () => {
+      comboBox.selectedItems = ['apple'];
+      comboBox.invalid = true;
+      document.body.appendChild(comboBox);
+      await nextRender();
+      expect(validateSpy.called).to.be.false;
+    });
+  });
+
   describe('basic', () => {
     beforeEach(() => {
       comboBox = fixtureSync(`<vaadin-multi-select-combo-box></vaadin-multi-select-combo-box>`);
-      comboBox.items = ['apple', 'banana', 'lemon', 'orange'];
+      comboBox.items = ['apple', 'banana'];
       validateSpy = sinon.spy(comboBox, 'validate');
     });
 
@@ -25,7 +58,7 @@ describe('validation', () => {
   describe('required', () => {
     beforeEach(() => {
       comboBox = fixtureSync(`<vaadin-multi-select-combo-box required></vaadin-multi-select-combo-box>`);
-      comboBox.items = ['apple', 'banana', 'lemon', 'orange'];
+      comboBox.items = ['apple', 'banana'];
     });
 
     it('should fail validation when selectedItems is empty', () => {
