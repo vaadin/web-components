aac74d50aaaf115baafeb3e04014c2b960e90e8d
Tomi Virkki <tomivirkki@users.noreply.github.com>
2022-08-19 16:28:11 +0300

    Update packages/tabsheet/src/vaadin-tabsheet.js
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- packages/tabsheet/src/vaadin-tabsheet.js
+++ packages/tabsheet/src/vaadin-tabsheet.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import '@vaadin/tabs/vaadin-tab.js';
+import '@vaadin/tabs/src/vaadin-tab.js';
 import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { ControllerMixin } from '@vaadin/component-base/src/controller-mixin.js';
