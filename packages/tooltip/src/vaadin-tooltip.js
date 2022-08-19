1f243eecac3a5075a063eb4c346b02febf9f977b
Serhii Kulykov <iamkulykov@gmail.com>
2022-08-19 16:27:52 +0300

    feat: add overlay, for and target properties (#4369)

--- packages/tooltip/src/vaadin-tooltip.js
+++ packages/tooltip/src/vaadin-tooltip.js
@@ -3,16 +3,21 @@
  * Copyright (c) 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import './vaadin-tooltip-overlay.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { addValueToAttribute, removeValueFromAttribute } from '@vaadin/component-base/src/dom-utils.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
+import { generateUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
+import { ThemePropertyMixin } from '@vaadin/vaadin-themable-mixin/vaadin-theme-property-mixin.js';
 
 /**
  * `<vaadin-tooltip>` is a Web Component for creating tooltips.
  *
  * @extends HTMLElement
  * @mixes ElementMixin
+ * @mixes ThemePropertyMixin
  */
-class Tooltip extends ElementMixin(PolymerElement) {
+class Tooltip extends ThemePropertyMixin(ElementMixin(PolymerElement)) {
   static get is() {
     return 'vaadin-tooltip';
   }
@@ -24,7 +29,68 @@ class Tooltip extends ElementMixin(PolymerElement) {
           display: none;
         }
       </style>
+      <vaadin-tooltip-overlay
+        id="[[_uniqueId]]"
+        role="tooltip"
+        theme$="[[_theme]]"
+        position-target="[[target]]"
+        modeless
+      ></vaadin-tooltip-overlay>
     `;
+  }
+
+  static get properties() {
+    return {
+      /**
+       * The id of the element used as a tooltip trigger.
+       * The element should be in the DOM by the time when
+       * the attribute is set, otherwise a warning is shown.
+       */
+      for: {
+        type: String,
+        observer: '__forChanged',
+      },
+
+      /**
+       * Reference to the element used as a tooltip trigger.
+       * The target must be placed in the same shadow scope.
+       * Defaults to an element referenced with `for`.
+       */
+      target: {
+        type: Object,
+        observer: '__targetChanged',
+      },
+    };
+  }
+
+  constructor() {
+    super();
+
+    this._uniqueId = `vaadin-tooltip-${generateUniqueId()}`;
+  }
+
+  /** @private */
+  __forChanged(forId) {
+    if (forId) {
+      const target = this.getRootNode().getElementById(forId);
+
+      if (target) {
+        this.target = target;
+      } else {
+        console.warn(`No element with id="${forId}" found to show tooltip.`);
+      }
+    }
+  }
+
+  /** @private */
+  __targetChanged(target, oldTarget) {
+    if (oldTarget) {
+      removeValueFromAttribute(oldTarget, 'aria-describedby', this._uniqueId);
+    }
+
+    if (target) {
+      addValueToAttribute(target, 'aria-describedby', this._uniqueId);
+    }
   }
 }
 
