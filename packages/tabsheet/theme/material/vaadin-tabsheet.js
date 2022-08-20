79dbc142bfa2d21e973adff5af21a6e6f1cbe55f
Tomi Virkki <virkki@vaadin.com>
2022-08-20 08:42:47 +0300

    fix: import tabs with theme

--- packages/tabsheet/theme/material/vaadin-tabsheet.js
+++ packages/tabsheet/theme/material/vaadin-tabsheet.js
@@ -1,2 +1,4 @@
+import '@vaadin/tabs/theme/material/vaadin-tabs.js';
+import '@vaadin/tabs/theme/material/vaadin-tab.js';
 import './vaadin-tabsheet-styles.js';
 import '../../src/vaadin-tabsheet.js';
