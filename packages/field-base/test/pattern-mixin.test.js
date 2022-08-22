af96f9a698875bbb532ebbac72117194a883b72f
Sergey Vinogradov <mr.vursen@gmail.com>
2022-08-22 16:41:28 +0300

    test: move some validation tests to field-base (#4378)

--- packages/field-base/test/pattern-mixin.test.js
+++ packages/field-base/test/pattern-mixin.test.js
@@ -1,9 +1,10 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
+import { fire, fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
 import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
 import { InputController } from '../src/input-controller.js';
 import { PatternMixin } from '../src/pattern-mixin.js';
+import { TextAreaController } from '../src/text-area-controller.js';
 import { define } from './helpers.js';
 
 const runTests = (baseClass) => {
@@ -25,15 +26,39 @@ const runTests = (baseClass) => {
       },
   );
 
+  const textareaTag = define[baseClass](
+    'pattern-mixin-textarea',
+    '<slot name="textarea"></slot>',
+    (Base) =>
+      class extends PatternMixin(Base) {
+        ready() {
+          super.ready();
+
+          this.addController(
+            new TextAreaController(this, (textarea) => {
+              this._setInputElement(textarea);
+              this.stateTarget = textarea;
+            }),
+          );
+        }
+      },
+  );
+
   let element, input;
 
-  beforeEach(async () => {
-    element = fixtureSync(`<${tag}></${tag}>`);
-    await nextRender();
-    input = element.querySelector('[slot=input]');
-  });
+  function commitInputValue(value) {
+    input.value = value;
+    fire(input, 'input');
+    fire(input, 'change');
+  }
 
   describe('pattern', () => {
+    beforeEach(async () => {
+      element = fixtureSync(`<${tag}></${tag}>`);
+      await nextRender();
+      input = element.querySelector('[slot=input]');
+    });
+
     it('should propagate pattern property to the input', async () => {
       element.pattern = '[-+\\d]';
       await nextFrame();
@@ -67,14 +92,110 @@ const runTests = (baseClass) => {
       await nextFrame();
       expect(element.invalid).to.be.false;
     });
+
+    // https://github.com/web-platform-tests/wpt/blob/7b0ebaccc62b566a1965396e5be7bb2bc06f841f/html/semantics/forms/constraints/form-validation-validity-patternMismatch.html
+    it('should pass validation when pattern property is not set', async () => {
+      element.pattern = null;
+      await nextFrame();
+      commitInputValue('abc');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when value property is empty', async () => {
+      element.pattern = '[A-Z]+';
+      await nextFrame();
+      commitInputValue('');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when value property matches the pattern', async () => {
+      element.pattern = '[A-Z]{1}';
+      await nextFrame();
+      commitInputValue('A');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when unicode value property matches the pattern', async () => {
+      element.pattern = '[A-Z]+';
+      await nextFrame();
+      commitInputValue('\u0041\u0042\u0043');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should fail validation when value property mismatches the pattern', async () => {
+      element.pattern = '[a-z]{3,}';
+      await nextFrame();
+      commitInputValue('ABCD');
+      expect(element.checkValidity()).to.be.false;
+    });
+
+    it('should fail validation when value property mismatches the pattern, even if a subset matches', async () => {
+      element.pattern = '[A-Z]+';
+      await nextFrame();
+      commitInputValue('ABC123');
+      expect(element.checkValidity()).to.be.false;
+    });
+
+    it('should pass validation when pattern contains invalid regular expression', async () => {
+      element.pattern = '(abc';
+      await nextFrame();
+      commitInputValue('de');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when pattern tries to escape a group', async () => {
+      element.pattern = 'a)(b';
+      await nextFrame();
+      commitInputValue('de');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when pattern uses Unicode features', async () => {
+      element.pattern = 'a\u{10FFFF}';
+      await nextFrame();
+      commitInputValue('a\u{10FFFF}');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should pass validation when value matches JavaScript-specific regular expression', async () => {
+      element.pattern = '\\u1234\\cx[5-[]{2}';
+      await nextFrame();
+      commitInputValue('\u1234\x18[6');
+      expect(element.checkValidity()).to.be.true;
+    });
+
+    it('should fail validation when value mismatches JavaScript-specific regular expression', async () => {
+      element.pattern = '\\u1234\\cx[5-[]{2}';
+      await nextFrame();
+      commitInputValue('\u1234\x18[4');
+      expect(element.checkValidity()).to.be.false;
+    });
+  });
+
+  describe('pattern + textarea', () => {
+    beforeEach(async () => {
+      element = fixtureSync(`<${textareaTag}></${textareaTag}>`);
+      await nextRender();
+      input = element.querySelector('[slot=textarea]');
+    });
+
+    it('should pass validation when value property matches the pattern (multiline)', async () => {
+      element.pattern = '[A-Z\n]{3}';
+      await nextFrame();
+      commitInputValue('A\nJ');
+      expect(element.checkValidity()).to.be.true;
+    });
   });
 
   describe('prevent invalid input', () => {
     beforeEach(async () => {
+      element = fixtureSync(`<${tag}></${tag}>`);
+      await nextRender();
       sinon.stub(console, 'warn');
       element.preventInvalidInput = true;
       element.value = '1';
-      element.inputElement.focus();
+      input = element.querySelector('[slot=input]');
+      input.focus();
       await nextFrame();
     });
 
