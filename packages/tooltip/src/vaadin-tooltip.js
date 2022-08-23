5711ccc9520037bbfb5564b9edf87f849052d122
Serhii Kulykov <iamkulykov@gmail.com>
2022-08-23 14:48:32 +0300

    refactor: set no-vertical-overlap to tooltip overlay (#4384)

--- packages/tooltip/src/vaadin-tooltip.js
+++ packages/tooltip/src/vaadin-tooltip.js
@@ -37,6 +37,7 @@ class Tooltip extends ThemePropertyMixin(ElementMixin(PolymerElement)) {
         theme$="[[_theme]]"
         opened="[[_autoOpened]]"
         position-target="[[target]]"
+        no-vertical-overlap
         modeless
       ></vaadin-tooltip-overlay>
     `;
