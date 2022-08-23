66c5115001dcbd4d0adfd8a1a2e49dcac0e896e8
Serhii Kulykov <iamkulykov@gmail.com>
2022-08-23 10:53:02 +0300

    feat: add TooltipController allowing to set target (#4373)

---
+++ packages/tooltip/src/vaadin-tooltip-controller.js
@@ -0,0 +1,46 @@
+/**
+ * @license
+ * Copyright (c) 2022 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
+
+/**
+ * A controller that manages the slotted tooltip element.
+ */
+export class TooltipController extends SlotController {
+  constructor(host) {
+    // Do not provide slot factory to create tooltip lazily.
+    super(host, 'tooltip');
+
+    this.setTarget(host);
+
+    host.addEventListener('tooltip-target-changed', (e) => {
+      this.setTarget(e.detail.target);
+    });
+  }
+
+  /**
+   * Override to initialize the newly added custom tooltip.
+   *
+   * @param {Node} tooltipNode
+   * @protected
+   * @override
+   */
+  initCustomNode(tooltipNode) {
+    tooltipNode.target = this.target;
+  }
+
+  /**
+   * Set an HTML element to attach the tooltip to.
+   * @param {HTMLElement} target
+   */
+  setTarget(target) {
+    this.target = target;
+
+    const tooltipNode = this.node;
+    if (tooltipNode) {
+      tooltipNode.target = target;
+    }
+  }
+}
