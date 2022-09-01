6c47b5d1502404674300dcc68c63113406a2a8c4
Serhii Kulykov <iamkulykov@gmail.com>
2022-09-01 11:16:56 +0300

    refactor: move TooltipController to component-base (#4429)

--- packages/component-base/src/tooltip-controller.js
+++ packages/component-base/src/tooltip-controller.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
+import { SlotController } from './slot-controller.js';
 
 /**
  * A controller that manages the slotted tooltip element.
