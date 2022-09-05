ef549d91fa641500c9b7a9e2343cba2f8b33af65
Tomi Virkki <tomivirkki@users.noreply.github.com>
2022-09-05 11:22:29 +0300

    test: use a shared IT file for simple tooltip integrations (#4474)

---
+++ integration/tests/component-tooltip.test.js
@@ -0,0 +1,34 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync } from '@vaadin/testing-helpers';
+import '@vaadin/tooltip';
+import { Button } from '@vaadin/button';
+import { Checkbox } from '@vaadin/checkbox';
+import { Details } from '@vaadin/details';
+
+[
+  { tagName: Checkbox.is },
+  { tagName: Button.is },
+  { tagName: Details.is, targetSelector: '[part="summary"]', position: 'bottom-start' },
+].forEach(({ tagName, targetSelector, position }) => {
+  describe(`${tagName} with a slotted tooltip`, () => {
+    let element, tooltip;
+
+    beforeEach(() => {
+      element = fixtureSync(`
+        <${tagName}>
+          <vaadin-tooltip slot="tooltip" text="Tooltip text"></vaadin-tooltip>
+        </${tagName}>
+      `);
+      tooltip = element.querySelector('vaadin-tooltip');
+    });
+
+    it('should set tooltip target', () => {
+      const target = targetSelector ? element.shadowRoot.querySelector(targetSelector) : element;
+      expect(tooltip.target).to.equal(target);
+    });
+
+    it('should set tooltip position', () => {
+      expect(tooltip.position).to.equal(position || 'bottom');
+    });
+  });
+});
