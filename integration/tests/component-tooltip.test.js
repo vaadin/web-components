654fdf3fe2541f85f597d6aca3cc8198dbaa2c60
Tomi Virkki <tomivirkki@users.noreply.github.com>
2022-09-05 13:43:07 +0300

    feat: add tooltip support to vaadin-select (#4482)

--- integration/tests/component-tooltip.test.js
+++ integration/tests/component-tooltip.test.js
@@ -6,6 +6,7 @@ import { Checkbox } from '@vaadin/checkbox';
 import { Details } from '@vaadin/details';
 import { EmailField } from '@vaadin/email-field';
 import { PasswordField } from '@vaadin/password-field';
+import { Select } from '@vaadin/select';
 import { Tab } from '@vaadin/tabs/vaadin-tab.js';
 import { TextField } from '@vaadin/text-field';
 
@@ -15,6 +16,7 @@ import { TextField } from '@vaadin/text-field';
   { tagName: Details.is, targetSelector: '[part="summary"]', position: 'bottom-start' },
   { tagName: EmailField.is },
   { tagName: PasswordField.is },
+  { tagName: Select.is },
   { tagName: Tab.is },
   { tagName: TextField.is },
 ].forEach(({ tagName, targetSelector, position }) => {
