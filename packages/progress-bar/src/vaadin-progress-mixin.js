d43583f973d2e5ac5091c0f3d378180890a6fad7
Tomi Virkki <virkki@vaadin.com>
2022-09-05 15:30:12 +0300

    chore: cleanup

--- packages/progress-bar/src/vaadin-progress-mixin.js
+++ packages/progress-bar/src/vaadin-progress-mixin.js
@@ -1,10 +1,10 @@
-import { TooltipController } from '@vaadin/component-base/src/tooltip-controller.js';
-
 /**
  * @license
  * Copyright (c) 2017 - 2022 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+
+import { TooltipController } from '@vaadin/component-base/src/tooltip-controller.js';
 
 /**
  * @polymerMixin
