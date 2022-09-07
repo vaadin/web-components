7c582e8b8e9a0bff90ec59ff1a25326a0b028771
Serhii Kulykov <iamkulykov@gmail.com>
2022-09-07 13:16:23 +0300

    Update packages/menu-bar/src/vaadin-menu-bar-interactions-mixin.d.ts

--- packages/menu-bar/src/vaadin-menu-bar-interactions-mixin.d.ts
+++ packages/menu-bar/src/vaadin-menu-bar-interactions-mixin.d.ts
@@ -4,7 +4,7 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import type { Constructor } from '@open-wc/dedupe-mixin';
-import type { FocusMixinClass } from '@vaadin/component-base/src/focus-mixin';
+import type { FocusMixinClass } from '@vaadin/component-base/src/focus-mixin.js';
 
 export declare function InteractionsMixin<T extends Constructor<HTMLElement>>(
   base: T,
