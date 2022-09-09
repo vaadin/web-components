11dd0880c053b6a0e4724d3ef804e727fef21d25
web-padawan <iamkulykov@gmail.com>
2022-09-09 17:01:33 +0300

    refactor: use KeyboardDirectionMixin in menu-bar

--- packages/menu-bar/src/vaadin-menu-bar-interactions-mixin.d.ts
+++ packages/menu-bar/src/vaadin-menu-bar-interactions-mixin.d.ts
@@ -5,11 +5,16 @@
  */
 import type { Constructor } from '@open-wc/dedupe-mixin';
 import type { FocusMixinClass } from '@vaadin/component-base/src/focus-mixin.js';
+import type { KeyboardDirectionMixinClass } from '@vaadin/component-base/src/keyboard-direction-mixin.js';
 import type { KeyboardMixinClass } from '@vaadin/component-base/src/keyboard-mixin.js';
 
 export declare function InteractionsMixin<T extends Constructor<HTMLElement>>(
   base: T,
-): Constructor<FocusMixinClass> & Constructor<InteractionsMixinClass> & Constructor<KeyboardMixinClass> & T;
+): Constructor<FocusMixinClass> &
+  Constructor<InteractionsMixinClass> &
+  Constructor<KeyboardDirectionMixinClass> &
+  Constructor<KeyboardMixinClass> &
+  T;
 
 export declare class InteractionsMixinClass {
   /**
