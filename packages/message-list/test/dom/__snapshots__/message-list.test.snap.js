3fb32f9bfdb54d9173405c4e7fc37b26ee495ac0
web-padawan <iamkulykov@gmail.com>
2022-10-12 09:57:04 +0300

    test: update message-list snapshot tests

--- packages/message-list/test/dom/__snapshots__/message-list.test.snap.js
+++ packages/message-list/test/dom/__snapshots__/message-list.test.snap.js
@@ -2,35 +2,75 @@
 export const snapshots = {};
 
 snapshots["vaadin-message-list default"] = 
-`<div
-  part="list"
-  role="list"
+`<vaadin-message-list
+  aria-relevant="additions"
+  role="log"
 >
-  <slot>
-  </slot>
-</div>
+</vaadin-message-list>
 `;
 /* end snapshot vaadin-message-list default */
 
 snapshots["vaadin-message-list items"] = 
-`<div
-  part="list"
-  role="list"
+`<vaadin-message-list
+  aria-relevant="additions"
+  role="log"
 >
-  <slot>
-  </slot>
-</div>
+  <vaadin-message
+    role="listitem"
+    tabindex="0"
+  >
+    Hi folks!
+    <vaadin-avatar
+      abbr="JD"
+      aria-hidden="true"
+      name="Jane Doe"
+      role="button"
+      slot="avatar"
+      tabindex="-1"
+    >
+    </vaadin-avatar>
+  </vaadin-message>
+  <vaadin-message
+    role="listitem"
+    tabindex="-1"
+  >
+    Good morning!
+    <vaadin-avatar
+      abbr="LR"
+      aria-hidden="true"
+      name="Lina Roy"
+      role="button"
+      slot="avatar"
+      tabindex="-1"
+    >
+    </vaadin-avatar>
+  </vaadin-message>
+</vaadin-message-list>
 `;
 /* end snapshot vaadin-message-list items */
 
 snapshots["vaadin-message-list theme"] = 
-`<div
-  part="list"
-  role="list"
+`<vaadin-message-list
+  aria-relevant="additions"
+  role="log"
 >
-  <slot>
-  </slot>
-</div>
+  <vaadin-message
+    role="listitem"
+    tabindex="0"
+    theme="danger"
+  >
+    Partial service outage.
+    <vaadin-avatar
+      abbr="A"
+      aria-hidden="true"
+      name="Admin"
+      role="button"
+      slot="avatar"
+      tabindex="-1"
+    >
+    </vaadin-avatar>
+  </vaadin-message>
+</vaadin-message-list>
 `;
 /* end snapshot vaadin-message-list theme */
 
