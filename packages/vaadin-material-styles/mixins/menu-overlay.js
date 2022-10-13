7d140439e54de661da952967dfaa8c4c31c32e03
Serhii Kulykov <iamkulykov@gmail.com>
2022-10-13 14:33:55 +0300

    refactor: use relative import in menu-overlay mixin (#4740)

--- packages/vaadin-material-styles/mixins/menu-overlay.js
+++ packages/vaadin-material-styles/mixins/menu-overlay.js
@@ -4,9 +4,8 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import '../color.js';
-import './overlay.js';
-import { overlay } from '@vaadin/vaadin-material-styles/mixins/overlay.js';
 import { registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { overlay } from './overlay.js';
 
 const menuOverlay = overlay;
 
