6a375554fcac350c457ce0a66e7992e2f6d71745
Serhii Kulykov <iamkulykov@gmail.com>
2022-11-04 11:43:09 +0200

    fix: make context menu renderer arguments required (#4918)

--- packages/context-menu/test/typings/context-menu.types.ts
+++ packages/context-menu/test/typings/context-menu.types.ts
@@ -1,8 +1,11 @@
 import '../../vaadin-context-menu.js';
 import type {
+  ContextMenu,
   ContextMenuItem,
   ContextMenuItemSelectedEvent,
   ContextMenuOpenedChangedEvent,
+  ContextMenuRenderer,
+  ContextMenuRendererContext,
 } from '../../vaadin-context-menu.js';
 
 const menu = document.createElement('vaadin-context-menu');
@@ -18,3 +21,12 @@ menu.addEventListener('item-selected', (event) => {
   assertType<ContextMenuItemSelectedEvent>(event);
   assertType<ContextMenuItem>(event.detail.value);
 });
+
+const renderer: ContextMenuRenderer = (root, contextMenu, context) => {
+  assertType<HTMLElement>(root);
+  assertType<ContextMenu>(contextMenu);
+  assertType<ContextMenuRendererContext>(context);
+  assertType<HTMLElement>(context.target);
+};
+
+menu.renderer = renderer;
