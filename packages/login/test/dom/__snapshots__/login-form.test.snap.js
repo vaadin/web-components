051aeed95f559cc42be91eb9991eb43ac270d1d7
web-padawan <iamkulykov@gmail.com>
2022-11-07 11:33:23 +0200

    test: add login overlay and form snapshot tests

---
+++ packages/login/test/dom/__snapshots__/login-form.test.snap.js
@@ -0,0 +1,332 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-form-form host default"] = 
+`<vaadin-login-form>
+  <vaadin-login-form-wrapper>
+    <form
+      method="POST"
+      slot="form"
+    >
+      <input
+        id="csrf"
+        type="hidden"
+      >
+      <vaadin-text-field
+        autocapitalize="none"
+        autocorrect="off"
+        focused=""
+        has-label=""
+        id="vaadinLoginUsername"
+        name="username"
+        required=""
+        spellcheck="false"
+      >
+        <input
+          aria-labelledby="label-vaadin-text-field-0"
+          autocorrect="off"
+          id="input-vaadin-text-field-6"
+          name="username"
+          required=""
+          slot="input"
+          type="text"
+        >
+        <label
+          for="input-vaadin-text-field-6"
+          id="label-vaadin-text-field-0"
+          slot="label"
+        >
+          Username
+        </label>
+        <div
+          hidden=""
+          id="error-message-vaadin-text-field-2"
+          slot="error-message"
+        >
+        </div>
+      </vaadin-text-field>
+      <vaadin-password-field
+        autocomplete="current-password"
+        has-label=""
+        id="vaadinLoginPassword"
+        name="password"
+        required=""
+        spellcheck="false"
+      >
+        <input
+          aria-labelledby="label-vaadin-password-field-3"
+          autocapitalize="off"
+          autocomplete="current-password"
+          id="input-vaadin-password-field-7"
+          name="password"
+          required=""
+          slot="input"
+          type="password"
+        >
+        <label
+          for="input-vaadin-password-field-7"
+          id="label-vaadin-password-field-3"
+          slot="label"
+        >
+          Password
+        </label>
+        <div
+          hidden=""
+          id="error-message-vaadin-password-field-5"
+          slot="error-message"
+        >
+        </div>
+        <vaadin-password-field-button
+          aria-label="Show password"
+          aria-pressed="false"
+          role="button"
+          slot="reveal"
+          tabindex="0"
+        >
+        </vaadin-password-field-button>
+      </vaadin-password-field>
+      <vaadin-button
+        role="button"
+        tabindex="0"
+        theme="primary contained submit"
+      >
+        Log in
+      </vaadin-button>
+    </form>
+  </vaadin-login-form-wrapper>
+</vaadin-login-form>
+`;
+/* end snapshot vaadin-form-form host default */
+
+snapshots["vaadin-form-form host i18n"] = 
+`<vaadin-login-form>
+  <vaadin-login-form-wrapper>
+    <form
+      method="POST"
+      slot="form"
+    >
+      <input
+        id="csrf"
+        type="hidden"
+      >
+      <vaadin-text-field
+        autocapitalize="none"
+        autocorrect="off"
+        focused=""
+        has-label=""
+        id="vaadinLoginUsername"
+        name="username"
+        required=""
+        spellcheck="false"
+      >
+        <input
+          aria-labelledby="label-vaadin-text-field-0"
+          autocorrect="off"
+          id="input-vaadin-text-field-6"
+          name="username"
+          required=""
+          slot="input"
+          type="text"
+        >
+        <label
+          for="input-vaadin-text-field-6"
+          id="label-vaadin-text-field-0"
+          slot="label"
+        >
+          Käyttäjänimi
+        </label>
+        <div
+          hidden=""
+          id="error-message-vaadin-text-field-2"
+          slot="error-message"
+        >
+        </div>
+      </vaadin-text-field>
+      <vaadin-password-field
+        autocomplete="current-password"
+        has-label=""
+        id="vaadinLoginPassword"
+        name="password"
+        required=""
+        spellcheck="false"
+      >
+        <input
+          aria-labelledby="label-vaadin-password-field-3"
+          autocapitalize="off"
+          autocomplete="current-password"
+          id="input-vaadin-password-field-7"
+          name="password"
+          required=""
+          slot="input"
+          type="password"
+        >
+        <label
+          for="input-vaadin-password-field-7"
+          id="label-vaadin-password-field-3"
+          slot="label"
+        >
+          Salasana
+        </label>
+        <div
+          hidden=""
+          id="error-message-vaadin-password-field-5"
+          slot="error-message"
+        >
+        </div>
+        <vaadin-password-field-button
+          aria-label="Show password"
+          aria-pressed="false"
+          role="button"
+          slot="reveal"
+          tabindex="0"
+        >
+        </vaadin-password-field-button>
+      </vaadin-password-field>
+      <vaadin-button
+        role="button"
+        tabindex="0"
+        theme="primary contained submit"
+      >
+        Kirjaudu sisään
+      </vaadin-button>
+    </form>
+  </vaadin-login-form-wrapper>
+</vaadin-login-form>
+`;
+/* end snapshot vaadin-form-form host i18n */
+
+snapshots["vaadin-form-form shadow default"] = 
+`<section part="form">
+  <h2 part="form-title">
+    Log in
+  </h2>
+  <div
+    hidden=""
+    part="error-message"
+  >
+    <h5 part="error-message-title">
+      Incorrect username or password
+    </h5>
+    <p part="error-message-description">
+      Check that you have entered the correct username and password and try again.
+    </p>
+  </div>
+  <slot name="form">
+  </slot>
+  <vaadin-button
+    id="forgotPasswordButton"
+    role="button"
+    tabindex="0"
+    theme="tertiary small forgot-password"
+  >
+    Forgot password
+  </vaadin-button>
+  <div part="footer">
+    <p>
+    </p>
+  </div>
+</section>
+`;
+/* end snapshot vaadin-form-form shadow default */
+
+snapshots["vaadin-form-form shadow error"] = 
+`<section part="form">
+  <h2 part="form-title">
+    Log in
+  </h2>
+  <div part="error-message">
+    <h5 part="error-message-title">
+      Incorrect username or password
+    </h5>
+    <p part="error-message-description">
+      Check that you have entered the correct username and password and try again.
+    </p>
+  </div>
+  <slot name="form">
+  </slot>
+  <vaadin-button
+    id="forgotPasswordButton"
+    role="button"
+    tabindex="0"
+    theme="tertiary small forgot-password"
+  >
+    Forgot password
+  </vaadin-button>
+  <div part="footer">
+    <p>
+    </p>
+  </div>
+</section>
+`;
+/* end snapshot vaadin-form-form shadow error */
+
+snapshots["vaadin-form-form shadow noForgotPassword"] = 
+`<section part="form">
+  <h2 part="form-title">
+    Log in
+  </h2>
+  <div
+    hidden=""
+    part="error-message"
+  >
+    <h5 part="error-message-title">
+      Incorrect username or password
+    </h5>
+    <p part="error-message-description">
+      Check that you have entered the correct username and password and try again.
+    </p>
+  </div>
+  <slot name="form">
+  </slot>
+  <vaadin-button
+    hidden=""
+    id="forgotPasswordButton"
+    role="button"
+    tabindex="0"
+    theme="tertiary small forgot-password"
+  >
+    Forgot password
+  </vaadin-button>
+  <div part="footer">
+    <p>
+    </p>
+  </div>
+</section>
+`;
+/* end snapshot vaadin-form-form shadow noForgotPassword */
+
+snapshots["vaadin-form-form shadow i18n"] = 
+`<section part="form">
+  <h2 part="form-title">
+    Kirjaudu sisään
+  </h2>
+  <div
+    hidden=""
+    part="error-message"
+  >
+    <h5 part="error-message-title">
+      Väärä käyttäjätunnus tai salasana
+    </h5>
+    <p part="error-message-description">
+      Tarkista että käyttäjätunnus ja salasana ovat oikein ja yritä uudestaan.
+    </p>
+  </div>
+  <slot name="form">
+  </slot>
+  <vaadin-button
+    id="forgotPasswordButton"
+    role="button"
+    tabindex="0"
+    theme="tertiary small forgot-password"
+  >
+    Unohtuiko salasana?
+  </vaadin-button>
+  <div part="footer">
+    <p>
+      Jos tarvitset lisätietoja käyttäjälle.
+    </p>
+  </div>
+</section>
+`;
+/* end snapshot vaadin-form-form shadow i18n */
+
---
+++ packages/login/test/dom/login-overlay.test.js
@@ -0,0 +1,56 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync, nextFrame } from '@vaadin/testing-helpers';
+import '../../vaadin-login-overlay.js';
+import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
+
+describe('vaadin-form-form', () => {
+  let overlay, wrapper;
+
+  const I18N_FINNISH = {
+    header: {
+      title: 'Sovelluksen nimi',
+      description: 'Sovelluksen kuvaus',
+    },
+    form: {
+      title: 'Kirjaudu sisään',
+      username: 'Käyttäjänimi',
+      password: 'Salasana',
+      submit: 'Kirjaudu sisään',
+      forgotPassword: 'Unohtuiko salasana?',
+    },
+    errorMessage: {
+      title: 'Väärä käyttäjätunnus tai salasana',
+      message: 'Tarkista että käyttäjätunnus ja salasana ovat oikein ja yritä uudestaan.',
+    },
+    additionalInformation: 'Jos tarvitset lisätietoja käyttäjälle.',
+  };
+
+  beforeEach(async () => {
+    resetUniqueId();
+    overlay = fixtureSync('<vaadin-login-overlay opened></vaadin-login-overlay>');
+    await nextFrame();
+    wrapper = document.querySelector('vaadin-login-overlay-wrapper');
+  });
+
+  describe('host', () => {
+    it('default', async () => {
+      await expect(wrapper).dom.to.equalSnapshot();
+    });
+
+    it('i18n', async () => {
+      overlay.i18n = I18N_FINNISH;
+      await expect(wrapper).dom.to.equalSnapshot();
+    });
+  });
+
+  describe('shadow', () => {
+    it('default', async () => {
+      await expect(wrapper).shadowDom.to.equalSnapshot();
+    });
+
+    it('i18n', async () => {
+      overlay.i18n = I18N_FINNISH;
+      await expect(wrapper).shadowDom.to.equalSnapshot();
+    });
+  });
+});
