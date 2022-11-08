c09272644a5541f528072106bbb9a06e2f68e23c
Serhii Kulykov <iamkulykov@gmail.com>
2022-11-08 09:33:15 +0200

    refactor: do not use unnecessary mixins in form wrapper (#4945)

--- packages/login/src/vaadin-login-form-wrapper.js
+++ packages/login/src/vaadin-login-form-wrapper.js
@@ -4,20 +4,16 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import { LoginMixin } from './vaadin-login-mixin.js';
 
 /**
  * An element used internally by `<vaadin-login-form>`. Not intended to be used separately.
  *
  * @extends HTMLElement
- * @mixes ElementMixin
  * @mixes ThemableMixin
- * @mixes LoginMixin
  * @private
  */
-class LoginFormWrapper extends LoginMixin(ElementMixin(ThemableMixin(PolymerElement))) {
+class LoginFormWrapper extends ThemableMixin(PolymerElement) {
   static get template() {
     return html`
       <style>
@@ -65,6 +61,28 @@ class LoginFormWrapper extends LoginMixin(ElementMixin(ThemableMixin(PolymerElem
 
   static get is() {
     return 'vaadin-login-form-wrapper';
+  }
+
+  static get properties() {
+    return {
+      /**
+       * If set, the error message is shown. The message is hidden by default.
+       * When set, it changes the disabled state of the submit button.
+       * @type {boolean}
+       */
+      error: {
+        type: Boolean,
+        value: false,
+        reflectToAttribute: true,
+      },
+
+      /**
+       * The object used to localize this component.
+       */
+      i18n: {
+        type: Object,
+      },
+    };
   }
 }
 
