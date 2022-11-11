adba635d22cd345c83e4850c8689dcee52d2278c
web-padawan <iamkulykov@gmail.com>
2022-11-11 14:07:06 +0200

    refactor: remove usage of updateStyles from grid

--- packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
+++ packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
@@ -160,7 +160,8 @@ export const DragAndDropMixin = (superClass) =>
 
         requestAnimationFrame(() => {
           row.removeAttribute('dragstart');
-          this.updateStyles({ '--_grid-drag-start-x': '', '--_grid-drag-start-y': '' });
+          this.style.setProperty('--_grid-drag-start-x', '');
+          this.style.setProperty('--_grid-drag-start-y', '');
         });
 
         const event = new CustomEvent('grid-dragstart', {
