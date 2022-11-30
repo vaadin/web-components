2204714d7c1e98201eca1d5c609cddf88d71a1b0
Tomi Virkki <tomivirkki@users.noreply.github.com>
2022-11-30 12:58:51 +0200

    fix: ignore sorter click if event is canceled (#5089)

--- packages/grid/test/sorting.test.js
+++ packages/grid/test/sorting.test.js
@@ -18,7 +18,7 @@ import {
 
 describe('sorting', () => {
   describe('sorter', () => {
-    let sorter, button, orderIndicator;
+    let sorter, title, button, orderIndicator;
 
     beforeEach(() => {
       sorter = fixtureSync(`
@@ -27,6 +27,7 @@ describe('sorting', () => {
         </vaadin-grid-sorter>
       `);
       button = sorter.querySelector('button');
+      title = sorter.querySelector('.title');
       orderIndicator = sorter.shadowRoot.querySelector('[part="order"]');
     });
 
@@ -46,6 +47,12 @@ describe('sorting', () => {
     it('should not toggle on focusable click', () => {
       button.focus();
       click(button);
+      expect(sorter.direction).to.equal(null);
+    });
+
+    it('should not toggle if click event is already consumed', () => {
+      title.addEventListener('click', (e) => e.preventDefault());
+      click(title);
       expect(sorter.direction).to.equal(null);
     });
 
