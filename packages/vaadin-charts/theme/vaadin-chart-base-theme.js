532a783007149d8857ca9d1ba26ecbd6a31ba5dc
web-padawan <iamkulykov@gmail.com>
2022-11-30 10:55:40 +0200

    chore: update license for vaadin- prefixed packages

--- packages/vaadin-charts/theme/vaadin-chart-base-theme.js
+++ packages/vaadin-charts/theme/vaadin-chart-base-theme.js
@@ -1,6 +1,11 @@
 /**
  * @license
- * Copyright (c) 2015 - 2022 Vaadin Ltd.
- * This program is available under Commercial Vaadin Developer License 4.0, available at https://vaadin.com/license/cvdl-4.0.
+ * Copyright (c) 2000 - 2022 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
  */
 import '@vaadin/charts/theme/vaadin-chart-base-theme.js';
--- packages/vaadin-grid-pro/src/vaadin-grid-pro.d.ts
+++ packages/vaadin-grid-pro/src/vaadin-grid-pro.d.ts
@@ -1,8 +1,12 @@
 /**
  * @license
- * Copyright (c) 2019 - 2022 Vaadin Ltd.
- * This program is available under Commercial Vaadin Developer License 4.0 (CVDLv4).
- * See <a href="https://vaadin.com/license/cvdl-4.0">the website</a> for the complete license.
+ * Copyright (c) 2000 - 2022 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
  */
 import type { GridDefaultItem } from '@vaadin/grid/src/vaadin-grid.js';
 import type { GridPro } from '@vaadin/grid-pro/src/vaadin-grid-pro.js';
