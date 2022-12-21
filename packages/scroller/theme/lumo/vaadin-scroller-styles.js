5cadbb81bc8df2541d0de8c709406dfff6538c39
Jouni Koivuviita <jouni@vaadin.com>
2022-12-21 14:43:18 +0200

    feat: add Scroller `overflow-indicators` variant

--- packages/scroller/theme/lumo/vaadin-scroller-styles.js
+++ packages/scroller/theme/lumo/vaadin-scroller-styles.js
@@ -9,6 +9,34 @@ const scroller = css`
   :host([focus-ring]) {
     box-shadow: 0 0 0 2px var(--lumo-primary-color-50pct);
   }
+
+  /* Show dividers when content overflows */
+
+  :host([theme~='overflow-indicators'])::before,
+  :host([theme~='overflow-indicators'])::after {
+    content: '';
+    display: none;
+    position: sticky;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 9999;
+    height: 1px;
+    margin-bottom: -1px;
+    background: var(--lumo-contrast-10pct);
+  }
+
+  :host([theme~='overflow-indicators'])::after {
+    top: auto;
+    bottom: 0;
+    margin-bottom: 0;
+    margin-top: -1px;
+  }
+
+  :host([theme~='overflow-indicators'][overflow~='top'])::before,
+  :host([theme~='overflow-indicators'][overflow~='bottom'])::after {
+    display: block;
+  }
 `;
 
 registerStyles('vaadin-scroller', scroller, { moduleId: 'lumo-scroller' });
