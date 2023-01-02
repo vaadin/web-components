ed28a0f1f3b2a40ed26c25ffbfe915a1487b9cc3
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-02 09:38:04 +0200

    chore: update license headers copyright year to 2023 (#5199)

--- packages/map/src/vaadin-map.js
+++ packages/map/src/vaadin-map.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2000 - 2022 Vaadin Ltd.
+ * Copyright (c) 2000 - 2023 Vaadin Ltd.
  *
  * This program is available under Vaadin Commercial License and Service Terms.
  *
--- packages/select/src/vaadin-select.d.ts
+++ packages/select/src/vaadin-select.d.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2017 - 2022 Vaadin Ltd.
+ * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { DelegateFocusMixin } from '@vaadin/component-base/src/delegate-focus-mixin.js';
