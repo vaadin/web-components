4afed96ab31564ff26ce10308da0e034fb1eadee
web-padawan <iamkulykov@gmail.com>
2023-01-02 15:29:51 +0200

    docs: add private JSDoc annotation to theme classes

--- packages/vaadin-lumo-styles/version.js
+++ packages/vaadin-lumo-styles/version.js
@@ -3,6 +3,13 @@
  * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+
+/**
+ * Dummy custom element used for collecting
+ * development time usage statistics.
+ *
+ * @private
+ */
 class Lumo extends HTMLElement {
   static get version() {
     return '24.0.0-alpha8';
