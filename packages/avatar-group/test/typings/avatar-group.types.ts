03d5a91926c25ccee6834496a4e85d179eafb084
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-06 13:40:24 +0200

    test: add missing typings tests to vaadin-avatar-group (#5243)

---
+++ packages/avatar-group/test/typings/avatar-group.types.ts
@@ -0,0 +1,21 @@
+import '../../vaadin-avatar-group.js';
+import type { ControllerMixinClass } from '@vaadin/component-base/src/controller-mixin.js';
+import type { ElementMixinClass } from '@vaadin/component-base/src/element-mixin.js';
+import type { ResizeMixinClass } from '@vaadin/component-base/src/resize-mixin.js';
+import type { ThemableMixinClass } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import type { AvatarGroupI18n, AvatarGroupItem } from '../../vaadin-avatar-group.js';
+
+const assertType = <TExpected>(value: TExpected) => value;
+
+const group = document.createElement('vaadin-avatar-group');
+
+// Properties
+assertType<AvatarGroupItem[] | undefined>(group.items);
+assertType<number | null | undefined>(group.maxItemsVisible);
+assertType<AvatarGroupI18n>(group.i18n);
+
+// Mixins
+assertType<ControllerMixinClass>(group);
+assertType<ElementMixinClass>(group);
+assertType<ResizeMixinClass>(group);
+assertType<ThemableMixinClass>(group);
