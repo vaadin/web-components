10447048c996966ad55f00ebbf978cedce4f6ef2
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-06 13:41:54 +0200

    test: add missing avatar-group overlay snapshot tests (#5244)

--- packages/avatar-group/test/dom/avatar-group.test.js
+++ packages/avatar-group/test/dom/avatar-group.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync, nextFrame } from '@vaadin/testing-helpers';
+import { fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
 import '../../src/vaadin-avatar-group.js';
 
 describe('vaadin-avatar-group', () => {
@@ -24,5 +24,29 @@ describe('vaadin-avatar-group', () => {
     await nextFrame();
     group.setAttribute('theme', 'small');
     await expect(group).shadowDom.to.equalSnapshot();
+  });
+
+  describe('opened', () => {
+    const SNAPSHOT_CONFIG = {
+      // Some inline CSS styles related to the overlay's position
+      // may slightly change depending on the environment, so ignore them.
+      ignoreAttributes: ['style'],
+    };
+
+    beforeEach(async () => {
+      group.maxItemsVisible = 3;
+      group.items = [{ name: 'Abc Def' }, { name: 'Ghi Jkl' }, { name: 'Mno Pqr' }, { name: 'Stu Vwx' }];
+      await nextRender();
+      group._overflow.click();
+      await nextRender();
+    });
+
+    it('default', async () => {
+      await expect(group).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
+    });
+
+    it('overlay', async () => {
+      await expect(group.$.overlay).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
+    });
   });
 });
