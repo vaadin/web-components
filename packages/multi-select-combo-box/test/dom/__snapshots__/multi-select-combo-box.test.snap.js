231f0aa26d25e9490f06ae02d6036d8132cbc3d8
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-11 11:36:38 +0200

    test: improve vaadin-multi-select-combo-box snapshots (#5282)

--- packages/multi-select-combo-box/test/dom/__snapshots__/multi-select-combo-box.test.snap.js
+++ packages/multi-select-combo-box/test/dom/__snapshots__/multi-select-combo-box.test.snap.js
@@ -37,6 +37,353 @@ snapshots["vaadin-multi-select-combo-box host default"] =
 `;
 /* end snapshot vaadin-multi-select-combo-box host default */
 
+snapshots["vaadin-multi-select-combo-box host label"] = 
+`<vaadin-multi-select-combo-box
+  has-label=""
+  placeholder=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+    Label
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-expanded="false"
+    aria-labelledby="label-vaadin-multi-select-combo-box-0"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host label */
+
+snapshots["vaadin-multi-select-combo-box host helper"] = 
+`<vaadin-multi-select-combo-box
+  has-helper=""
+  placeholder=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-describedby="helper-vaadin-multi-select-combo-box-1"
+    aria-expanded="false"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+  <div
+    id="helper-vaadin-multi-select-combo-box-1"
+    slot="helper"
+  >
+    Helper
+  </div>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host helper */
+
+snapshots["vaadin-multi-select-combo-box host error"] = 
+`<vaadin-multi-select-combo-box
+  has-error-message=""
+  invalid=""
+  placeholder=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    id="error-message-vaadin-multi-select-combo-box-2"
+    role="alert"
+    slot="error-message"
+  >
+    Error
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-describedby="error-message-vaadin-multi-select-combo-box-2"
+    aria-expanded="false"
+    aria-invalid="true"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    invalid=""
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host error */
+
+snapshots["vaadin-multi-select-combo-box host required"] = 
+`<vaadin-multi-select-combo-box
+  placeholder=""
+  required=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-expanded="false"
+    aria-required="true"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host required */
+
+snapshots["vaadin-multi-select-combo-box host disabled"] = 
+`<vaadin-multi-select-combo-box
+  aria-disabled="true"
+  disabled=""
+  placeholder=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-expanded="false"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    disabled=""
+    id="input-vaadin-multi-select-combo-box-4"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+    tabindex="-1"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    disabled=""
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host disabled */
+
+snapshots["vaadin-multi-select-combo-box host readonly"] = 
+`<vaadin-multi-select-combo-box
+  placeholder=""
+  readonly=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-expanded="false"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    readonly=""
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    readonly=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host readonly */
+
+snapshots["vaadin-multi-select-combo-box host placeholder"] = 
+`<vaadin-multi-select-combo-box placeholder="Placeholder">
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-expanded="false"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    placeholder="Placeholder"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host placeholder */
+
+snapshots["vaadin-multi-select-combo-box host opened default"] = 
+`<vaadin-multi-select-combo-box
+  opened=""
+  placeholder=""
+>
+  <label
+    for="input-vaadin-multi-select-combo-box-4"
+    id="label-vaadin-multi-select-combo-box-0"
+    slot="label"
+  >
+  </label>
+  <div
+    hidden=""
+    id="error-message-vaadin-multi-select-combo-box-2"
+    slot="error-message"
+  >
+  </div>
+  <input
+    aria-autocomplete="list"
+    aria-controls="vaadin-multi-select-combo-box-scroller-3"
+    aria-expanded="true"
+    autocapitalize="off"
+    autocomplete="off"
+    autocorrect="off"
+    id="input-vaadin-multi-select-combo-box-4"
+    role="combobox"
+    slot="input"
+    spellcheck="false"
+  >
+  <vaadin-multi-select-combo-box-chip
+    count="0"
+    hidden=""
+    slot="overflow"
+    title=""
+  >
+  </vaadin-multi-select-combo-box-chip>
+</vaadin-multi-select-combo-box>
+`;
+/* end snapshot vaadin-multi-select-combo-box host opened default */
+
+snapshots["vaadin-multi-select-combo-box host opened overlay"] = 
+`<vaadin-multi-select-combo-box-overlay
+  id="overlay"
+  no-vertical-overlap=""
+>
+  <vaadin-multi-select-combo-box-scroller
+    aria-multiselectable="true"
+    id="vaadin-multi-select-combo-box-scroller-3"
+    role="listbox"
+  >
+  </vaadin-multi-select-combo-box-scroller>
+</vaadin-multi-select-combo-box-overlay>
+`;
+/* end snapshot vaadin-multi-select-combo-box host opened overlay */
+
 snapshots["vaadin-multi-select-combo-box shadow default"] = 
 `<div class="vaadin-multi-select-combo-box-container">
   <div part="label">
@@ -95,4 +442,197 @@ snapshots["vaadin-multi-select-combo-box shadow default"] =
 </slot>
 `;
 /* end snapshot vaadin-multi-select-combo-box shadow default */
+
+snapshots["vaadin-multi-select-combo-box shadow disabled"] = 
+`<div class="vaadin-multi-select-combo-box-container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="required-indicator"
+    >
+    </span>
+  </div>
+  <vaadin-multi-select-combo-box-internal
+    aria-disabled="true"
+    disabled=""
+    id="comboBox"
+  >
+    <vaadin-multi-select-combo-box-container
+      disabled=""
+      part="input-field"
+    >
+      <slot
+        name="overflow"
+        slot="prefix"
+      >
+      </slot>
+      <div
+        id="chips"
+        part="chips"
+        slot="prefix"
+      >
+        <slot name="chip">
+        </slot>
+      </div>
+      <slot name="input">
+      </slot>
+      <div
+        aria-hidden="true"
+        id="clearButton"
+        part="clear-button"
+        slot="suffix"
+      >
+      </div>
+      <div
+        aria-hidden="true"
+        class="toggle-button"
+        id="toggleButton"
+        part="toggle-button"
+        slot="suffix"
+      >
+      </div>
+    </vaadin-multi-select-combo-box-container>
+  </vaadin-multi-select-combo-box-internal>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+<slot name="tooltip">
+</slot>
+`;
+/* end snapshot vaadin-multi-select-combo-box shadow disabled */
+
+snapshots["vaadin-multi-select-combo-box shadow readonly"] = 
+`<div class="vaadin-multi-select-combo-box-container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="required-indicator"
+    >
+    </span>
+  </div>
+  <vaadin-multi-select-combo-box-internal
+    id="comboBox"
+    readonly=""
+  >
+    <vaadin-multi-select-combo-box-container
+      part="input-field"
+      readonly=""
+    >
+      <slot
+        name="overflow"
+        slot="prefix"
+      >
+      </slot>
+      <div
+        id="chips"
+        part="chips"
+        slot="prefix"
+      >
+        <slot name="chip">
+        </slot>
+      </div>
+      <slot name="input">
+      </slot>
+      <div
+        aria-hidden="true"
+        id="clearButton"
+        part="clear-button"
+        slot="suffix"
+      >
+      </div>
+      <div
+        aria-hidden="true"
+        class="toggle-button"
+        id="toggleButton"
+        part="toggle-button"
+        slot="suffix"
+      >
+      </div>
+    </vaadin-multi-select-combo-box-container>
+  </vaadin-multi-select-combo-box-internal>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+<slot name="tooltip">
+</slot>
+`;
+/* end snapshot vaadin-multi-select-combo-box shadow readonly */
+
+snapshots["vaadin-multi-select-combo-box shadow invalid"] = 
+`<div class="vaadin-multi-select-combo-box-container">
+  <div part="label">
+    <slot name="label">
+    </slot>
+    <span
+      aria-hidden="true"
+      part="required-indicator"
+    >
+    </span>
+  </div>
+  <vaadin-multi-select-combo-box-internal id="comboBox">
+    <vaadin-multi-select-combo-box-container
+      invalid=""
+      part="input-field"
+    >
+      <slot
+        name="overflow"
+        slot="prefix"
+      >
+      </slot>
+      <div
+        id="chips"
+        part="chips"
+        slot="prefix"
+      >
+        <slot name="chip">
+        </slot>
+      </div>
+      <slot name="input">
+      </slot>
+      <div
+        aria-hidden="true"
+        id="clearButton"
+        part="clear-button"
+        slot="suffix"
+      >
+      </div>
+      <div
+        aria-hidden="true"
+        class="toggle-button"
+        id="toggleButton"
+        part="toggle-button"
+        slot="suffix"
+      >
+      </div>
+    </vaadin-multi-select-combo-box-container>
+  </vaadin-multi-select-combo-box-internal>
+  <div part="helper-text">
+    <slot name="helper">
+    </slot>
+  </div>
+  <div part="error-message">
+    <slot name="error-message">
+    </slot>
+  </div>
+</div>
+<slot name="tooltip">
+</slot>
+`;
+/* end snapshot vaadin-multi-select-combo-box shadow invalid */
 
--- packages/multi-select-combo-box/test/dom/multi-select-combo-box.test.js
+++ packages/multi-select-combo-box/test/dom/multi-select-combo-box.test.js
@@ -1,22 +1,99 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync } from '@vaadin/testing-helpers';
+import { aTimeout, fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import '../../src/vaadin-multi-select-combo-box.js';
+import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
 
 describe('vaadin-multi-select-combo-box', () => {
-  let multiSelectComboBox;
+  let multiSelectComboBox, comboBox;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    resetUniqueId();
     multiSelectComboBox = fixtureSync('<vaadin-multi-select-combo-box></vaadin-multi-select-combo-box>');
+    await nextRender();
+    comboBox = multiSelectComboBox.$.comboBox;
   });
 
   describe('host', () => {
     it('default', async () => {
       await expect(multiSelectComboBox).dom.to.equalSnapshot();
     });
+
+    it('label', async () => {
+      multiSelectComboBox.label = 'Label';
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('helper', async () => {
+      multiSelectComboBox.helperText = 'Helper';
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('error', async () => {
+      multiSelectComboBox.errorMessage = 'Error';
+      multiSelectComboBox.invalid = true;
+      await aTimeout(0);
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('required', async () => {
+      multiSelectComboBox.required = true;
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('disabled', async () => {
+      multiSelectComboBox.disabled = true;
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('readonly', async () => {
+      multiSelectComboBox.readonly = true;
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    it('placeholder', async () => {
+      multiSelectComboBox.placeholder = 'Placeholder';
+      await expect(multiSelectComboBox).dom.to.equalSnapshot();
+    });
+
+    describe('opened', () => {
+      const SNAPSHOT_CONFIG = {
+        // Some inline CSS styles related to the overlay's position
+        // may slightly change depending on the environment, so ignore them.
+        ignoreAttributes: ['style'],
+      };
+
+      beforeEach(async () => {
+        multiSelectComboBox.opened = true;
+        await nextRender();
+      });
+
+      it('default', async () => {
+        await expect(multiSelectComboBox).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
+      });
+
+      it('overlay', async () => {
+        await expect(comboBox.$.overlay).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
+      });
+    });
   });
 
   describe('shadow', () => {
     it('default', async () => {
+      await expect(multiSelectComboBox).shadowDom.to.equalSnapshot();
+    });
+
+    it('disabled', async () => {
+      multiSelectComboBox.disabled = true;
+      await expect(multiSelectComboBox).shadowDom.to.equalSnapshot();
+    });
+
+    it('readonly', async () => {
+      multiSelectComboBox.readonly = true;
+      await expect(multiSelectComboBox).shadowDom.to.equalSnapshot();
+    });
+
+    it('invalid', async () => {
+      multiSelectComboBox.invalid = true;
       await expect(multiSelectComboBox).shadowDom.to.equalSnapshot();
     });
   });
