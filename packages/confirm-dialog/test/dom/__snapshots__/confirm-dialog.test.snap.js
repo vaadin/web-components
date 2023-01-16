f0aab054a65bd9b023598e22d731d358f1b2cabb
web-padawan <iamkulykov@gmail.com>
2023-01-16 16:02:51 +0200

    test: add missing confirm-dialog snapshots and types tests

---
+++ packages/confirm-dialog/test/dom/__snapshots__/confirm-dialog.test.snap.js
@@ -0,0 +1,82 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-confirm-dialog overlay"] = 
+`<vaadin-confirm-dialog-overlay
+  aria-label="Unsaved changes"
+  focus-trap=""
+  has-footer=""
+  has-header=""
+  id="overlay"
+  opened=""
+  role="dialog"
+  with-backdrop=""
+>
+  <h3 slot="header">
+    Unsaved changes
+  </h3>
+  Do you want to save or discard the changes?
+  <vaadin-button
+    hidden=""
+    slot="cancel-button"
+    theme="tertiary"
+  >
+    Cancel
+  </vaadin-button>
+  <vaadin-button
+    slot="confirm-button"
+    theme="primary"
+  >
+    Confirm
+  </vaadin-button>
+  <vaadin-button
+    hidden=""
+    slot="reject-button"
+    theme="error tertiary"
+  >
+    Reject
+  </vaadin-button>
+</vaadin-confirm-dialog-overlay>
+`;
+/* end snapshot vaadin-confirm-dialog overlay */
+
+snapshots["vaadin-confirm-dialog overlay theme"] = 
+`<vaadin-confirm-dialog-overlay
+  aria-label="Unsaved changes"
+  focus-trap=""
+  has-footer=""
+  has-header=""
+  id="overlay"
+  opened=""
+  role="dialog"
+  theme="custom"
+  with-backdrop=""
+>
+  <h3 slot="header">
+    Unsaved changes
+  </h3>
+  Do you want to save or discard the changes?
+  <vaadin-button
+    hidden=""
+    slot="cancel-button"
+    theme="tertiary"
+  >
+    Cancel
+  </vaadin-button>
+  <vaadin-button
+    slot="confirm-button"
+    theme="primary"
+  >
+    Confirm
+  </vaadin-button>
+  <vaadin-button
+    hidden=""
+    slot="reject-button"
+    theme="error tertiary"
+  >
+    Reject
+  </vaadin-button>
+</vaadin-confirm-dialog-overlay>
+`;
+/* end snapshot vaadin-confirm-dialog overlay theme */
+
