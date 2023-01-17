cd1292ace37ec231336c14ea6f92e8fd3e849ea6
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-17 11:44:36 +0200

    test: add missing notification snapshots and types tests (#5318)

---
+++ packages/notification/test/dom/__snapshots__/notification.test.snap.js
@@ -0,0 +1,26 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-notification card"] = 
+`<vaadin-notification-card
+  aria-live="polite"
+  role="alert"
+  slot="bottom-start"
+>
+  content
+</vaadin-notification-card>
+`;
+/* end snapshot vaadin-notification card */
+
+snapshots["vaadin-notification card theme"] = 
+`<vaadin-notification-card
+  aria-live="polite"
+  role="alert"
+  slot="bottom-start"
+  theme="custom"
+>
+  content
+</vaadin-notification-card>
+`;
+/* end snapshot vaadin-notification card theme */
+
