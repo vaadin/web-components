2e1d0ce759b15968aab3c0649ba04ebd8e90ba6f
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-23 13:41:07 +0200

    Update packages/menu-bar/src/vaadin-menu-bar.d.ts
    Co-authored-by: Tomi Virkki <tomivirkki@users.noreply.github.com>

--- packages/menu-bar/src/vaadin-menu-bar.d.ts
+++ packages/menu-bar/src/vaadin-menu-bar.d.ts
@@ -102,7 +102,7 @@ export interface MenuBarEventMap extends HTMLElementEventMap, MenuBarCustomEvent
  *
  * - `<vaadin-menu-bar-button>` - has the same API as [`<vaadin-button>`](#/elements/vaadin-button).
  * - `<vaadin-context-menu-item>` - has the same API as [`<vaadin-item>`](#/elements/vaadin-item).
- * - `<<vaadin-menu-bar-list-box>` - has the same API as [`<vaadin-list-box>`](#/elements/vaadin-list-box).
+ * - `<vaadin-menu-bar-list-box>` - has the same API as [`<vaadin-list-box>`](#/elements/vaadin-list-box).
  * - `<vaadin-menu-bar-overlay>` - has the same API as [`<vaadin-overlay>`](#/elements/vaadin-overlay).
  *
  * @fires {CustomEvent} item-selected - Fired when a submenu item or menu bar button without children is clicked.
