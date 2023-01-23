325b55d2c1a8cc9a3d2891f53e345d40b98e686b
web-padawan <iamkulykov@gmail.com>
2023-01-23 16:29:32 +0200

    feat: export context-menu item styles as CSS literals

--- packages/context-menu/theme/lumo/vaadin-context-menu-styles.js
+++ packages/context-menu/theme/lumo/vaadin-context-menu-styles.js
@@ -1,9 +1,5 @@
 import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
-import '@vaadin/vaadin-lumo-styles/font-icons.js';
-import '@vaadin/vaadin-lumo-styles/color.js';
-import '@vaadin/vaadin-lumo-styles/sizing.js';
-import '@vaadin/vaadin-lumo-styles/typography.js';
 import { menuOverlay } from '@vaadin/vaadin-lumo-styles/mixins/menu-overlay.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
@@ -33,43 +29,3 @@ const contextMenuOverlay = css`
 registerStyles('vaadin-context-menu-overlay', [menuOverlay, contextMenuOverlay], {
   moduleId: 'lumo-context-menu-overlay',
 });
-
-registerStyles(
-  'vaadin-context-menu-item',
-  css`
-    /* :hover needed to workaround https://github.com/vaadin/web-components/issues/3133 */
-    :host(:hover) {
-      user-select: none;
-      -ms-user-select: none;
-      -webkit-user-select: none;
-    }
-
-    :host([role='menuitem'][menu-item-checked]) [part='checkmark']::before {
-      opacity: 1;
-    }
-
-    :host([aria-haspopup='true'])::after {
-      font-family: lumo-icons;
-      font-size: var(--lumo-icon-size-xs);
-      content: var(--lumo-icons-angle-right);
-      color: var(--lumo-tertiary-text-color);
-    }
-
-    :host(:not([dir='rtl'])[aria-haspopup='true'])::after {
-      margin-right: calc(var(--lumo-space-m) * -1);
-      padding-left: var(--lumo-space-m);
-    }
-
-    :host([expanded]) {
-      background-color: var(--lumo-primary-color-10pct);
-    }
-
-    /* RTL styles */
-    :host([dir='rtl'][aria-haspopup='true'])::after {
-      content: var(--lumo-icons-angle-left);
-      margin-left: calc(var(--lumo-space-m) * -1);
-      padding-right: var(--lumo-space-m);
-    }
-  `,
-  { moduleId: 'lumo-context-menu-item' },
-);
