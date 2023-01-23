109098c42ac70cd5f01584153489dd338209e59c
web-padawan <iamkulykov@gmail.com>
2023-01-23 16:05:03 +0200

    feat: export context-menu list-box styles as CSS literals

--- packages/context-menu/theme/lumo/vaadin-context-menu-styles.js
+++ packages/context-menu/theme/lumo/vaadin-context-menu-styles.js
@@ -35,46 +35,6 @@ registerStyles('vaadin-context-menu-overlay', [menuOverlay, contextMenuOverlay],
 });
 
 registerStyles(
-  'vaadin-context-menu-list-box',
-  css`
-    :host {
-      --_lumo-list-box-item-selected-icon-display: block;
-    }
-
-    /* Normal item */
-    [part='items'] ::slotted([role='menuitem']) {
-      -webkit-tap-highlight-color: var(--lumo-primary-color-10pct);
-      cursor: default;
-      outline: none;
-      border-radius: var(--lumo-border-radius-m);
-      padding-left: calc(var(--lumo-border-radius-m) / 4);
-      padding-right: calc(var(--lumo-space-l) + var(--lumo-border-radius-m) / 4);
-    }
-
-    /* Hovered item */
-    /* TODO a workaround until we have "focus-follows-mouse". After that, use the hover style for focus-ring as well */
-    [part='items'] ::slotted([role='menuitem']:hover:not([disabled])),
-    [part='items'] ::slotted([role='menuitem'][expanded]:not([disabled])) {
-      background-color: var(--lumo-primary-color-10pct);
-    }
-
-    /* RTL styles */
-    :host([dir='rtl']) [part='items'] ::slotted([role='menuitem']) {
-      padding-left: calc(var(--lumo-space-l) + var(--lumo-border-radius-m) / 4);
-      padding-right: calc(var(--lumo-border-radius-m) / 4);
-    }
-
-    /* Focused item */
-    @media (pointer: coarse) {
-      [part='items'] ::slotted([role='menuitem']:hover:not([expanded]):not([disabled])) {
-        background-color: transparent;
-      }
-    }
-  `,
-  { moduleId: 'lumo-context-menu-list-box' },
-);
-
-registerStyles(
   'vaadin-context-menu-item',
   css`
     /* :hover needed to workaround https://github.com/vaadin/web-components/issues/3133 */
