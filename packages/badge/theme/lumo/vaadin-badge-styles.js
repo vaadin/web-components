81f2e0c358c25b985a693169d0c894789180016d
Jouni Koivuviita <jouni@vaadin.com>
2023-01-24 20:36:14 +0200

    clean up Lumo theme

--- packages/badge/theme/lumo/vaadin-badge-styles.js
+++ packages/badge/theme/lumo/vaadin-badge-styles.js
@@ -5,121 +5,114 @@ import { loader } from '@vaadin/vaadin-lumo-styles/mixins/loader.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
 const badge = css`
-  span {
-    padding: 0.4em calc(0.5em + var(--lumo-border-radius-s) / 4);
+  :host {
+    --_padding: 0.4em;
+    padding: 0 var(--_padding);
     color: var(--lumo-primary-text-color);
     background-color: var(--lumo-primary-color-10pct);
     border-radius: var(--lumo-border-radius-s);
     font-family: var(--lumo-font-family);
     font-size: var(--lumo-font-size-s);
-    min-width: calc(var(--lumo-line-height-xs) * 1em + 0.45em);
+    font-weight: 500;
+    gap: calc(var(--_padding) - 0.1em);
+    min-height: 1.5rem;
+    min-width: 1.5rem;
   }
 
-  :host([theme~='small']) span {
+  /* Compensate for gap */
+  :host::before {
+    margin-inline-start: calc(var(--_padding) * -1 + 0.1em);
+  }
+
+  :host([theme~='small']) {
     font-size: var(--lumo-font-size-xxs);
-    line-height: 1;
+    min-height: 1.25rem;
+    min-width: 1.25rem;
   }
 
   /* Colors */
 
-  :host([theme~='success']) span {
+  :host([theme~='success']) {
     color: var(--lumo-success-text-color);
     background-color: var(--lumo-success-color-10pct);
   }
 
-  :host([theme~='error']) span {
+  :host([theme~='error']) {
     color: var(--lumo-error-text-color);
     background-color: var(--lumo-error-color-10pct);
   }
 
-  :host([theme~='contrast']) span {
+  :host([theme~='contrast']) {
     color: var(--lumo-contrast-80pct);
     background-color: var(--lumo-contrast-5pct);
   }
 
   /* Primary */
 
-  :host([theme~='primary']) span {
+  :host([theme~='primary']) {
     color: var(--lumo-primary-contrast-color);
     background-color: var(--lumo-primary-color);
   }
 
-  :host([theme~='success'][theme~='primary']) span {
+  :host([theme~='success'][theme~='primary']) {
     color: var(--lumo-success-contrast-color);
     background-color: var(--lumo-success-color);
   }
 
-  :host([theme~='error'][theme~='primary']) span {
+  :host([theme~='error'][theme~='primary']) {
     color: var(--lumo-error-contrast-color);
     background-color: var(--lumo-error-color);
   }
 
-  :host([theme~='contrast'][theme~='primary']) span {
+  :host([theme~='contrast'][theme~='primary']) {
     color: var(--lumo-base-color);
     background-color: var(--lumo-contrast);
   }
 
-  /* Icon */
-
-  iron-icon {
-    margin: -0.25em 0;
-    --iron-icon-width: 1.5em;
-    --iron-icon-height: 1.5em;
-  }
-
-  iron-icon:first-child {
-    margin-left: -0.375em;
-  }
-
-  iron-icon:last-child {
-    margin-right: -0.375em;
-  }
-
-  [icon] {
-    min-width: 0;
-    padding: 0;
-    font-size: 1rem;
-    --iron-icon-width: var(--lumo-icon-size-m);
-    --iron-icon-height: var(--lumo-icon-size-m);
-  }
-
-  [icon][theme~='small'] {
-    --iron-icon-width: var(--lumo-icon-size-s);
-    --iron-icon-height: var(--lumo-icon-size-s);
-  }
-
   /* Empty */
 
-  :not([icon]):empty {
+  :host(:empty) {
     min-width: 0;
-    width: 1em;
-    height: 1em;
+    min-height: 0;
+    width: 1rem;
+    height: 1rem;
     padding: 0;
     border-radius: 50%;
     background-color: var(--lumo-primary-color);
   }
 
-  :host([theme~='small']) :not([icon]):empty {
-    width: 0.75em;
-    height: 0.75em;
+  :host([theme~='small']:empty) {
+    width: 0.75rem;
+    height: 0.75rem;
   }
 
-  :host([theme~='contrast']) :not([icon]):empty {
+  :host([theme~='contrast']:empty) {
     background-color: var(--lumo-contrast);
   }
 
-  :host([theme~='success']) :not([icon]):empty {
+  :host([theme~='success']:empty) {
     background-color: var(--lumo-success-color);
   }
 
-  :host([theme~='error']) :not([icon]):empty {
+  :host([theme~='error']:empty) {
     background-color: var(--lumo-error-color);
   }
 
   /* Pill */
 
   :host([theme~='pill']) {
-    --lumo-border-radius-s: 1em;
+    border-radius: 1em;
+    --_padding: 0.6em;
+  }
+
+  /* Icon */
+
+  ::slotted(vaadin-icon) {
+    margin: 0 calc(var(--_padding) * -1 + 0.2em);
+  }
+
+  ::slotted(vaadin-icon:only-child) {
+    margin: 0 calc(var(--_padding) * -1);
   }
 `;
 
