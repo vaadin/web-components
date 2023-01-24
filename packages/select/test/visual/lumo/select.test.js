62d2891e24702750fe6519dd0570b487c30f5c80
web-padawan <iamkulykov@gmail.com>
2023-01-24 11:45:32 +0200

    test: update visual tests to use items property

--- packages/select/test/visual/lumo/select.test.js
+++ packages/select/test/visual/lumo/select.test.js
@@ -14,15 +14,11 @@ describe('select', () => {
     div.style.display = 'inline-block';
     div.style.padding = '10px';
     element = fixtureSync('<vaadin-select></vaadin-select>', div);
-    element.renderer = (root) => {
-      root.innerHTML = `
-        <vaadin-list-box>
-          <vaadin-item>item 1</vaadin-item>
-          <vaadin-item>item 2</vaadin-item>
-          <vaadin-item>item 3</vaadin-item>
-        </vaadin-list-box>
-      `;
-    };
+    element.items = [
+      { label: 'item 1', value: 'value-1' },
+      { label: 'item 2', value: 'value-2' },
+      { label: 'item 3', value: 'value-3' },
+    ];
     await nextFrame();
   });
 
