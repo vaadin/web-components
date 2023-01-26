56352bd642001bc04acbd363e35a41dc019a065b
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-26 13:17:51 +0200

    fix!: set correct role attribute on the list-box and item (#5401)

--- packages/select/src/vaadin-select.js
+++ packages/select/src/vaadin-select.js
@@ -467,8 +467,6 @@ class Select extends DelegateFocusMixin(DelegateStateMixin(FieldMixin(ElementMix
         true,
       );
 
-      menuElement.setAttribute('role', 'listbox');
-
       // Store the menu element reference
       this.__lastMenuElement = menuElement;
     }
@@ -478,7 +476,6 @@ class Select extends DelegateFocusMixin(DelegateStateMixin(FieldMixin(ElementMix
   __initMenuItems(menuElement) {
     if (menuElement.items) {
       this._items = menuElement.items;
-      this._items.forEach((item) => item.setAttribute('role', 'option'));
     }
   }
 
@@ -639,14 +636,16 @@ class Select extends DelegateFocusMixin(DelegateStateMixin(FieldMixin(ElementMix
 
   /**
    * @param {!HTMLElement} itemElement
+   * @param {!HTMLElement} parent
    * @private
    */
   __appendValueItemElement(itemElement) {
+    this._valueButton.appendChild(itemElement);
+
     itemElement.removeAttribute('tabindex');
+    itemElement.removeAttribute('aria-selected');
     itemElement.removeAttribute('role');
     itemElement.setAttribute('id', this._fieldId);
-
-    this._valueButton.appendChild(itemElement);
   }
 
   /** @private */
