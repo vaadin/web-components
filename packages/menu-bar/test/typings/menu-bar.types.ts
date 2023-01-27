aed0c43a5da2ae61bb6db821aaa05f42cb3f362b
web-padawan <iamkulykov@gmail.com>
2023-01-27 11:25:40 +0200

    docs: add missing menu-bar item and list-box typings

--- packages/menu-bar/test/typings/menu-bar.types.ts
+++ packages/menu-bar/test/typings/menu-bar.types.ts
@@ -1,16 +1,22 @@
 import '../../vaadin-menu-bar.js';
 import type { ControllerMixinClass } from '@vaadin/component-base/src/controller-mixin.js';
+import type { DirMixinClass } from '@vaadin/component-base/src/dir-mixin.js';
 import type { FocusMixinClass } from '@vaadin/component-base/src/focus-mixin.js';
+import type { ListMixinClass } from '@vaadin/component-base/src/list-mixin.js';
 import type { ResizeMixinClass } from '@vaadin/component-base/src/resize-mixin.js';
+import type { ItemMixinClass } from '@vaadin/item/src/vaadin-item-mixin.js';
+import type { ThemableMixinClass } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import type { MenuBarItem } from '../../src/vaadin-menu-bar-item.js';
+import type { MenuBarListBox } from '../../src/vaadin-menu-bar-list-box.js';
 import type { MenuBarMixinClass } from '../../src/vaadin-menu-bar-mixin.js';
-import type { MenuBarItem, MenuBarItemSelectedEvent } from '../../vaadin-menu-bar.js';
+import type { MenuBarItem as MenuItem, MenuBarItemSelectedEvent } from '../../vaadin-menu-bar.js';
 
 const menu = document.createElement('vaadin-menu-bar');
 
 const assertType = <TExpected>(actual: TExpected) => actual;
 
 assertType<boolean | null | undefined>(menu.openOnHover);
-assertType<MenuBarItem[]>(menu.items);
+assertType<MenuItem[]>(menu.items);
 assertType<string>(menu.overlayClass);
 
 assertType<ResizeMixinClass>(menu);
@@ -20,7 +26,7 @@ assertType<MenuBarMixinClass>(menu);
 
 menu.addEventListener('item-selected', (event) => {
   assertType<MenuBarItemSelectedEvent>(event);
-  assertType<MenuBarItem>(event.detail.value);
+  assertType<MenuItem>(event.detail.value);
 });
 
 const menuItem = menu.items[0];
@@ -29,5 +35,21 @@ assertType<string | undefined>(menuItem.tooltip);
 assertType<string | undefined>(menuItem.text);
 assertType<boolean | undefined>(menuItem.disabled);
 assertType<string[] | string | undefined>(menuItem.theme);
-assertType<MenuBarItem[] | undefined>(menuItem.children);
+assertType<MenuItem[] | undefined>(menuItem.children);
 assertType<HTMLElement | string | undefined>(menuItem.component);
+
+// Item
+const item = document.createElement('vaadin-menu-bar-item');
+
+assertType<MenuBarItem>(item);
+assertType<ItemMixinClass>(item);
+assertType<DirMixinClass>(item);
+assertType<ThemableMixinClass>(item);
+
+// List-box
+const listBox = document.createElement('vaadin-menu-bar-list-box');
+
+assertType<MenuBarListBox>(listBox);
+assertType<DirMixinClass>(listBox);
+assertType<ListMixinClass>(listBox);
+assertType<ThemableMixinClass>(listBox);
