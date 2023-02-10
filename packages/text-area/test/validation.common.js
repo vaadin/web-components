dc920c14efc87ca1cf91d1584da905d19e92930b
Serhii Kulykov <iamkulykov@gmail.com>
2023-02-10 15:46:46 +0200

    experiment: add Lit based version of vaadin-text-area (#5539)

--- packages/text-area/test/validation.common.js
+++ packages/text-area/test/validation.common.js
@@ -1,7 +1,6 @@
 import { expect } from '@esm-bundle/chai';
 import { fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
-import '../src/vaadin-text-area.js';
 
 describe('validation', () => {
   let textArea, validateSpy;
