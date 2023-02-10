c517b1e0c2ee5c23df45f9eaf5a0e605824888cd
Serhii Kulykov <iamkulykov@gmail.com>
2023-02-10 13:18:44 +0200

    test: update text-area tests to allow async property updates (#5528)

--- packages/text-area/test/validation.test.js
+++ packages/text-area/test/validation.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import '../src/vaadin-text-area.js';
 
@@ -57,13 +57,16 @@ describe('validation', () => {
   });
 
   describe('basic', () => {
-    beforeEach(() => {
+    let validatedSpy;
+
+    beforeEach(async () => {
       textArea = fixtureSync('<vaadin-text-area></vaadin-text-area>');
+      await nextRender();
+      validatedSpy = sinon.spy();
+      textArea.addEventListener('validated', validatedSpy);
     });
 
     it('should fire a validated event on validation success', () => {
-      const validatedSpy = sinon.spy();
-      textArea.addEventListener('validated', validatedSpy);
       textArea.validate();
 
       expect(validatedSpy.calledOnce).to.be.true;
@@ -71,10 +74,9 @@ describe('validation', () => {
       expect(event.detail.valid).to.be.true;
     });
 
-    it('should fire a validated event on validation failure', () => {
-      const validatedSpy = sinon.spy();
-      textArea.addEventListener('validated', validatedSpy);
+    it('should fire a validated event on validation failure', async () => {
       textArea.required = true;
+      await nextFrame();
       textArea.validate();
 
       expect(validatedSpy.calledOnce).to.be.true;
@@ -84,37 +86,45 @@ describe('validation', () => {
   });
 
   describe('minlength', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       textArea = fixtureSync('<vaadin-text-area></vaadin-text-area>');
+      await nextRender();
     });
 
-    it('should not validate the field when minlength is set', () => {
+    it('should not validate the field when minlength is set', async () => {
       textArea.minlength = 2;
+      await nextFrame();
       expect(textArea.invalid).to.be.false;
     });
 
-    it('should validate the field when invalid after minlength is changed', () => {
+    it('should validate the field when invalid after minlength is changed', async () => {
       textArea.invalid = true;
+      await nextFrame();
       const spy = sinon.spy(textArea, 'validate');
       textArea.minlength = 2;
+      await nextFrame();
       expect(spy.calledOnce).to.be.true;
     });
   });
 
   describe('maxlength', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       textArea = fixtureSync('<vaadin-text-area></vaadin-text-area>');
+      await nextRender();
     });
 
-    it('should not validate the field when maxlength is set', () => {
+    it('should not validate the field when maxlength is set', async () => {
       textArea.maxlength = 6;
+      await nextFrame();
       expect(textArea.invalid).to.be.false;
     });
 
-    it('should validate the field when invalid after maxlength is changed', () => {
+    it('should validate the field when invalid after maxlength is changed', async () => {
       textArea.invalid = true;
+      await nextFrame();
       const spy = sinon.spy(textArea, 'validate');
       textArea.maxlength = 6;
+      await nextFrame();
       expect(spy.calledOnce).to.be.true;
     });
   });
