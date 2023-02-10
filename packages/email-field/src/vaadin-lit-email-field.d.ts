6a995d075c23d4136dd0ec3a8ce0a3c682501f72
Serhii Kulykov <iamkulykov@gmail.com>
2023-02-10 16:47:22 +0200

    experiment: add Lit based version of vaadin-email-field (#5549)

---
+++ packages/email-field/src/vaadin-lit-email-field.d.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2023 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { TextField } from '@vaadin/text-field/src/vaadin-lit-text-field.js';
+
+/**
+ * LitElement based version of `<vaadin-email-field>` web component.
+ *
+ * ## Disclaimer
+ *
+ * This component is an experiment not intended for publishing to npm.
+ * There is no ETA regarding specific Vaadin version where it'll land.
+ * Feel free to try this code in your apps as per Apache 2.0 license.
+ */
+declare class EmailField extends TextField {}
+
+export { EmailField };
