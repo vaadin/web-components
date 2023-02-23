2ec91c44db4809214c17e91251876da3be468c48
web-padawan <iamkulykov@gmail.com>
2023-02-23 13:44:32 +0200

    fix: add missing exports to root leven entrypoints

--- packages/a11y-base/index.js
+++ packages/a11y-base/index.js
@@ -4,6 +4,13 @@ export { DelegateFocusMixin } from './src/delegate-focus-mixin.js';
 export { DisabledMixin } from './src/disabled-mixin.js';
 export { FocusMixin } from './src/focus-mixin.js';
 export { FocusTrapController } from './src/focus-trap-controller.js';
+export {
+  getFocusableElements,
+  isElementFocusable,
+  isElementFocused,
+  isElementHidden,
+  isKeyboardActive,
+} from './src/focus-utils.js';
 export { KeyboardDirectionMixin } from './src/keyboard-direction-mixin.js';
 export { KeyboardMixin } from './src/keyboard-mixin.js';
 export { ListMixin } from './src/list-mixin.js';
--- packages/component-base/index.js
+++ packages/component-base/index.js
@@ -1,5 +1,6 @@
 export { ControllerMixin } from './src/controller-mixin.js';
+export { DelegateStateMixin } from './src/delegate-state-mixin.js';
 export { DirMixin } from './src/dir-mixin.js';
 export { ElementMixin } from './src/element-mixin.js';
-export { KeyboardMixin } from './src/keyboard-mixin.js';
+export { ResizeMixin } from './src/resize-mixin.js';
 export { SlotController } from './src/slot-controller.js';
