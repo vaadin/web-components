ff8101d36c3bce51ba63c96da1cd051063022a26
web-padawan <iamkulykov@gmail.com>
2023-02-27 20:20:50 +0200

    feat: add AriaModalController to handle modal dialogs behavior

---
+++ packages/a11y-base/src/aria-modal-controller.js
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2023 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { hideOthers } from './aria-hidden.js';
+import { FocusTrapController } from './focus-trap-controller.js';
+
+/**
+ * A controller for handling modal state on the elements with `dialog` and `alertdialog` role.
+ * See https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-modal
+ *
+ * Note, the actual `role` and `aria-modal` attributes are supposed to be handled by the
+ * consumer web component. This is done in to ensure the controller only does one thing.
+ */
+export class AriaModalController {
+  constructor(host) {
+    this.host = host;
+  }
+
+  /**
+   * Make the controller host modal by hiding other elements from screen readers
+   * using `aria-hidden` attribute (can be replaced with `inert` in the future).
+   *
+   * The method name is chosen to align with the one provided by native `<dialog>`:
+   * https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/showModal
+   */
+  showModal() {
+    this.__showOthers = hideOthers(this.host);
+  }
+
+  /**
+   * Remove `aria-hidden` from other elements unless there are any other
+   * controller hosts on the page activated by using `showModal()` call.
+   */
+  close() {
+    if (this.__showOthers) {
+      this.__showOthers();
+      this.__showOthers = null;
+    }
+  }
+}
