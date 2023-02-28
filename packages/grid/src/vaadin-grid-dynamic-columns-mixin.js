9274b04267f88dc901a94dd084ce69cb21765935
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-02-28 14:20:53 +0200

    refactor: reduce grid updateColumnTree call count (#5384)

--- packages/grid/src/vaadin-grid-dynamic-columns-mixin.js
+++ packages/grid/src/vaadin-grid-dynamic-columns-mixin.js
@@ -5,7 +5,7 @@
  */
 import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { timeOut } from '@vaadin/component-base/src/async.js';
+import { microTask, timeOut } from '@vaadin/component-base/src/async.js';
 import { Debouncer } from '@vaadin/component-base/src/debounce.js';
 import { updateCellState } from './vaadin-grid-helpers.js';
 
@@ -91,6 +91,13 @@ export const DynamicColumnsMixin = (superClass) =>
     }
 
     /** @protected */
+    _debounceUpdateColumnTree() {
+      this.__updateColumnTreeDebouncer = Debouncer.debounce(this.__updateColumnTreeDebouncer, microTask, () =>
+        this._updateColumnTree(),
+      );
+    }
+
+    /** @protected */
     _updateColumnTree() {
       const columnTree = this._getColumnTree();
       if (!arrayEquals(columnTree, this._columnTree)) {
@@ -109,7 +116,7 @@ export const DynamicColumnsMixin = (superClass) =>
 
           this.__removeSorters(this._sorters.filter(filterNotConnected));
           this.__removeFilters(this._filters.filter(filterNotConnected));
-          this._updateColumnTree();
+          this._debounceUpdateColumnTree();
         }
 
         this._debouncerCheckImports = Debouncer.debounce(
