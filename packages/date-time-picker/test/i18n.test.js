d76d98ab6084a6e271184e92ad97737fb2f79170
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-03-01 09:26:12 +0200

    Update packages/date-time-picker/test/i18n.test.js
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

--- packages/date-time-picker/test/i18n.test.js
+++ packages/date-time-picker/test/i18n.test.js
@@ -109,7 +109,7 @@ describe('i18n set before added to the DOM', () => {
   });
 
   it('should have the correct i18n', async () => {
-    await nextFrame();
+    await aTimeout(0);
     document.body.appendChild(dateTimePicker);
     expect(datePicker.i18n.cancel).to.equal('CUSTOM');
   });
