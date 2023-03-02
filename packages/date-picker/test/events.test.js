62e5a87bb7d1763f151baa106248325b936626d5
Sergey Vinogradov <mr.vursen@gmail.com>
2023-03-02 12:22:13 +0200

    test: move event tests to a separate file

---
+++ packages/date-picker/test/events.test.js
@@ -0,0 +1,113 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { sendKeys } from '@web/test-runner-commands';
+import sinon from 'sinon';
+import './not-animated-styles.js';
+import '../vaadin-date-picker.js';
+import { close, open, waitForOverlayRender } from './helpers.js';
+
+describe('events', () => {
+  let datePicker;
+
+  describe('change event', () => {
+    let changeSpy;
+
+    beforeEach(async () => {
+      datePicker = fixtureSync('<vaadin-date-picker></vaadin-date-picker>');
+      await nextRender();
+      changeSpy = sinon.spy();
+      datePicker.addEventListener('change', changeSpy);
+      datePicker.inputElement.focus();
+    });
+
+    it('should not fire the event on focused date change', async () => {
+      await sendKeys({ type: '1/2/2000' });
+      await waitForOverlayRender();
+      expect(changeSpy.called).to.be.false;
+    });
+
+    it('should fire the event when committing user input with Enter', async () => {
+      await sendKeys({ type: '1/2/2000' });
+      await waitForOverlayRender();
+      await sendKeys({ press: 'Enter' });
+      expect(changeSpy.called).to.be.true;
+    });
+
+    it('should fire the event after the value-changed event', async () => {
+      const valueChangedSpy = sinon.spy();
+      datePicker.addEventListener('value-changed', valueChangedSpy);
+
+      await sendKeys({ type: '1/2/2000' });
+      await waitForOverlayRender();
+      await sendKeys({ press: 'Enter' });
+
+      expect(valueChangedSpy.calledOnce).to.be.true;
+      expect(changeSpy.calledAfter(valueChangedSpy)).to.be.true;
+    });
+
+    it('should fire the event when selecting a date with Enter', async () => {
+      // Open the overlay
+      await open(datePicker);
+
+      // Move focus to the calendar
+      await sendKeys({ press: 'ArrowDown' });
+      await nextRender(datePicker);
+
+      await sendKeys({ press: 'Enter' });
+      expect(changeSpy.calledOnce).to.be.true;
+    });
+
+    it('should fire the event when selecting a date with Space', async () => {
+      // Open the overlay
+      await open(datePicker);
+
+      // Move focus to the calendar
+      await sendKeys({ press: 'ArrowDown' });
+      await nextRender(datePicker);
+
+      await sendKeys({ press: 'Space' });
+      expect(changeSpy.calledOnce).to.be.true;
+    });
+
+    it('should fire the event on clear button click', () => {
+      datePicker.clearButtonVisible = true;
+      datePicker.value = '2000-01-01';
+      datePicker.$.clearButton.click();
+      expect(changeSpy.calledOnce).to.be.true;
+    });
+
+    it('should not fire the event on programmatic value change', () => {
+      datePicker.value = '2000-01-01';
+      expect(changeSpy.called).to.be.false;
+    });
+
+    it('should not fire the event on programmatic value change when opened', async () => {
+      await open(datePicker);
+      datePicker.value = '2000-01-01';
+      await close(datePicker);
+      expect(changeSpy.called).to.be.false;
+    });
+
+    it('should not fire the event on programmatic value change when having user input', async () => {
+      await sendKeys({ type: '1/2/2000' });
+      await waitForOverlayRender();
+      datePicker.value = '2000-01-01';
+      await close(datePicker);
+      expect(changeSpy.called).to.be.false;
+    });
+
+    it('should not fire the event on Enter if the value has not changed', async () => {
+      datePicker.value = '2000-01-01';
+      await open(datePicker);
+      await sendKeys({ press: 'Enter' });
+      expect(changeSpy.called).to.be.false;
+    });
+
+    it('should not fire the event when reverting the user input with Escape', async () => {
+      await sendKeys({ type: '1/2/2000' });
+      await waitForOverlayRender();
+      await sendKeys({ press: 'Escape' });
+      expect(changeSpy.called).to.be.false;
+    });
+  });
+});
