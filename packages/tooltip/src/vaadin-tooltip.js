dd7b0966ccbd207fe5125b9d359eccfc2d598901
Břetislav Wajtr <bretislav@vaadin.com>
2023-03-17 09:43:56 +0100

    fix: process all intersection observer event entries (#5668)
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

--- packages/tooltip/src/vaadin-tooltip.js
+++ packages/tooltip/src/vaadin-tooltip.js
@@ -474,8 +474,8 @@ class Tooltip extends OverlayClassMixin(ThemePropertyMixin(ElementMixin(PolymerE
     this.__onOverlayOpen = this.__onOverlayOpen.bind(this);
 
     this.__targetVisibilityObserver = new IntersectionObserver(
-      ([entry]) => {
-        this.__onTargetVisibilityChange(entry.isIntersecting);
+      (entries) => {
+        entries.forEach((entry) => this.__onTargetVisibilityChange(entry.isIntersecting));
       },
       { threshold: 1 },
     );
