73c643e5ba350ce7e4a8a2a718cd9f382e739dee
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-03-23 15:34:29 +0200

    refactor: debounce tasks ran after creating grid rows (#5707)

--- packages/grid/src/vaadin-grid.js
+++ packages/grid/src/vaadin-grid.js
@@ -5,10 +5,9 @@
  */
 import './vaadin-grid-column.js';
 import './vaadin-grid-styles.js';
-import { beforeNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { TabindexMixin } from '@vaadin/a11y-base/src/tabindex-mixin.js';
-import { microTask } from '@vaadin/component-base/src/async.js';
+import { animationFrame, microTask } from '@vaadin/component-base/src/async.js';
 import { isAndroid, isChrome, isFirefox, isIOS, isSafari, isTouch } from '@vaadin/component-base/src/browser-utils.js';
 import { ControllerMixin } from '@vaadin/component-base/src/controller-mixin.js';
 import { Debouncer } from '@vaadin/component-base/src/debounce.js';
@@ -741,12 +740,14 @@ class Grid extends ElementMixin(
       );
     }
 
-    beforeNextRender(this, () => {
-      this._updateFirstAndLastColumn();
-      this._resetKeyboardNavigation();
-      this._afterScroll();
-      this.__itemsReceived();
-    });
+    this.__afterCreateScrollerRowsDebouncer = Debouncer.debounce(
+      this.__afterCreateScrollerRowsDebouncer,
+      animationFrame,
+      () => {
+        this._afterScroll();
+        this.__itemsReceived();
+      },
+    );
     return rows;
   }
 
