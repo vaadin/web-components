db4fecfba9a7293c53c8cec6c256cc7dcb7e18e0
Artur Signell <artur@vaadin.com>
2023-04-14 12:44:51 +0300

    add doc

--- packages/vaadin-themable-mixin/register-styles.js
+++ packages/vaadin-themable-mixin/register-styles.js
@@ -5,6 +5,12 @@
  */
 export { registerStyles, css, unsafeCSS } from './vaadin-themable-mixin.js';
 
+/**
+ * This is for use internally by Lumo and Material styles.
+ *
+ * @param {string} id the id to set on the created element, only for informational purposes
+ * @param  {CSSResultGroup[]} styles the styles to add
+ */
 export const addGlobalThemeStyles = (id, ...styles) => {
   const styleTag = document.createElement('style');
   styleTag.id = id;
