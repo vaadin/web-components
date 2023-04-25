8d9894663af69f70611d4f70ca9bc98ecd140f51
ugur-vaadin <ugur@vaadin.com>
2023-04-25 08:53:48 +0300

    test: add lumo visual tests

---
+++ packages/side-nav/test/visual/not-animated-styles.js
@@ -0,0 +1,23 @@
+import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+registerStyles(
+  'vaadin-side-nav',
+  css`
+    :host,
+    :host::before,
+    :host::after {
+      transition: none !important;
+    }
+  `,
+);
+
+registerStyles(
+  'vaadin-side-nav-item',
+  css`
+    :host,
+    :host::before,
+    :host::after {
+      transition: none !important;
+    }
+  `,
+);
