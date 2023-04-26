f57beb2216644d54d49ce45fc957ae6ab3b2ea9d
Diego Cardoso <diego@vaadin.com>
2023-04-26 15:31:15 +0300

    feat: add sr-only styles to field-base

---
+++ packages/field-base/src/styles/sr-only-styles.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2023 Vaadin Ltd..
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const screenReaderOnly = css`
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+`;
