f2d0e1c93b7b7101e4968814e4eb1f065a96e7a9
ugur-vaadin <ugur@vaadin.com>
2023-04-26 09:38:54 +0300

    test: add placeholder material visual test

---
+++ packages/side-nav/theme/material/vaadin-side-nav.js
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2023 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import '../../src/vaadin-side-nav.js';
+import '../../src/vaadin-side-nav-item.js';
