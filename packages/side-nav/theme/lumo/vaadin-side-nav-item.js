7b7ef81b4a659bb7ddb9ba9aa610417c7def3f7b
Bretislav Wajtr <bretislav@vaadin.com>
2023-05-03 12:58:01 +0200

    chore: add Lumo style file for side-nav-item so Flow can detect it and use

--- packages/side-nav/theme/lumo/vaadin-side-nav-item.js
+++ packages/side-nav/theme/lumo/vaadin-side-nav-item.js
@@ -3,5 +3,5 @@
  * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import './vaadin-side-nav-item.js';
-import '../../src/vaadin-side-nav.js';
+import './vaadin-side-nav-styles.js';
+import '../../src/vaadin-side-nav-item.js';
