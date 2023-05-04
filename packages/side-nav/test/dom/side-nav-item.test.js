bb3606876ff0ceb6e17da565aad93964b2eecdad
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-05-04 13:01:39 +0300

    Update packages/side-nav/test/dom/side-nav-item.test.js
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

--- packages/side-nav/test/dom/side-nav-item.test.js
+++ packages/side-nav/test/dom/side-nav-item.test.js
@@ -64,7 +64,7 @@ describe('vaadin-side-nav-item', () => {
       await expect(sideNavItem).shadowDom.to.equalSnapshot();
     });
 
-    it('with path', async () => {
+    it('path', async () => {
       sideNavItem.path = 'path';
       await nextRender(sideNavItem);
       await expect(sideNavItem).shadowDom.to.equalSnapshot();
