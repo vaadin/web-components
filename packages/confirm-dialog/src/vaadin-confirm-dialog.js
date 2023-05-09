bbb760aebef0f6ae9fd7cd8f06d87013f40f2c89
Diego Cardoso <diego@vaadin.com>
2023-05-09 14:19:47 +0300

    Update packages/confirm-dialog/src/vaadin-confirm-dialog.js
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

--- packages/confirm-dialog/src/vaadin-confirm-dialog.js
+++ packages/confirm-dialog/src/vaadin-confirm-dialog.js
@@ -108,7 +108,7 @@ class ConfirmDialog extends ElementMixin(ThemePropertyMixin(ControllerMixin(Poly
        *
        * By default, all elements inside the message area are linked
        * through the `aria-describedby` attribute. However, there are
-       * cases where this can confuse screen reader users (eg. the dialog
+       * cases where this can confuse screen reader users (e.g. the dialog
        * may present a password confirmation form). For these cases,
        * it's better to associate only the elements that will help describe
        * the confirmation dialog through this API.
