3a9a9430c695845a31fdab66577fdc838e87b19c
Sergey Vinogradov <mr.vursen@gmail.com>
2023-05-16 12:18:00 +0300

    test: simplify and enhance focus restoration tests (#5855)

--- packages/overlay/test/restore-focus.test.js
+++ packages/overlay/test/restore-focus.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync } from '@vaadin/testing-helpers';
+import { aTimeout, fixtureSync } from '@vaadin/testing-helpers';
 import '../src/vaadin-overlay.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { isElementFocused } from '@vaadin/a11y-base/src/focus-utils.js';
@@ -10,7 +10,7 @@ customElements.define(
   class extends PolymerElement {
     static get template() {
       return html`
-        <vaadin-overlay id="overlay" renderer="[[renderer]]"></vaadin-overlay>
+        <vaadin-overlay id="overlay" renderer="[[renderer]]" focus-trap></vaadin-overlay>
         <input id="focusable" />
       `;
     }
@@ -54,9 +54,6 @@ describe('restore focus', () => {
     it('should not restore focus on close by default', async () => {
       focusInput.focus();
       await open(overlay);
-      // Emulate focus leaving the input
-      focusInput.blur();
-      document.body.focus();
       await close(overlay);
       expect(isElementFocused(focusInput)).to.be.false;
     });
@@ -69,9 +66,6 @@ describe('restore focus', () => {
       it('should restore focus on close', async () => {
         focusInput.focus();
         await open(overlay);
-        // Emulate focus leaving the input
-        focusInput.blur();
-        document.body.focus();
         await close(overlay);
         expect(isElementFocused(focusInput)).to.be.true;
       });
@@ -81,6 +75,14 @@ describe('restore focus', () => {
         await open(overlay);
         await close(overlay);
         expect(isElementFocused(focusable)).to.be.true;
+      });
+
+      it('should restore focus on close if focus was moved to body', async () => {
+        focusInput.focus();
+        await open(overlay);
+        overlay.blur();
+        await close(overlay);
+        expect(isElementFocused(focusInput)).to.be.true;
       });
 
       it('should not restore focus on close if focus was moved outside overlay', async () => {
@@ -100,9 +102,6 @@ describe('restore focus', () => {
       it('should not restore focus on close by default', async () => {
         focusInput.focus();
         await open(overlay);
-        // Emulate focus leaving the input
-        focusInput.blur();
-        document.body.focus();
         await close(overlay);
         expect(isElementFocused(focusInput)).to.be.false;
       });
@@ -115,7 +114,6 @@ describe('restore focus', () => {
         it('should restore focus to the restoreFocusNode', async () => {
           focusable.focus();
           await open(overlay);
-          focusable.blur();
           await close(overlay);
           expect(isElementFocused(focusInput)).to.be.true;
         });
