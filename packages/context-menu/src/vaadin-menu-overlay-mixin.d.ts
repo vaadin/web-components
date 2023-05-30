3df65049e0966760b7f4d9fe4e616b5d0e7bc703
Sergey Vinogradov <mr.vursen@gmail.com>
2023-05-30 09:41:58 +0300

    fix: restore focus on context menu overlay close (#5881)

--- packages/context-menu/src/vaadin-menu-overlay-mixin.d.ts
+++ packages/context-menu/src/vaadin-menu-overlay-mixin.d.ts
@@ -4,11 +4,12 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import type { Constructor } from '@open-wc/dedupe-mixin';
+import type { OverlayFocusMixinClass } from '@vaadin/overlay/src/vaadin-overlay-focus-mixin.js';
 import type { PositionMixinClass } from '@vaadin/overlay/src/vaadin-overlay-position-mixin.js';
 
 export declare function MenuOverlayMixin<T extends Constructor<HTMLElement>>(
   base: T,
-): Constructor<MenuOverlayMixinClass> & Constructor<PositionMixinClass> & T;
+): Constructor<MenuOverlayMixinClass> & Constructor<OverlayFocusMixinClass> & Constructor<PositionMixinClass> & T;
 
 export declare class MenuOverlayMixinClass {
   protected readonly parentOverlay: HTMLElement | undefined;
