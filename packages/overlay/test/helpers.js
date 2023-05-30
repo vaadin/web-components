579c60c25d1ed1dc585439dcae2904e00c32f6b5
Sergey Vinogradov <mr.vursen@gmail.com>
2023-05-30 12:12:15 +0300

    remove no longer used helpers

--- packages/overlay/test/helpers.js
+++ packages/overlay/test/helpers.js
@@ -12,24 +12,6 @@ export function createOverlay(text) {
   return overlay;
 }
 
-export function open(overlay) {
-  return new Promise((resolve) => {
-    overlay.addEventListener('vaadin-overlay-open', () => {
-      resolve();
-    });
-    overlay.opened = true;
-  });
-}
-
-export function close(overlay) {
-  return new Promise((resolve) => {
-    nextRender(overlay).then(() => {
-      resolve();
-    });
-    overlay.opened = false;
-  });
-}
-
 /**
  * Emulates clicking outside the dropdown overlay
  */
