384abe2ef5d93d572a1e2b23937751f77cd426fb
Sergey Vinogradov <mr.vursen@gmail.com>
2023-05-31 09:34:03 +0300

    test: assert sync focus restoration except when outside click (#5913)

--- packages/overlay/test/helpers.js
+++ packages/overlay/test/helpers.js
@@ -12,20 +12,14 @@ export function createOverlay(text) {
   return overlay;
 }
 
-export function open(overlay) {
-  return new Promise((resolve) => {
-    overlay.addEventListener('vaadin-overlay-open', () => {
-      resolve();
-    });
-    overlay.opened = true;
-  });
-}
-
-export function close(overlay) {
-  return new Promise((resolve) => {
-    nextRender(overlay).then(() => {
-      resolve();
-    });
-    overlay.opened = false;
-  });
+/**
+ * Emulates clicking outside the dropdown overlay
+ */
+export function outsideClick() {
+  // Move focus to body
+  document.body.tabIndex = 0;
+  document.body.focus();
+  document.body.tabIndex = -1;
+  // Outside click
+  document.body.click();
 }
