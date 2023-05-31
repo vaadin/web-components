e03f9ce811c510d72214e6acd9549749262a82ab
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-05-31 08:33:05 +0200

    fix: do not reset scroll position when hiding grid (CP:23.3)

--- packages/component-base/src/virtualizer-iron-list-adapter.js
+++ packages/component-base/src/virtualizer-iron-list-adapter.js
@@ -377,6 +377,11 @@ export class IronListAdapter {
   toggleScrollListener() {}
 
   _scrollHandler() {
+    // The scroll target is hidden.
+    if (this.scrollTarget.offsetHeight === 0) {
+      return;
+    }
+
     this._adjustVirtualIndexOffset(this._scrollTop - (this.__previousScrollTop || 0));
     const delta = this.scrollTarget.scrollTop - this._scrollPosition;
 
--- packages/grid/test/hidden-grid.test.js
+++ packages/grid/test/hidden-grid.test.js
@@ -1,14 +1,16 @@
 import { expect } from '@esm-bundle/chai';
 import { fixtureSync, nextFrame, oneEvent } from '@vaadin/testing-helpers';
+import sinon from 'sinon';
 import '@vaadin/polymer-legacy-adapter/template-renderer.js';
 import '../vaadin-grid.js';
-import { flushGrid, getBodyCellContent, infiniteDataProvider } from './helpers.js';
+import { fire, flushGrid, getBodyCellContent, infiniteDataProvider } from './helpers.js';
 
 describe('hidden grid', () => {
   let grid;
 
-  beforeEach(() => {
-    grid = fixtureSync(`
+  describe('initially hidden', () => {
+    beforeEach(() => {
+      grid = fixtureSync(`
       <vaadin-grid style="height: 200px; width: 200px;" hidden size="1">
         <vaadin-grid-column>
           <template class="header">foo</template>
@@ -16,19 +18,55 @@ describe('hidden grid', () => {
         </vaadin-grid-column>
       </vaadin-grid>
     `);
-    grid.dataProvider = infiniteDataProvider;
-    flushGrid(grid);
+      grid.dataProvider = infiniteDataProvider;
+      flushGrid(grid);
+    });
+
+    it('should be hidden', () => {
+      expect(grid.offsetWidth).to.equal(0);
+      expect(grid.offsetHeight).to.equal(0);
+    });
+
+    it('should have content on appear', async () => {
+      grid.removeAttribute('hidden');
+      await oneEvent(grid, 'animationend');
+      await nextFrame();
+      expect(getBodyCellContent(grid, 0, 0).textContent).to.equal('0');
+    });
   });
 
-  it('should be hidden', () => {
-    expect(grid.offsetWidth).to.equal(0);
-    expect(grid.offsetHeight).to.equal(0);
-  });
+  describe('hiding the grid', () => {
+    beforeEach(() => {
+      grid = fixtureSync(`
+        <vaadin-grid size="1000">
+          <vaadin-grid-column header="foo"></vaadin-grid-column>
+        </vaadin-grid>
+      `);
+      grid.querySelector('vaadin-grid-column').renderer = (root, _, model) => {
+        root.textContent = model.index + 1;
+      };
+      grid.dataProvider = sinon.spy(infiniteDataProvider);
+      flushGrid(grid);
+      grid.dataProvider.resetHistory();
+    });
 
-  it('should have content on appear', async () => {
-    grid.removeAttribute('hidden');
-    await oneEvent(grid, 'animationend');
-    await nextFrame();
-    expect(getBodyCellContent(grid, 0, 0).textContent).to.equal('0');
+    it('should keep scroll position when hiding while triggering a scroll event', () => {
+      // Scroll grid
+      grid.$.table.scrollTop = 300;
+      flushGrid(grid);
+
+      // Hide, simulate scroll event
+      // This is a simplified reproduction of something triggering a scroll
+      // event at the same time the grid is hidden.
+      grid.style.display = 'none';
+      fire('scroll', {}, { node: grid.$.table });
+
+      // Should not refresh data when hidden
+      expect(grid.dataProvider.called).to.be.false;
+
+      // Show grid again, should retain scroll position
+      grid.style.display = 'block';
+      expect(grid.$.table.scrollTop).to.equal(300);
+    });
   });
 });
