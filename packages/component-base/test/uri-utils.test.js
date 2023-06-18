e5fd50825e716e3696785e1cee10e66d36d3f415
ugur-vaadin <ugur@vaadin.com>
2023-06-18 17:08:55 +0300

    fix: add missing curly bracket

--- packages/component-base/test/uri-utils.test.js
+++ packages/component-base/test/uri-utils.test.js
@@ -3,30 +3,31 @@ import { isMatchingPath } from '../src/uri-utils.js';
 
 describe('url-utils', () => {
   describe('isMatchingPath', () => {
-  const pathOptions = ['', '/', '/path', 'base/path'];
+    const paths = ['', '/', '/path', 'base/path'];
 
-  it('should return true when paths match', () => {
-    pathOptions.forEach((path) => expect(isMatchingPath(path, path)).to.be.true);
-  });
-
-  it('should return false when paths do not match', () => {
-    expect(isMatchingPath('/path1', '/path2')).to.be.false;
-  });
-
-  it('should return true if only one of the paths has trailing and leading spaces', () => {
-    pathOptions.forEach((path) => {
-      const pathWithExtraSpace = ` ${path} `;
-      expect(isMatchingPath(pathWithExtraSpace, path)).to.be.true;
-      expect(isMatchingPath(path, pathWithExtraSpace)).to.be.true;
+    it('should return true when paths match', () => {
+      paths.forEach((path) => expect(isMatchingPath(path, path)).to.be.true);
     });
-  });
 
-  it('should return true if only one of the paths has a leading slash', () => {
-    pathOptions.forEach((path) => {
-      const pathWithoutLeadingSlash = path.startsWith('/') ? path.substring(1) : path;
-      const pathWithLeadingSlash = path.startsWith('/') ? path : `/${path}`;
-      expect(isMatchingPath(pathWithoutLeadingSlash, pathWithLeadingSlash)).to.be.true;
-      expect(isMatchingPath(pathWithLeadingSlash, pathWithoutLeadingSlash)).to.be.true;
+    it('should return false when paths do not match', () => {
+      expect(isMatchingPath('/path1', '/path2')).to.be.false;
+    });
+
+    it('should return true if only one of the paths has trailing and leading spaces', () => {
+      paths.forEach((path) => {
+        const pathWithExtraSpace = ` ${path} `;
+        expect(isMatchingPath(pathWithExtraSpace, path)).to.be.true;
+        expect(isMatchingPath(path, pathWithExtraSpace)).to.be.true;
+      });
+    });
+
+    it('should return true if only one of the paths has a leading slash', () => {
+      paths.forEach((path) => {
+        const pathWithoutLeadingSlash = path.startsWith('/') ? path.substring(1) : path;
+        const pathWithLeadingSlash = path.startsWith('/') ? path : `/${path}`;
+        expect(isMatchingPath(pathWithoutLeadingSlash, pathWithLeadingSlash)).to.be.true;
+        expect(isMatchingPath(pathWithLeadingSlash, pathWithoutLeadingSlash)).to.be.true;
+      });
     });
   });
 });
