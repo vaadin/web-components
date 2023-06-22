4e7c97eb10342cc6177f4ce93d729f6cce3839e1
web-padawan <iamkulykov@gmail.com>
2023-06-22 16:31:59 +0300

    refactor: extract ChildrenController to separate file

---
+++ packages/side-nav/src/vaadin-side-nav-children-controller.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright (c) 2023 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
+
+/**
+ * A controller to manage the item content children slot.
+ */
+export class ChildrenController extends SlotController {
+  constructor(host) {
+    super(host, 'children', null, { observe: true, multiple: true });
+  }
+
+  /**
+   * @protected
+   * @override
+   */
+  initAddedNode() {
+    this.host.requestUpdate();
+  }
+
+  /**
+   * @protected
+   * @override
+   */
+  teardownNode() {
+    this.host.requestUpdate();
+  }
+}
