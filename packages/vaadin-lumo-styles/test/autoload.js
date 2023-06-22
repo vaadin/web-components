32c122e6edfc3164a223df6e5b1a0bdcd29b24ce
Artur <artur@vaadin.com>
2023-06-22 17:07:14 +0300

    fix: Set typography styles only on body, not also html (#6023)
    Fixes https://github.com/vaadin/flow/issues/17070

--- packages/vaadin-lumo-styles/test/autoload.js
+++ packages/vaadin-lumo-styles/test/autoload.js
@@ -3,10 +3,7 @@
  * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { color, typography } from '../all-imports.js';
-
-const style = document.createElement('style');
-style.innerHTML = `${color.toString()} ${typography.toString()}`;
-document.head.appendChild(style);
+import '../color-global.js';
+import '../typography-global.js';
 
 export * from '../all-imports.js';
---
+++ packages/vaadin-lumo-styles/test/visual/font-size.test.js
@@ -0,0 +1,26 @@
+import { fixtureSync, nextFrame } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../typography-global.js';
+
+describe('font-size', () => {
+  let wrapper;
+
+  before(async () => {
+    wrapper = fixtureSync(`
+      <div style="display: flex; flex-wrap: wrap; width: 600px">
+        <style>
+          html {
+            --lumo-font-size-m: 1.5rem;
+          }
+        </style>
+        <span id="test" style="font-size: 1.5em">Font size 1.5em</span>
+        <span id="test">Using font size 1.5rem from html definition</span>
+      </div>
+    `);
+    await nextFrame();
+  });
+
+  it('basic', async () => {
+    await visualDiff(wrapper, 'font-size');
+  });
+});
