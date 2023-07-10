6f66f6fd4688cfc4ff5eb8e42f55e0c6ab06d9f9
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-10 15:08:27 +0300

    refactor!: update combo-box overlay to not extend vaadin-overlay (#6134)

--- packages/combo-box/src/vaadin-combo-box-overlay.d.ts
+++ packages/combo-box/src/vaadin-combo-box-overlay.d.ts
@@ -3,13 +3,15 @@
  * Copyright (c) 2015 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { Overlay } from '@vaadin/overlay/src/vaadin-overlay.js';
+import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
+import { OverlayMixin } from '@vaadin/overlay/src/vaadin-overlay-mixin.js';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { ComboBoxOverlayMixin } from './vaadin-combo-box-overlay-mixin.js';
 
 /**
  * An element used internally by `<vaadin-combo-box>`. Not intended to be used separately.
  */
-declare class ComboBoxOverlay extends ComboBoxOverlayMixin(Overlay) {}
+declare class ComboBoxOverlay extends ComboBoxOverlayMixin(OverlayMixin(DirMixin(ThemableMixin(HTMLElement)))) {}
 
 declare global {
   interface HTMLElementTagNameMap {
