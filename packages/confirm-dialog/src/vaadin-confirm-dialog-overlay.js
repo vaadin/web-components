b4b8d00cceacd12db1ee7210ca053de634ccf042
web-padawan <iamkulykov@gmail.com>
2023-07-10 17:07:45 +0300

    refactor!: update confirm-dialog overlay to not extend dialog overlay

--- packages/confirm-dialog/src/vaadin-confirm-dialog-overlay.js
+++ packages/confirm-dialog/src/vaadin-confirm-dialog-overlay.js
@@ -3,91 +3,80 @@
  * Copyright (c) 2018 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
 import { Dialog } from '@vaadin/dialog/src/vaadin-dialog.js';
-import { DialogOverlay } from '@vaadin/dialog/src/vaadin-dialog-overlay.js';
-import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { dialogOverlay } from '@vaadin/dialog/src/vaadin-dialog-styles.js';
+import { OverlayMixin } from '@vaadin/overlay/src/vaadin-overlay-mixin.js';
+import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
+import { css, registerStyles, ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-registerStyles(
-  'vaadin-confirm-dialog-overlay',
-  css`
-    :host {
-      --_vaadin-confirm-dialog-content-width: auto;
-      --_vaadin-confirm-dialog-content-height: auto;
-    }
+const confirmDialogOverlay = css`
+  :host {
+    --_vaadin-confirm-dialog-content-width: auto;
+    --_vaadin-confirm-dialog-content-height: auto;
+  }
 
-    [part='overlay'] {
-      width: var(--_vaadin-confirm-dialog-content-width);
-      height: var(--_vaadin-confirm-dialog-content-height);
-    }
+  [part='overlay'] {
+    width: var(--_vaadin-confirm-dialog-content-width);
+    height: var(--_vaadin-confirm-dialog-content-height);
+  }
 
-    /* Make buttons clickable */
-    [part='footer'] > * {
-      pointer-events: all;
-    }
-  `,
-  { moduleId: 'vaadin-confirm-dialog-overlay-styles' },
-);
-
-let memoizedTemplate;
-
-const footerTemplate = html`
-  <div part="cancel-button">
-    <slot name="cancel-button"></slot>
-  </div>
-  <div part="reject-button">
-    <slot name="reject-button"></slot>
-  </div>
-  <div part="confirm-button">
-    <slot name="confirm-button"></slot>
-  </div>
+  /* Make buttons clickable */
+  [part='footer'] > * {
+    pointer-events: all;
+  }
 `;
 
+registerStyles('vaadin-confirm-dialog-overlay', [overlayStyles, dialogOverlay, confirmDialogOverlay], {
+  moduleId: 'vaadin-confirm-dialog-overlay-styles',
+});
+
 /**
- * An extension of `<vaadin-dialog-overlay>` used internally by `<vaadin-confirm-dialog>`.
- * Not intended to be used separately.
+ * An element used internally by `<vaadin-confirm-dialog>`. Not intended to be used separately.
+ *
+ * @extends HTMLElement
+ * @mixes DirMixin
+ * @mixes OverlayMixin
+ * @mixes ThemableMixin
  * @private
  */
-class ConfirmDialogOverlay extends DialogOverlay {
+class ConfirmDialogOverlay extends OverlayMixin(DirMixin(ThemableMixin(PolymerElement))) {
   static get is() {
     return 'vaadin-confirm-dialog-overlay';
   }
 
   static get template() {
-    if (!memoizedTemplate) {
-      memoizedTemplate = super.template.cloneNode(true);
-
-      // Replace two header slots with a single one
-      const headerPart = memoizedTemplate.content.querySelector('[part="header"]');
-      headerPart.innerHTML = '';
-      const headerSlot = document.createElement('slot');
-      headerSlot.setAttribute('name', 'header');
-      headerPart.appendChild(headerSlot);
-
-      // Place default slot inside a "message" part
-      const contentPart = memoizedTemplate.content.querySelector('[part="content"]');
-      const defaultSlot = contentPart.querySelector('slot:not([name])');
-      const messagePart = document.createElement('div');
-      messagePart.setAttribute('part', 'message');
-      contentPart.appendChild(messagePart);
-      messagePart.appendChild(defaultSlot);
-
-      // Replace footer slot with button named slots
-      const footerPart = memoizedTemplate.content.querySelector('[part="footer"]');
-      footerPart.setAttribute('role', 'toolbar');
-      const footerSlot = footerPart.querySelector('slot');
-      footerPart.removeChild(footerSlot);
-      footerPart.appendChild(footerTemplate.content.cloneNode(true));
-    }
-    return memoizedTemplate;
+    return html`
+      <div part="backdrop" id="backdrop" hidden$="[[!withBackdrop]]"></div>
+      <div part="overlay" id="overlay" tabindex="0">
+        <section id="resizerContainer" class="resizer-container">
+          <header part="header"><slot name="header"></slot></header>
+          <div part="content" id="content">
+            <div part="message"><slot></slot></div>
+          </div>
+          <footer part="footer" role="toolbar">
+            <div part="cancel-button">
+              <slot name="cancel-button"></slot>
+            </div>
+            <div part="reject-button">
+              <slot name="reject-button"></slot>
+            </div>
+            <div part="confirm-button">
+              <slot name="confirm-button"></slot>
+            </div>
+          </footer>
+        </section>
+      </div>
+    `;
   }
 
   /**
    * @protected
    * @override
    */
-  _headerFooterRendererChange(headerRenderer, footerRenderer, opened) {
-    super._headerFooterRendererChange(headerRenderer, footerRenderer, opened);
+  ready() {
+    super.ready();
 
     // ConfirmDialog has header and footer but does not use renderers
     this.setAttribute('has-header', '');
