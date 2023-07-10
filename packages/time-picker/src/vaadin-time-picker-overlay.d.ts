8b1b58d8bdde5db5b7a1ae5c70757046ed6b15e1
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-10 15:14:08 +0300

    refactor!: update time-picker overlay to not extend vaadin-overlay (#6136)

--- packages/time-picker/src/vaadin-time-picker-overlay.d.ts
+++ packages/time-picker/src/vaadin-time-picker-overlay.d.ts
@@ -4,12 +4,14 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { ComboBoxOverlayMixin } from '@vaadin/combo-box/src/vaadin-combo-box-overlay-mixin.js';
-import { Overlay } from '@vaadin/overlay/src/vaadin-overlay.js';
+import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
+import { OverlayMixin } from '@vaadin/overlay/src/vaadin-overlay-mixin.js';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
 /**
  * An element used internally by `<vaadin-time-picker>`. Not intended to be used separately.
  */
-declare class TimePickerOverlay extends ComboBoxOverlayMixin(Overlay) {}
+declare class TimePickerOverlay extends ComboBoxOverlayMixin(OverlayMixin(DirMixin(ThemableMixin(HTMLElement)))) {}
 
 declare global {
   interface HTMLElementTagNameMap {
--- packages/time-picker/theme/lumo/vaadin-time-picker.js
+++ packages/time-picker/theme/lumo/vaadin-time-picker.js
@@ -4,6 +4,5 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import '@vaadin/input-container/theme/lumo/vaadin-input-container.js';
-import '@vaadin/overlay/theme/lumo/vaadin-overlay.js';
 import './vaadin-time-picker-styles.js';
 import '../../src/vaadin-time-picker.js';
