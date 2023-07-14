fc253d2f41e69c7e1a3ab2bd56864a0ed51039b2
Sergey Vinogradov <mr.vursen@gmail.com>
2023-07-14 16:25:38 +0300

    minimize the diff

--- packages/combo-box/src/vaadin-combo-box.js
+++ packages/combo-box/src/vaadin-combo-box.js
@@ -255,6 +255,22 @@ class ComboBox extends ComboBoxDataProviderMixin(
   }
 
   /**
+   * Override method inherited from `FocusMixin` to validate on blur.
+   * @param {boolean} focused
+   * @protected
+   * @override
+   */
+  _setFocused(focused) {
+    super._setFocused(focused);
+
+    // Do not validate when focusout is caused by document
+    // losing focus, which happens on browser tab switch.
+    if (!focused && document.hasFocus()) {
+      this.validate();
+    }
+  }
+
+  /**
    * Override the method from `InputControlMixin`
    * to stop event propagation to prevent `ComboBoxMixin`
    * from handling this click event also on its own.
@@ -278,22 +294,6 @@ class ComboBox extends ComboBoxDataProviderMixin(
     // Open dropdown only when clicking on the label or input field
     if (path.includes(this._labelNode) || path.includes(this._positionTarget)) {
       super._onHostClick(event);
-    }
-  }
-
-  /**
-   * Override method inherited from `FocusMixin` to validate on blur.
-   * @param {boolean} focused
-   * @protected
-   * @override
-   */
-  _setFocused(focused) {
-    super._setFocused(focused);
-
-    // Do not validate when focusout is caused by document
-    // losing focus, which happens on browser tab switch.
-    if (!focused && document.hasFocus()) {
-      this.validate();
     }
   }
 }
