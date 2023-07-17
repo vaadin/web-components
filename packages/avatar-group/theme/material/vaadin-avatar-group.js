09bb7f94f585fb0d09fcc014cb28d085514ca95d
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-17 17:01:16 +0300

    refactor!: update avatar-group overlay to not extend vaadin-overlay (#6179)

--- packages/avatar-group/theme/material/vaadin-avatar-group.js
+++ packages/avatar-group/theme/material/vaadin-avatar-group.js
@@ -1,4 +1,3 @@
 import '@vaadin/avatar/theme/material/vaadin-avatar.js';
-import '@vaadin/overlay/theme/material/vaadin-overlay.js';
 import './vaadin-avatar-group-styles.js';
 import '../../src/vaadin-avatar-group.js';
