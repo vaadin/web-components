3c2aeb7e70851dc979196d3179a565baad7e8125
web-padawan <iamkulykov@gmail.com>
2023-07-18 21:08:12 +0300

    docs: re-export typings from Polymer entrypoint

--- packages/dialog/src/vaadin-lit-dialog-overlay.d.ts
+++ packages/dialog/src/vaadin-lit-dialog-overlay.d.ts
@@ -3,17 +3,4 @@
  * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
-import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import { DialogOverlayMixin } from './vaadin-dialog-overlay-mixin.js';
-
-/**
- * An element used internally by `<vaadin-dialog>`. Not intended to be used separately.
- */
-export class DialogOverlay extends DialogOverlayMixin(DirMixin(ThemableMixin(HTMLElement))) {}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'vaadin-dialog-overlay': DialogOverlay;
-  }
-}
+export * from './vaadin-dialog-overlay.js';
