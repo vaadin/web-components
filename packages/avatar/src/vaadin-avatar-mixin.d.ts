48574920750a739be54b07227b75aa3fa293e453
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-18 09:49:18 +0300

    Update packages/avatar/src/vaadin-avatar-mixin.d.ts

--- packages/avatar/src/vaadin-avatar-mixin.d.ts
+++ packages/avatar/src/vaadin-avatar-mixin.d.ts
@@ -4,7 +4,7 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import type { Constructor } from '@open-wc/dedupe-mixin';
-import type { FocusMixinClass } from '@vaadin/a11y-base/src/focus-mixin';
+import type { FocusMixinClass } from '@vaadin/a11y-base/src/focus-mixin.js';
 
 export interface AvatarI18n {
   anonymous: string;
