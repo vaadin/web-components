f773f25de83686c3f5895b81cc3925e7dc3c5aba
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-07-18 16:14:42 +0200

    refactor: reuse existing TS definitions for Lit-based elements

--- packages/avatar/src/vaadin-lit-avatar.d.ts
+++ packages/avatar/src/vaadin-lit-avatar.d.ts
@@ -3,29 +3,4 @@
  * Copyright (c) 2020 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { LitElement } from 'lit';
-import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
-import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
-import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import { AvatarMixin } from './vaadin-avatar-mixin.js';
-
-export { AvatarI18n } from './vaadin-avatar-mixin.js';
-
-/**
- * LitElement based version of `<vaadin-avatar>` web component.
- *
- * ## Disclaimer
- *
- * This component is an experiment not intended for publishing to npm.
- * There is no ETA regarding specific Vaadin version where it'll land.
- * Feel free to try this code in your apps as per Apache 2.0 license.
- */
-declare class Avatar extends AvatarMixin(ElementMixin(ThemableMixin(PolylitMixin(LitElement)))) {}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'vaadin-avatar': Avatar;
-  }
-}
-
-export { Avatar };
+export * from './vaadin-avatar.js';
