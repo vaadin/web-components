44bc0bdeb4e7ecb92c0af8270b6017d9d1e9dda7
web-padawan <iamkulykov@gmail.com>
2023-07-20 12:28:51 +0300

    refactor!: update menu-bar overlay to not extend vaadin-overlay

--- packages/menu-bar/src/vaadin-menu-bar-overlay.js
+++ packages/menu-bar/src/vaadin-menu-bar-overlay.js
@@ -3,23 +3,42 @@
  * Copyright (c) 2019 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
 import { MenuOverlayMixin } from '@vaadin/context-menu/src/vaadin-menu-overlay-mixin.js';
 import { styles } from '@vaadin/context-menu/src/vaadin-menu-overlay-styles.js';
-import { Overlay } from '@vaadin/overlay/src/vaadin-overlay.js';
-import { registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { OverlayMixin } from '@vaadin/overlay/src/vaadin-overlay-mixin.js';
+import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
+import { registerStyles, ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-registerStyles('vaadin-menu-bar-overlay', styles, { moduleId: 'vaadin-menu-bar-overlay-styles' });
+registerStyles('vaadin-menu-bar-overlay', [overlayStyles, styles], {
+  moduleId: 'vaadin-menu-bar-overlay-styles',
+});
 
 /**
  * An element used internally by `<vaadin-menu-bar>`. Not intended to be used separately.
  *
- * @extends Overlay
+ * @extends HTMLElement
+ * @mixes DirMixin
  * @mixes MenuOverlayMixin
- * @private
+ * @mixes OverlayMixin
+ * @mixes ThemableMixin
+ * @protected
  */
-class MenuBarOverlay extends MenuOverlayMixin(Overlay) {
+export class MenuBarOverlay extends MenuOverlayMixin(OverlayMixin(DirMixin(ThemableMixin(PolymerElement)))) {
   static get is() {
     return 'vaadin-menu-bar-overlay';
+  }
+
+  static get template() {
+    return html`
+      <div id="backdrop" part="backdrop" hidden$="[[!withBackdrop]]"></div>
+      <div part="overlay" id="overlay" tabindex="0">
+        <div part="content" id="content">
+          <slot></slot>
+        </div>
+      </div>
+    `;
   }
 }
 
