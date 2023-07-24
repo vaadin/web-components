0663799b893961973f0a5af7c3e5ff2cc387f8c6
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-24 14:10:43 +0300

    Apply suggestions from code review
    Co-authored-by: Sergey Vinogradov <mr.vursen@gmail.com>

--- packages/component-base/test/slot-observer.test.js
+++ packages/component-base/test/slot-observer.test.js
@@ -27,9 +27,9 @@ describe('SlotObserver', () => {
 
     expect(addedNodes).to.be.an('array');
     expect(addedNodes.length).to.equal(3);
-    expect(addedNodes[0]).to.eql(host.childNodes[0]);
-    expect(addedNodes[1]).to.eql(host.childNodes[1]);
-    expect(addedNodes[2]).to.eql(host.childNodes[2]);
+    expect(addedNodes[0]).to.equal(host.childNodes[0]);
+    expect(addedNodes[1]).to.equal(host.childNodes[1]);
+    expect(addedNodes[2]).to.equal(host.childNodes[2]);
   });
 
   it('should run callback asynchronously after node is added', async () => {
@@ -46,7 +46,7 @@ describe('SlotObserver', () => {
     expect(spy.calledOnce).to.be.true;
     const addedNodes = spy.firstCall.args[0].addedNodes;
     expect(addedNodes.length).to.equal(1);
-    expect(addedNodes[0]).to.eql(div);
+    expect(addedNodes[0]).to.equal(div);
   });
 
   it('should run callback asynchronously after node is removed', async () => {
@@ -63,7 +63,7 @@ describe('SlotObserver', () => {
     expect(spy.calledOnce).to.be.true;
     const removedNodes = spy.firstCall.args[0].removedNodes;
     expect(removedNodes.length).to.equal(1);
-    expect(removedNodes[0]).to.eql(div);
+    expect(removedNodes[0]).to.equal(div);
   });
 
   it('should run callback asynchronously after node is moved', async () => {
@@ -80,8 +80,8 @@ describe('SlotObserver', () => {
     expect(spy.calledOnce).to.be.true;
     const movedNodes = spy.firstCall.args[0].movedNodes;
     expect(movedNodes.length).to.equal(2);
-    expect(movedNodes[0]).to.eql(nodes[1]);
-    expect(movedNodes[1]).to.eql(nodes[0]);
+    expect(movedNodes[0]).to.equal(nodes[1]);
+    expect(movedNodes[1]).to.equal(nodes[0]);
   });
 
   it('should run callback synchronously when calling flush()', () => {
@@ -96,6 +96,6 @@ describe('SlotObserver', () => {
 
     expect(spy.calledOnce).to.be.true;
     const addedNodes = spy.firstCall.args[0].addedNodes;
-    expect(addedNodes[0]).to.eql(div);
+    expect(addedNodes[0]).to.equal(div);
   });
 });
