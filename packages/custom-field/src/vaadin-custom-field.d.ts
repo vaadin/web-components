85c98aae110cb53969d64717c33ffefb09e4da4a
web-padawan <iamkulykov@gmail.com>
2023-07-25 15:49:54 +0300

    refactor: extract custom-field logic to reusable mixin

--- packages/custom-field/src/vaadin-custom-field.d.ts
+++ packages/custom-field/src/vaadin-custom-field.d.ts
@@ -9,10 +9,6 @@ import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { FieldMixin } from '@vaadin/field-base/src/field-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-export type CustomFieldParseValueFn = (value: string) => unknown[];
-
-export type CustomFieldFormatValueFn = (inputValues: unknown[]) => string;
-
 /**
  * Fired when the user commits a value change.
  */
@@ -100,59 +96,6 @@ export interface CustomFieldEventMap extends HTMLElementEventMap, CustomFieldCus
  * @fires {CustomEvent} validated - Fired whenever the field is validated.
  */
 declare class CustomField extends FieldMixin(FocusMixin(KeyboardMixin(ThemableMixin(ElementMixin(HTMLElement))))) {
-  /**
-   * Array of available input nodes
-   */
-  readonly inputs: HTMLElement[] | undefined;
-
-  /**
-   * A function to format the values of the individual fields contained by
-   * the custom field into a single component value. The function receives
-   * an array of all values of the individual fields in the order of their
-   * presence in the DOM, and must return a single component value.
-   * This function is called each time a value of an internal field is
-   * changed.
-   *
-   * Example:
-   * ```js
-   * customField.formatValue = (fieldValues) => {
-   *   return fieldValues.join("-");
-   * }
-   * ```
-   */
-  formatValue: CustomFieldFormatValueFn | undefined;
-
-  /**
-   * A function to parse the component value into values for the individual
-   * fields contained by the custom field. The function receives the
-   * component value, and must return an array of values for the individual
-   * fields in the order of their presence in the DOM.
-   * The function is called each time the value of the component changes.
-   *
-   * Example:
-   * ```js
-   * customField.parseValue = (componentValue) => {
-   *   return componentValue.split("-");
-   * }
-   * ```
-   */
-  parseValue: CustomFieldParseValueFn | undefined;
-
-  /**
-   * The name of the control, which is submitted with the form data.
-   */
-  name: string | null | undefined;
-
-  /**
-   * The value of the field. When wrapping several inputs, it will contain `\t`
-   * (Tab character) as a delimiter indicating parts intended to be used as the
-   * corresponding inputs values.
-   * Use the [`formatValue`](#/elements/vaadin-custom-field#property-formatValue)
-   * and [`parseValue`](#/elements/vaadin-custom-field#property-parseValue)
-   * properties to customize this behavior.
-   */
-  value: string | null | undefined;
-
   addEventListener<K extends keyof CustomFieldEventMap>(
     type: K,
     listener: (this: CustomField, ev: CustomFieldEventMap[K]) => void,
