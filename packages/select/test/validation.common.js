fa1b30507a7e059b82b4780c8e62cbe8bf128dce
Sergey Vinogradov <mr.vursen@gmail.com>
2023-07-26 17:58:16 +0300

    test: improve validation test coverage

--- packages/select/test/validation.common.js
+++ packages/select/test/validation.common.js
@@ -1,30 +1,34 @@
 import { expect } from '@esm-bundle/chai';
-import { enterKeyDown, escKeyDown, fixtureSync, nextRender, nextUpdate, oneEvent } from '@vaadin/testing-helpers';
+import {
+  enterKeyDown,
+  escKeyDown,
+  fixtureSync,
+  nextRender,
+  nextUpdate,
+  oneEvent,
+  outsideClick,
+} from '@vaadin/testing-helpers';
+import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
 import '@vaadin/item/vaadin-item.js';
 import '@vaadin/list-box/vaadin-list-box.js';
-import { html, render } from 'lit';
 
 describe('validation', () => {
-  let select, valueButton;
+  let select, valueButton, validateSpy, changeSpy;
 
   describe('basic', () => {
     beforeEach(async () => {
       select = fixtureSync('<vaadin-select></vaadin-select>');
+      select.items = [
+        { label: 'Option 1', value: '' },
+        { label: 'Option 2', value: 'option-2' },
+        { label: 'Option 3', value: 'option-3' },
+      ];
       await nextRender();
-      select.renderer = (root) => {
-        render(
-          html`
-            <vaadin-list-box>
-              <vaadin-item value="v1" label="o2">Option 2</vaadin-item>
-              <vaadin-item value="v2" label="o2">Option 2</vaadin-item>
-              <vaadin-item value="">Option 3</vaadin-item>
-            </vaadin-list-box>
-          `,
-          root,
-        );
-      };
       valueButton = select.querySelector('vaadin-select-value-button');
+      validateSpy = sinon.spy(select, 'validate');
+      changeSpy = sinon.spy();
+      select.addEventListener('change', changeSpy);
     });
 
     it('should pass the validation when the field is valid', () => {
@@ -61,10 +65,9 @@ describe('validation', () => {
       select.required = true;
       await nextUpdate(select);
 
-      const spy = sinon.spy(select, 'validate');
       select.required = false;
       await nextUpdate(select);
-      expect(spy.calledOnce).to.be.true;
+      expect(validateSpy.calledOnce).to.be.true;
     });
 
     it('should update invalid state when required property is removed', async () => {
@@ -79,32 +82,47 @@ describe('validation', () => {
       expect(select.invalid).to.be.false;
     });
 
-    it('should validate when closing the overlay', async () => {
-      const spy = sinon.spy(select, 'validate');
-      select.opened = true;
-      await nextUpdate(select);
-
-      select.opened = false;
-      await nextUpdate(select);
-      expect(spy.called).to.be.true;
+    it('should validate on blur', () => {
+      select.focus();
+      select.blur();
+      expect(validateSpy.called).to.be.true;
     });
 
     it('should validate on programmatic blur', () => {
-      const spy = sinon.spy(select, 'validate');
       select.blur();
+      expect(validateSpy.called).to.be.true;
+    });
 
-      expect(spy.called).to.be.true;
+    it('should validate on outside click', async () => {
+      select.focus();
+      valueButton.click();
+      await nextRender();
+
+      outsideClick();
+      await nextUpdate(select);
+      expect(validateSpy.called).to.be.true;
+    });
+
+    it('should validate before change event on Enter', async () => {
+      select.focus();
+      valueButton.click();
+      await nextRender();
+
+      await sendKeys({ press: 'Enter' });
+      await nextUpdate(select);
+      expect(changeSpy.calledOnce).to.be.true;
+      expect(validateSpy.called).to.be.true;
+      expect(validateSpy.calledBefore(changeSpy)).to.be.true;
     });
 
     it('should validate when setting value property', async () => {
-      const spy = sinon.spy(select, 'validate');
-      select.value = 'v2';
+      select.value = 'option-2';
       await nextUpdate(select);
-      expect(spy.callCount).to.be.equal(1);
+      expect(validateSpy.callCount).to.be.equal(1);
 
       select.value = '';
       await nextUpdate(select);
-      expect(spy.callCount).to.be.equal(2);
+      expect(validateSpy.callCount).to.be.equal(2);
     });
 
     it('should fire a validated event on validation success', () => {
@@ -139,10 +157,8 @@ describe('validation', () => {
       });
 
       it('should not validate on blur when document does not have focus', () => {
-        const spy = sinon.spy(select, 'validate');
         select.blur();
-
-        expect(spy.called).to.be.false;
+        expect(validateSpy.called).to.be.false;
       });
     });
   });
