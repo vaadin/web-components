fb95038894b65eb98668c8072c19668aac57ecb8
Serhii Kulykov <iamkulykov@gmail.com>
2023-07-26 12:39:13 +0300

    refactor: use slot controllers to handle CRUD grid and form (#6232)

---
+++ packages/crud/src/vaadin-crud-controllers.d.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2023 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
+ */
+import { SlotController } from '@vaadin/component-base/src/slot-controller.js';
+
+/**
+ * A controller for initializing slotted button.
+ */
+export class ButtonSlotController extends SlotController {
+  constructor(host: HTMLElement);
+}
+
+/**
+ * A controller for initializing slotted form.
+ */
+export class FormSlotController extends SlotController {
+  constructor(host: HTMLElement);
+}
+
+/**
+ * A controller for initializing slotted grid.
+ */
+export class GridSlotController extends SlotController {
+  constructor(host: HTMLElement);
+}
