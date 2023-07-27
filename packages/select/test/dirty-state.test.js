b8202a3d6505e077fbc8b4eb12727d77698ca5e4
Sergey Vinogradov <mr.vursen@gmail.com>
2023-07-27 09:21:44 +0300

    remove unused imports

--- packages/select/test/dirty-state.test.js
+++ packages/select/test/dirty-state.test.js
@@ -1,5 +1,5 @@
 import { expect } from '@esm-bundle/chai';
-import { fixtureSync, nextFrame, nextRender, outsideClick } from '@vaadin/testing-helpers';
+import { fixtureSync, nextRender, outsideClick } from '@vaadin/testing-helpers';
 import { sendKeys } from '@web/test-runner-commands';
 import sinon from 'sinon';
 import '../src/vaadin-select.js';
