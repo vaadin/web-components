24037c38d78d7ac82a8cc56ba53974ce4a99e092
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2023-08-25 09:09:26 +0300

    chore: remove side nav feature flag (#6071)

--- packages/side-nav/test/side-nav.test.js
+++ packages/side-nav/test/side-nav.test.js
@@ -1,7 +1,6 @@
 import { expect } from '@esm-bundle/chai';
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
-import '../enable.js';
 import '../vaadin-side-nav.js';
 
 describe('side-nav', () => {
