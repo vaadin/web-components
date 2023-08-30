fed947276a870c69c7f680dbe7dd587e18e874fe
ugur-vaadin <ugur@vaadin.com>
2023-08-30 15:59:28 +0300

    fix: handle the case with no cells

--- packages/grid/src/vaadin-grid-keyboard-navigation-mixin.js
+++ packages/grid/src/vaadin-grid-keyboard-navigation-mixin.js
@@ -689,9 +689,12 @@ export const KeyboardNavigationMixin = (superClass) =>
         // Assume frozen columns to always be inside the viewport
         return true;
       }
-      const firstCell = column._allCells.at(0);
+      const allCells = column._allCells;
+      if (!allCells) {
+        return false;
+      }
+      const firstCell = allCells.at(0);
       return (
-        firstCell &&
         firstCell.offsetLeft + firstCell.offsetWidth >= this._scrollLeft &&
         firstCell.offsetLeft <= this._scrollLeft + this.clientWidth
       );
