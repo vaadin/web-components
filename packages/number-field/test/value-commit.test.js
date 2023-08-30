9ca6191dfe57473d829e51c41d0a430095121451
Sergey Vinogradov <mr.vursen@gmail.com>
2023-08-30 17:10:26 +0300

    test: improve test coverage of value commit scenarios

---
+++ packages/number-field/test/value-commit.test.js
@@ -0,0 +1,251 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync, nextRender, outsideClick } from '@vaadin/testing-helpers';
+import { sendKeys } from '@web/test-runner-commands';
+import sinon from 'sinon';
+import '../src/vaadin-number-field.js';
+
+describe('value commit', () => {
+  let numberField, valueChangedSpy, validateSpy, changeSpy;
+
+  function expectNoValueCommit() {
+    expect(valueChangedSpy).to.be.not.called;
+    expect(validateSpy).to.be.not.called;
+    expect(changeSpy).to.be.not.called;
+  }
+
+  function expectValueCommit(value) {
+    expect(valueChangedSpy).to.be.calledOnce;
+    // TODO: Optimize the number of validation runs.
+    expect(validateSpy).to.be.called;
+    expect(validateSpy.firstCall).to.be.calledAfter(valueChangedSpy.firstCall);
+    expect(changeSpy).to.be.calledOnce;
+    expect(changeSpy.firstCall).to.be.calledAfter(validateSpy.firstCall);
+    expect(numberField.value).to.equal(value);
+  }
+
+  function expectValidationOnly() {
+    expect(valueChangedSpy).to.be.not.called;
+    expect(validateSpy).to.be.calledOnce;
+    expect(changeSpy).to.be.not.called;
+  }
+
+  beforeEach(async () => {
+    numberField = fixtureSync(`<vaadin-number-field></vaadin-number-field>`);
+    await nextRender();
+    validateSpy = sinon.spy(numberField, 'validate').named('validateSpy');
+
+    valueChangedSpy = sinon.spy().named('valueChangedSpy');
+    numberField.addEventListener('value-changed', valueChangedSpy);
+
+    changeSpy = sinon.spy().named('changeSpy');
+    numberField.addEventListener('change', changeSpy);
+
+    numberField.focus();
+  });
+
+  describe('default', () => {
+    it('should not commit but validate on blur', () => {
+      numberField.blur();
+      expectValidationOnly();
+    });
+
+    it('should not commit on Enter', async () => {
+      await sendKeys({ press: 'Enter' });
+      expectNoValueCommit();
+    });
+
+    it('should not commit on Escape', async () => {
+      await sendKeys({ press: 'Escape' });
+      expectNoValueCommit();
+    });
+  });
+
+  describe('entering parsable input', () => {
+    beforeEach(async () => {
+      await sendKeys({ type: '1' });
+    });
+
+    it('should commit on blur', () => {
+      numberField.blur();
+      expectValueCommit('1');
+    });
+
+    it('should commit on Enter', async () => {
+      await sendKeys({ press: 'Enter' });
+      expectValueCommit('1');
+    });
+  });
+
+  describe('entering unparsable input', () => {
+    beforeEach(async () => {
+      await sendKeys({ type: '-' });
+    });
+
+    it('should not commit by default', () => {
+      expectNoValueCommit();
+    });
+
+    it('should not commit but validate on blur', () => {
+      numberField.blur();
+      expectValidationOnly();
+      expect(numberField.inputElement.validity.badInput).to.be.true;
+    });
+
+    it('should not commit but validate on Enter', async () => {
+      await sendKeys({ press: 'Enter' });
+      expectValidationOnly();
+      expect(numberField.inputElement.validity.badInput).to.be.true;
+    });
+
+    describe('clearing input with Backspace', () => {
+      beforeEach(async () => {
+        await sendKeys({ press: 'Enter' });
+        valueChangedSpy.resetHistory();
+        validateSpy.resetHistory();
+
+        numberField.inputElement.select();
+        await sendKeys({ press: 'Backspace' });
+      });
+
+      it('should not commit but validate on blur', () => {
+        numberField.blur();
+        expectValidationOnly();
+      });
+
+      it('should not commit but validate on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectValidationOnly();
+      });
+    });
+  });
+
+  describe('with value', () => {
+    beforeEach(() => {
+      numberField.value = '1234';
+      valueChangedSpy.resetHistory();
+      validateSpy.resetHistory();
+    });
+
+    describe('default', () => {
+      it('should not commit but validate on blur', () => {
+        numberField.blur();
+        expectValidationOnly();
+      });
+
+      it('should not commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectNoValueCommit();
+      });
+
+      it('should not commit on Escape', async () => {
+        await sendKeys({ press: 'Escape' });
+        expectNoValueCommit();
+      });
+    });
+
+    describe('entering parsable input', () => {
+      beforeEach(async () => {
+        numberField.inputElement.select();
+        await sendKeys({ type: '1' });
+      });
+
+      it('should commit on blur', () => {
+        numberField.blur();
+        expectValueCommit('1');
+      });
+
+      it('should commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectValueCommit('1');
+      });
+
+      it('should commit on close with outside click', () => {
+        outsideClick();
+        expectValueCommit('1');
+      });
+    });
+
+    describe('entering unparsable input', () => {
+      beforeEach(async () => {
+        numberField.inputElement.select();
+        await sendKeys({ type: '-' });
+      });
+
+      it('should commit an empty value on blur', () => {
+        numberField.blur();
+        expectValueCommit('');
+        expect(numberField.inputElement.validity.badInput).to.be.true;
+      });
+
+      it('should commit an empty value on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectValueCommit('');
+        expect(numberField.inputElement.validity.badInput).to.be.true;
+      });
+    });
+
+    describe('clearing input with Backspace', () => {
+      beforeEach(async () => {
+        numberField.inputElement.select();
+        await sendKeys({ press: 'Backspace' });
+      });
+
+      it('should commit on blur', () => {
+        numberField.blur();
+        expectValueCommit('');
+      });
+
+      it('should commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectValueCommit('');
+      });
+    });
+
+    describe('with clear button', () => {
+      beforeEach(() => {
+        numberField.clearButtonVisible = true;
+      });
+
+      it('should clear on clear button click', () => {
+        numberField.$.clearButton.click();
+        expectValueCommit('');
+      });
+
+      it('should clear on Escape', async () => {
+        await sendKeys({ press: 'Escape' });
+        expectValueCommit('');
+      });
+    });
+  });
+
+  describe('stepping', () => {
+    it('should commit on ArrowDown', async () => {
+      await sendKeys({ press: 'ArrowDown' });
+      expectValueCommit('-1');
+    });
+
+    it('should commit on ArrowUp', async () => {
+      await sendKeys({ press: 'ArrowUp' });
+      expectValueCommit('1');
+    });
+
+    describe('with value committed using an arrow key', () => {
+      beforeEach(async () => {
+        await sendKeys({ press: 'ArrowDown' });
+        valueChangedSpy.resetHistory();
+        validateSpy.resetHistory();
+        changeSpy.resetHistory();
+      });
+
+      it('should not commit but validate on blur', () => {
+        numberField.blur();
+        expectValidationOnly();
+      });
+
+      it('should not commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectNoValueCommit();
+      });
+    });
+  });
+});
