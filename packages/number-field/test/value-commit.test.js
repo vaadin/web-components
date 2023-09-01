466436fa3337569780ef5b352e0cb2f14cc04e19
Sergey Vinogradov <mr.vursen@gmail.com>
2023-09-01 12:43:45 +0300

    reorganize new tests, remove old tests

--- packages/number-field/test/value-commit.test.js
+++ packages/number-field/test/value-commit.test.js
@@ -60,7 +60,7 @@ describe('value commit', () => {
     });
   });
 
-  describe('entering parsable input', () => {
+  describe('parsable input entered', () => {
     beforeEach(async () => {
       await sendKeys({ type: '1' });
     });
@@ -76,7 +76,34 @@ describe('value commit', () => {
     });
   });
 
-  describe('entering unparsable input', () => {
+  describe('parsable input committed', () => {
+    beforeEach(async () => {
+      await sendKeys({ type: '1' });
+      await sendKeys({ press: 'Enter' });
+      changeSpy.resetHistory();
+      valueChangedSpy.resetHistory();
+      validateSpy.resetHistory();
+    });
+
+    describe('input cleared with Backspace', () => {
+      beforeEach(async () => {
+        numberField.inputElement.select();
+        await sendKeys({ press: 'Backspace' });
+      });
+
+      it('should commit on blur', () => {
+        numberField.blur();
+        expectValueCommit('');
+      });
+
+      it('should commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectValueCommit('');
+      });
+    });
+  });
+
+  describe('unparsable input entered', () => {
     beforeEach(async () => {
       await sendKeys({ type: '-' });
     });
@@ -96,13 +123,17 @@ describe('value commit', () => {
       expectValidationOnly();
       expect(numberField.inputElement.validity.badInput).to.be.true;
     });
+  });
 
-    describe('clearing input with Backspace', () => {
+  describe('unparsable input committed', () => {
+    beforeEach(async () => {
+      await sendKeys({ type: '-' });
+      await sendKeys({ type: 'Enter' });
+      validateSpy.resetHistory();
+    });
+
+    describe('input cleared with Backspace', () => {
       beforeEach(async () => {
-        await sendKeys({ press: 'Enter' });
-        valueChangedSpy.resetHistory();
-        validateSpy.resetHistory();
-
         numberField.inputElement.select();
         await sendKeys({ press: 'Backspace' });
       });
@@ -119,7 +150,7 @@ describe('value commit', () => {
     });
   });
 
-  describe('with value', () => {
+  describe('value set programmatically', () => {
     beforeEach(() => {
       numberField.value = '1234';
       valueChangedSpy.resetHistory();
@@ -143,7 +174,7 @@ describe('value commit', () => {
       });
     });
 
-    describe('entering parsable input', () => {
+    describe('parsable input entered', () => {
       beforeEach(async () => {
         numberField.inputElement.select();
         await sendKeys({ type: '1' });
@@ -165,7 +196,7 @@ describe('value commit', () => {
       });
     });
 
-    describe('entering unparsable input', () => {
+    describe('unparsable input entered', () => {
       beforeEach(async () => {
         numberField.inputElement.select();
         await sendKeys({ type: '-' });
@@ -183,55 +214,78 @@ describe('value commit', () => {
         expect(numberField.inputElement.validity.badInput).to.be.true;
       });
     });
+  });
 
-    describe('clearing input with Backspace', () => {
-      beforeEach(async () => {
-        numberField.inputElement.select();
-        await sendKeys({ press: 'Backspace' });
-      });
-
-      it('should commit on blur', () => {
-        numberField.blur();
-        expectValueCommit('');
-      });
-
-      it('should commit on Enter', async () => {
-        await sendKeys({ press: 'Enter' });
-        expectValueCommit('');
-      });
+  describe('with clear button', () => {
+    beforeEach(() => {
+      numberField.value = '1';
+      numberField.clearButtonVisible = true;
+      valueChangedSpy.resetHistory();
     });
 
-    describe('with clear button', () => {
-      beforeEach(() => {
-        numberField.clearButtonVisible = true;
-      });
+    it('should clear on clear button click', () => {
+      numberField.$.clearButton.click();
+      expectValueCommit('');
+    });
 
-      it('should clear on clear button click', () => {
-        numberField.$.clearButton.click();
-        expectValueCommit('');
-      });
-
-      it('should clear on Escape', async () => {
-        await sendKeys({ press: 'Escape' });
-        expectValueCommit('');
-      });
+    it('should clear on Escape', async () => {
+      await sendKeys({ press: 'Escape' });
+      expectValueCommit('');
     });
   });
 
-  describe('stepping', () => {
-    it('should commit on ArrowDown', async () => {
-      await sendKeys({ press: 'ArrowDown' });
-      expectValueCommit('-1');
-    });
-
+  describe('keyboard stepping', () => {
     it('should commit on ArrowUp', async () => {
       await sendKeys({ press: 'ArrowUp' });
       expectValueCommit('1');
     });
 
-    describe('with value committed using an arrow key', () => {
+    it('should commit on ArrowDown', async () => {
+      await sendKeys({ press: 'ArrowDown' });
+      expectValueCommit('-1');
+    });
+
+    describe('arrow key committed', () => {
       beforeEach(async () => {
-        await sendKeys({ press: 'ArrowDown' });
+        await sendKeys({ press: 'ArrowUp' });
+        valueChangedSpy.resetHistory();
+        validateSpy.resetHistory();
+        changeSpy.resetHistory();
+      });
+
+      it('should not commit but validate on blur', () => {
+        numberField.blur();
+        expectValidationOnly();
+      });
+
+      it('should not commit on Enter', async () => {
+        await sendKeys({ press: 'Enter' });
+        expectNoValueCommit();
+      });
+    });
+  });
+
+  describe('spinner buttons', () => {
+    let increaseButton, decreaseButton;
+
+    beforeEach(() => {
+      increaseButton = numberField.shadowRoot.querySelector('[part=increase-button]');
+      decreaseButton = numberField.shadowRoot.querySelector('[part=decrease-button]');
+    });
+
+    it('should commit on increase button click', () => {
+      increaseButton.click();
+      expectValueCommit('1');
+    });
+
+    it('should commit on decrease button click', () => {
+      decreaseButton.click();
+      expectValueCommit('-1');
+    });
+
+    describe('click committed', () => {
+      beforeEach(async () => {
+        increaseButton.click();
         valueChangedSpy.resetHistory();
         validateSpy.resetHistory();
         changeSpy.resetHistory();
