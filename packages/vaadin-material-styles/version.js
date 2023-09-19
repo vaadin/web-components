ef13678d1a45883bfda8a9c5e9bd2ac16c8ef51f
Artur Signell <artur@vaadin.com>
2023-09-19 14:06:51 +0300

    fix import

--- packages/vaadin-material-styles/version.js
+++ packages/vaadin-material-styles/version.js
@@ -4,7 +4,7 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 
-import { defineCustomElement } from '@vaadin/component-base/src/element-mixin';
+import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 
 /**
  * Dummy custom element used for collecting
