e21818eebc08a368844fae274ab9832a8cbb4962
web-padawan <iamkulykov@gmail.com>
2023-09-19 14:43:52 +0300

    refactor: apply code review suggestions

--- packages/vaadin-material-styles/version.js
+++ packages/vaadin-material-styles/version.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2017 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 
 /**
