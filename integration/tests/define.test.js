d64f86b2bd67949bab0a0b2791ac711298a5d262
web-padawan <iamkulykov@gmail.com>
2023-09-20 16:41:29 +0300

    test: update to use sinon stub for console methods

--- integration/tests/define.test.js
+++ integration/tests/define.test.js
@@ -1,30 +1,41 @@
 import { expect } from '@esm-bundle/chai';
+import sinon from 'sinon';
 import '@vaadin/button';
 import { Button } from '@vaadin/button';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 
-it('should warn when component with same version is loaded twice', () => {
-  const oldWarn = console.warn;
-  const warningMessages = [];
-  console.warn = (message, ...args) => {
-    warningMessages.push(message);
-  };
-  defineCustomElement({ is: 'vaadin-button', version: Button.version });
-  expect(warningMessages.length).to.equal(1);
-  expect(warningMessages[0]).to.equal('The component vaadin-button has been loaded twice');
-  console.warn = oldWarn;
-});
+describe('define', () => {
+  describe('same version', () => {
+    beforeEach(() => {
+      sinon.stub(console, 'warn');
+    });
 
-it('should print an error when a component with different version is loaded twice', () => {
-  const oldError = console.error;
-  const errorMessages = [];
-  console.error = (message, ...args) => {
-    errorMessages.push(message);
-  };
-  defineCustomElement({ is: 'vaadin-button', version: '0.0.1' });
-  expect(errorMessages.length).to.equal(1);
-  expect(errorMessages[0]).to.equal(
-    'Tried to define vaadin-button version 0.0.1 when version 24.2.0-alpha16 is already in use. Something will probably break.',
-  );
-  console.error = oldError;
+    afterEach(() => {
+      console.warn.restore();
+    });
+
+    it('should warn when component with same version is loaded twice', () => {
+      defineCustomElement({ is: 'vaadin-button', version: Button.version });
+      expect(console.warn.calledOnce).to.be.true;
+      expect(console.warn.firstCall.args[0]).to.equal('The component vaadin-button has been loaded twice');
+    });
+  });
+
+  describe('different version', () => {
+    beforeEach(() => {
+      sinon.stub(console, 'error');
+    });
+
+    afterEach(() => {
+      console.error.restore();
+    });
+
+    it('should log an error when component with same version is loaded twice', () => {
+      defineCustomElement({ is: 'vaadin-button', version: '0.0.1' });
+      expect(console.error.calledOnce).to.be.true;
+      expect(console.error.firstCall.args[0]).to.equal(
+        `Tried to define vaadin-button version 0.0.1 when version ${Button.version} is already in use. Something will probably break.`,
+      );
+    });
+  });
 });
