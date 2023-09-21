e5c179c5fc67c374b52f24455b026fb242da6ce1
Artur <artur@vaadin.com>
2023-09-21 16:26:26 +0300

    refactor: prevent duplicate custom element definition errors (#6510)
    Co-authored-by: web-padawan <iamkulykov@gmail.com>

--- packages/component-base/src/define.js
+++ packages/component-base/src/define.js
@@ -4,5 +4,18 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 export function defineCustomElement(CustomElement) {
-  customElements.define(CustomElement.is, CustomElement);
+  const defined = customElements.get(CustomElement.is);
+  if (!defined) {
+    customElements.define(CustomElement.is, CustomElement);
+  } else {
+    const definedVersion = defined.version;
+    if (definedVersion && CustomElement.version && definedVersion === CustomElement.version) {
+      // Just loading the same thing again
+      console.warn(`The component ${CustomElement.is} has been loaded twice`);
+    } else {
+      console.error(
+        `Tried to define ${CustomElement.is} version ${CustomElement.version} when version ${defined.version} is already in use. Something will probably break.`,
+      );
+    }
+  }
 }
