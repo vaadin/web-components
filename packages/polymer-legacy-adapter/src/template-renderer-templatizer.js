0385d15fababd3606007a20339a6dcd9909c878d
Sergey Vinogradov <mr.vursen@gmail.com>
2023-09-22 14:35:32 +0300

    chore: update main branch to Vaadin 24.3 (#6518)

--- packages/polymer-legacy-adapter/src/template-renderer-templatizer.js
+++ packages/polymer-legacy-adapter/src/template-renderer-templatizer.js
@@ -13,7 +13,7 @@ export class Templatizer extends PolymerElement {
   }
 
   static get version() {
-    return '24.2.0-beta1';
+    return '24.3.0-alpha0';
   }
 
   static create(component, template) {
