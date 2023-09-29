bd747acddf402b0778d2881da827d7abe6d6f7a8
Serhii Kulykov <iamkulykov@gmail.com>
2023-09-29 15:06:42 +0300

    refactor: extract input-container styles into css literal (#6575)

--- packages/input-container/src/vaadin-input-container.js
+++ packages/input-container/src/vaadin-input-container.js
@@ -6,8 +6,11 @@
 import { html, PolymerElement } from '@polymer/polymer';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
-import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { registerStyles, ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { InputContainerMixin } from './vaadin-input-container-mixin.js';
+import { inputContainerStyles } from './vaadin-input-container-styles.js';
+
+registerStyles('vaadin-input-container', inputContainerStyles, { moduleId: 'vaadin-input-container-styles' });
 
 /**
  * @customElement
@@ -23,72 +26,6 @@ export class InputContainer extends InputContainerMixin(ThemableMixin(DirMixin(P
 
   static get template() {
     return html`
-      <style>
-        :host {
-          display: flex;
-          align-items: center;
-          flex: 0 1 auto;
-          border-radius:
-            /* See https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius */
-            var(--vaadin-input-field-top-start-radius, var(--__border-radius))
-            var(--vaadin-input-field-top-end-radius, var(--__border-radius))
-            var(--vaadin-input-field-bottom-end-radius, var(--__border-radius))
-            var(--vaadin-input-field-bottom-start-radius, var(--__border-radius));
-          --_border-radius: var(--vaadin-input-field-border-radius, 0px);
-          --_input-border-width: var(--vaadin-input-field-border-width, 0);
-          --_input-border-color: var(--vaadin-input-field-border-color, transparent);
-          box-shadow: inset 0 0 0 var(--_input-border-width, 0) var(--_input-border-color);
-        }
-
-        :host([dir='rtl']) {
-          border-radius:
-            /* Don't use logical props, see https://github.com/vaadin/vaadin-time-picker/issues/145 */
-            var(--vaadin-input-field-top-end-radius, var(--_border-radius))
-            var(--vaadin-input-field-top-start-radius, var(--_border-radius))
-            var(--vaadin-input-field-bottom-start-radius, var(--_border-radius))
-            var(--vaadin-input-field-bottom-end-radius, var(--_border-radius));
-        }
-
-        :host([hidden]) {
-          display: none !important;
-        }
-
-        /* Reset the native input styles */
-        ::slotted(input) {
-          -webkit-appearance: none;
-          -moz-appearance: none;
-          flex: auto;
-          white-space: nowrap;
-          overflow: hidden;
-          width: 100%;
-          height: 100%;
-          outline: none;
-          margin: 0;
-          padding: 0;
-          border: 0;
-          border-radius: 0;
-          min-width: 0;
-          font: inherit;
-          line-height: normal;
-          color: inherit;
-          background-color: transparent;
-          /* Disable default invalid style in Firefox */
-          box-shadow: none;
-        }
-
-        ::slotted(*) {
-          flex: none;
-        }
-
-        ::slotted(:is(input, textarea))::placeholder {
-          /* Use ::slotted(input:placeholder-shown) in themes to style the placeholder. */
-          /* because ::slotted(...)::placeholder does not work in Safari. */
-          font: inherit;
-          color: inherit;
-          /* Override default opacity in Firefox */
-          opacity: 1;
-        }
-      </style>
       <slot name="prefix"></slot>
       <slot></slot>
       <slot name="suffix"></slot>
