16240469ef70a592de69042bfa9967d82f03e2cf
web-padawan <iamkulykov@gmail.com>
2023-09-29 11:55:33 +0300

    refactor: extract rich-text-editor logic into reusable mixin

--- packages/rich-text-editor/src/vaadin-rich-text-editor.d.ts
+++ packages/rich-text-editor/src/vaadin-rich-text-editor.d.ts
@@ -10,34 +10,8 @@
  */
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-
-export interface RichTextEditorI18n {
-  undo: string;
-  redo: string;
-  bold: string;
-  italic: string;
-  underline: string;
-  strike: string;
-  h1: string;
-  h2: string;
-  h3: string;
-  subscript: string;
-  superscript: string;
-  listOrdered: string;
-  listBullet: string;
-  alignLeft: string;
-  alignCenter: string;
-  alignRight: string;
-  image: string;
-  link: string;
-  blockquote: string;
-  codeBlock: string;
-  clean: string;
-  linkDialogTitle: string;
-  ok: string;
-  cancel: string;
-  remove: string;
-}
+import { type RichTextEditorCustomEventMap, RichTextEditorMixin } from './vaadin-rich-text-editor-mixin.js';
+export * from './vaadin-rich-text-editor-mixin.js';
 
 /**
  * Fired when the user commits a value change.
@@ -45,22 +19,6 @@ export interface RichTextEditorI18n {
 export type RichTextEditorChangeEvent = Event & {
   target: RichTextEditor;
 };
-
-/**
- * Fired when the `htmlValue` property changes.
- */
-export type RichTextEditorHtmlValueChangedEvent = CustomEvent<{ value: string }>;
-
-/**
- * Fired when the `value` property changes.
- */
-export type RichTextEditorValueChangedEvent = CustomEvent<{ value: string }>;
-
-export interface RichTextEditorCustomEventMap {
-  'html-value-changed': RichTextEditorHtmlValueChangedEvent;
-
-  'value-changed': RichTextEditorValueChangedEvent;
-}
 
 export interface RichTextEditorEventMap extends HTMLElementEventMap, RichTextEditorCustomEventMap {
   change: RichTextEditorChangeEvent;
@@ -134,55 +92,7 @@ export interface RichTextEditorEventMap extends HTMLElementEventMap, RichTextEdi
  * @fires {CustomEvent} html-value-changed - Fired when the `htmlValue` property changes.
  * @fires {CustomEvent} value-changed - Fired when the `value` property changes.
  */
-declare class RichTextEditor extends ElementMixin(ThemableMixin(HTMLElement)) {
-  /**
-   * Value is a list of the operations which describe change to the document.
-   * Each of those operations describe the change at the current index.
-   * They can be an `insert`, `delete` or `retain`. The format is as follows:
-   *
-   * ```js
-   *  [
-   *    { insert: 'Hello World' },
-   *    { insert: '!', attributes: { bold: true }}
-   *  ]
-   * ```
-   *
-   * See also https://github.com/quilljs/delta for detailed documentation.
-   */
-  value: string;
-
-  /**
-   * HTML representation of the rich text editor content.
-   */
-  readonly htmlValue: string | null | undefined;
-
-  /**
-   * When true, the user can not modify, nor copy the editor content.
-   */
-  disabled: boolean;
-
-  /**
-   * When true, the user can not modify the editor content, but can copy it.
-   */
-  readonly: boolean;
-
-  /**
-   * An object used to localize this component. The properties are used
-   * e.g. as the tooltips for the editor toolbar buttons.
-   */
-  i18n: RichTextEditorI18n;
-
-  /**
-   * Sets content represented by HTML snippet into the editor.
-   * The snippet is interpreted by [Quill's Clipboard matchers](https://quilljs.com/docs/modules/clipboard/#matchers),
-   * which may not produce the exactly input HTML.
-   *
-   * **NOTE:** Improper handling of HTML can lead to cross site scripting (XSS) and failure to sanitize
-   * properly is both notoriously error-prone and a leading cause of web vulnerabilities.
-   * This method is aptly named to ensure the developer has taken the necessary precautions.
-   */
-  dangerouslySetHtmlValue(htmlValue: string): void;
-
+declare class RichTextEditor extends RichTextEditorMixin(ElementMixin(ThemableMixin(HTMLElement))) {
   addEventListener<K extends keyof RichTextEditorEventMap>(
     type: K,
     listener: (this: RichTextEditor, ev: RichTextEditorEventMap[K]) => void,
