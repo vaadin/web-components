463ce1f909156594819c3af97916f146a1f5fe55
Joacim Päivärinne <joacim@vaadin.com>
2023-10-12 15:39:39 +0300

    Update transition.js

--- packages/vaadin-lumo-styles/utilities/transition.js
+++ packages/vaadin-lumo-styles/utilities/transition.js
@@ -7,13 +7,15 @@ import { css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 
 export const transition = css`
   .transition {
-    transition: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow,
+      transform, filter, backdrop-filter 150ms cubic-bezier(0.4, 0, 0.2, 1);
   }
   .transition-all {
     transition: all 150ms cubic-bezier(0.4, 0, 0.2, 1);
   }
   .transition-colors {
-    transition: color, background-color, border-color, text-decoration-color, fill, stroke 150ms cubic-bezier(0.4, 0, 0.2, 1);
+    transition: color, background-color, border-color, text-decoration-color, fill,
+      stroke 150ms cubic-bezier(0.4, 0, 0.2, 1);
   }
   .transition-none {
     transition: none;
