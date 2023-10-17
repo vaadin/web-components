c60f2ae6b5584fee0f4964dd570e4d89ea7c5bac
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-10-17 09:11:46 +0300

    fix: update grid header and footer on column tree change (#6648)

--- packages/grid/src/vaadin-grid-mixin.js
+++ packages/grid/src/vaadin-grid-mixin.js
@@ -879,6 +879,7 @@ export const GridMixin = (superClass) =>
       this.__updateFooterPositioning();
       this.generateCellClassNames();
       this.generateCellPartNames();
+      this.__updateHeaderAndFooter();
     }
 
     /** @private */
@@ -1001,19 +1002,22 @@ export const GridMixin = (superClass) =>
      * It is not guaranteed that the update happens immediately (synchronously) after it is requested.
      */
     requestContentUpdate() {
-      if (this._columnTree) {
-        // Header and footer renderers
-        this._columnTree.forEach((level) => {
-          level.forEach((column) => {
-            if (column._renderHeaderAndFooter) {
-              column._renderHeaderAndFooter();
-            }
-          });
-        });
+      // Header and footer renderers
+      this.__updateHeaderAndFooter();
 
-        // Body and row details renderers
-        this.__updateVisibleRows();
-      }
+      // Body and row details renderers
+      this.__updateVisibleRows();
+    }
+
+    /** @private */
+    __updateHeaderAndFooter() {
+      (this._columnTree || []).forEach((level) => {
+        level.forEach((column) => {
+          if (column._renderHeaderAndFooter) {
+            column._renderHeaderAndFooter();
+          }
+        });
+      });
     }
 
     /** @protected */
