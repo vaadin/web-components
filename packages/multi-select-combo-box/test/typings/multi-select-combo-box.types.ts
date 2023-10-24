4b3f110bc1b4e8e8db3baf99ef84783b6d8733c7
web-padawan <iamkulykov@gmail.com>
2023-10-24 13:43:17 +0300

    test: add groupSelectedItems property to typings test

--- packages/multi-select-combo-box/test/typings/multi-select-combo-box.types.ts
+++ packages/multi-select-combo-box/test/typings/multi-select-combo-box.types.ts
@@ -104,6 +104,7 @@ assertType<boolean>(narrowedComboBox.readonly);
 assertType<string | null | undefined>(narrowedComboBox.label);
 assertType<boolean>(narrowedComboBox.required);
 assertType<string>(narrowedComboBox.overlayClass);
+assertType<boolean>(narrowedComboBox.groupSelectedItems);
 
 // Mixins
 assertType<ControllerMixinClass>(narrowedComboBox);
