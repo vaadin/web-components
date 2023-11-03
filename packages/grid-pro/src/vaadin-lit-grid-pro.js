b25ac5f6b226ed2a1b609b8ac0bc4d76ef882dd0
Tomi Virkki <virkki@vaadin.com>
2023-11-03 14:32:55 +0200

    wip

---
+++ packages/grid-pro/src/vaadin-lit-grid-pro.js
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2023 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
+ */
+import { defineCustomElement } from '@vaadin/component-base/src/define.js';
+import { Grid } from '@vaadin/grid/src/vaadin-lit-grid.js';
+import { InlineEditingMixin } from './vaadin-grid-pro-inline-editing-mixin.js';
+
+/**
+ * LitElement based version of `<vaadin-grid-pro>` web component.
+ *
+ * ## Disclaimer
+ *
+ * This component is an experiment not intended for publishing to npm.
+ * There is no ETA regarding specific Vaadin version where it'll land.
+ * Feel free to try this code in your apps as per Apache 2.0 license.
+ */
+class GridPro extends InlineEditingMixin(Grid) {
+  static get is() {
+    return 'vaadin-grid-pro';
+  }
+
+  static get cvdlName() {
+    return 'vaadin-grid-pro';
+  }
+}
+
+defineCustomElement(GridPro);
+
+export { GridPro };
