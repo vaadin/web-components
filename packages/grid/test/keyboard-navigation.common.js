196db67c3cc6b3396a095309174679ee04bba44a
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-11-06 09:31:35 +0200

    fix: check if grid focus target is in viewport (#6731)

--- packages/grid/test/keyboard-navigation.common.js
+++ packages/grid/test/keyboard-navigation.common.js
@@ -1147,6 +1147,26 @@ describe('keyboard navigation', () => {
         shiftTab();
         expect(grid.$.table.scrollLeft).to.be.at.least(100);
       });
+
+      it('should not throw when focusing a group header cell', () => {
+        // Wrap the columns in a group
+        const group = document.createElement('vaadin-grid-column-group');
+        group.append(...grid.querySelectorAll('vaadin-grid-column'));
+        group.header = 'group';
+        grid.appendChild(group);
+        flushGrid(grid);
+
+        // Move focused header cell to the group header row
+        tabToBody();
+        shiftTab();
+        up();
+
+        // Tab to body
+        tab();
+
+        // Tab back to header
+        shiftTab();
+      });
     });
 
     describe('vertical scrolling', () => {
