4f63d23a417c0390ac2682cb95b182f5b0954afa
Tomi Virkki <virkki@vaadin.com>
2023-11-08 13:55:29 +0200

    chore: remove item and list box style imports from grid pro

--- packages/grid-pro/theme/lumo/vaadin-grid-pro-edit-select.js
+++ packages/grid-pro/theme/lumo/vaadin-grid-pro-edit-select.js
@@ -1,5 +1,3 @@
-import '@vaadin/item/theme/lumo/vaadin-item.js';
-import '@vaadin/list-box/theme/lumo/vaadin-list-box.js';
 import '@vaadin/select/theme/lumo/vaadin-select.js';
 import './vaadin-grid-pro-edit-select-styles.js';
 import '../../src/vaadin-grid-pro-edit-select.js';
