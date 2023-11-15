92a141f50f943375eaaeaa1d00245bb237d3112e
Tomi Virkki <virkki@vaadin.com>
2023-11-15 11:14:15 +0200

    experiment: add imports for LitElement-based confirm-dialog

---
+++ packages/confirm-dialog/theme/lumo/vaadin-lit-confirm-dialog.js
@@ -0,0 +1,4 @@
+import '@vaadin/dialog/theme/lumo/vaadin-dialog-styles.js';
+import '@vaadin/button/theme/lumo/vaadin-button-styles.js';
+import './vaadin-confirm-dialog-styles.js';
+import '../../src/vaadin-lit-confirm-dialog.js';
---
+++ packages/confirm-dialog/theme/material/vaadin-lit-confirm-dialog.js
@@ -0,0 +1,4 @@
+import '@vaadin/dialog/theme/material/vaadin-dialog-styles.js';
+import '@vaadin/button/theme/material/vaadin-button-styles.js';
+import './vaadin-confirm-dialog-styles.js';
+import '../../src/vaadin-lit-confirm-dialog.js';
