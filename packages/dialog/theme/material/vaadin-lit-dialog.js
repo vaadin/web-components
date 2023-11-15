713d9db05bb02b7314da7e7a5ec16f802ccb9443
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-11-15 11:08:11 +0200

    experiment: add imports for LitElement-based dialog (#6790)

---
+++ packages/dialog/theme/material/vaadin-lit-dialog.js
@@ -0,0 +1,3 @@
+import '@vaadin/overlay/theme/material/vaadin-overlay-styles.js';
+import './vaadin-dialog-styles.js';
+import '../../src/vaadin-lit-dialog.js';
---
+++ packages/dialog/vaadin-lit-dialog.js
@@ -0,0 +1,2 @@
+import './theme/lumo/vaadin-lit-dialog.js';
+export * from './src/vaadin-lit-dialog.js';
