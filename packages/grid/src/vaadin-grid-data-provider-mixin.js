3c6c66b612f94ffe0755b397315ad2124c1a8e9b
Sergey Vinogradov <mr.vursen@gmail.com>
2023-11-18 07:52:57 +0200

    Update packages/grid/src/vaadin-grid-data-provider-mixin.js

--- packages/grid/src/vaadin-grid-data-provider-mixin.js
+++ packages/grid/src/vaadin-grid-data-provider-mixin.js
@@ -365,6 +365,7 @@ export const DataProviderMixin = (superClass) =>
     clearCache() {
       this._dataProviderController.clearCache();
       this._dataProviderController.rootCache.size = this.size;
+      this._dataProviderController.recalculateFlatSize();
       this._hasData = false;
       this.__updateVisibleRows();
 
