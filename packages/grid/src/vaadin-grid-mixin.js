c0bf69fe5e2d19a529e7c22c03d0d6413f059676
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-11-21 09:39:11 +0200

    fix: measure grid column auto-width from column cell (#6815)

--- packages/grid/src/vaadin-grid-mixin.js
+++ packages/grid/src/vaadin-grid-mixin.js
@@ -422,8 +422,19 @@ export const GridMixin = (superClass) =>
         })
         .forEach((cell) => {
           cell.__measuringAutoWidth = autoWidth;
-          cell._content.style.width = autoWidth ? 'auto' : '';
-          cell._content.style.position = autoWidth ? 'absolute' : '';
+
+          if (cell.__measuringAutoWidth) {
+            // Store the original inline width of the cell to restore it later
+            cell.__originalWidth = cell.style.width;
+            // Prepare the cell for having its intrinsic width measured
+            cell.style.width = 'auto';
+            cell.style.position = 'absolute';
+          } else {
+            // Restore the original width
+            cell.style.width = cell.__originalWidth;
+            delete cell.__originalWidth;
+            cell.style.position = '';
+          }
         });
     }
 
@@ -437,7 +448,7 @@ export const GridMixin = (superClass) =>
       // Note: _allCells only contains cells which are currently rendered in DOM
       return col._allCells.reduce((width, cell) => {
         // Add 1px buffer to the offset width to avoid too narrow columns (sub-pixel rendering)
-        return cell.__measuringAutoWidth ? Math.max(width, cell._content.offsetWidth + 1) : width;
+        return cell.__measuringAutoWidth ? Math.max(width, cell.offsetWidth + 1) : width;
       }, 0);
     }
 
