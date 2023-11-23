cec117a028ec5834dc77933576baea549fc0bb80
Sergey Vinogradov <mr.vursen@gmail.com>
2023-11-23 10:15:48 +0200

    fix: fix label color in disabled state (#6838)

--- packages/radio-group/theme/lumo/vaadin-radio-group-styles.js
+++ packages/radio-group/theme/lumo/vaadin-radio-group-styles.js
@@ -31,6 +31,11 @@ const radioGroup = css`
     flex-direction: column;
   }
 
+  :host([disabled]) [part='label'] {
+    color: var(--lumo-disabled-text-color);
+    -webkit-text-fill-color: var(--lumo-disabled-text-color);
+  }
+
   :host([focused]:not([readonly])) [part='label'] {
     color: var(--lumo-primary-text-color);
   }
