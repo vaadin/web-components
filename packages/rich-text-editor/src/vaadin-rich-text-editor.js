1c64645add8f892878c93d8a6aed998aa6244461
Tomi Virkki <tomivirkki@users.noreply.github.com>
2023-11-28 16:20:23 +0200

    fix: cache pending html value for unattached rte (#6871)

--- packages/rich-text-editor/src/vaadin-rich-text-editor.js
+++ packages/rich-text-editor/src/vaadin-rich-text-editor.js
@@ -667,6 +667,9 @@ class RichTextEditor extends ElementMixin(ThemableMixin(PolymerElement)) {
     });
 
     this._editor.on('selection-change', this.__announceFormatting.bind(this));
+
+    // Flush pending htmlValue only once the editor is fully initialized
+    this.__flushPendingHtmlValue();
   }
 
   /** @protected */
@@ -1013,6 +1016,14 @@ class RichTextEditor extends ElementMixin(ThemableMixin(PolymerElement)) {
    * @param {string} htmlValue
    */
   dangerouslySetHtmlValue(htmlValue) {
+    if (!this._editor) {
+      // The editor isn't ready yet, store the value for later
+      this.__pendingHtmlValue = htmlValue;
+      // Clear a possible value to prevent it from clearing the pending htmlValue once the editor property is set
+      this.value = '';
+      return;
+    }
+
     const whitespaceCharacters = {
       '\t': '__VAADIN_RICH_TEXT_EDITOR_TAB',
       '  ': '__VAADIN_RICH_TEXT_EDITOR_DOUBLE_SPACE',
@@ -1034,6 +1045,13 @@ class RichTextEditor extends ElementMixin(ThemableMixin(PolymerElement)) {
     });
 
     this._editor.setContents(deltaFromHtml, SOURCE.API);
+  }
+
+  /** @private */
+  __flushPendingHtmlValue() {
+    if (this.__pendingHtmlValue) {
+      this.dangerouslySetHtmlValue(this.__pendingHtmlValue);
+    }
   }
 
   /** @private */
@@ -1148,6 +1166,11 @@ class RichTextEditor extends ElementMixin(ThemableMixin(PolymerElement)) {
 
   /** @private */
   _valueChanged(value, editor) {
+    if (value && this.__pendingHtmlValue) {
+      // A non-empty value is set explicitly. Clear pending htmlValue to prevent it from overriding the value.
+      this.__pendingHtmlValue = undefined;
+    }
+
     if (editor === undefined) {
       return;
     }
