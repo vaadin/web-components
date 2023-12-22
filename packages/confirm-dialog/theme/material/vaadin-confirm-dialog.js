0f4ad327b7327788b872de3d60e581dd590a08ea
web-padawan <iamkulykov@gmail.com>
2023-12-22 16:38:09 +0200

    refactor: remove dialog imports from confirm-dialog and crud

--- packages/confirm-dialog/theme/material/vaadin-confirm-dialog.js
+++ packages/confirm-dialog/theme/material/vaadin-confirm-dialog.js
@@ -1,4 +1,3 @@
-import '@vaadin/dialog/theme/material/vaadin-dialog.js';
 import '@vaadin/button/theme/material/vaadin-button.js';
 import './vaadin-confirm-dialog-styles.js';
 import '../../src/vaadin-confirm-dialog.js';
