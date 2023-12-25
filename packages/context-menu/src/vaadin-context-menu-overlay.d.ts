4f89150297143f181959fe43c9bda39ff71e92f9
web-padawan <iamkulykov@gmail.com>
2023-12-25 10:13:15 +0200

    fix: use correct mixins in context-menu overlay typings

--- packages/context-menu/src/vaadin-context-menu-overlay.d.ts
+++ packages/context-menu/src/vaadin-context-menu-overlay.d.ts
@@ -3,13 +3,15 @@
  * Copyright (c) 2016 - 2023 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { Overlay } from '@vaadin/overlay/src/vaadin-overlay.js';
+import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
+import { OverlayMixin } from '@vaadin/overlay/src/vaadin-overlay-mixin.js';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { MenuOverlayMixin } from './vaadin-menu-overlay-mixin.js';
 
 /**
  * An element used internally by `<vaadin-context-menu>`. Not intended to be used separately.
  */
-declare class ContextMenuOverlay extends MenuOverlayMixin(Overlay) {}
+declare class ContextMenuOverlay extends MenuOverlayMixin(OverlayMixin(DirMixin(ThemableMixin(HTMLElement)))) {}
 
 declare global {
   interface HTMLElementTagNameMap {
