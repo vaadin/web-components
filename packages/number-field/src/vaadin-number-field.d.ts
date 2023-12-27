47a57b07254914f50e13a795b11e326b460a89fb
web-padawan <iamkulykov@gmail.com>
2023-12-27 13:54:44 +0200

    docs: remove unused InputFieldMixin import from typings

--- packages/number-field/src/vaadin-number-field.d.ts
+++ packages/number-field/src/vaadin-number-field.d.ts
@@ -4,7 +4,6 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
-import { InputFieldMixin } from '@vaadin/field-base/src/input-field-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { NumberFieldMixin } from './vaadin-number-field-mixin.js';
 
