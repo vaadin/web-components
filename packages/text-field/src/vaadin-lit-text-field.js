fc529371a0824dacd11343a746d02d801910d7f5
Serhii Kulykov <iamkulykov@gmail.com>
2024-01-12 17:05:40 +0200

    refactor: remove no longer needed flex-grow CSS override (#7081)

--- packages/text-field/src/vaadin-lit-text-field.js
+++ packages/text-field/src/vaadin-lit-text-field.js
@@ -4,7 +4,7 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import '@vaadin/input-container/src/vaadin-lit-input-container.js';
-import { css, html, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
@@ -28,14 +28,7 @@ export class TextField extends TextFieldMixin(ThemableMixin(ElementMixin(Polylit
   }
 
   static get styles() {
-    return [
-      inputFieldShared,
-      css`
-        [part='input-field'] {
-          flex-grow: 0;
-        }
-      `,
-    ];
+    return [inputFieldShared];
   }
 
   /** @protected */
