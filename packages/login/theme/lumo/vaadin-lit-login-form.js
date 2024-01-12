93b7fae9b6c04dfaef9296de16c1cef9c388052d
Serhii Kulykov <iamkulykov@gmail.com>
2024-01-12 14:56:17 +0200

    experiment: prepare LitElement based login for publishing (#7009)

---
+++ packages/login/theme/lumo/vaadin-lit-login-form.js
@@ -0,0 +1,5 @@
+import '@vaadin/button/theme/lumo/vaadin-button-styles.js';
+import '@vaadin/text-field/theme/lumo/vaadin-text-field-styles.js';
+import '@vaadin/password-field/theme/lumo/vaadin-password-field-styles.js';
+import './vaadin-login-form-wrapper-styles.js';
+import '../../src/vaadin-lit-login-form.js';
