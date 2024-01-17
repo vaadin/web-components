da4028817f2702ebbc821839687c8c56a585deb2
Serhii Kulykov <iamkulykov@gmail.com>
2024-01-17 13:20:48 +0200

    refactor!: remove column re-exports from grid root entrypoint (#7090)

--- packages/grid/vaadin-grid.js
+++ packages/grid/vaadin-grid.js
@@ -1,4 +1,3 @@
 import './theme/lumo/vaadin-grid.js';
 
-export * from './src/vaadin-grid-column.js';
 export * from './src/vaadin-grid.js';
--- packages/grid/vaadin-lit-grid.js
+++ packages/grid/vaadin-lit-grid.js
@@ -1,4 +1,3 @@
 import './theme/lumo/vaadin-lit-grid.js';
 
-export * from './src/vaadin-lit-grid-column.js';
 export * from './src/vaadin-lit-grid.js';
