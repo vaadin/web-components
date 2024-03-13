9777e3e8a1dbacbe716f20e7ca640c49a9831b7f
ugur-vaadin <ugur@vaadin.com>
2024-03-13 15:24:01 +0200

    refactor: revert max pages value update

--- packages/component-base/src/virtualizer-iron-list-adapter.js
+++ packages/component-base/src/virtualizer-iron-list-adapter.js
@@ -26,9 +26,7 @@ export class IronListAdapter {
     this.elementsContainer = elementsContainer || scrollContainer;
     this.reorderElements = reorderElements;
     // Iron-list uses this value to determine how many pages of elements to render.
-    // Lower values result in inconsistent grid height when all rows are visible and row heights vary.
-    // See related issue: https://github.com/vaadin/web-components/issues/7160
-    this._maxPages = 1.7;
+    this._maxPages = 1.3;
 
     // Placeholder height (used for sizing elements that have intrinsic 0 height after update)
     this.__placeholderHeight = 200;
