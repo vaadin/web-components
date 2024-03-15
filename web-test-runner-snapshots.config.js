d01c337d868cfd6bc92e971a6c93b44a84560532
web-padawan <iamkulykov@gmail.com>
2024-03-15 15:58:32 +0200

    chore: also use old headless mode for snapshots and ITs

--- web-test-runner-snapshots.config.js
+++ web-test-runner-snapshots.config.js
@@ -1,4 +1,13 @@
 /* eslint-env node */
+const { chromeLauncher } = require('@web/test-runner-chrome');
 const { createSnapshotTestsConfig } = require('./wtr-utils.js');
 
-module.exports = createSnapshotTestsConfig();
+module.exports = createSnapshotTestsConfig({
+  browsers: [
+    chromeLauncher({
+      launchOptions: {
+        headless: 'shell',
+      },
+    }),
+  ],
+});
