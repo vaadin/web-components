f38b8b3147510b80857d2b49ca30038f1033a059
Serhii Kulykov <iamkulykov@gmail.com>
2024-03-26 09:57:05 +0200

    refactor: extract checkbox-group logic to reusable mixin (#7258)

---
+++ packages/checkbox-group/src/vaadin-checkbox-group-mixin.d.ts
@@ -0,0 +1,36 @@
+/**
+ * @license
+ * Copyright (c) 2018 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import type { Constructor } from '@open-wc/dedupe-mixin';
+import type { DisabledMixinClass } from '@vaadin/a11y-base/src/disabled-mixin.js';
+import type { FocusMixinClass } from '@vaadin/a11y-base/src/focus-mixin.js';
+import type { ControllerMixinClass } from '@vaadin/component-base/src/controller-mixin.js';
+import type { FieldMixinClass } from '@vaadin/field-base/src/field-mixin.js';
+import type { LabelMixinClass } from '@vaadin/field-base/src/label-mixin.js';
+import type { ValidateMixinClass } from '@vaadin/field-base/src/validate-mixin.js';
+
+/**
+ * A mixin providing common checkbox-group functionality.
+ */
+export declare function CheckboxGroupMixin<T extends Constructor<HTMLElement>>(
+  base: T,
+): Constructor<CheckboxGroupMixinClass> &
+  Constructor<ControllerMixinClass> &
+  Constructor<DisabledMixinClass> &
+  Constructor<FieldMixinClass> &
+  Constructor<FocusMixinClass> &
+  Constructor<LabelMixinClass> &
+  Constructor<ValidateMixinClass> &
+  T;
+
+export declare class CheckboxGroupMixinClass {
+  /**
+   * An array containing values of the currently checked checkboxes.
+   *
+   * The array is immutable so toggling checkboxes always results in
+   * creating a new array.
+   */
+  value: string[];
+}
