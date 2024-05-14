129e6de01934e9d0ba3946508f423fd4fdd3b2c4
web-padawan <iamkulykov@gmail.com>
2024-05-14 15:22:39 +0300

    refactor: remove not necessary property override

--- packages/popover/src/vaadin-popover-overlay.js
+++ packages/popover/src/vaadin-popover-overlay.js
@@ -53,22 +53,6 @@ class PopoverOverlay extends PopoverOverlayMixin(DirMixin(ThemableMixin(PolylitM
     ];
   }
 
-  static get properties() {
-    return {
-      /**
-       * When true, the overlay is visible and attached to body.
-       * This property config is overridden to set `sync: true`.
-       */
-      opened: {
-        type: Boolean,
-        notify: true,
-        observer: '_openedChanged',
-        reflectToAttribute: true,
-        sync: true,
-      },
-    };
-  }
-
   /** @protected */
   render() {
     return html`
