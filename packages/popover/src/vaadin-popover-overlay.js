34753a251b132331a722554b42b2e24f24ef36b9
web-padawan <iamkulykov@gmail.com>
2024-05-14 13:01:01 +0300

    feat: apply popover offset custom CSS properties

--- packages/popover/src/vaadin-popover-overlay.js
+++ packages/popover/src/vaadin-popover-overlay.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
@@ -27,7 +27,30 @@ class PopoverOverlay extends PopoverOverlayMixin(DirMixin(ThemableMixin(PolylitM
   }
 
   static get styles() {
-    return [overlayStyles];
+    return [
+      overlayStyles,
+      css`
+        :host([position^='top'][top-aligned]) [part='overlay'],
+        :host([position^='bottom'][top-aligned]) [part='overlay'] {
+          margin-top: var(--vaadin-popover-offset-top, 0);
+        }
+
+        :host([position^='top'][bottom-aligned]) [part='overlay'],
+        :host([position^='bottom'][bottom-aligned]) [part='overlay'] {
+          margin-bottom: var(--vaadin-popover-offset-bottom, 0);
+        }
+
+        :host([position^='start'][start-aligned]) [part='overlay'],
+        :host([position^='end'][start-aligned]) [part='overlay'] {
+          margin-inline-start: var(--vaadin-popover-offset-start, 0);
+        }
+
+        :host([position^='start'][end-aligned]) [part='overlay'],
+        :host([position^='end'][end-aligned]) [part='overlay'] {
+          margin-inline-end: var(--vaadin-popover-offset-end, 0);
+        }
+      `,
+    ];
   }
 
   static get properties() {
