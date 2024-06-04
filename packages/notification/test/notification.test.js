607dba8d9aa990d83c6ca195cb49090c8bca0dea
Diego Cardoso <diego@vaadin.com>
2024-06-04 14:34:07 +0300

    test: add tests for the closed event

--- packages/notification/test/notification.test.js
+++ packages/notification/test/notification.test.js
@@ -53,6 +53,27 @@ describe('vaadin-notification', () => {
     expect(notification.opened).to.be.true;
   });
 
+  it('should dispatch closed event', async () => {
+    const spy = sinon.spy();
+    listenOnce(notification, 'closed', spy);
+    notification.opened = false;
+    await aTimeout(0);
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it('closed event should be called after overlay is closed', async () => {
+    const closedPromise = new Promise((resolve) => {
+      const closedListener = () => {
+        expect(notification._container.parentElement).to.be.not.ok;
+        resolve();
+      };
+      listenOnce(notification, 'closed', closedListener);
+    });
+    notification.opened = false;
+    await aTimeout(0);
+    await closedPromise;
+  });
+
   describe('vaadin-notification-container', () => {
     it('should be in the body when notification opens', () => {
       expect(document.body.querySelectorAll('vaadin-notification-container').length).to.be.equal(1);
