5d88cb2d55fdac80eff2f9c25b2861ecff460467
Fredrik Wasström <fredrikw@vaadin.com>
2024-06-18 12:56:46 +0300

    test

--- packages/notification/test/visual/lumo/notification.test.js
+++ packages/notification/test/visual/lumo/notification.test.js
@@ -36,3 +36,67 @@ describe('notification', () => {
     });
   });
 });
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { sendKeys } from '@web/test-runner-commands';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../not-animated-styles.js';
+import '../../../theme/lumo/vaadin-notification.js';
+
+describe('notification', () => {
+  let element;
+
+  beforeEach(() => {
+    element = fixtureSync('<vaadin-notification duration="0"></vaadin-notification>');
+    element.renderer = (root) => {
+      root.textContent = element.position;
+    };
+  });
+
+  afterEach(() => {
+    element.opened = false;
+  });
+
+  [
+    'top-stretch',
+    'top-start',
+    'top-center',
+    'top-end',
+    'middle',
+    'bottom-start',
+    'bottom-center',
+    'bottom-end',
+    'bottom-stretch',
+  ].forEach((position) => {
+    it(position, async () => {
+      element.position = position;
+      element.opened = true;
+      await nextRender(element);
+      await visualDiff(document.body, `${position}`);
+    });
+  });
+});
+describe('themes', () => {
+  let element;
+
+  beforeEach(() => {
+    element = fixtureSync('<vaadin-notification duration="0"></vaadin-notification>');
+    element.renderer = (root) => {
+      root.innerHTML = '<vaadin-button>Button</vaadin-button>';
+    };
+    element.opened = true;
+  });
+
+  afterEach(() => {
+    element.opened = false;
+  });
+
+  ['primary', 'warning', 'success', 'error'].forEach((variant) => {
+    it(variant, async () => {
+      element.setAttribute('theme', variant);
+      const notification = document.querySelector('vaadin-notification-card');
+
+      await sendKeys({ press: 'Tab' });
+      await visualDiff(notification, `notification-${variant}-focus-ring-contrast`);
+    });
+  });
+});
