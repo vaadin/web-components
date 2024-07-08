2e26a1f3d20b18949ae8f3b1c15362c159d5fa64
Serhii Kulykov <iamkulykov@gmail.com>
2024-07-08 14:20:34 +0300

    fix: select date on Today click if today is max date (#7516) (#7520)

--- packages/date-picker/src/vaadin-date-picker-overlay-content-mixin.js
+++ packages/date-picker/src/vaadin-date-picker-overlay-content-mixin.js
@@ -508,7 +508,7 @@ export const DatePickerOverlayContentMixin = (superClass) =>
 
     /** @private */
     _onTodayTap() {
-      const today = new Date();
+      const today = this._getTodayMidnight();
 
       if (Math.abs(this._monthScroller.position - this._differenceInMonths(today, this._originDate)) < 0.001) {
         // Select today only if the month scroller is positioned approximately
@@ -1015,12 +1015,17 @@ export const DatePickerOverlayContentMixin = (superClass) =>
 
     /** @private */
     _isTodayAllowed(min, max) {
+      return this._dateAllowed(this._getTodayMidnight(), min, max);
+    }
+
+    /** @private */
+    _getTodayMidnight() {
       const today = new Date();
       const todayMidnight = new Date(0, 0);
       todayMidnight.setFullYear(today.getFullYear());
       todayMidnight.setMonth(today.getMonth());
       todayMidnight.setDate(today.getDate());
-      return this._dateAllowed(todayMidnight, min, max);
+      return todayMidnight;
     }
 
     /**
