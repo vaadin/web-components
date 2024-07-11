b071047970c104a842c71f0e95c440abbf5dd34f
Jonte Päivärinne <joacim@vaadin.com>
2024-07-11 11:48:22 +0300

    feat: add new utility classes, sort existing classes (#7515)

--- packages/vaadin-lumo-styles/utilities/background.js
+++ packages/vaadin-lumo-styles/utilities/background.js
@@ -15,106 +15,106 @@ export const background = css`
     background-color: transparent;
   }
 
-  .bg-tint-5 {
-    background-color: var(--lumo-tint-5pct);
-  }
-  .bg-tint-10 {
-    background-color: var(--lumo-tint-10pct);
-  }
-  .bg-tint-20 {
-    background-color: var(--lumo-tint-20pct);
-  }
-  .bg-tint-30 {
-    background-color: var(--lumo-tint-30pct);
-  }
-  .bg-tint-40 {
-    background-color: var(--lumo-tint-40pct);
-  }
-  .bg-tint-50 {
-    background-color: var(--lumo-tint-50pct);
-  }
-  .bg-tint-60 {
-    background-color: var(--lumo-tint-60pct);
-  }
-  .bg-tint-70 {
-    background-color: var(--lumo-tint-70pct);
-  }
-  .bg-tint-80 {
-    background-color: var(--lumo-tint-80pct);
+  .bg-tint {
+    background-color: var(--lumo-tint);
   }
   .bg-tint-90 {
     background-color: var(--lumo-tint-90pct);
   }
-  .bg-tint {
-    background-color: var(--lumo-tint);
+  .bg-tint-80 {
+    background-color: var(--lumo-tint-80pct);
+  }
+  .bg-tint-70 {
+    background-color: var(--lumo-tint-70pct);
+  }
+  .bg-tint-60 {
+    background-color: var(--lumo-tint-60pct);
+  }
+  .bg-tint-50 {
+    background-color: var(--lumo-tint-50pct);
+  }
+  .bg-tint-40 {
+    background-color: var(--lumo-tint-40pct);
+  }
+  .bg-tint-30 {
+    background-color: var(--lumo-tint-30pct);
+  }
+  .bg-tint-20 {
+    background-color: var(--lumo-tint-20pct);
+  }
+  .bg-tint-10 {
+    background-color: var(--lumo-tint-10pct);
+  }
+  .bg-tint-5 {
+    background-color: var(--lumo-tint-5pct);
   }
 
-  .bg-shade-5 {
-    background-color: var(--lumo-shade-5pct);
-  }
-  .bg-shade-10 {
-    background-color: var(--lumo-shade-10pct);
-  }
-  .bg-shade-20 {
-    background-color: var(--lumo-shade-20pct);
-  }
-  .bg-shade-30 {
-    background-color: var(--lumo-shade-30pct);
-  }
-  .bg-shade-40 {
-    background-color: var(--lumo-shade-40pct);
-  }
-  .bg-shade-50 {
-    background-color: var(--lumo-shade-50pct);
-  }
-  .bg-shade-60 {
-    background-color: var(--lumo-shade-60pct);
-  }
-  .bg-shade-70 {
-    background-color: var(--lumo-shade-70pct);
-  }
-  .bg-shade-80 {
-    background-color: var(--lumo-shade-80pct);
+  .bg-shade {
+    background-color: var(--lumo-shade);
   }
   .bg-shade-90 {
     background-color: var(--lumo-shade-90pct);
   }
-  .bg-shade {
-    background-color: var(--lumo-shade);
+  .bg-shade-80 {
+    background-color: var(--lumo-shade-80pct);
+  }
+  .bg-shade-70 {
+    background-color: var(--lumo-shade-70pct);
+  }
+  .bg-shade-60 {
+    background-color: var(--lumo-shade-60pct);
+  }
+  .bg-shade-50 {
+    background-color: var(--lumo-shade-50pct);
+  }
+  .bg-shade-40 {
+    background-color: var(--lumo-shade-40pct);
+  }
+  .bg-shade-30 {
+    background-color: var(--lumo-shade-30pct);
+  }
+  .bg-shade-20 {
+    background-color: var(--lumo-shade-20pct);
+  }
+  .bg-shade-10 {
+    background-color: var(--lumo-shade-10pct);
+  }
+  .bg-shade-5 {
+    background-color: var(--lumo-shade-5pct);
   }
 
-  .bg-contrast-5 {
-    background-color: var(--lumo-contrast-5pct);
-  }
-  .bg-contrast-10 {
-    background-color: var(--lumo-contrast-10pct);
-  }
-  .bg-contrast-20 {
-    background-color: var(--lumo-contrast-20pct);
-  }
-  .bg-contrast-30 {
-    background-color: var(--lumo-contrast-30pct);
-  }
-  .bg-contrast-40 {
-    background-color: var(--lumo-contrast-40pct);
-  }
-  .bg-contrast-50 {
-    background-color: var(--lumo-contrast-50pct);
-  }
-  .bg-contrast-60 {
-    background-color: var(--lumo-contrast-60pct);
-  }
-  .bg-contrast-70 {
-    background-color: var(--lumo-contrast-70pct);
-  }
-  .bg-contrast-80 {
-    background-color: var(--lumo-contrast-80pct);
+  .bg-contrast {
+    background-color: var(--lumo-contrast);
   }
   .bg-contrast-90 {
     background-color: var(--lumo-contrast-90pct);
   }
-  .bg-contrast {
-    background-color: var(--lumo-contrast);
+  .bg-contrast-80 {
+    background-color: var(--lumo-contrast-80pct);
+  }
+  .bg-contrast-70 {
+    background-color: var(--lumo-contrast-70pct);
+  }
+  .bg-contrast-60 {
+    background-color: var(--lumo-contrast-60pct);
+  }
+  .bg-contrast-50 {
+    background-color: var(--lumo-contrast-50pct);
+  }
+  .bg-contrast-40 {
+    background-color: var(--lumo-contrast-40pct);
+  }
+  .bg-contrast-30 {
+    background-color: var(--lumo-contrast-30pct);
+  }
+  .bg-contrast-20 {
+    background-color: var(--lumo-contrast-20pct);
+  }
+  .bg-contrast-10 {
+    background-color: var(--lumo-contrast-10pct);
+  }
+  .bg-contrast-5 {
+    background-color: var(--lumo-contrast-5pct);
   }
 
   .bg-primary {
--- packages/vaadin-lumo-styles/utilities/transition.js
+++ packages/vaadin-lumo-styles/utilities/transition.js
@@ -6,19 +6,19 @@
 import { css } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 
 export const transition = css`
-  .transition {
-    transition: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow,
-      transform, filter, backdrop-filter 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  .transition-none {
+    transition: none;
   }
   .transition-all {
     transition: all 150ms cubic-bezier(0.4, 0, 0.2, 1);
   }
+  .transition {
+    transition: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow,
+      transform, filter, backdrop-filter 150ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
   .transition-colors {
     transition: color, background-color, border-color, text-decoration-color, fill,
       stroke 150ms cubic-bezier(0.4, 0, 0.2, 1);
-  }
-  .transition-none {
-    transition: none;
   }
   .transition-opacity {
     transition: opacity 150ms cubic-bezier(0.4, 0, 0.2, 1);
