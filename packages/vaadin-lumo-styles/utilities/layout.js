5e3c433bb65f9bc283468cc5d5110a544ea880df
Jonte Päivärinne <joacim@vaadin.com>
2024-07-11 11:48:22 +0300

    feat: add new utility classes, sort existing classes (#7515)

--- packages/vaadin-lumo-styles/utilities/layout.js
+++ packages/vaadin-lumo-styles/utilities/layout.js
@@ -258,6 +258,9 @@ export const layout = css`
   }
 
   /* === Z-index === */
+  .z-0 {
+    z-index: 0;
+  }
   .z-10 {
     z-index: 10;
   }
@@ -272,6 +275,9 @@ export const layout = css`
   }
   .z-50 {
     z-index: 50;
+  }
+  .z-auto {
+    z-index: auto;
   }
 
   /* === Responsive design === */
