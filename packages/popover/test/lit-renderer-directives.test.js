baff00b4795d9c5cbade58807ed5a638d46ba086
web-padawan <iamkulykov@gmail.com>
2024-08-01 15:53:53 +0300

    feat: add Lit renderer directive for popover

---
+++ packages/popover/test/lit-renderer-directives.test.js
@@ -0,0 +1,69 @@
+import { expect } from '@esm-bundle/chai';
+import { fixtureSync, nextFrame, nextUpdate } from '@vaadin/testing-helpers';
+import sinon from 'sinon';
+import './not-animated-styles.js';
+import '../vaadin-popover.js';
+import { html, nothing, render } from 'lit';
+import { popoverRenderer } from '../lit.js';
+
+async function renderOpenedPopover(container, { content }) {
+  render(
+    html`
+      <vaadin-popover opened ${content ? popoverRenderer(() => html`${content}`, content) : nothing}></vaadin-popover>
+    `,
+    container,
+  );
+  const popover = container.querySelector('vaadin-popover');
+  await nextUpdate(popover);
+  return popover;
+}
+
+describe('lit renderer directives', () => {
+  let container, popover, overlay;
+
+  beforeEach(() => {
+    container = fixtureSync('<div></div>');
+  });
+
+  describe('popoverRenderer', () => {
+    describe('basic', () => {
+      beforeEach(async () => {
+        popover = await renderOpenedPopover(container, { content: 'Content' });
+        overlay = popover._overlayElement;
+      });
+
+      it('should set `renderer` property when the directive is attached', () => {
+        expect(popover.renderer).to.exist;
+      });
+
+      it('should unset `renderer` property when the directive is detached', async () => {
+        await renderOpenedPopover(container, {});
+        expect(popover.renderer).not.to.exist;
+      });
+
+      it('should render the content with the renderer', () => {
+        expect(overlay.textContent).to.equal('Content');
+      });
+
+      it('should re-render the content when a renderer dependency changes', async () => {
+        await renderOpenedPopover(container, { content: 'New Content' });
+        expect(overlay.textContent).to.equal('New Content');
+      });
+    });
+
+    describe('arguments', () => {
+      let rendererSpy;
+
+      beforeEach(async () => {
+        rendererSpy = sinon.spy();
+        render(html`<vaadin-popover opened ${popoverRenderer(rendererSpy)}></vaadin-popover>`, container);
+        await nextFrame();
+        popover = container.querySelector('vaadin-popover');
+      });
+
+      it('should pass the popover instance to the renderer', () => {
+        expect(rendererSpy.firstCall.args[0]).to.equal(popover);
+      });
+    });
+  });
+});
