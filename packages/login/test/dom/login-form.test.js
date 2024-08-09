da3667818e1212be3ed0c5790d5ec1cbccee9285
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:20:16 +0300

    chore: update login package to chai v5

--- packages/login/test/dom/login-form.test.js
+++ packages/login/test/dom/login-form.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import '../../vaadin-login-form.js';
 import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
--- packages/login/test/login-form.common.js
+++ packages/login/test/login-form.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { enter, fixtureSync, nextRender, nextUpdate, tap } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import { fillUsernameAndPassword } from './helpers.js';
