c95e2c92918edf31862a490936926260781bf298
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:25:50 +0300

    chore: update progress-bar package to chai v5

--- packages/progress-bar/test/progress-bar.common.js
+++ packages/progress-bar/test/progress-bar.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, nextRender, nextUpdate } from '@vaadin/testing-helpers';
 
 describe('progress bar', () => {
