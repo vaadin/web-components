9e3b986e1fcdaf58f525d4705284214967cf20e9
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:28:24 +0300

    chore: update tabs package to chai v5

--- packages/tabs/test/scroll.common.js
+++ packages/tabs/test/scroll.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { arrowDown, arrowLeft, arrowRight, arrowUp, fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
 
 describe('scrollable tabs', () => {
