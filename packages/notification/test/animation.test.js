121346b1b1ebc6784fcd19ef743202e03d63a5d1
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:23:12 +0300

    chore: update notification package to chai v5

--- packages/notification/test/animation.test.js
+++ packages/notification/test/animation.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { aTimeout, fixtureSync, oneEvent } from '@vaadin/testing-helpers';
 import '../vaadin-notification.js';
 
