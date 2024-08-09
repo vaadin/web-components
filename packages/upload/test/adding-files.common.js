be77cd478e9a97f10231fb0a2e1265e435e43400
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:30:24 +0300

    chore: update upload package to chai v5

--- packages/upload/test/adding-files.common.js
+++ packages/upload/test/adding-files.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { change, fixtureSync, nextRender, nextUpdate } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import { createFile, createFiles, touchDevice, xhrCreator } from './helpers.js';
--- packages/upload/test/upload.common.js
+++ packages/upload/test/upload.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, nextRender, nextUpdate } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
