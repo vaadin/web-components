d3b908387a46546d6e5ccc6eecbd4626ef94cddc
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:26:11 +0300

    chore: update radio-group package to chai v5

--- packages/radio-group/test/dom/radio-button.test.js
+++ packages/radio-group/test/dom/radio-button.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync } from '@vaadin/testing-helpers';
 import '../../vaadin-radio-button.js';
 import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
