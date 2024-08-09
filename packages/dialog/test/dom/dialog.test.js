1ec2537aa0407ae1e6ff7f2311223ddb1cb11bdb
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:02:20 +0300

    chore: update dialog package to chai v5

--- packages/dialog/test/dom/dialog.test.js
+++ packages/dialog/test/dom/dialog.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, nextRender, oneEvent } from '@vaadin/testing-helpers';
 import '../../src/vaadin-dialog.js';
 
