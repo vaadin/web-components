a97917e616ba8f6b81f830af6214f73acbf7c541
web-padawan <iamkulykov@gmail.com>
2024-08-09 11:55:32 +0300

    chore: update charts package to chai v5

--- packages/charts/test/events.test.js
+++ packages/charts/test/events.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, oneEvent } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 import '../vaadin-chart.js';
