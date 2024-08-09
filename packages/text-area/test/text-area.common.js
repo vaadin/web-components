a8c2ec361ffac864e0894be24fae915ef1357d4c
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:29:08 +0300

    chore: update text-area package to chai v5

--- packages/text-area/test/text-area.common.js
+++ packages/text-area/test/text-area.common.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fire, fixtureSync, nextFrame, nextRender, nextUpdate, oneEvent } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
 
