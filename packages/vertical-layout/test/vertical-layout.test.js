cc1f28504f24b52a7e3d7368caa91d4e035ee75c
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:31:44 +0300

    chore: update vertical-layout package to chai v5

--- packages/vertical-layout/test/vertical-layout.test.js
+++ packages/vertical-layout/test/vertical-layout.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync } from '@vaadin/testing-helpers';
 import '../vaadin-vertical-layout.js';
 import { getComputedCSSPropertyValue } from './helpers.js';
