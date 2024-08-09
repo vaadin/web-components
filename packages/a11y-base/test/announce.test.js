c9c2c30a5e4e791e9db06f3d76e4f9bc8f216248
web-padawan <iamkulykov@gmail.com>
2024-08-09 11:40:19 +0300

    chore: update a11y-base package to chai v5

--- packages/a11y-base/test/announce.test.js
+++ packages/a11y-base/test/announce.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import sinon from 'sinon';
 import { announce } from '../src/announce.js';
 
--- packages/a11y-base/test/list-mixin.test.js
+++ packages/a11y-base/test/list-mixin.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import {
   arrowDown,
   arrowLeft,
