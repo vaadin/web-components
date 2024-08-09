9a9992e38fa845de8b70b409cfe0eaa558f8d91a
web-padawan <iamkulykov@gmail.com>
2024-08-09 13:21:49 +0300

    chore: update message-input package to chai v5

--- packages/message-input/test/dom/message-input.test.js
+++ packages/message-input/test/dom/message-input.test.js
@@ -1,4 +1,4 @@
-import { expect } from '@esm-bundle/chai';
+import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync } from '@vaadin/testing-helpers';
 import '../../src/vaadin-message-input.js';
 import { resetUniqueId } from '@vaadin/component-base/src/unique-id-utils.js';
