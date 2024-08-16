20c40f00243eedbe6d0c3978c03c37e308c3b4aa
Tomi Virkki <virkki@vaadin.com>
2024-08-16 13:23:45 +0300

    use layout styles in dashboard

--- packages/dashboard/theme/lumo/vaadin-dashboard-layout-styles.js
+++ packages/dashboard/theme/lumo/vaadin-dashboard-layout-styles.js
@@ -1,13 +1,11 @@
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-registerStyles(
-  'vaadin-dashboard-layout',
-  css`
-    :host {
-      --vaadin-dashboard-gap: var(--lumo-space-m);
-    }
-  `,
-  {
-    moduleId: 'lumo-dashboard-layout',
-  },
-);
+export const dashboardLayoutStyles = css`
+  :host {
+    --vaadin-dashboard-gap: var(--lumo-space-m);
+  }
+`;
+
+registerStyles('vaadin-dashboard-layout', [dashboardLayoutStyles], {
+  moduleId: 'lumo-dashboard-layout',
+});
--- packages/dashboard/theme/material/vaadin-dashboard-layout-styles.js
+++ packages/dashboard/theme/material/vaadin-dashboard-layout-styles.js
@@ -1,13 +1,11 @@
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
-registerStyles(
-  'vaadin-dashboard-layout',
-  css`
-    :host {
-      --vaadin-dashboard-gap: 1rem;
-    }
-  `,
-  {
-    moduleId: 'material-dashboard-layout',
-  },
-);
+export const dashboardLayoutStyles = css`
+  :host {
+    --vaadin-dashboard-gap: 1rem;
+  }
+`;
+
+registerStyles('vaadin-dashboard-layout', [dashboardLayoutStyles], {
+  moduleId: 'material-dashboard-layout',
+});
