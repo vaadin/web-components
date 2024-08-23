c44c6287be66a1acd41581a634b59f0c632b7513
Sascha Ißbrücker <sissbruecker@vaadin.com>
2024-08-23 19:57:13 +0200

    cleanup

--- packages/side-nav/src/location.js
+++ packages/side-nav/src/location.js
@@ -1,8 +1,13 @@
 /**
+ * @license
+ * Copyright (c) 2023 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
  * Facade for `document.location`, can be stubbed for testing.
  *
  * For internal use only.
- *
  */
 export const location = {
   get pathname() {
