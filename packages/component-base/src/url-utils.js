dbd5f59164a538f0f0928be30a2fa2f84370f54c
Sascha Ißbrücker <sissbruecker@vaadin.com>
2024-08-28 08:13:50 +0200

    feat: allow matching navigation hierarchies with side nav item (#7693)
    * feat: allow matching navigation hierarchies with side nav item
    
    * cleanup
    
    * rename property to matchExact
    
    * address review comments
    
    * fix test case

--- packages/component-base/src/url-utils.js
+++ packages/component-base/src/url-utils.js
@@ -27,15 +27,17 @@ function containsQueryParams(actual, expected) {
  *
  * @param {string} actual The actual URL to match.
  * @param {string} expected The expected URL to match.
+ * @param {Object} matchOptions Options for path matching.
  */
-export function matchPaths(actual, expected) {
+export function matchPaths(actual, expected, matchOptions = { matchNested: false }) {
   const base = document.baseURI;
   const actualUrl = new URL(actual, base);
   const expectedUrl = new URL(expected, base);
 
-  return (
-    actualUrl.origin === expectedUrl.origin &&
-    actualUrl.pathname === expectedUrl.pathname &&
-    containsQueryParams(actualUrl.searchParams, expectedUrl.searchParams)
-  );
+  const matchesOrigin = actualUrl.origin === expectedUrl.origin;
+  const matchesPath = matchOptions.matchNested
+    ? actualUrl.pathname === expectedUrl.pathname || actualUrl.pathname.startsWith(`${expectedUrl.pathname}/`)
+    : actualUrl.pathname === expectedUrl.pathname;
+
+  return matchesOrigin && matchesPath && containsQueryParams(actualUrl.searchParams, expectedUrl.searchParams);
 }
---
+++ packages/side-nav/src/location.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2023 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
+ * Facade for `document.location`, can be stubbed for testing.
+ *
+ * For internal use only.
+ */
+export const location = {
+  get pathname() {
+    return document.location.pathname;
+  },
+  get search() {
+    return document.location.search;
+  },
+};
