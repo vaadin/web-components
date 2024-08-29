8a8383668d6b53da0e74260baff8bb1bb1ddcf4a
Serhii Kulykov <iamkulykov@gmail.com>
2024-08-29 13:33:14 +0300

    chore: use puppeteer launcher to run Chrome tests (#7701) (#7711)

--- web-test-runner-it.config.js
+++ web-test-runner-it.config.js
@@ -1,8 +1,17 @@
 /* eslint-env node */
+const { puppeteerLauncher } = require('@web/test-runner-puppeteer');
 const { createIntegrationTestsConfig } = require('./wtr-utils.js');
 const devServerConfig = require('./web-dev-server.config.js');
 
-const unitTestsConfig = createIntegrationTestsConfig();
+const unitTestsConfig = createIntegrationTestsConfig({
+  browsers: [
+    puppeteerLauncher({
+      launchOptions: {
+        headless: 'shell',
+      },
+    }),
+  ],
+});
 
 module.exports = {
   ...unitTestsConfig,
