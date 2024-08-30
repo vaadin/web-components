2b16c2a5ea5e4ad06951dd6d5b3a7a9b3a429b58
web-padawan <iamkulykov@gmail.com>
2024-08-30 15:54:17 +0300

    test: remove no longer needed dom-repeat import

--- packages/grid/test/row-details.common.js
+++ packages/grid/test/row-details.common.js
@@ -1,7 +1,6 @@
 import { expect } from '@vaadin/chai-plugins';
 import { aTimeout, click, fixtureSync, nextFrame } from '@vaadin/testing-helpers';
 import sinon from 'sinon';
-import '@polymer/polymer/lib/elements/dom-repeat.js';
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import {
   buildDataSet,
