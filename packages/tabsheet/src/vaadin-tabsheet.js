db1254aea51cf304b5f186e553abac4cb57d060a
web-padawan <iamkulykov@gmail.com>
2024-09-04 15:54:55 +0300

    docs: fix broken link to styling docs in tabsheet JSDoc

--- packages/tabsheet/src/vaadin-tabsheet.js
+++ packages/tabsheet/src/vaadin-tabsheet.js
@@ -48,7 +48,7 @@ import { TabSheetMixin } from './vaadin-tabsheet-mixin.js';
  * `loading` | Set when a tab without associated content is selected
  * `overflow`   | Set to `top`, `bottom`, `start`, `end`, all of them, or none.
  *
- * See [Styling Components](hhttps://vaadin.com/docs/latest/components/ds-resources/customization/styling-components) documentation.
+ * See [Styling Components](https://vaadin.com/docs/latest/styling/styling-components) documentation.
  *
  * @fires {CustomEvent} items-changed - Fired when the `items` property changes.
  * @fires {CustomEvent} selected-changed - Fired when the `selected` property changes.
