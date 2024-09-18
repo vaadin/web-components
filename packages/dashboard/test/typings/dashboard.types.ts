528e9676a7f56a9595830763b2ec3951453b8525
Tomi Virkki <virkki@vaadin.com>
2024-09-18 09:47:54 +0300

    chore: remove unnecessary definitions

--- packages/dashboard/test/typings/dashboard.types.ts
+++ packages/dashboard/test/typings/dashboard.types.ts
@@ -1,5 +1,4 @@
 import type { ElementMixinClass } from '@vaadin/component-base/src/element-mixin.js';
-import { TitleController } from '../../src/title-controller.js';
 import type { DashboardLayoutMixinClass } from '../../src/vaadin-dashboard-layout-mixin.js';
 import type {
   Dashboard,
@@ -101,8 +100,3 @@ const section = document.createElement('vaadin-dashboard-section');
 assertType<DashboardSection>(section);
 
 assertType<string | null | undefined>(section.sectionTitle);
-
-/* TitleController */
-const titleController = new TitleController(layout);
-assertType<TitleController>(titleController);
-assertType<(title: string | null | undefined) => void>(titleController.setTitle);
