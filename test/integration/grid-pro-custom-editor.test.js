3cb175232b79090789192ea73d050c13a3cbd40f
web-padawan <iamkulykov@gmail.com>
2024-09-19 15:58:03 +0300

    test: use single beforeEach to reduce duplication

--- test/integration/grid-pro-custom-editor.test.js
+++ test/integration/grid-pro-custom-editor.test.js
@@ -8,38 +8,48 @@ import '@vaadin/grid-pro/vaadin-grid-pro-edit-column.js';
 import { flushGrid, getContainerCell } from '@vaadin/grid-pro/test/helpers.js';
 
 describe('grid-pro custom editor', () => {
+  let grid;
+
+  beforeEach(async () => {
+    grid = fixtureSync(`
+      <vaadin-grid-pro>
+        <vaadin-grid-pro-edit-column path="firstName"></vaadin-grid-pro-edit-column>
+        <vaadin-grid-pro-edit-column path="lastName"></vaadin-grid-pro-edit-column>
+        <vaadin-grid-pro-edit-column path="birthday" editor-type="custom"></vaadin-grid-pro-edit-column>
+        <vaadin-grid-pro-edit-column path="status" editor-type="custom"></vaadin-grid-pro-edit-column>
+      </vaadin-gri-pro>
+    `);
+
+    grid.items = [
+      { firstName: 'Aria', lastName: 'Bailey', birthday: '1984-01-13', status: 'suspended' },
+      { firstName: 'Aaliyah', lastName: 'Butler', birthday: '1977-07-12', status: 'active' },
+      { firstName: 'Eleanor', lastName: 'Price', birthday: '1976-12-14', status: 'suspended' },
+      { firstName: 'Allison', lastName: 'Torres', birthday: '1972-12-04', status: 'active' },
+      { firstName: 'Madeline', lastName: 'Lewis', birthday: '1978-02-03', status: 'active' },
+    ];
+
+    grid.querySelector('[path="birthday"]').editModeRenderer = (root, _, model) => {
+      root.innerHTML = `
+        <vaadin-date-picker value="${model.item.birthday}" auto-open-disabled>
+        </vaadin-date-picker>
+      `;
+    };
+
+    grid.querySelector('[path="status"]').editModeRenderer = (root, _, model) => {
+      if (!root.firstChild) {
+        const comboBox = document.createElement('vaadin-combo-box');
+        comboBox.autoOpenDisabled = true;
+        comboBox.items = ['active', 'suspended'];
+        root.appendChild(comboBox);
+      }
+      root.firstChild.value = model.item.status;
+    };
+
+    flushGrid(grid);
+    await nextRender();
+  });
+
   describe('date-picker', () => {
-    let grid;
-
-    beforeEach(async () => {
-      grid = fixtureSync(`
-        <vaadin-grid-pro>
-          <vaadin-grid-pro-edit-column path="firstName"></vaadin-grid-pro-edit-column>
-          <vaadin-grid-pro-edit-column path="lastName"></vaadin-grid-pro-edit-column>
-          <vaadin-grid-pro-edit-column path="birthday" editor-type="custom"></vaadin-grid-pro-edit-column>
-        </vaadin-gri-pro>
-      `);
-
-      grid.querySelector('[path="birthday"]').editModeRenderer = (root, _, model) => {
-        root.innerHTML = `
-          <vaadin-date-picker value="${model.item.birthday}" auto-open-disabled>
-          </vaadin-date-picker>
-        `;
-      };
-
-      grid.items = [
-        { firstName: 'Aria', lastName: 'Bailey', birthday: '1984-01-13' },
-        { firstName: 'Aaliyah', lastName: 'Butler', birthday: '1977-07-12' },
-        { firstName: 'Eleanor', lastName: 'Price', birthday: '1976-12-14' },
-        { firstName: 'Allison', lastName: 'Torres', birthday: '1972-12-04' },
-        { firstName: 'Madeline', lastName: 'Lewis', birthday: '1978-02-03' },
-      ];
-
-      flushGrid(grid);
-
-      await nextRender();
-    });
-
     it('should apply the updated date to the cell when exiting on Tab', async () => {
       const cell = getContainerCell(grid.$.items, 0, 2);
       cell.focus();
@@ -66,41 +76,8 @@ describe('grid-pro custom editor', () => {
   });
 
   describe('combo-box', () => {
-    let grid;
-
-    beforeEach(async () => {
-      grid = fixtureSync(`
-        <vaadin-grid-pro>
-          <vaadin-grid-pro-edit-column path="firstName"></vaadin-grid-pro-edit-column>
-          <vaadin-grid-pro-edit-column path="lastName"></vaadin-grid-pro-edit-column>
-          <vaadin-grid-pro-edit-column path="status" editor-type="custom"></vaadin-grid-pro-edit-column>
-        </vaadin-gri-pro>
-      `);
-
-      grid.querySelector('[path="status"]').editModeRenderer = (root, _, model) => {
-        if (!root.firstChild) {
-          const comboBox = document.createElement('vaadin-combo-box');
-          comboBox.autoOpenDisabled = true;
-          comboBox.items = ['active', 'suspended'];
-          root.appendChild(comboBox);
-        }
-        root.firstChild.value = model.item.status;
-      };
-
-      grid.items = [
-        { firstName: 'Aria', lastName: 'Bailey', status: 'suspended' },
-        { firstName: 'Aaliyah', lastName: 'Butler', status: 'active' },
-        { firstName: 'Eleanor', lastName: 'Price', status: 'suspended' },
-        { firstName: 'Allison', lastName: 'Torres', status: 'active' },
-        { firstName: 'Madeline', lastName: 'Lewis', status: 'active' },
-      ];
-
-      flushGrid(grid);
-      await nextRender();
-    });
-
     it('should apply the updated value to the cell when exiting on Tab', async () => {
-      const cell = getContainerCell(grid.$.items, 0, 2);
+      const cell = getContainerCell(grid.$.items, 0, 3);
       cell.focus();
 
       await sendKeys({ press: 'Enter' });
@@ -112,7 +89,7 @@ describe('grid-pro custom editor', () => {
     });
 
     it('should apply the updated value to the cell when exiting on Enter', async () => {
-      const cell = getContainerCell(grid.$.items, 0, 2);
+      const cell = getContainerCell(grid.$.items, 0, 3);
       cell.focus();
 
       await sendKeys({ press: 'Enter' });
