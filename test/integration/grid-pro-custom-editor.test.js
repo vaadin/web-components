674b92cd0d9a4f1742f7346feaa9ad0196656281
web-padawan <iamkulykov@gmail.com>
2024-09-19 15:51:06 +0300

    test: add integration test with combo-box as cell editor

--- test/integration/grid-pro-custom-editor.test.js
+++ test/integration/grid-pro-custom-editor.test.js
@@ -1,6 +1,7 @@
 import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { sendKeys } from '@web/test-runner-commands';
+import '@vaadin/combo-box';
 import '@vaadin/date-picker';
 import '@vaadin/grid-pro';
 import '@vaadin/grid-pro/vaadin-grid-pro-edit-column.js';
@@ -61,6 +62,65 @@ describe('grid-pro custom editor', () => {
       await sendKeys({ press: 'Enter' });
 
       expect(cell._content.textContent).to.equal('1984-01-12');
+    });
+  });
+
+  describe('combo-box', () => {
+    let grid;
+
+    beforeEach(async () => {
+      grid = fixtureSync(`
+        <vaadin-grid-pro>
+          <vaadin-grid-pro-edit-column path="firstName"></vaadin-grid-pro-edit-column>
+          <vaadin-grid-pro-edit-column path="lastName"></vaadin-grid-pro-edit-column>
+          <vaadin-grid-pro-edit-column path="status" editor-type="custom"></vaadin-grid-pro-edit-column>
+        </vaadin-gri-pro>
+      `);
+
+      grid.querySelector('[path="status"]').editModeRenderer = (root, _, model) => {
+        if (!root.firstChild) {
+          const comboBox = document.createElement('vaadin-combo-box');
+          comboBox.autoOpenDisabled = true;
+          comboBox.items = ['active', 'suspended'];
+          root.appendChild(comboBox);
+        }
+        root.firstChild.value = model.item.status;
+      };
+
+      grid.items = [
+        { firstName: 'Aria', lastName: 'Bailey', status: 'suspended' },
+        { firstName: 'Aaliyah', lastName: 'Butler', status: 'active' },
+        { firstName: 'Eleanor', lastName: 'Price', status: 'suspended' },
+        { firstName: 'Allison', lastName: 'Torres', status: 'active' },
+        { firstName: 'Madeline', lastName: 'Lewis', status: 'active' },
+      ];
+
+      flushGrid(grid);
+      await nextRender();
+    });
+
+    it('should apply the updated value to the cell when exiting on Tab', async () => {
+      const cell = getContainerCell(grid.$.items, 0, 2);
+      cell.focus();
+
+      await sendKeys({ press: 'Enter' });
+
+      await sendKeys({ type: 'active' });
+      await sendKeys({ press: 'Tab' });
+
+      expect(cell._content.textContent).to.equal('active');
+    });
+
+    it('should apply the updated value to the cell when exiting on Enter', async () => {
+      const cell = getContainerCell(grid.$.items, 0, 2);
+      cell.focus();
+
+      await sendKeys({ press: 'Enter' });
+
+      await sendKeys({ type: 'active' });
+      await sendKeys({ press: 'Enter' });
+
+      expect(cell._content.textContent).to.equal('active');
     });
   });
 });
