c27260840d8acdff061a667671c84e0307b589d5
web-padawan <iamkulykov@gmail.com>
2024-09-23 17:01:52 +0300

    fix: check if element is focusable when preventing mousedown

--- packages/combo-box/src/vaadin-combo-box-overlay-mixin.js
+++ packages/combo-box/src/vaadin-combo-box-overlay-mixin.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2015 - 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import { isElementFocusable } from '@vaadin/a11y-base/src/focus-utils.js';
 import { PositionMixin } from '@vaadin/overlay/src/vaadin-overlay-position-mixin.js';
 
 /**
@@ -53,8 +54,9 @@ export const ComboBoxOverlayMixin = (superClass) =>
     _mouseDownListener(event) {
       super._mouseDownListener(event);
 
-      // Prevent global mousedown event to avoid losing focus on outside click.
-      if (this._shouldCloseOnOutsideClick(event) && event.target === document.documentElement) {
+      // Prevent global mousedown event to avoid losing focus on outside click,
+      // unless the clicked element is also focusable (e.g. in date-time-picker).
+      if (this._shouldCloseOnOutsideClick(event) && !isElementFocusable(event.composedPath()[0])) {
         event.preventDefault();
       }
     }
