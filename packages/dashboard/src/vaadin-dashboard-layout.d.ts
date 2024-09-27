7d55d838141e6ff59b29ec98bba24fdec122d443
ugur-vaadin <ugur@vaadin.com>
2024-09-27 16:49:15 +0300

    fix: make dashboard layout extend layout mixin

--- packages/dashboard/src/vaadin-dashboard-layout.d.ts
+++ packages/dashboard/src/vaadin-dashboard-layout.d.ts
@@ -10,11 +10,12 @@
  */
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { DashboardLayoutMixin } from './vaadin-dashboard-layout-mixin';
 
 /**
  * A responsive, grid-based dashboard layout component
  */
-declare class DashboardLayout extends ElementMixin(ThemableMixin(HTMLElement)) {}
+declare class DashboardLayout extends DashboardLayoutMixin(ElementMixin(ThemableMixin(HTMLElement))) {}
 
 declare global {
   interface HTMLElementTagNameMap {
