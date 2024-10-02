a10650952c31bb231faecc2993d324dad8f52eb8
Serhii Kulykov <iamkulykov@gmail.com>
2024-10-02 15:24:47 +0300

    fix: use correct ComboBoxScrollerMixin import in typings (#7917)

--- packages/combo-box/src/vaadin-combo-box-scroller.d.ts
+++ packages/combo-box/src/vaadin-combo-box-scroller.d.ts
@@ -3,7 +3,7 @@
  * Copyright (c) 2015 - 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { ComboBoxScrollerMixin } from './vaadin-combo-box-overlay-mixin.js';
+import { ComboBoxScrollerMixin } from './vaadin-combo-box-scroller-mixin.js';
 
 /**
  * An element used internally by `<vaadin-combo-box>`. Not intended to be used separately.
