02cc35014f03d46079897f3f1ef1261303cc38ba
Serhii Kulykov <iamkulykov@gmail.com>
2024-10-02 14:53:51 +0300

    refactor: use correct import in template-renderer entrypoint (#7919)

--- packages/polymer-legacy-adapter/template-renderer.js
+++ packages/polymer-legacy-adapter/template-renderer.js
@@ -3,4 +3,4 @@
  * Copyright (c) 2021 - 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-export * from './src/template-renderer.js';
+import './src/template-renderer.js';
