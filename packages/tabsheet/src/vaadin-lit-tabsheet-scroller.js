bb9943cc3aaa44790d90960c3b5f514f166b0fc7
web-padawan <iamkulykov@gmail.com>
2024-10-04 15:25:26 +0300

    experiment: add Lit based version of vaadin-tabsheet

---
+++ packages/tabsheet/src/vaadin-lit-tabsheet-scroller.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright (c) 2022 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { defineCustomElement } from '@vaadin/component-base/src/define.js';
+import { Scroller } from '@vaadin/scroller/src/vaadin-lit-scroller.js';
+
+/**
+ * An element used internally by `<vaadin-tabsheet>`. Not intended to be used separately.
+ *
+ * @extends Scroller
+ * @private
+ */
+class TabsheetScroller extends Scroller {
+  static get is() {
+    return 'vaadin-tabsheet-scroller';
+  }
+}
+
+defineCustomElement(TabsheetScroller);
---
+++ packages/tabsheet/test/tabsheet-lit.test.js
@@ -0,0 +1,3 @@
+import '@vaadin/tabs/src/vaadin-lit-tabs.js';
+import '../src/vaadin-lit-tabsheet.js';
+import './tabsheet.common.js';
