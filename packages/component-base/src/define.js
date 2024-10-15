30b1caac09eee40aa06afc31cfaa5de78f53d304
web-padawan <iamkulykov@gmail.com>
2024-10-15 10:47:10 +0300

    --wip-- [skip ci]

--- packages/component-base/src/define.js
+++ packages/component-base/src/define.js
@@ -4,12 +4,38 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 
+window.Vaadin ||= {};
+window.Vaadin.featureFlags ||= {};
+
+function dashToCamelCase(dash) {
+  return dash.replace(/-[a-z]/gu, (m) => m[1].toUpperCase());
+}
+
 export function defineCustomElement(CustomElement, version = '24.6.0-alpha2') {
   Object.defineProperty(CustomElement, 'version', {
     get() {
       return version;
     },
   });
+
+  if (CustomElement.experimental) {
+    const featureFlagKey = `${dashToCamelCase(CustomElement.is)}Component`;
+    if (!window.Vaadin.featureFlags[featureFlagKey]) {
+      // Add setter to define experimental component when it's set to true
+      Object.defineProperty(window.Vaadin.featureFlags, featureFlagKey, {
+        get() {
+          return !!customElements.get(CustomElement.is);
+        },
+        set(value) {
+          if (!!value && !customElements.get(CustomElement.is)) {
+            customElements.define(CustomElement.is, CustomElement);
+          }
+        },
+      });
+
+      return;
+    }
+  }
 
   const defined = customElements.get(CustomElement.is);
   if (!defined) {
