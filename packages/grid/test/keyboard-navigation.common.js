4c3612d2038330099d77eae7b61e0d00dfa00312
Sergey Vinogradov <mr.vursen@gmail.com>
2024-10-17 18:18:00 +0400

    extract getFocusedCell, getFocusedRow into separate file

--- packages/grid/test/keyboard-navigation.common.js
+++ packages/grid/test/keyboard-navigation.common.js
@@ -1321,8 +1321,8 @@ describe('keyboard navigation', () => {
         grid.$.table.scrollTop = grid.$.table.scrollHeight / 2;
         flushGrid(grid);
         down();
-        expect(getFocusedRowIndex()).to.equal(1);
-        expect(getFocusedCellIndex()).to.equal(0);
+        expect(getFocusedRowIndex(grid)).to.equal(1);
+        expect(getFocusedCellIndex(grid)).to.equal(0);
       });
 
       it('should scroll focused row into view on Tab', () => {
@@ -1333,8 +1333,8 @@ describe('keyboard navigation', () => {
         grid.$.table.scrollTop = grid.$.table.scrollHeight / 2;
         flushGrid(grid);
         tab();
-        expect(getFocusedRowIndex()).to.equal(0);
-        expect(getFocusedCellIndex()).to.equal(0);
+        expect(getFocusedRowIndex(grid)).to.equal(0);
+        expect(getFocusedCellIndex(grid)).to.equal(0);
       });
 
       it('should hide navigation mode when a focused row goes off screen', () => {
