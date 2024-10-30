10be4ea47188affbb5f4e4fc176a2183825cf0ef
web-padawan <iamkulykov@gmail.com>
2024-10-30 13:11:25 +0200

    experiment: prepare LitElement based combo-box for publishing

--- packages/combo-box/src/vaadin-lit-combo-box-light.js
+++ packages/combo-box/src/vaadin-lit-combo-box-light.js
@@ -18,7 +18,7 @@ import { ComboBoxLightMixin } from './vaadin-combo-box-light-mixin.js';
  *
  * ## Disclaimer
  *
- * This component is an experiment not intended for publishing to npm.
+ * This component is an experiment and not yet a part of Vaadin platform.
  * There is no ETA regarding specific Vaadin version where it'll land.
  * Feel free to try this code in your apps as per Apache 2.0 license.
  */
---
+++ packages/combo-box/vaadin-lit-combo-box.js
@@ -0,0 +1,2 @@
+import './theme/lumo/vaadin-lit-combo-box.js';
+export * from './src/vaadin-lit-combo-box.js';
