db9deb43d2edceb97b88741222eb7ea87753315c
web-padawan <iamkulykov@gmail.com>
2024-10-31 11:20:25 +0200

    fix: import src version of vaadin-tabs in tabsheet

--- packages/tabsheet/src/vaadin-lit-tabsheet.js
+++ packages/tabsheet/src/vaadin-lit-tabsheet.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2022 - 2024 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import '@vaadin/tabs/src/vaadin-lit-tabs.js';
 import './vaadin-lit-tabsheet-scroller.js';
 import { css, html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
