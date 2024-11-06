6d3e615406b305009debf223c3f46ea4ae0370d6
Sergey Vinogradov <mr.vursen@gmail.com>
2024-11-06 11:19:11 +0400

    minor documentation improvements

--- packages/field-base/src/validate-mixin.js
+++ packages/field-base/src/validate-mixin.js
@@ -26,12 +26,14 @@ export const ValidateMixin = dedupingMixin(
           },
 
           /**
-           * Set to true to enable manual validation mode. In this mode, automatic constraint
-           * validation is disabled, allowing you to control the validation process by yourself.
-           * The field can still be validated programmatically by calling the `validate()` method.
-           * To check the validity without changing the invalid state, use the `checkValidity()` method.
-           * This mode also allows you to manipulate the `invalid` property directly without getting
-           * conflicts with the built-in constraint validation.
+           * Set to true to enable manual validation mode.
+           *
+           * This mode disables automatic constraint validation, providing full control
+           * over the validation process. You can still trigger constraint validation
+           * manually with the `validate()` method or check validity without affecting
+           * the invalid state using `checkValidity()`. Use this mode also to prevent
+           * conflicts with automatic validation if your custom validation logic requires
+           * manipulating the `invalid` property directly.
            *
            * @attr {boolean} manual-validation
            */
