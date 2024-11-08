c3d1cebdc7a60b21ac49363f90b183e4252ab7d4
Serhii Kulykov <iamkulykov@gmail.com>
2024-11-08 10:38:42 +0200

    experiment: prepare LitElement based tabsheet for publishing (#8107)

---
+++ packages/tabsheet/theme/lumo/vaadin-lit-tabsheet.js
@@ -0,0 +1,4 @@
+import '@vaadin/tabs/theme/lumo/vaadin-lit-tabs.js';
+import '@vaadin/scroller/theme/lumo/vaadin-lit-scroller.js';
+import './vaadin-tabsheet-styles.js';
+import '../../src/vaadin-lit-tabsheet.js';
---
+++ packages/tabsheet/vaadin-lit-tabsheet.js
@@ -0,0 +1,2 @@
+import './theme/lumo/vaadin-lit-tabsheet.js';
+export * from './src/vaadin-lit-tabsheet.js';
