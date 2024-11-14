03e05f23fd146d0f1710d93b9960e9e60c0902de
Jouni Koivuviita <jouni@vaadin.com>
2024-11-14 15:21:26 +0200

    first pass at all the things

---
+++ packages/card/src/vaadin-card.js
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css, html, LitElement } from 'lit';
+import { defineCustomElement } from '@vaadin/component-base/src/define.js';
+import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
+import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+/**
+ * <vaadin-card> is a visual content container.
+ *
+ * ```html
+ * <vaadin-card class="flex flex-col overflow-hidden">
+ *   <img
+ *     src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+ *     alt="">
+ *   <div class="flex flex-col items-start p-m">
+ *     <h3>Card Title</h3>
+ *     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+ *     <span theme="badge">Label</span>
+ *   </div>
+ * </vaadin-card>
+ * ```
+ *
+ * [<img src="https://raw.githubusercontent.com/vaadin/web-components/main/packages/card/screenshot.png" width="296" alt="Screenshot of vaadin-card">](https://vaadin.com/docs/latest/components/button)
+ *
+ * @customElement
+ * @extends HTMLElement
+ * @mixes ElementMixin
+ * @mixes ThemableMixin
+ */
+class Card extends ElementMixin(ThemableMixin(PolylitMixin(LitElement))) {
+  static get is() {
+    return 'vaadin-card';
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+      }
+    `;
+  }
+
+  /** @protected */
+  render() {
+    return html`<slot></slot>`;
+  }
+}
+
+defineCustomElement(Card);
+
+export { Card };
