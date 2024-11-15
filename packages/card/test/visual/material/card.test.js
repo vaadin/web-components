0d39fc47debd3ea319ab6200a8733b80573ccbd7
Jouni Koivuviita <jouni@vaadin.com>
2024-11-15 13:39:30 +0200

    update screenshots

--- packages/card/test/visual/material/card.test.js
+++ packages/card/test/visual/material/card.test.js
@@ -2,14 +2,16 @@ import { fixtureSync } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
 import '../../../theme/material/vaadin-card.js';
 
+window.Vaadin.featureFlags.vaadinCardComponent = true;
+
 describe('card', () => {
   let div, element;
 
   beforeEach(() => {
     div = document.createElement('div');
     div.style.display = 'inline-block';
-    div.style.padding = '10px';
-    element = fixtureSync('<vaadin-card><div style="padding: 10px;">Card</div></vaadin-card>', div);
+    div.style.padding = '20px';
+    element = fixtureSync('<vaadin-card><div style="padding: 20px;">Card</div></vaadin-card>', div);
   });
 
   describe('theme', () => {
