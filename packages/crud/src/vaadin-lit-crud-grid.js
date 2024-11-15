3ea6157fc293a95cfd1c3e7beb1be8f9ae00a108
Tomi Virkki <virkki@vaadin.com>
2024-11-15 16:16:03 +0200

    manual changes

--- packages/crud/src/vaadin-lit-crud-grid.js
+++ packages/crud/src/vaadin-lit-crud-grid.js
@@ -8,14 +8,14 @@
  * See https://vaadin.com/commercial-license-and-service-terms for the full
  * license.
  */
-import '@vaadin/grid/src/vaadin-grid-column-group.js';
-import '@vaadin/grid/src/vaadin-grid-column.js';
-import '@vaadin/grid/src/vaadin-grid-filter.js';
-import '@vaadin/grid/src/vaadin-grid-sorter.js';
-import './vaadin-crud-edit-column.js';
+import '@vaadin/grid/src/vaadin-lit-grid-column-group.js';
+import '@vaadin/grid/src/vaadin-lit-grid-column.js';
+import '@vaadin/grid/src/vaadin-lit-grid-filter.js';
+import '@vaadin/grid/src/vaadin-lit-grid-sorter.js';
+import './vaadin-lit-crud-edit-column.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
-import { Grid } from '@vaadin/grid/src/vaadin-grid.js';
+import { Grid } from '@vaadin/grid/src/vaadin-lit-grid.js';
 import { CrudGridMixin } from './vaadin-crud-grid-mixin.js';
 
 /**
@@ -27,7 +27,7 @@ import { CrudGridMixin } from './vaadin-crud-grid-mixin.js';
  * There is no ETA regarding specific Vaadin version where it'll land.
  * Feel free to try this code in your apps as per Apache 2.0 license.
  */
-class CrudGrid extends CrudGridMixin(PolylitMixin(LitElement)) {
+class CrudGrid extends CrudGridMixin(PolylitMixin(Grid)) {
   static get is() {
     return 'vaadin-crud-grid';
   }
