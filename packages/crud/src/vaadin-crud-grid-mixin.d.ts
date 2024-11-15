6bda63616d42b338228554f1d710f7ad80970962
Tomi Virkki <virkki@vaadin.com>
2024-11-15 15:59:25 +0200

    refactor: extract crud logic into reusable mixins

---
+++ packages/crud/src/vaadin-crud-grid-mixin.d.ts
@@ -0,0 +1,29 @@
+import type { Constructor } from '@open-wc/dedupe-mixin';
+import type { IncludedMixinClass } from './vaadin-crud-include-mixin.js';
+
+/**
+ * A mixin providing common crud grid functionality.
+ */
+export declare function CrudGridMixin<T extends Constructor<HTMLElement>>(
+  base: T,
+): Constructor<CrudGridMixinClass> & Constructor<IncludedMixinClass> & T;
+
+export declare class CrudGridMixinClass {
+  /**
+   * Disable filtering in the generated columns.
+   * @attr {boolean} no-filter
+   */
+  noFilter: boolean | null | undefined;
+
+  /**
+   * Disable sorting in the generated columns.
+   * @attr {boolean} no-sort
+   */
+  noSort: boolean | null | undefined;
+
+  /**
+   * Do not add headers to columns.
+   * @attr {boolean} no-head
+   */
+  noHead: boolean | null | undefined;
+}
