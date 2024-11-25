2e96e63b4d9f121affb4eecddcb05add80956d89
web-padawan <iamkulykov@gmail.com>
2024-11-25 16:41:50 +0200

    refactor: remove no longer needed opened property override

--- packages/combo-box/src/vaadin-lit-combo-box-overlay.js
+++ packages/combo-box/src/vaadin-lit-combo-box-overlay.js
@@ -45,22 +45,6 @@ export class ComboBoxOverlay extends ComboBoxOverlayMixin(
     return [overlayStyles, comboBoxOverlayStyles, comboBoxOverlayStyles];
   }
 
-  static get properties() {
-    return {
-      /**
-       * When true, the overlay is visible and attached to body.
-       * This property config is overridden to set `sync: true`.
-       */
-      opened: {
-        type: Boolean,
-        notify: true,
-        observer: '_openedChanged',
-        reflectToAttribute: true,
-        sync: true,
-      },
-    };
-  }
-
   /** @protected */
   render() {
     return html`
