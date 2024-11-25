82e68fcc46bbe3f1016e908b6156dd45e7c7b74a
web-padawan <iamkulykov@gmail.com>
2024-11-25 15:40:03 +0200

    fix: make multi-select-combo-box overlay opened property sync

--- packages/multi-select-combo-box/src/vaadin-lit-multi-select-combo-box-overlay.js
+++ packages/multi-select-combo-box/src/vaadin-lit-multi-select-combo-box-overlay.js
@@ -49,6 +49,22 @@ class MultiSelectComboBoxOverlay extends ComboBoxOverlayMixin(
     return [overlayStyles, multiSelectComboBoxOverlayStyles];
   }
 
+  static get properties() {
+    return {
+      /**
+       * When true, the overlay is visible and attached to body.
+       * This property config is overridden to set `sync: true`.
+       */
+      opened: {
+        type: Boolean,
+        notify: true,
+        observer: '_openedChanged',
+        reflectToAttribute: true,
+        sync: true,
+      },
+    };
+  }
+
   /** @protected */
   render() {
     return html`
