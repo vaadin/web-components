96c47dfea1ce4fe6961279063ebb49a44c120255
Sergey Vinogradov <mr.vursen@gmail.com>
2024-11-26 15:37:12 +0400

    add missing sync: true

--- packages/grid/src/vaadin-grid-column-group-mixin.js
+++ packages/grid/src/vaadin-grid-column-group-mixin.js
@@ -41,6 +41,7 @@ export const GridColumnGroupMixin = (superClass) =>
         width: {
           type: String,
           readOnly: true,
+          sync: true,
         },
 
         /** @private */
