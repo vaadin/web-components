c63be0f5baad51ccf722a4c6052a358163be9dff
Serhii Kulykov <iamkulykov@gmail.com>
2024-11-29 12:47:09 +0200

    refactor: extract message styles into reusable CSS literal (#8242)

---
+++ packages/message-list/src/vaadin-message-styles.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2024 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const messageStyles = css`
+  :host {
+    display: flex;
+    flex-direction: row;
+    outline: none;
+  }
+
+  :host([hidden]) {
+    display: none !important;
+  }
+
+  [part='content'] {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  [part='header'] {
+    align-items: baseline;
+    display: flex;
+    flex-flow: row wrap;
+  }
+
+  [part='name'] {
+    font-weight: 500;
+  }
+
+  [part='message'] {
+    white-space: pre-wrap;
+  }
+
+  ::slotted([slot='avatar']) {
+    --vaadin-avatar-outline-width: 0;
+    flex-shrink: 0;
+  }
+`;
