02ed201d47a9726166ceb1c5b5997d940c647c9e
Sergey Vinogradov <mr.vursen@gmail.com>
2024-12-09 16:37:30 +0400

    explain component teleportation

--- packages/component-base/src/polylit-mixin.js
+++ packages/component-base/src/polylit-mixin.js
@@ -204,6 +204,11 @@ const PolylitMixinImplementation = (superclass) => {
     connectedCallback() {
       super.connectedCallback();
 
+      // Components like `vaadin-overlay` teleport themselves to the body element when opened.
+      // If their opened state is set as an attribute, the teleportation happens immediately
+      // after they are connected to the DOM. This means they won't be found by querySelector
+      // in the parent component's `firstUpdated()`. To ensure their reference is still registered
+      // in the parent host component, we propagate the reference here.
       const parentHost = this.getRootNode().host;
       if (parentHost && parentHost.__hasPolylitMixin && this.id) {
         parentHost.$ ||= {};
@@ -219,6 +224,9 @@ const PolylitMixinImplementation = (superclass) => {
         this.$ = {};
       }
 
+      // Register all elements with an id in the $ map, including those
+      // that might have been already teleported to a different element
+      // (e.g. opened overlays are teleported to the body element).
       [...Object.values(this.$), this.renderRoot].forEach((node) => {
         node.querySelectorAll('[id]').forEach((node) => {
           this.$[node.id] = node;
