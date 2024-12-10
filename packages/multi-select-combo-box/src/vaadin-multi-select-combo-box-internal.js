9c8e9f25031fbbb96008b4a99f2e7af484387894
Serhii Kulykov <iamkulykov@gmail.com>
2024-12-10 14:08:26 +0200

    fix: prevent unselecting already committed value on outside click (#8301) (#8310)

--- packages/multi-select-combo-box/src/vaadin-multi-select-combo-box-internal.js
+++ packages/multi-select-combo-box/src/vaadin-multi-select-combo-box-internal.js
@@ -364,6 +364,19 @@ class MultiSelectComboBoxInternal extends ComboBoxDataProviderMixin(ComboBoxMixi
   /**
    * Override method inherited from the combo-box
    * to not commit an already selected item again
+   * after closing overlay on outside click.
+   * @protected
+   * @override
+   */
+  _onClosed() {
+    this._ignoreCommitValue = true;
+
+    super._onClosed();
+  }
+
+  /**
+   * Override method inherited from the combo-box
+   * to not commit an already selected item again
    * on blur, which would result in un-selecting.
    * @protected
    * @override
@@ -373,7 +386,7 @@ class MultiSelectComboBoxInternal extends ComboBoxDataProviderMixin(ComboBoxMixi
       this._ignoreCommitValue = false;
 
       // Reset internal combo-box state
-      this.selectedItem = null;
+      this.clear();
       this._inputElementValue = '';
       return;
     }
--- packages/multi-select-combo-box/test/selecting-items.test.js
+++ packages/multi-select-combo-box/test/selecting-items.test.js
@@ -1,6 +1,6 @@
 import { expect } from '@vaadin/chai-plugins';
 import { fixtureSync, keyboardEventFor, nextRender } from '@vaadin/testing-helpers';
-import { sendKeys } from '@web/test-runner-commands';
+import { resetMouse, sendKeys, sendMouse } from '@web/test-runner-commands';
 import sinon from 'sinon';
 import './not-animated-styles.js';
 import '../vaadin-multi-select-combo-box.js';
@@ -126,6 +126,36 @@ describe('selecting items', () => {
       await sendKeys({ down: 'Enter' });
       await sendKeys({ down: 'Tab' });
       expect(comboBox.selectedItems).to.deep.equal(['apple']);
+    });
+
+    it('should not unselect previously committed item on outside click', async () => {
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'Enter' });
+      await sendMouse({ type: 'click', position: [200, 200] });
+      await resetMouse();
+      expect(comboBox.selectedItems).to.deep.equal(['apple']);
+    });
+
+    it('should not unselect previously committed item on blur after outside click with allow custom value', async () => {
+      comboBox.allowCustomValue = true;
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'Enter' });
+      await sendMouse({ type: 'click', position: [200, 200] });
+      await resetMouse();
+      await sendKeys({ down: 'Tab' });
+      expect(comboBox.selectedItems).to.deep.equal(['apple']);
+    });
+
+    it('should not set previously committed item to input on blur with allow custom value', async () => {
+      comboBox.allowCustomValue = true;
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'ArrowDown' });
+      await sendKeys({ down: 'Enter' });
+      await sendKeys({ down: 'Tab' });
+      expect(comboBox.filter).to.equal('');
+      expect(inputElement.value).to.equal('');
     });
 
     it('should un-select item when using clear() method', () => {
