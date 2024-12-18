8c03947aa3f5bdd6d2b7b6de16d6a14dd7704964
Sergey Vinogradov <mr.vursen@gmail.com>
2024-12-18 12:52:13 +0400

    fix focusout issue

--- web-test-runner-firefox.config.js
+++ web-test-runner-firefox.config.js
@@ -4,7 +4,14 @@ const { createUnitTestsConfig } = require('./wtr-utils.js');
 const devServerConfig = require('./web-dev-server.config.js');
 
 const unitTestsConfig = createUnitTestsConfig({
-  browsers: [playwrightLauncher({ product: 'firefox' })],
+  browsers: [
+    playwrightLauncher({
+      product: 'firefox',
+      launchOptions: {
+        firefoxUserPrefs: { 'dom.disable_tab_focus_to_root_element': false },
+      },
+    }),
+  ],
 });
 
 module.exports = {
