e3999c4b96f64449e85cef46fed0c4cd9ef22c94
web-padawan <iamkulykov@gmail.com>
2024-12-30 13:24:49 +0200

    test: update cookie-consent screenshots to use 610px height

--- packages/cookie-consent/test/visual/material/cookie-consent.test.js
+++ packages/cookie-consent/test/visual/material/cookie-consent.test.js
@@ -1,9 +1,14 @@
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { executeServerCommand } from '@web/test-runner-commands';
 import { visualDiff } from '@web/test-runner-visual-regression';
 import '../../../theme/material/vaadin-cookie-consent.js';
 
 describe('cookie-consent', () => {
   let element;
+
+  before(async () => {
+    await executeServerCommand('set-window-height', { height: 610 });
+  });
 
   beforeEach(() => {
     element = fixtureSync('<vaadin-cookie-consent></vaadin-cookie-consent>');
