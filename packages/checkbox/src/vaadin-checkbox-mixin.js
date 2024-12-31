2b22d9c035b2b29163b58540ca1bb62da585a0e0
Serhii Kulykov <iamkulykov@gmail.com>
2024-12-31 14:11:23 +0200

    refactor: do not override tabindex property to set default value (#8418)

--- packages/checkbox/src/vaadin-checkbox-mixin.js
+++ packages/checkbox/src/vaadin-checkbox-mixin.js
@@ -60,18 +60,6 @@ export const CheckboxMixin = (superclass) =>
           value: false,
           reflectToAttribute: true,
         },
-
-        /**
-         * Indicates whether the element can be focused and where it participates in sequential keyboard navigation.
-         *
-         * @override
-         * @protected
-         */
-        tabindex: {
-          type: Number,
-          value: 0,
-          reflectToAttribute: true,
-        },
       };
     }
 
@@ -99,6 +87,10 @@ export const CheckboxMixin = (superclass) =>
       // Set the string "on" as the default value for the checkbox following the HTML specification:
       // https://html.spec.whatwg.org/multipage/input.html#dom-input-value-default-on
       this.value = 'on';
+
+      // Set tabindex to 0 by default to not lose focus on click in Safari
+      // See https://github.com/vaadin/web-components/pull/6780
+      this.tabindex = 0;
     }
 
     /** @protected */
