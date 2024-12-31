a62a79adac44b9b5d4f6a6344c98a7c5a59ba6db
web-padawan <iamkulykov@gmail.com>
2024-12-31 12:52:39 +0200

    test: update Chrome version in Material visual tests to 130

--- web-test-runner-material.config.js
+++ web-test-runner-material.config.js
@@ -1,4 +1,4 @@
 /* eslint-env node */
 const { createVisualTestsConfig } = require('./wtr-utils.js');
 
-module.exports = createVisualTestsConfig('material', '88');
+module.exports = createVisualTestsConfig('material', '130');
