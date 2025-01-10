3674edd2b508182ce1ee165b946cb0fecdfadb9a
Tomi Virkki <virkki@vaadin.com>
2025-01-10 12:31:22 +0200

    refactor: extract map styles into reusable css literal

---
+++ packages/map/src/vaadin-map-styles.d.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2025 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
+ */
+import type { CSSResult } from 'lit';
+
+export const mapStyles: CSSResult;
