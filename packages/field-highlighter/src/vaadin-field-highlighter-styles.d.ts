abc75154783293bf367f884bfec5d39900bad6b0
Tomi Virkki <tomivirkki@users.noreply.github.com>
2025-01-10 15:34:41 +0200

    refactor: extract field highlighter styles into reusable css literals (#8490)

---
+++ packages/field-highlighter/src/vaadin-field-highlighter-styles.d.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import type { CSSResult } from 'lit';
+
+export const fieldOutlineStyles: CSSResult;
+
+export const userTagStyles: CSSResult;
+
+export const userTagsOverlayStyles: CSSResult;
