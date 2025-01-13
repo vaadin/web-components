912b959b7eaef254560f028ec70d6a009bae8fef
Tomi Virkki <virkki@vaadin.com>
2025-01-13 12:48:31 +0200

    fix: do not stop ignored native dnd events in grid

--- packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
+++ packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
@@ -222,6 +222,9 @@ export const DragAndDropMixin = (superClass) =>
 
     /** @private */
     _onDragEnd(e) {
+      if (!this.__draggedItems.length) {
+        return;
+      }
       this.toggleAttribute('dragging-rows', false);
       e.stopPropagation();
       const event = new CustomEvent('grid-dragend');
@@ -234,6 +237,9 @@ export const DragAndDropMixin = (superClass) =>
 
     /** @private */
     _onDragLeave(e) {
+      if (!this.dropMode) {
+        return;
+      }
       e.stopPropagation();
       this._clearDragStyles();
     }
