295b3046dfbd9ca924debc8de8b520d459b90c9d
Sergey Vinogradov <mr.vursen@gmail.com>
2025-01-16 14:11:47 +0400

    add some tests

---
+++ packages/menu-bar/test/focusable-disabled-buttons-lit.test.js
@@ -0,0 +1,3 @@
+import './not-animated-styles.js';
+import '../theme/lumo/vaadin-lit-menu-bar.js';
+import './menu-bar.common.js';
---
+++ packages/menu-bar/test/focusable-disabled-buttons-polymer.test.js
@@ -0,0 +1,3 @@
+import './not-animated-styles.js';
+import '../vaadin-menu-bar.js';
+import './focusable-disabled-buttons.common.js';
