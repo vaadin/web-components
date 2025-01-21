dc87442bb489e6d447c42fdcb95b4e2921abf86e
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-01-21 15:04:37 +0200

    refactor: extract vertical layout styles into CSS literal (#8546)

---
+++ packages/vertical-layout/src/vaadin-vertical-layout-styles.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+export const verticalLayoutStyles = css`
+  :host {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    box-sizing: border-box;
+  }
+
+  :host([hidden]) {
+    display: none !important;
+  }
+
+  /* Theme variations */
+  :host([theme~='margin']) {
+    margin: 1em;
+  }
+
+  :host([theme~='padding']) {
+    padding: 1em;
+  }
+
+  :host([theme~='spacing']) {
+    gap: 1em;
+  }
+`;
