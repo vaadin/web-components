3e57357dff48909f704cb0dfc450c76bb331fc0c
rolfsmeds <rolf@vaadin.com>
2025-01-24 16:29:27 +0200

    Added comments (and removed some old cruft)

--- packages/form-layout/src/vaadin-form-layout-styles.js
+++ packages/form-layout/src/vaadin-form-layout-styles.js
@@ -15,6 +15,7 @@ export const formLayoutStyles = css`
     container-name: form-grid;
     --_label-col-width: var(--vaadin-form-item-label-width, 8em);
     --_label-col-spacing: var(--vaadin-form-item-label-spacing, 1em);
+    --_grid-cols: auto-fill; /* Number of cols, defined by breakpoints. Default value is probably pointless. */
 
     /* CSS API for host */
     /* Let's not define defaults here – that way they can be set globally
@@ -22,10 +23,6 @@ export const formLayoutStyles = css`
     --vaadin-form-item-label-spacing: 1em;
     --vaadin-form-item-row-spacing: 1em;
     --vaadin-form-layout-column-spacing: 2em; */
-
-    /* Private shit */
-    --_grid-cols: auto-fill; /* col count defaults to "whatever fits" if not provided - this does not actually work with colspan capping */
-    --_default-field-column: auto; /* auto: auto-rows; 1: single-col default */
   }
 
   @keyframes vaadin-form-layout-appear {
@@ -40,6 +37,9 @@ export const formLayoutStyles = css`
 
   #layout {
     display: grid;
+    /* --_vaadin-form-layout-column-max-width below
+      is set to 1fr by the WC expandColumns property
+      to make the cols expand to fill available space .*/
     grid-template-columns: repeat(
       var(--_grid-cols),
       minmax(
@@ -56,32 +56,40 @@ export const formLayoutStyles = css`
   }
 
   #layout ::slotted(*) {
+    /* Unless auto-rows is on, each field starts on a new line */
     grid-column-start: 1;
+    /* Span is set via custom prop set by the WC; capped by current breakpoint's column count */
     grid-column-end: span min(var(--vaadin-form-layout-colspan), var(--_grid-cols));
   }
 
   :host([auto-rows]) #layout ::slotted(*) {
+    /* In auto-rows mode, fields are laid out in multiple cols,
+      but the WC overrides this to 1 using --_vaadin-form-layout-start-col
+      following a <br>, vaadin-form-row or vaadin-form-section */
     grid-column-start: var(--_vaadin-form-layout-start-col, auto);
   }
 
   #layout ::slotted(br) {
-    grid-column: 1 / -1;
+    display: none;
   }
 
   :host([expand-columns='always']) #layout {
     --_vaadin-form-layout-column-max-width: 1fr;
   }
 
+  /* If expand-columns="mobile", column expansion is enabled on small viewports */
   @media (max-width: 420px) {
     :host([expand-columns='mobile']) #layout {
       --_vaadin-form-layout-column-max-width: 1fr;
     }
   }
 
+  /* Items are stretched to fit column when fit-fields is enabled */
   :host([fit-fields='always']) #layout {
     --_vaadin-form-item-fit-content: 1;
   }
 
+  /* If fit-fields="mobile", field fitting is enabled on small viewports */
   @media (max-width: 420px) {
     :host([fit-fields='mobile']) #layout {
       --_vaadin-form-item-fit-content: 1;
@@ -94,6 +102,9 @@ export const formLayoutStyles = css`
 export const formItemStyles = css`
   :host {
     display: inline-flex;
+    /* This is a bit convoluted, but the end result is that
+       LABELS ABOVE => align-items: stretch; justify-content: normal;
+       LABELS ASIDE => align-items: baseline; justify-content: baseline; */
     flex-flow: var(--_vaadin-form-layout-label-position, column) nowrap;
     place-items: var(--_vaadin-form-layout-label-position, stretch) baseline;
     justify-content: normal;
@@ -104,7 +115,11 @@ export const formItemStyles = css`
   }
 
   #label {
+    /* This inverts the label-positioning property... */
     --_not-label-pos: var(--_vaadin-form-layout-label-position, ' ') initial;
+    /* ...so that it can be used to set flex-basis to
+      --_label-col-width for LABELS ASIDE
+      auto for LABELS ABOVE */
     flex: var(--_not-label-pos, var(--_label-col-width)) var(--_vaadin-form-layout-label-position, auto);
     flex-shrink: 0;
     flex-grow: 0;
@@ -122,10 +137,13 @@ export const formItemStyles = css`
 
   #content ::slotted(*) {
     box-sizing: border-box;
+    /* Sets width to 100% if fit-content is on */
     width: calc(100% * var(--_vaadin-form-item-fit-content));
+    /* Sets min-width to 0 if fit-content is on */
     min-width: calc(1 - var(--_vaadin-form-item-fit-content));
   }
 
+  /* Legacy full-width classname support */
   #content ::slotted(.full-width) {
     --_vaadin-form-item-fit-content: 1;
   }
@@ -137,6 +155,7 @@ export const formSectionStyles = css`
   }
 
   [part~='header'] {
+    /* Stretch header element across all available cols */
     grid-column: 1 / -1;
   }
 
