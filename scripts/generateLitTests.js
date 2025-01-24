6d1a63dc96bd1616dcbc92f67c1a20234fe30283
Sergey Vinogradov <mr.vursen@gmail.com>
2025-01-24 14:19:04 +0400

    do not generate files for not migrated packages

--- scripts/generateLitTests.js
+++ scripts/generateLitTests.js
@@ -2,15 +2,22 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-glob.sync('packages/*/test/*.test.{js,ts}').forEach((testPath) => {
-  if (/(-polymer|-lit)\.test/u.test(testPath)) {
-    return;
-  }
-
-  const litTestPath = testPath.replace('.test.', '-generated-lit.test.');
-  if (fs.existsSync(litTestPath)) {
-    return;
-  }
-
-  fs.symlinkSync(path.relative(path.dirname(testPath), testPath), litTestPath, 'file');
+const litPackages = fs.readdirSync('packages').filter((package) => {
+  return fs.existsSync(`packages/${package}/vaadin-lit-${package}.js`);
 });
+
+litPackages
+  .flatMap((package) => {
+    return glob.sync(`packages/${package}/test/*.test.{js,ts}`);
+  })
+  .filter((testPath) => {
+    return !/(-polymer|-lit)\.test/u.test(testPath);
+  })
+  .forEach((testPath) => {
+    const litTestPath = testPath.replace('.test.', '-generated-lit.test.');
+    if (fs.existsSync(litTestPath)) {
+      return;
+    }
+
+    fs.symlinkSync(path.relative(path.dirname(testPath), testPath), litTestPath, 'file');
+  });
