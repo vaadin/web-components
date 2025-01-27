3baf97abebf75771fed4176593d63565564229f7
Sergey Vinogradov <mr.vursen@gmail.com>
2025-01-27 19:41:17 +0400

    decorate new sendKeys with JSDoc

--- test/test-runner-commands/index.js
+++ test/test-runner-commands/index.js
@@ -2,8 +2,26 @@ import { executeServerCommand } from '@web/test-runner-commands';
 
 export * from '@web/test-runner-commands';
 
-export async function sendKeys(options) {
-  const { press } = options;
+/**
+ * Extends the `sendKeys` command to support sending multiple keys
+ * simultaneously when provided in the format "Shift+Tab". This format
+ * is natively supported by Playwright but not by Puppeteer. This wrapper
+ * enables the same syntax to be used in Puppeteer.
+ *
+ * In WebDriver, this functionality is still unavailable because
+ * web-test-runner does not provide an API for holding keys down.
+ *
+ * For more documentation on the original command, please see
+ * https://modern-web.dev/docs/test-runner/commands/#send-keys
+ *
+ * @typedef {{ type: string }} TypePayload
+ * @typedef {{ press: string }} PressPayload
+ * @typedef {{ down: string }} DownPayload
+ * @typedef {{ up: string }} UpPayload
+ * @param payload {TypePayload | PressPayload | DownPayload | UpPayload}
+ **/
+export async function sendKeys(payload) {
+  const { press } = payload;
   if (press && press.includes('+')) {
     const keys = press.split('+').map((key) => key.trim());
 
@@ -18,5 +36,5 @@ export async function sendKeys(options) {
     return;
   }
 
-  await executeServerCommand('send-keys', options);
+  await executeServerCommand('send-keys', payload);
 }
