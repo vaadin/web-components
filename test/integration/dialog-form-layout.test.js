72d3d93c483d120063dcdc21b7f8a490991e7c0f
Sergey Vinogradov <mr.vursen@gmail.com>
2025-02-25 13:54:07 +0400

    add integration test with dialog

--- test/integration/dialog-form-layout.test.js
+++ test/integration/dialog-form-layout.test.js
@@ -1,6 +1,6 @@
 import { expect } from '@vaadin/chai-plugins';
 import { setViewport } from '@vaadin/test-runner-commands';
-import { fixtureSync, nextFrame, nextRender } from '@vaadin/testing-helpers';
+import { fixtureSync, nextFrame, nextRender, nextResize } from '@vaadin/testing-helpers';
 import './not-animated-styles.js';
 import '@vaadin/form-layout';
 import '@vaadin/form-layout/vaadin-form-item.js';
@@ -8,8 +8,26 @@ import '@vaadin/dialog';
 
 function assertFormLayoutGrid(layout, { columns, rows }) {
   const children = [...layout.children];
-  expect(new Set(children.map((child) => child.offsetLeft)).size).to.equal(columns);
-  expect(new Set(children.map((child) => child.offsetTop)).size).to.equal(rows);
+  expect(new Set(children.map((child) => child.offsetLeft)).size).to.equal(columns, `expected ${columns} columns`);
+  expect(new Set(children.map((child) => child.offsetTop)).size).to.equal(rows, `expected ${rows} rows`);
+}
+
+function assertFormLayoutLabelPosition(layout, { position }) {
+  const columnWidth = parseFloat(layout.columnWidth);
+  const labelWidth = parseFloat(getComputedStyle(layout).getPropertyValue('--vaadin-form-layout-label-width'));
+  const labelSpacing = parseFloat(getComputedStyle(layout).getPropertyValue('--vaadin-form-layout-label-spacing'));
+
+  [...layout.children].forEach((child) => {
+    if (child.localName !== 'vaadin-form-item') {
+      return;
+    }
+
+    if (position === 'aside') {
+      expect(child.offsetWidth).to.equal(columnWidth + labelWidth + labelSpacing, 'expected labels to be aside');
+    } else {
+      expect(child.offsetWidth).to.equal(columnWidth, 'expected labels to be above');
+    }
+  });
 }
 
 describe('form-layout in dialog', () => {
@@ -60,59 +78,157 @@ describe('form-layout in dialog', () => {
   });
 
   describe('auto-responsive', () => {
-    beforeEach(async () => {
-      dialog = fixtureSync(`<vaadin-dialog></vaadin-dialog>`);
-      dialog.renderer = (root) => {
-        root.innerHTML = `
-          <vaadin-form-layout
-            auto-responsive
-            auto-rows
-            max-columns="3"
-            column-width="100px"
-            style="--vaadin-form-layout-column-spacing: 0px;"
-          >
-            <input placeholder="First name">
-            <input placeholder="Last Name">
-            <input placeholder="Email">
-            <input placeholder="Phone">
-          </vaadin-form-layout>
-        `;
-      };
-      dialog.opened = true;
-      await nextRender();
-      layout = dialog.$.overlay.querySelector('vaadin-form-layout');
+    // Dialog adds a total gap of 80px between the layout and the viewport
+    const DIALOG_PADDING = 80;
+
+    describe('basic', () => {
+      beforeEach(async () => {
+        dialog = fixtureSync(`<vaadin-dialog></vaadin-dialog>`);
+        dialog.renderer = (root) => {
+          root.innerHTML = `
+            <vaadin-form-layout
+              auto-responsive
+              auto-rows
+              max-columns="3"
+              column-width="100px"
+              style="--vaadin-form-layout-column-spacing: 0px;"
+            >
+              <input placeholder="First name">
+              <input placeholder="Last Name">
+              <input placeholder="Email">
+              <input placeholder="Phone">
+            </vaadin-form-layout>
+          `;
+        };
+        dialog.opened = true;
+        await nextRender();
+        layout = dialog.$.overlay.querySelector('vaadin-form-layout');
+      });
+
+      afterEach(async () => {
+        dialog.opened = false;
+        await nextFrame();
+      });
+
+      it('should start with max number of columns', () => {
+        assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+      });
+
+      it('should adjust number of columns based on dialog width', async () => {
+        await setViewport({ width: 300 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+
+        await setViewport({ width: 200 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+
+        await setViewport({ width: 100 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+
+        await setViewport({ width: 50 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+
+        await setViewport({ width: 200 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+
+        await setViewport({ width: 300 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+      });
     });
 
-    afterEach(async () => {
-      dialog.opened = false;
-      await nextFrame();
-    });
+    describe('labels aside', () => {
+      beforeEach(async () => {
+        dialog = fixtureSync(`<vaadin-dialog></vaadin-dialog>`);
+        dialog.renderer = (root) => {
+          root.innerHTML = `
+            <vaadin-form-layout
+              auto-responsive
+              auto-rows
+              max-columns="2"
+              column-width="100px"
+              labels-aside
+              style="
+                --vaadin-form-layout-column-spacing: 0px;
+                --vaadin-form-layout-label-width: 100px;
+                --vaadin-form-layout-label-spacing: 50px;
+              "
+            >
+              <vaadin-form-item>
+                <label slot="label">First name</label>
+                <input />
+              </vaadin-form-item>
+              <vaadin-form-item>
+                <label slot="label">Last Name</label>
+                <input />
+              </vaadin-form-item>
+              <vaadin-form-item>
+                <label slot="label">Email</label>
+                <input />
+              </vaadin-form-item>
+              <vaadin-form-item>
+                <label slot="label">Phone</label>
+                <input />
+              </vaadin-form-item>
+            </vaadin-form-layout>
+          `;
+        };
+        dialog.opened = true;
+        await nextRender();
+        layout = dialog.$.overlay.querySelector('vaadin-form-layout');
+      });
 
-    it('should start with max number of form layout columns', () => {
-      assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
-    });
+      afterEach(async () => {
+        dialog.opened = false;
+        await nextFrame();
+      });
 
-    it('should adjust number of form layout columns based on dialog width', async () => {
-      // Dialog adds a total gap of 80px between the layout and the viewport
-      const dialogGap = 80;
+      it('should start with max number of columns and labels positioned aside', () => {
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
+      });
 
-      await setViewport({ width: 300 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+      it('should adjust number of columns and label position based on dialog width', async () => {
+        await setViewport({ width: 500 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
 
-      await setViewport({ width: 200 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        await setViewport({ width: 250 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
 
-      await setViewport({ width: 100 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+        await setViewport({ width: 200 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        assertFormLayoutLabelPosition(layout, { position: 'above' });
 
-      await setViewport({ width: 50 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+        await setViewport({ width: 100 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+        assertFormLayoutLabelPosition(layout, { position: 'above' });
 
-      await setViewport({ width: 200 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        await setViewport({ width: 200 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        assertFormLayoutLabelPosition(layout, { position: 'above' });
 
-      await setViewport({ width: 300 + dialogGap, height: 768 });
-      assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+        await setViewport({ width: 250 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 1, rows: 4 });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
+
+        await setViewport({ width: 500 + DIALOG_PADDING, height: 768 });
+        await nextResize(layout);
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+        assertFormLayoutLabelPosition(layout, { position: 'aside' });
+      });
     });
   });
 });
