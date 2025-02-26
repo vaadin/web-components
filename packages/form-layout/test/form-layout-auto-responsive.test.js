1b4778e3b13a65e9cd5ac4a5781a2eb067d1cb94
Diego Cardoso <diego@vaadin.com>
2025-02-26 18:29:06 +0200

    test: add unit tests

--- packages/form-layout/test/form-layout-auto-responsive.test.js
+++ packages/form-layout/test/form-layout-auto-responsive.test.js
@@ -140,4 +140,121 @@ describe('form-layout auto responsive', () => {
       }
     });
   });
+
+  describe('colspan', () => {
+    let container;
+    beforeEach(async () => {
+      container = fixtureSync(`
+        <div>
+          <vaadin-form-layout auto-responsive auto-rows>
+            <input placeholder="First name">
+            <input placeholder="Last Name">
+            <input placeholder="Email">
+            <input placeholder="Phone">
+            <input placeholder="Address" colspan="2">
+          </vaadin-form-layout>
+        </div>
+      `);
+      layout = container.firstElementChild;
+      await nextFrame();
+    });
+
+    it('should make element with colspan take the right amount of columns', () => {
+      expect(getComputedStyle(layout.children[4]).gridColumnEnd).to.equal('span 2');
+    });
+
+    it('should cap colspan if the layout get smaller', async () => {
+      container.style.width = '100px';
+      await nextResize(layout);
+      expect(getComputedStyle(layout.children[4]).gridColumnEnd).to.equal('span 1');
+    });
+
+    it('should be able to define colspan with data-colspan attribute', async () => {
+      layout.children[2].setAttribute('data-colspan', '3');
+      await nextFrame();
+      expect(getComputedStyle(layout.children[2]).gridColumnEnd).to.equal('span 3');
+    });
+  });
+
+  describe('max columns', () => {
+    describe('default', () => {
+      beforeEach(async () => {
+        layout = fixtureSync(`
+          <vaadin-form-layout
+            auto-responsive
+            auto-rows
+            column-width="10em"
+            style="--vaadin-form-layout-column-spacing: 0px;"
+          >
+            <input placeholder="First name">
+            <input placeholder="Last Name">
+            <input placeholder="Email">
+            <input placeholder="Phone">
+          </vaadin-form-layout>
+        `);
+        await nextFrame();
+      });
+
+      it('should start with max number of columns', () => {
+        assertFormLayoutGrid(layout, { columns: 4, rows: 1 });
+      });
+
+      it('should account for line-breaks when calculating max columns', async () => {
+        const br = document.createElement('br');
+        layout.insertBefore(br, layout.children[2]);
+        await nextFrame();
+        assertFormLayoutGrid(layout, { columns: 2, rows: 2 });
+      });
+    });
+
+    describe('explicity rows', () => {
+      beforeEach(async () => {
+        layout = fixtureSync(`
+          <vaadin-form-layout
+            auto-responsive
+            max-columns="2"
+            column-width="10em" 
+            style="--vaadin-form-layout-column-spacing: 0px;
+          >
+            <vaadin-form-row>
+              <input placeholder="First name">
+              <input placeholder="Last Name">
+            </vaadin-form-row>
+            <vaadin-form-row>
+              <input placeholder="Email" colspan="2">
+              <input placeholder="Phone">
+            </vaadin-form-row>
+          </vaadin-form-layout>
+
+          <style>
+            input {
+              justify-self: stretch;
+            }
+          </style>
+        `);
+        await nextFrame();
+      });
+
+      it('should respect max columns attribute', () => {
+        assertFormLayoutGrid(layout, { columns: 2, rows: 3 });
+      });
+
+      it('should calculate max columns based on explicit rows', async () => {
+        layout.maxColumns = 10;
+        await nextFrame();
+        assertFormLayoutGrid(layout, { columns: 3, rows: 2 });
+      });
+
+      it('should account for line-breaks when calculating max columns', async () => {
+        layout.maxColumns = 10;
+        const br = document.createElement('br');
+        layout.insertBefore(br, layout.children[2]);
+        const input = document.createElement('input');
+        input.setAttribute('colspan', '4');
+        layout.insertBefore(input, layout.children[2]);
+        await nextFrame();
+        expect(getComputedStyle(input).gridColumnEnd).to.equal('span 4');
+      });
+    });
+  });
 });
--- packages/form-layout/test/helpers.js
+++ packages/form-layout/test/helpers.js
@@ -4,7 +4,9 @@ import { expect } from '@vaadin/chai-plugins';
  * Asserts that the given form layout has the specified number of columns and rows.
  */
 export function assertFormLayoutGrid(layout, { columns, rows }) {
-  const children = [...layout.children];
+  const children = [...layout.children].flatMap((child) =>
+    child.localName === 'vaadin-form-row' ? [...child.children] : child,
+  );
   expect(new Set(children.map((child) => child.offsetLeft)).size).to.equal(columns, `expected ${columns} columns`);
   expect(new Set(children.map((child) => child.offsetTop)).size).to.equal(rows, `expected ${rows} rows`);
 }
