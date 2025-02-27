530d42466d31a3897a9231c15e61772614a8f72b
Diego Cardoso <diego@vaadin.com>
2025-02-27 13:44:25 +0200

    update snapshot reference

--- packages/form-layout/test/dom/__snapshots__/form-layout.test.snap.js
+++ packages/form-layout/test/dom/__snapshots__/form-layout.test.snap.js
@@ -4,7 +4,7 @@ export const snapshots = {};
 snapshots["vaadin-form-layout auto-responsive basic host default"] = 
 `<vaadin-form-layout
   auto-responsive=""
-  style="--_column-width: 13em; --_max-columns: 4;"
+  style="--_column-width: 13em; --_max-columns: 1;"
 >
   <input placeholder="First name">
   <input placeholder="Last name">
@@ -30,7 +30,7 @@ snapshots["vaadin-form-layout auto-responsive basic host maxColumns"] =
 snapshots["vaadin-form-layout auto-responsive basic host columnWidth"] = 
 `<vaadin-form-layout
   auto-responsive=""
-  style="--_column-width: 15em; --_max-columns: 4;"
+  style="--_column-width: 15em; --_max-columns: 1;"
 >
   <input placeholder="First name">
   <input placeholder="Last name">
