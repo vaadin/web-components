4a20ab819245c5d11ea238ba0f2f9790ba5d001c
web-padawan <iamkulykov@gmail.com>
2025-03-11 10:13:22 +0200

    fix: add missing .js extension to form-layout imports

--- packages/form-layout/src/layouts/responsive-steps-layout.js
+++ packages/form-layout/src/layouts/responsive-steps-layout.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { isElementHidden } from '@vaadin/a11y-base/src/focus-utils';
+import { isElementHidden } from '@vaadin/a11y-base/src/focus-utils.js';
 import { AbstractLayout } from './abstract-layout.js';
 
 function isValidCSSLength(value) {
