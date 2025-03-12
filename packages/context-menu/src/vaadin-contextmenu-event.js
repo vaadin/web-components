b83cdef0c81d204d4db0270336fef56a89eeaf56
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-03-12 08:40:18 +0100

    Update packages/context-menu/src/vaadin-contextmenu-event.js
    Co-authored-by: Serhii Kulykov <iamkulykov@gmail.com>

--- packages/context-menu/src/vaadin-contextmenu-event.js
+++ packages/context-menu/src/vaadin-contextmenu-event.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2016 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { isKeyboardActive } from '@vaadin/a11y-base';
+import { isKeyboardActive } from '@vaadin/a11y-base/src/focus-utils.js';
 import { isFirefox, isIOS } from '@vaadin/component-base/src/browser-utils.js';
 import { prevent, register } from '@vaadin/component-base/src/gestures.js';
 
