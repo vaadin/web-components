45c9c11bbd1f02876757a42c1fe912986a73ec4b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-03-25 15:20:04 +0100

    add missing copyright

--- packages/master-detail-layout/src/vaadin-master-detail-layout-transition-styles.js
+++ packages/master-detail-layout/src/vaadin-master-detail-layout-transition-styles.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2025 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import { css } from 'lit';
 
 export const transitionStyles = css`
