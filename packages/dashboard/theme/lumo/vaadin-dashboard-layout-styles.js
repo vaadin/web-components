df144654da3bcc3473fb309103cb74997725a233
rolfsmeds <rolf@vaadin.com>
2025-04-04 19:29:04 +0300

    Added 3 theme variants

--- packages/dashboard/theme/lumo/vaadin-dashboard-layout-styles.js
+++ packages/dashboard/theme/lumo/vaadin-dashboard-layout-styles.js
@@ -1,6 +1,21 @@
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
 export const dashboardLayoutStyles = css`
+  :host([theme~='shaded-background']) {
+    background: var(--lumo-shade-5pct);
+  }
+
+  :host([theme~='elevated-widgets']) {
+    --vaadin-dashboard-widget-shadow: var(--lumo-box-shadow-xs);
+    --vaadin-dashboard-widget-border-color: var(--lumo-contrast-10pct);
+    --vaadin-dashboard-widget-background: linear-gradient(var(--lumo-tint-5pct), var(--lumo-tint-5pct))
+      var(--lumo-base-color);
+  }
+
+  :host([theme~='widget-padding']) {
+    --vaadin-dashboard-widget-padding: var(--lumo-space-m);
+  }
+
   #grid {
     --_vaadin-dashboard-default-gap: var(--lumo-space-m);
     --_vaadin-dashboard-default-padding: var(--lumo-space-m);
