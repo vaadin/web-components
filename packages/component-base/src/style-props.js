a8376933662f79123c8f3fc645428d715e5269c8
Jouni Koivuviita <jouni@vaadin.com>
2025-04-07 12:04:44 +0300

    feat: add `component-base/style-props.js` (#8914)

---
+++ packages/component-base/src/style-props.js
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+// TODO: add themable-mixin as a dependency to package.json
+import { addGlobalThemeStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
+
+addGlobalThemeStyles(
+  'vaadin-base',
+  css`
+    @layer vaadin.base {
+      html {
+        /* Background color */
+        --_vaadin-bg: hsl(0 0 100);
+      }
+    }
+  `,
+);
