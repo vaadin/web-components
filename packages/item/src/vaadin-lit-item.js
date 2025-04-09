16cfc46582713a614cedd2c73ee4f2996df71334
web-padawan <iamkulykov@gmail.com>
2025-04-09 15:47:07 +0300

    Move CSS injection logic to ThemableMixin package

--- packages/item/src/vaadin-lit-item.js
+++ packages/item/src/vaadin-lit-item.js
@@ -4,10 +4,10 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { css, html, LitElement } from 'lit';
-import { CssInjectionMixin } from '@vaadin/component-base/src/css-injection-mixin.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
+import { CssInjectionMixin } from '@vaadin/vaadin-themable-mixin/css-injection-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { ItemMixin } from './vaadin-item-mixin.js';
 
