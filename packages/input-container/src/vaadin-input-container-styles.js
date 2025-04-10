1a9a532c40240e1037d5126049e4c1fb4acc1764
Jouni Koivuviita <jouni@vaadin.com>
2025-04-10 14:04:22 +0300

    refactor!: Base text field styles (#8917)
    * refactor!: text-field base styles
    
    * add "helper-above-field" variant to dev page
    
    * add an example with long texts to the dev page
    
    * add `--vaadin-input-field-require-indicator-color` prop
    
    * remove commented code
    
    * Update common.js
    
    * fix: adjust readonly focus styles

--- packages/input-container/src/vaadin-input-container-styles.js
+++ packages/input-container/src/vaadin-input-container-styles.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import '@vaadin/component-base/src/style-props.js';
 import { css } from 'lit';
 
 export const inputContainerStyles = css`
@@ -10,26 +11,33 @@ export const inputContainerStyles = css`
     display: flex;
     align-items: center;
     flex: 0 1 auto;
+    --_radius: var(--vaadin-input-field-border-radius, var(--_vaadin-radius-m));
     border-radius:
-            /* See https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius */
-      var(--vaadin-input-field-top-start-radius, var(--__border-radius))
-      var(--vaadin-input-field-top-end-radius, var(--__border-radius))
-      var(--vaadin-input-field-bottom-end-radius, var(--__border-radius))
-      var(--vaadin-input-field-bottom-start-radius, var(--__border-radius));
-    --_border-radius: var(--vaadin-input-field-border-radius, 0);
-    --_input-border-width: var(--vaadin-input-field-border-width, 0px);
-    --_input-border-color: var(--vaadin-input-field-border-color, transparent);
-    /* stylelint-disable-next-line length-zero-no-unit */
-    box-shadow: inset 0 0 0 var(--_input-border-width, 0) var(--_input-border-color);
+      /* See https://developer.mozilla.org/en-US/docs/Web/CSS/border-radius */
+      var(--vaadin-input-field-top-start-radius, var(--_radius))
+      var(--vaadin-input-field-top-end-radius, var(--_radius))
+      var(--vaadin-input-field-bottom-end-radius, var(--_radius))
+      var(--vaadin-input-field-bottom-start-radius, var(--_radius));
+    border: var(--vaadin-input-field-border-width, 1px) solid
+      var(--vaadin-input-field-border-color, var(--_vaadin-border-color-strong));
+    box-sizing: border-box;
+    padding: var(--vaadin-input-field-padding, var(--_vaadin-padding-container));
+    gap: var(--vaadin-input-field-gap, var(--_vaadin-gap-container-inline));
+    background: var(--vaadin-input-field-background, var(--_vaadin-background));
+    color: var(--vaadin-input-field-value-color, var(--_vaadin-color-strong));
+    font-size: var(--vaadin-input-field-value-font-size, inherit);
+    line-height: var(--vaadin-input-field-value-line-height, inherit);
+    font-weight: var(--vaadin-input-field-value-font-weight, 400);
   }
 
   :host([dir='rtl']) {
+    --_radius: var(--vaadin-input-field-border-radius, var(--_vaadin-radius-m));
     border-radius:
-            /* Don't use logical props, see https://github.com/vaadin/vaadin-time-picker/issues/145 */
-      var(--vaadin-input-field-top-end-radius, var(--_border-radius))
-      var(--vaadin-input-field-top-start-radius, var(--_border-radius))
-      var(--vaadin-input-field-bottom-start-radius, var(--_border-radius))
-      var(--vaadin-input-field-bottom-end-radius, var(--_border-radius));
+      /* Don't use logical props, see https://github.com/vaadin/vaadin-time-picker/issues/145 */
+      var(--vaadin-input-field-top-end-radius, var(--_radius))
+      var(--vaadin-input-field-top-start-radius, var(--_radius))
+      var(--vaadin-input-field-bottom-start-radius, var(--_radius))
+      var(--vaadin-input-field-bottom-end-radius, var(--_radius));
   }
 
   :host([hidden]) {
@@ -38,13 +46,12 @@ export const inputContainerStyles = css`
 
   /* Reset the native input styles */
   ::slotted(input) {
-    -webkit-appearance: none;
-    -moz-appearance: none;
+    appearance: none;
     flex: auto;
     white-space: nowrap;
     overflow: hidden;
     width: 100%;
-    height: 100%;
+    height: auto;
     outline: none;
     margin: 0;
     padding: 0;
@@ -52,11 +59,8 @@ export const inputContainerStyles = css`
     border-radius: 0;
     min-width: 0;
     font: inherit;
-    line-height: normal;
     color: inherit;
-    background-color: transparent;
-    /* Disable default invalid style in Firefox */
-    box-shadow: none;
+    background: transparent;
   }
 
   ::slotted(*) {
@@ -68,7 +72,32 @@ export const inputContainerStyles = css`
     /* because ::slotted(...)::placeholder does not work in Safari. */
     font: inherit;
     color: inherit;
-    /* Override default opacity in Firefox */
-    opacity: 1;
+  }
+
+  ::slotted(input:placeholder-shown) {
+    color: var(--vaadin-input-field-placeholder-color, var(--_vaadin-color));
+  }
+
+  :host(:focus-within) {
+    outline: var(--vaadin-focus-ring-width) solid var(--vaadin-focus-ring-color);
+    outline-offset: calc(var(--vaadin-input-field-border-width, 1px) * -1);
+  }
+
+  :host([readonly]) {
+    border-style: dashed;
+  }
+
+  :host([readonly]:focus-within) {
+    outline-style: dashed;
+    --vaadin-input-field-border-color: transparent;
+  }
+
+  :host([disabled]) {
+    --vaadin-input-field-value-color: var(--vaadin-input-field-disabled-text-color, var(--_vaadin-color-subtle));
+    --vaadin-input-field-background: var(
+      --vaadin-input-field-disabled-background,
+      var(--_vaadin-background-container-strong)
+    );
+    --vaadin-input-field-border-color: transparent;
   }
 `;
