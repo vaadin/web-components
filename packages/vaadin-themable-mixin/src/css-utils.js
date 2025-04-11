fbbac5a9b239f6ae964105aabd6ca76c464d7128
Sergey Vinogradov <mr.vursen@gmail.com>
2025-04-11 17:32:29 +0400

    improve JSDoc

--- packages/vaadin-themable-mixin/src/css-utils.js
+++ packages/vaadin-themable-mixin/src/css-utils.js
@@ -5,21 +5,29 @@
  */
 
 /**
+ * Injects the given stylesheet into the shadow root of the component
+ * through the adoptedStyleSheets API. The stylesheet is always added
+ * at the beginning of the adoptedStyleSheets array.
+ *
  * @param {HTMLElement} component
  * @param {CSSStyleSheet} styleSheet
  */
 export function injectStyleSheet(component, styleSheet) {
+  // Remove the old stylesheet to avoid duplicates
   const adoptedStyleSheets = component.shadowRoot.adoptedStyleSheets.filter(
     (s) => s !== component.__cssInjectorStyleSheet,
   );
 
+  // Add the new stylesheet at the beginning of the adoptedStyleSheets array
   component.shadowRoot.adoptedStyleSheets = [styleSheet, ...adoptedStyleSheets];
   component.__cssInjectorStyleSheet = styleSheet;
 }
 
 /**
+ * Removes the stylesheet from the component's shadow root that was added
+ * by the `injectStyleSheet` function.
+ *
  * @param {HTMLElement} component
- * @return {CSSStyleSheet}
  */
 export function cleanupStyleSheet(component) {
   const adoptedStyleSheets = component.shadowRoot.adoptedStyleSheets.filter(
@@ -31,8 +39,12 @@ export function cleanupStyleSheet(component) {
 }
 
 /**
+ * Returns the stylesheet injected into the shadow root of the component
+ * by the `injectStyleSheet` function, or undefined if no stylesheet was
+ * injected.
+ *
  * @param {HTMLElement} component
- * @return {CSSStyleSheet}
+ * @return {CSSStyleSheet | undefined}
  */
 export function getInjectedStyleSheet(component) {
   return component.__cssInjectorStyleSheet;
