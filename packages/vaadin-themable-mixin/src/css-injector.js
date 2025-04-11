d60637464951ac4ac7019ecc0ef47628aeb37d59
Sergey Vinogradov <mr.vursen@gmail.com>
2025-04-11 17:32:29 +0400

    improve JSDoc

--- packages/vaadin-themable-mixin/src/css-injector.js
+++ packages/vaadin-themable-mixin/src/css-injector.js
@@ -9,6 +9,34 @@ import StyleObserver from 'style-observer';
 import { collectTagScopedCSSRules } from './css-rules.js';
 import { cleanupStyleSheet, injectStyleSheet } from './css-utils.js';
 
+/**
+ * Implements auto-injection of component-scoped CSS styles from document
+ * style sheets into the Shadow DOM of the corresponding Vaadin components.
+ *
+ * Styles are scoped to a component using the following syntax:
+ *
+ * 1. `@media vaadin-text-field { ... }` - a media query with a tag name
+ * 2. `@import "styles.css" vaadin-text-field` - an import rule with a tag name
+ *
+ * The class observes the custom property `--{tagName}-css-inject`,
+ * which indicates the presence of styles for the given component in
+ * the document style sheets. When the property is set to `1`, the class
+ * recursively searches all document style sheets for any CSS rules that
+ * are scoped to the given component tag name using the syntax described
+ * above. The found rules are then injected into the shadow DOM of all
+ * subscribed components through the adoptedStyleSheets API.
+ *
+ * The class also observes the custom property to remove the styles when
+ * the property is set to `0`.
+ *
+ * If a root element is provided, the class will additionally search for
+ * component-scoped styles in the root element's style sheets. This is
+ * useful for embedded Flow applications that are fully isolated from
+ * the main document and load styles into a component's shadow DOM
+ * rather than the main document.
+ *
+ * WARNING: For internal use only. Do not use this class in custom components.
+ */
 export class CSSInjector {
   /** @type {Document | ShadowRoot} */
   #root;
@@ -31,10 +59,19 @@ export class CSSInjector {
     });
   });
 
-  constructor(root) {
+  constructor(root = document) {
     this.#root = root;
   }
 
+  /**
+   * Adds a component to the list of elements monitored for component-scoped
+   * styles in global style sheets. If the styles have already been detected,
+   * they are injected into the component's shadow DOM immediately. Otherwise,
+   * the class watches the custom property `--{tagName}-css-inject` to trigger
+   * injection when the styles are added to the document or root element.
+   *
+   * @param {HTMLElement} component
+   */
   componentConnected(component) {
     const { is: tagName, cssInjectPropName } = component.constructor;
 
@@ -61,6 +98,13 @@ export class CSSInjector {
     this.#styleObserver.observe(this.rootHost, cssInjectPropName);
   }
 
+  /**
+   * Removes the component from the list of elements monitored for
+   * component-scoped styles and cleans up any previously injected
+   * styles from the component's shadow DOM.
+   *
+   * @param {HTMLElement} component
+   */
   componentDisconnected(component) {
     const { is: tagName } = component.constructor;
 
