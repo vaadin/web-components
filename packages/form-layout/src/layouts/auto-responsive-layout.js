d65c1bcad4a30db7d3745e4704e285e7a891e769
ugur-vaadin <ugur@vaadin.com>
2025-04-11 09:06:08 +0300

    chore: cleanup debugging leftovers

--- packages/form-layout/src/layouts/auto-responsive-layout.js
+++ packages/form-layout/src/layouts/auto-responsive-layout.js
@@ -130,11 +130,6 @@ export class AutoResponsiveLayout extends AbstractLayout {
       host.style.removeProperty('--_column-width');
     }
 
-    console.warn(`maxColumns: ${maxColumns}`);
-    console.warn(`props.minColumns: ${props.minColumns}`);
-    console.warn(`props.maxColumns: ${props.maxColumns}`);
-    console.warn(`--_min-columns: ${Math.max(props.minColumns, 1)}`);
-    console.warn(`--_max-columns: ${Math.min(Math.max(props.minColumns, props.maxColumns, 1), maxColumns)}`);
     host.style.setProperty('--_min-columns', Math.max(props.minColumns, 1));
     host.style.setProperty('--_max-columns', Math.min(Math.max(props.minColumns, props.maxColumns, 1), maxColumns));
 
