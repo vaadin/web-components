7d7c8c118728412547ff267cdfb089eae48553a5
web-padawan <iamkulykov@gmail.com>
2025-04-11 13:07:53 +0300

    Add some basic tests

---
+++ packages/vaadin-themable-mixin/test/css-injection-mixin-lit.test.js
@@ -0,0 +1,348 @@
+import { expect } from '@vaadin/chai-plugins';
+import { fixtureSync, nextRender, oneEvent } from '@vaadin/testing-helpers';
+import { css, html, LitElement } from 'lit';
+import { CssInjectionMixin } from '../css-injection-mixin.js';
+
+class TestFoo extends CssInjectionMixin(LitElement) {
+  static get is() {
+    return 'test-foo';
+  }
+
+  static get styles() {
+    return css`
+      :host {
+        display: inline-block;
+      }
+
+      [part='content'] {
+        transition: background-color 1ms linear;
+      }
+    `;
+  }
+
+  render() {
+    return html`<div part="content">Content</div>`;
+  }
+}
+
+customElements.define(TestFoo.is, TestFoo);
+
+const TEST_FOO_STYLES = `
+  html, :host {
+    --test-foo-css-inject: 1;
+  }
+
+  @media test-foo {
+    [part='content'] {
+      background: green;
+    }
+  }
+`;
+
+function createTestFooStyleSheet() {
+  const path = import.meta.url.split('/').slice(0, -1).join('/');
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = `${path}/css-injection-test-foo.css`;
+  return link;
+}
+
+describe('CSS injection', () => {
+  let element, content;
+
+  async function contentTransition() {
+    // Use custom 1ms background transition on the content part to not
+    // rely on global transitions used by StyleObserver under the hood.
+    await oneEvent(content, 'transitionend');
+  }
+
+  function assertStyleApplies() {
+    expect(getComputedStyle(content).backgroundColor).to.equal('rgb(0, 128, 0)');
+  }
+
+  function assertStyleDoesNotApply() {
+    expect(getComputedStyle(content).backgroundColor).to.equal('rgba(0, 0, 0, 0)');
+  }
+
+  describe('in global scope', () => {
+    describe('styles added after element is connected', () => {
+      beforeEach(async () => {
+        element = fixtureSync('<test-foo></test-foo>');
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+      });
+
+      it('should inject matching styles added to document using style tag', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        document.head.appendChild(style);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        style.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using link element', async () => {
+        const link = createTestFooStyleSheet();
+        document.head.appendChild(link);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        link.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using adoptedStyleSheets', async () => {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(TEST_FOO_STYLES);
+        document.adoptedStyleSheets.push(sheet);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        document.adoptedStyleSheets.pop();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+    });
+
+    describe('styles added before element is connected', () => {
+      beforeEach(() => {
+        element = document.createElement('test-foo');
+      });
+
+      afterEach(() => {
+        element.remove();
+      });
+
+      it('should inject matching styles added to document using style tag', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        document.head.appendChild(style);
+        await nextRender();
+
+        document.body.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+        assertStyleApplies();
+
+        style.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using link element', async () => {
+        const link = createTestFooStyleSheet();
+        document.head.appendChild(link);
+        await nextRender();
+
+        document.body.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+        assertStyleApplies();
+
+        link.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using adoptedStyleSheets', async () => {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(TEST_FOO_STYLES);
+        document.adoptedStyleSheets.push(sheet);
+        await nextRender();
+
+        document.body.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+
+        assertStyleApplies();
+
+        document.adoptedStyleSheets.pop();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+    });
+  });
+
+  describe('in shadow scope', () => {
+    let shadowHost;
+
+    beforeEach(() => {
+      shadowHost = fixtureSync('<div></div>');
+      shadowHost.attachShadow({ mode: 'open' });
+      element = document.createElement('test-foo');
+    });
+
+    describe('styles added after element is connected', () => {
+      beforeEach(async () => {
+        shadowHost.shadowRoot.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+      });
+
+      it('should inject matching styles added to document using style tag when in shadow scope', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        document.head.appendChild(style);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        style.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using link element when in shadow scope', async () => {
+        const link = createTestFooStyleSheet();
+        document.head.appendChild(link);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        link.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to document using adoptedStyleSheets when in shadow scope', async () => {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(TEST_FOO_STYLES);
+        document.adoptedStyleSheets.push(sheet);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        document.adoptedStyleSheets.pop();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to enclosing shadow root using style tag', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        shadowHost.shadowRoot.appendChild(style);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        style.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to enclosing shadow root using link element', async () => {
+        const link = createTestFooStyleSheet();
+        shadowHost.shadowRoot.appendChild(link);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        link.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to enclosing shadow root using adoptedStyleSheets', async () => {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(TEST_FOO_STYLES);
+        shadowHost.shadowRoot.adoptedStyleSheets.push(sheet);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        shadowHost.shadowRoot.adoptedStyleSheets.pop();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should not apply styles added to enclosing shadow root after moving to document', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        shadowHost.shadowRoot.appendChild(style);
+
+        await contentTransition();
+        assertStyleApplies();
+
+        document.body.appendChild(element);
+        assertStyleDoesNotApply();
+
+        shadowHost.shadowRoot.appendChild(element);
+        assertStyleApplies();
+      });
+    });
+
+    describe('styles added before element is connected', () => {
+      it('should inject matching styles added to enclosing shadow root using style tag', async () => {
+        const style = document.createElement('style');
+        style.textContent = TEST_FOO_STYLES;
+        shadowHost.shadowRoot.appendChild(style);
+        await nextRender();
+
+        shadowHost.shadowRoot.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+
+        assertStyleApplies();
+
+        style.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to enclosing shadow root using link element', async () => {
+        const link = createTestFooStyleSheet();
+        shadowHost.shadowRoot.appendChild(link);
+        await nextRender();
+
+        shadowHost.shadowRoot.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+
+        assertStyleApplies();
+
+        link.remove();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+
+      it('should inject matching styles added to enclosing shadow root using adoptedStyleSheets', async () => {
+        const sheet = new CSSStyleSheet();
+        sheet.replaceSync(TEST_FOO_STYLES);
+        shadowHost.shadowRoot.adoptedStyleSheets.push(sheet);
+        await nextRender();
+
+        shadowHost.shadowRoot.appendChild(element);
+        await nextRender();
+        content = element.shadowRoot.querySelector('[part="content"]');
+
+        assertStyleApplies();
+
+        shadowHost.shadowRoot.adoptedStyleSheets.pop();
+
+        await contentTransition();
+        assertStyleDoesNotApply();
+      });
+    });
+  });
+});
