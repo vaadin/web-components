76f7feb7870beb6be873ce51a69cb3e030e2a138
Sergey Vinogradov <mr.vursen@gmail.com>
2025-04-11 16:34:37 +0400

    add missing license header

--- packages/vaadin-themable-mixin/src/css-injector.js
+++ packages/vaadin-themable-mixin/src/css-injector.js
@@ -1,3 +1,9 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
 /* eslint-disable es/no-optional-chaining */
 import StyleObserver from 'style-observer';
 import { collectTagScopedCSSRules } from './css-rules.js';
