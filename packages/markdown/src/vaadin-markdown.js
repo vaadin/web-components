58def3eba9c3a6e86eb4cf1b19dd7605be56c72a
Tomi Virkki <virkki@vaadin.com>
2025-04-25 09:31:50 +0300

    update license

--- packages/markdown/src/vaadin-markdown.js
+++ packages/markdown/src/vaadin-markdown.js
@@ -1,12 +1,7 @@
 /**
  * @license
- * Copyright (c) 2000 - 2025 Vaadin Ltd.
- *
- * This program is available under Vaadin Commercial License and Service Terms.
- *
- *
- * See https://vaadin.com/commercial-license-and-service-terms for the full
- * license.
+ * Copyright (c) 2017 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { css, html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
