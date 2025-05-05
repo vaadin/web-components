abdeddb979e7fb545e648f4728c81a6c39c4daf0
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-05 19:13:59 +0400

    port base Lumo styles

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -4,6 +4,33 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 /** @return {import('@web/test-runner').TestRunnerPlugin} */
+function cssImportPlugin() {
+  return {
+    name: 'css-import',
+    transformImport({ source }) {
+      if (source.endsWith('.css')) {
+        return `${source}?injectCSS`;
+      }
+    },
+    transform(context) {
+      if (context.query.injectCSS !== undefined) {
+        return `
+          const link = document.createElement('link');
+          link.rel = 'stylesheet';
+          link.href = ${JSON.stringify(context.path)};
+          document.head.appendChild(link);
+        `;
+      }
+    },
+    resolveMimeType(context) {
+      if (context.query.injectCSS !== undefined) {
+        return 'text/javascript';
+      }
+    },
+  };
+}
+
+/** @return {import('@web/test-runner').TestRunnerPlugin} */
 function generatedLitTestsPlugin() {
   return {
     name: 'generated-lit-tests',
@@ -75,6 +102,7 @@ export default {
     },
     esbuildPlugin({ ts: true }),
     generatedLitTestsPlugin(),
+    cssImportPlugin(),
   ],
   nodeResolve: {
     // Use Lit in production mode
