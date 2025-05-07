8b43d955200b33ae29cf154183095756a2d37df2
Serhii Kulykov <iamkulykov@gmail.com>
2025-05-07 15:35:04 +0300

    refactor: replace usage of Polymer helpers in charts (#9064)

--- packages/charts/src/vaadin-chart-mixin.js
+++ packages/charts/src/vaadin-chart-mixin.js
@@ -28,12 +28,11 @@ import 'highcharts/es-modules/masters/modules/xrange.src.js';
 import 'highcharts/es-modules/masters/modules/bullet.src.js';
 import 'highcharts/es-modules/masters/modules/gantt.src.js';
 import 'highcharts/es-modules/masters/modules/draggable-points.src.js';
-import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
-import { beforeNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 import Pointer from 'highcharts/es-modules/Core/Pointer.js';
 import Highcharts from 'highcharts/es-modules/masters/highstock.src.js';
 import { get } from '@vaadin/component-base/src/path-utils.js';
 import { ResizeMixin } from '@vaadin/component-base/src/resize-mixin.js';
+import { SlotObserver } from '@vaadin/component-base/src/slot-observer.js';
 import { deepMerge, inflateFunctions } from './helpers.js';
 
 ['exportChart', 'exportChartLocal', 'getSVG'].forEach((methodName) => {
@@ -753,7 +752,7 @@ export const ChartMixin = (superClass) =>
     connectedCallback() {
       super.connectedCallback();
       this.__updateStyles();
-      beforeNextRender(this, () => {
+      queueMicrotask(() => {
         // Detect if the chart had already been initialized. This might happen in
         // environments where the chart is lazily attached (e.g Grid).
         if (this.configuration) {
@@ -803,7 +802,7 @@ export const ChartMixin = (superClass) =>
 
     /** @private */
     __addChildObserver() {
-      this._childObserver = new FlattenedNodesObserver(this.$.slot, (info) => {
+      this._childObserver = new SlotObserver(this.$.slot, (info) => {
         this.__addSeries(info.addedNodes.filter(this.__filterSeriesNodes));
         this.__removeSeries(info.removedNodes.filter(this.__filterSeriesNodes));
         this.__cleanupAfterSeriesRemoved(info.removedNodes.filter(this.__filterSeriesNodes));
@@ -1048,7 +1047,7 @@ export const ChartMixin = (superClass) =>
       inflateFunctions(configCopy);
       this._jsonConfigurationBuffer = this.__makeConfigurationBuffer(this._jsonConfigurationBuffer, configCopy);
 
-      beforeNextRender(this, () => {
+      queueMicrotask(() => {
         if (!this.configuration || !this._jsonConfigurationBuffer) {
           return;
         }
