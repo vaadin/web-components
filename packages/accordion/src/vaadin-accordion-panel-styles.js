96158327660d6ff38d0984f9985d1f2d382cfefb
Jouni Koivuviita <jouni@vaadin.com>
2025-05-08 11:23:17 +0300

    remove open/close transition
    Too many complications around this. Postponing for later, at least. Perhaps best to leave for themes to implement and introduce limitations how the content part can be styled.

--- packages/accordion/src/vaadin-accordion-panel-styles.js
+++ packages/accordion/src/vaadin-accordion-panel-styles.js
@@ -15,49 +15,11 @@ export const accordionPanel = css`
   }
 
   [part='content'] {
-    overflow: hidden;
+    box-sizing: border-box;
   }
 
   :host(:not([opened])) [part='content'] {
-    content-visibility: hidden;
-    contain-intrinsic-height: auto none;
-    opacity: 0;
-  }
-
-  /*
-  Safari doesn't transition content-visibility gracefully,
-  even with 'transition-behavior: allow-discrete'.
-  Let Safari transition display instead.
-  Using 'display: none' breaks the transition in Firefox.
-  */
-  @supports (font: -apple-system-body) {
-    :host(:not([opened])) [part='content'] {
-      content-visibility: initial;
-      display: none;
-    }
-  }
-
-  @media (prefers-reduced-motion: no-preference) {
-    :host {
-      display: grid;
-      transition-property: grid-template-rows;
-      transition-duration: 150ms;
-      grid-template-rows: min-content 0fr;
-      @starting-style {
-        /* Needed for Safari, which otherwise transitions the initial rendered state */
-        grid-template-rows: min-content 1fr;
-      }
-    }
-
-    :host([opened]) {
-      grid-template-rows: min-content 1fr;
-    }
-
-    [part='content'] {
-      transition-behavior: allow-discrete;
-      transition-duration: inherit;
-      transition-property: display, content-visibility, opacity;
-    }
+    display: none !important;
   }
 
   :host([focus-ring]) {
