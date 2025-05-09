d6a4350597c22cf5f3a6f2f4edeea5c867ac1886
Jouni Koivuviita <jouni@vaadin.com>
2025-05-09 11:34:17 +0300

    fix(button): prevent double-tap-to-zoom on iOS

--- packages/button/src/vaadin-button-base.js
+++ packages/button/src/vaadin-button-base.js
@@ -38,6 +38,7 @@ export const buttonStyles = css`
         var(--vaadin-button-border-width, 1px) solid var(--vaadin-button-border-color, var(--_vaadin-border-color))
       );
       border-radius: var(--vaadin-button-border-radius, var(--_vaadin-radius-m));
+      touch-action: manipulation;
     }
 
     :host([hidden]) {
