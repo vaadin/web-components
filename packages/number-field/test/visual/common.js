89a524146b5e9ede0e1bacd82228e95168d1b5da
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-13 10:22:34 +0400

    test: disable animation on label, helper, required indicator (#9121)

--- packages/number-field/test/visual/common.js
+++ packages/number-field/test/visual/common.js
@@ -8,10 +8,20 @@ registerStyles(
       caret-color: transparent;
     }
 
-    /* Show error message immediately */
-    [part='error-message'] {
-      animation: none !important;
-      transition: none !important;
+    /* Disable animation */
+    [part='label'],
+    [part$='button'],
+    [part='helper-text'],
+    [part='input-field'],
+    [part='error-message'],
+    [part='required-indicator'],
+    ::slotted(:is(input, textarea):placeholder-shown) {
+      &,
+      &::before,
+      &::after {
+        animation: none !important;
+        transition: none !important;
+      }
     }
   `,
 );
