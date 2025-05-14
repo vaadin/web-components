5f2ad458313f82395f8efd1083b7c6e895a526f2
Jouni Koivuviita <jouni@vaadin.com>
2025-05-14 13:44:22 +0300

    refactor!: Details base styles (#9106)

--- packages/details/src/vaadin-lit-details-summary.js
+++ packages/details/src/vaadin-lit-details-summary.js
@@ -3,12 +3,13 @@
  * Copyright (c) 2019 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { css, html, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { ButtonMixin } from '@vaadin/button/src/vaadin-button-mixin.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { detailsSummary } from './vaadin-details-summary-styles.js';
 
 /**
  * LitElement based version of `<vaadin-details-summary>` web component.
@@ -25,23 +26,7 @@ class DetailsSummary extends ButtonMixin(DirMixin(ThemableMixin(PolylitMixin(Lit
   }
 
   static get styles() {
-    return css`
-      :host {
-        display: block;
-        outline: none;
-        white-space: nowrap;
-        -webkit-user-select: none;
-        user-select: none;
-      }
-
-      :host([hidden]) {
-        display: none !important;
-      }
-
-      :host([disabled]) {
-        pointer-events: none;
-      }
-    `;
+    return detailsSummary();
   }
 
   static get properties() {
