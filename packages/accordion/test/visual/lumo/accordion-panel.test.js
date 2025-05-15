adfa63b1dbc5a09d64ff64698c068b799f29e0b5
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-15 10:54:32 +0400

    refactor: port accordion Lumo styles to CSS files

--- packages/accordion/test/visual/lumo/accordion-panel.test.js
+++ packages/accordion/test/visual/lumo/accordion-panel.test.js
@@ -1,6 +1,8 @@
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
 import { visualDiff } from '@web/test-runner-visual-regression';
-import '../../../theme/lumo/vaadin-accordion.js';
+import '@vaadin/vaadin-lumo-styles/props.css';
+import '@vaadin/vaadin-lumo-styles/components/accordion-panel.css';
+import '../../../src/vaadin-lit-accordion.js';
 
 describe('accordion-panel', () => {
   let div, element;
