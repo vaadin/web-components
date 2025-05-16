128bde26ea58d97d7c0d135c47ff15755024eee2
web-padawan <iamkulykov@gmail.com>
2025-05-16 17:30:24 +0300

    experiment: add select base styles and visual tests

---
+++ packages/select/src/vaadin-select-value-button-base-styles.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (c) 2017 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const valueButton = css`
+  :host {
+    min-height: 1lh;
+    outline: none;
+    overflow: hidden;
+    white-space: nowrap;
+    width: 100%;
+  }
+
+  ::slotted(*) {
+    padding: 0;
+  }
+
+  .vaadin-button-container,
+  [part='label'] {
+    display: contents;
+  }
+
+  :host([disabled]) {
+    pointer-events: none;
+  }
+`;
