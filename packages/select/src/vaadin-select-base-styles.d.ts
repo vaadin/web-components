c57aec3c7ad82e62d06b901d80b04e704e637b77
Serhii Kulykov <iamkulykov@gmail.com>
2025-05-19 10:20:08 +0300

    experiment: add select base styles and visual tests (#9189)

--- packages/select/src/vaadin-select-base-styles.d.ts
+++ packages/select/src/vaadin-select-base-styles.d.ts
@@ -3,4 +3,6 @@
  * Copyright (c) 2017 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-export { overlayStyles } from './vaadin-overlay-core-styles.js';
+import type { CSSResult } from 'lit';
+
+export const selectStyles: CSSResult;
