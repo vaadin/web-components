5000efbb9626b64d22ad01be82311a211f166b24
Serhii Kulykov <iamkulykov@gmail.com>
2025-05-19 10:20:08 +0300

    experiment: add select base styles and visual tests (#9189)

--- packages/select/src/vaadin-select-core-styles.js
+++ packages/select/src/vaadin-select-core-styles.js
@@ -3,4 +3,14 @@
  * Copyright (c) 2017 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-export { overlayStyles } from './vaadin-overlay-core-styles.js';
+import { css } from 'lit';
+
+export const selectStyles = css`
+  :host {
+    position: relative;
+  }
+
+  ::slotted([slot='value']) {
+    flex-grow: 1;
+  }
+`;
--- packages/select/src/vaadin-select-item.js
+++ packages/select/src/vaadin-select-item.js
@@ -6,8 +6,11 @@
 import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
+import { itemStyles } from '@vaadin/item/src/vaadin-item-core-styles.js';
 import { ItemMixin } from '@vaadin/item/src/vaadin-item-mixin.js';
-import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { registerStyles, ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+registerStyles('vaadin-select-item', itemStyles, { moduleId: 'vaadin-select-item-styles' });
 
 /**
  * An element used internally by `<vaadin-select>`. Not intended to be used separately.
@@ -26,15 +29,6 @@ class SelectItem extends ItemMixin(ThemableMixin(DirMixin(PolymerElement))) {
 
   static get template() {
     return html`
-      <style>
-        :host {
-          display: inline-block;
-        }
-
-        :host([hidden]) {
-          display: none !important;
-        }
-      </style>
       <span part="checkmark" aria-hidden="true"></span>
       <div part="content">
         <slot></slot>
