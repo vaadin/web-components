303569df3b5d43a0f69708e082986b571fee346e
web-padawan <iamkulykov@gmail.com>
2025-05-20 14:32:34 +0300

    refactor!: update tooltip to be based on LitElement

--- packages/tooltip/src/vaadin-tooltip-overlay.js
+++ packages/tooltip/src/vaadin-tooltip-overlay.js
@@ -3,17 +3,14 @@
  * Copyright (c) 2022 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
+import { html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { DirMixin } from '@vaadin/component-base/src/dir-mixin.js';
+import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
 import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
 import { PopoverOverlayMixin } from '@vaadin/popover/src/vaadin-popover-overlay-mixin.js';
-import { registerStyles, ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { tooltipOverlayStyles } from './vaadin-tooltip-overlay-styles.js';
-
-registerStyles('vaadin-tooltip-overlay', [overlayStyles, tooltipOverlayStyles], {
-  moduleId: 'vaadin-tooltip-overlay-styles',
-});
 
 /**
  * An element used internally by `<vaadin-tooltip>`. Not intended to be used separately.
@@ -25,18 +22,13 @@ registerStyles('vaadin-tooltip-overlay', [overlayStyles, tooltipOverlayStyles], 
  * @mixes PopoverOverlayMixin
  * @private
  */
-class TooltipOverlay extends PopoverOverlayMixin(DirMixin(ThemableMixin(PolymerElement))) {
+class TooltipOverlay extends PopoverOverlayMixin(DirMixin(ThemableMixin(PolylitMixin(LitElement)))) {
   static get is() {
     return 'vaadin-tooltip-overlay';
   }
 
-  static get template() {
-    return html`
-      <div id="backdrop" part="backdrop" hidden></div>
-      <div part="overlay" id="overlay">
-        <div part="content" id="content"><slot></slot></div>
-      </div>
-    `;
+  static get styles() {
+    return [overlayStyles, tooltipOverlayStyles];
   }
 
   /**
@@ -46,6 +38,16 @@ class TooltipOverlay extends PopoverOverlayMixin(DirMixin(ThemableMixin(PolymerE
    */
   get _tagNamePrefix() {
     return 'vaadin-tooltip';
+  }
+
+  /** @protected */
+  render() {
+    return html`
+      <div id="backdrop" part="backdrop" hidden></div>
+      <div part="overlay" id="overlay">
+        <div part="content" id="content"><slot></slot></div>
+      </div>
+    `;
   }
 
   requestContentUpdate() {
