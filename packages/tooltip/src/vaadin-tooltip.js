ae975328919fdd1e8df5b63fd2db257af2f99d5d
web-padawan <iamkulykov@gmail.com>
2025-05-20 14:32:34 +0300

    refactor!: update tooltip to be based on LitElement

--- packages/tooltip/src/vaadin-tooltip.js
+++ packages/tooltip/src/vaadin-tooltip.js
@@ -4,10 +4,11 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import './vaadin-tooltip-overlay.js';
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { ControllerMixin } from '@vaadin/component-base/src/controller-mixin.js';
+import { css, html, LitElement } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
+import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
 import { ThemePropertyMixin } from '@vaadin/vaadin-themable-mixin/vaadin-theme-property-mixin.js';
 import { TooltipMixin } from './vaadin-tooltip-mixin.js';
 
@@ -51,53 +52,47 @@ import { TooltipMixin } from './vaadin-tooltip-mixin.js';
  *
  * @customElement
  * @extends HTMLElement
- * @mixes ControllerMixin
  * @mixes ElementMixin
  * @mixes ThemePropertyMixin
  * @mixes TooltipMixin
  */
-class Tooltip extends TooltipMixin(ThemePropertyMixin(ElementMixin(ControllerMixin(PolymerElement)))) {
+class Tooltip extends TooltipMixin(ThemePropertyMixin(ElementMixin(PolylitMixin(LitElement)))) {
   static get is() {
     return 'vaadin-tooltip';
   }
 
-  static get template() {
+  static get styles() {
+    return css`
+      :host {
+        display: none;
+      }
+    `;
+  }
+
+  /** @protected */
+  render() {
+    const effectivePosition = this.__effectivePosition;
+
     return html`
-      <style>
-        :host {
-          display: none;
-        }
-      </style>
       <vaadin-tooltip-overlay
         id="overlay"
-        owner="[[__overlayOwner]]"
-        renderer="[[_renderer]]"
-        theme$="[[_theme]]"
-        opened="[[__computeOpened(manual, opened, _autoOpened, _isConnected)]]"
-        position-target="[[target]]"
-        position="[[__effectivePosition]]"
-        no-horizontal-overlap$="[[__computeNoHorizontalOverlap(__effectivePosition)]]"
-        no-vertical-overlap$="[[__computeNoVerticalOverlap(__effectivePosition)]]"
-        horizontal-align="[[__computeHorizontalAlign(__effectivePosition)]]"
-        vertical-align="[[__computeVerticalAlign(__effectivePosition)]]"
-        on-mouseenter="__onOverlayMouseEnter"
-        on-mouseleave="__onOverlayMouseLeave"
+        .renderer="${this._renderer}"
+        .owner="${this}"
+        theme="${ifDefined(this._theme)}"
+        .opened="${this._isConnected && (this.manual ? this.opened : this._autoOpened)}"
+        .positionTarget="${this.target}"
+        .position="${effectivePosition}"
+        ?no-horizontal-overlap="${this.__computeNoHorizontalOverlap(effectivePosition)}"
+        ?no-vertical-overlap="${this.__computeNoVerticalOverlap(effectivePosition)}"
+        .horizontalAlign="${this.__computeHorizontalAlign(effectivePosition)}"
+        .verticalAlign="${this.__computeVerticalAlign(effectivePosition)}"
+        @mouseenter="${this.__onOverlayMouseEnter}"
+        @mouseleave="${this.__onOverlayMouseLeave}"
         modeless
       ></vaadin-tooltip-overlay>
 
       <slot name="sr-label"></slot>
     `;
-  }
-
-  static get properties() {
-    return {
-      /** @private */
-      __overlayOwner: {
-        value() {
-          return this;
-        },
-      },
-    };
   }
 }
 
