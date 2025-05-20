1e126cf46dc9fa1fa40cebd5e5222d5553f3e696
web-padawan <iamkulykov@gmail.com>
2025-05-20 13:41:26 +0300

    refactor!: update tabsheet to be based on LitElement

--- packages/tabsheet/theme/lumo/vaadin-lit-tabsheet.js
+++
@@ -1,4 +0,0 @@
-import '@vaadin/tabs/theme/lumo/vaadin-lit-tabs.js';
-import '@vaadin/scroller/theme/lumo/vaadin-lit-scroller.js';
-import './vaadin-tabsheet-styles.js';
-import '../../src/vaadin-lit-tabsheet.js';
