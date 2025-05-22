12bb5dc91f475d01775502e96e4ffb03f63634c9
Jouni Koivuviita <jouni@vaadin.com>
2025-05-22 16:53:06 +0300

    base styles for app layout

--- packages/app-layout/src/vaadin-drawer-toggle-styles.js
+++ packages/app-layout/src/vaadin-drawer-toggle-styles.js
@@ -3,41 +3,27 @@
  * Copyright (c) 2018 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import '@vaadin/component-base/src/style-props.js';
 import { css } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
 export const drawerToggle = css`
-  :host {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    cursor: default;
-    position: relative;
-    outline: none;
-    height: 24px;
-    width: 24px;
-    padding: 4px;
-  }
+  @layer base {
+    [part='icon'] {
+      background: currentColor;
+      display: block;
+      height: var(--vaadin-icon-size, 1lh);
+      mask-image: var(--_vaadin-icon-menu);
+      width: var(--vaadin-icon-size, 1lh);
+    }
 
-  [part='icon'],
-  [part='icon']::after,
-  [part='icon']::before {
-    position: absolute;
-    top: 8px;
-    height: 3px;
-    width: 24px;
-    background-color: #000;
-  }
+    [hidden] {
+      display: none !important;
+    }
 
-  [part='icon']::after,
-  [part='icon']::before {
-    content: '';
-  }
-
-  [part='icon']::after {
-    top: 6px;
-  }
-
-  [part='icon']::before {
-    top: 12px;
+    @media (forced-colors: active) {
+      [part='icon'] {
+        background: CanvasText;
+      }
+    }
   }
 `;
