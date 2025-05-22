ea8016c1cfc00caee16f40ef4a8ca8ba1173863d
web-padawan <iamkulykov@gmail.com>
2025-05-22 14:30:27 +0300

    experiment: add combo-box base styles and visual tests

---
+++ packages/combo-box/src/styles/vaadin-combo-box-overlay-core-styles.js
@@ -0,0 +1,18 @@
+/**
+ * @license
+ * Copyright (c) 2015 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const comboBoxOverlayStyles = css`
+  #overlay {
+    width: var(--vaadin-combo-box-overlay-width, var(--_vaadin-combo-box-overlay-default-width, auto));
+  }
+
+  [part='content'] {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+`;
---
+++ packages/combo-box/src/styles/vaadin-combo-box-scroller-base-styles.js
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright (c) 2015 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const comboBoxScrollerStyles = css`
+  :host {
+    /* Fixes scrollbar disappearing when 'Show scroll bars: Always' enabled in Safari */
+    box-shadow: 0 0 0 white;
+    display: block;
+    min-height: 1px;
+    overflow: auto;
+    /* Fixes item background from getting on top of scrollbars on Safari */
+    transform: translate3d(0, 0, 0);
+  }
+
+  #selector {
+    border: var(--vaadin-item-overlay-padding, 4px) solid transparent;
+    position: relative;
+    forced-color-adjust: none;
+    min-height: var(--_items-min-height, auto);
+  }
+
+  #selector > * {
+    forced-color-adjust: auto;
+  }
+`;
