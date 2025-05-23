cbb07b08258fb6277a8665cb64b4069175bc7834
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-23 16:57:31 +0400

    fix: add Lumo colors import to vaadin-item

--- packages/item/theme/lumo/vaadin-item-styles.js
+++ packages/item/theme/lumo/vaadin-item-styles.js
@@ -2,6 +2,7 @@ import '@vaadin/vaadin-lumo-styles/font-icons.js';
 import '@vaadin/vaadin-lumo-styles/sizing.js';
 import '@vaadin/vaadin-lumo-styles/spacing.js';
 import '@vaadin/vaadin-lumo-styles/style.js';
+import '@vaadin/vaadin-lumo-styles/color.js';
 import '@vaadin/vaadin-lumo-styles/typography.js';
 import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 
