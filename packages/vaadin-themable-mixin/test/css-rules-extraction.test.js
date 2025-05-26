b327f3771a18470aed179918a571e7a37f756ec2
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-26 15:37:59 +0400

    fix: deduplicate tag-scoped media rules

--- packages/vaadin-themable-mixin/test/css-rules-extraction.test.js
+++ packages/vaadin-themable-mixin/test/css-rules-extraction.test.js
@@ -5,9 +5,21 @@ import { extractTagScopedCSSRules } from '../src/css-rules.js';
 const BASE_PATH = import.meta.url.split('/').slice(0, -1).join('/');
 
 describe('CSS rules extraction', () => {
-  it('should extract rules from tag-scoped @media', () => {
+  it('should extract and deduplicate rules from tag-scoped @media', () => {
     fixtureSync(`
       <style>
+        @media test-button {
+          :host {
+            color: black;
+          }
+        }
+
+        @media test-button {
+          :host {
+            color: red;
+          }
+        }
+
         @media test-button {
           :host {
             color: black;
@@ -35,8 +47,9 @@ describe('CSS rules extraction', () => {
 
     {
       const rules = extractTagScopedCSSRules(document, 'test-button');
-      expect(rules).to.have.lengthOf(1);
-      expect(rules[0].cssText).to.equal(':host { color: black; }');
+      expect(rules).to.have.lengthOf(2);
+      expect(rules[0].cssText).to.equal(':host { color: red; }');
+      expect(rules[1].cssText).to.equal(':host { color: black; }');
     }
     {
       const rules = extractTagScopedCSSRules(document, 'test-text-field');
@@ -47,10 +60,12 @@ describe('CSS rules extraction', () => {
     }
   });
 
-  it('should extract rules from tag-scoped @import', async () => {
+  it('should extract and deduplicate rules from tag-scoped @import', async () => {
     const style = fixtureSync(`
       <style>
-        @import '${BASE_PATH}/css-rules-extraction-test-button.css' test-button;
+        @import '${BASE_PATH}/css-rules-extraction-test-button-black.css' test-button;
+        @import '${BASE_PATH}/css-rules-extraction-test-button-red.css' test-button;
+        @import '${BASE_PATH}/css-rules-extraction-test-button-black.css' test-button;
         @import '${BASE_PATH}/css-rules-extraction-test-text-field.css' test-text-field;
       </style>
     `);
@@ -58,8 +73,9 @@ describe('CSS rules extraction', () => {
 
     {
       const rules = extractTagScopedCSSRules(document, 'test-button');
-      expect(rules).to.have.lengthOf(1);
-      expect(rules[0].cssText).to.equal(':host { color: black; }');
+      expect(rules).to.have.lengthOf(2);
+      expect(rules[0].cssText).to.equal(':host { color: red; }');
+      expect(rules[1].cssText).to.equal(':host { color: black; }');
     }
     {
       const rules = extractTagScopedCSSRules(document, 'test-text-field');
@@ -69,7 +85,7 @@ describe('CSS rules extraction', () => {
     }
   });
 
-  it('should extract rules from tag-scoped @media and @import inside an imported stylesheet', async () => {
+  it('should extract and deduplicate rules from tag-scoped @media and @import inside an imported stylesheet', async () => {
     const style = fixtureSync(`
       <style>
         @import '${BASE_PATH}/css-rules-extraction-test-shared.css';
@@ -79,8 +95,9 @@ describe('CSS rules extraction', () => {
 
     {
       const rules = extractTagScopedCSSRules(document, 'test-button');
-      expect(rules).to.have.lengthOf(1);
-      expect(rules[0].cssText).to.equal(':host { color: black; }');
+      expect(rules).to.have.lengthOf(2);
+      expect(rules[0].cssText).to.equal(':host { color: red; }');
+      expect(rules[1].cssText).to.equal(':host { color: black; }');
     }
     {
       const rules = extractTagScopedCSSRules(document, 'test-text-field');
