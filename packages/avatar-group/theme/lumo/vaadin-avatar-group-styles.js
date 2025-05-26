3a9e30f44e9951baee07f9e90c034a3596c55211
web-padawan <iamkulykov@gmail.com>
2025-05-26 12:10:57 +0300

    Revert Lumo change which should not be needed

--- packages/avatar-group/theme/lumo/vaadin-avatar-group-styles.js
+++ packages/avatar-group/theme/lumo/vaadin-avatar-group-styles.js
@@ -56,20 +56,7 @@ registerStyles('vaadin-avatar-group-overlay', [overlay, menuOverlayCore, avatarG
   moduleId: 'lumo-avatar-group-overlay',
 });
 
-registerStyles(
-  'vaadin-avatar-group-menu',
-  [
-    listBox,
-    css`
-      @layer base {
-        :host {
-          padding: revert-layer;
-        }
-      }
-    `,
-  ],
-  { moduleId: 'lumo-avatar-group-menu' },
-);
+registerStyles('vaadin-avatar-group-menu', listBox, { moduleId: 'lumo-avatar-group-menu' });
 
 registerStyles(
   'vaadin-avatar-group-menu-item',
