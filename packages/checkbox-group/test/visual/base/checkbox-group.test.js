23766b1bb191f83c48155ea1e43dfb6503f761e2
Serhii Kulykov <iamkulykov@gmail.com>
2025-05-27 16:09:37 +0300

    experiment: add checkbox-group base styles and visual tests (#9317)

---
+++ packages/checkbox-group/test/visual/base/checkbox-group.test.js
@@ -0,0 +1,124 @@
+import { sendKeys } from '@vaadin/test-runner-commands';
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../common.js';
+import '../../../src/vaadin-checkbox-group.js';
+
+describe('checkbox-group', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.style.padding = '10px';
+
+    element = fixtureSync(
+      `
+        <vaadin-checkbox-group>
+          <vaadin-checkbox value="a" label="A"></vaadin-checkbox>
+          <vaadin-checkbox value="b" label="B"></vaadin-checkbox>
+          <vaadin-checkbox value="c" label="C"></vaadin-checkbox>
+        </vaadin-checkbox-group>
+      `,
+      div,
+    );
+  });
+
+  describe('states', () => {
+    it('basic', async () => {
+      await visualDiff(div, 'state-basic');
+    });
+
+    it('value', async () => {
+      element.value = ['a', 'c'];
+      await visualDiff(div, 'state-value');
+    });
+
+    it('disabled', async () => {
+      element.disabled = true;
+      await visualDiff(div, 'state-disabled');
+    });
+
+    it('disabled value', async () => {
+      element.disabled = true;
+      element.value = ['a', 'c'];
+      await visualDiff(div, 'state-disabled-value');
+    });
+
+    it('disabled label', async () => {
+      element.label = 'Label';
+      element.disabled = true;
+      await visualDiff(div, 'state-disabled-label');
+    });
+
+    it('readonly', async () => {
+      element.readonly = true;
+      element.value = ['a', 'c'];
+      await visualDiff(div, 'state-readonly');
+    });
+
+    it('required', async () => {
+      element.label = 'Label';
+      element.required = true;
+      await visualDiff(div, 'state-required');
+    });
+
+    describe('focus', () => {
+      it('keyboard focus', async () => {
+        await sendKeys({ press: 'Tab' });
+        await visualDiff(div, 'state-focus');
+      });
+
+      it('readonly focus', async () => {
+        element.readonly = true;
+        await sendKeys({ press: 'Tab' });
+        await visualDiff(div, 'state-focus-readonly');
+      });
+    });
+  });
+
+  describe('features', () => {
+    ['ltr', 'rtl'].forEach((dir) => {
+      describe(dir, () => {
+        before(() => {
+          document.documentElement.setAttribute('dir', dir);
+        });
+
+        after(() => {
+          document.documentElement.removeAttribute('dir');
+        });
+
+        it('horizontal', async () => {
+          element.setAttribute('theme', 'horizontal');
+          await visualDiff(div, `${dir}-horizontal`);
+        });
+
+        it('horizontal wrapped', async () => {
+          element.setAttribute('theme', 'horizontal');
+          element.style.width = '150px';
+          await visualDiff(div, `${dir}-horizontal-wrapped`);
+        });
+
+        it('error message', async () => {
+          element.label = 'Label';
+          element.errorMessage = 'This field is required';
+          element.required = true;
+          element.validate();
+          await visualDiff(div, `${dir}-error-message`);
+        });
+
+        it('helper text', async () => {
+          element.helperText = 'Helper text';
+          await visualDiff(div, `${dir}-helper-text`);
+        });
+
+        it('helper above field', async () => {
+          element.label = 'Label';
+          element.helperText = 'Helper text';
+          element.setAttribute('theme', 'helper-above-field');
+          await visualDiff(div, `${dir}-helper-above-field`);
+        });
+      });
+    });
+  });
+});
