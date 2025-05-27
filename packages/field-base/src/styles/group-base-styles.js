16991f6706aa7b2116aee74bc2fe4c26da1bb613
web-padawan <iamkulykov@gmail.com>
2025-05-27 15:05:27 +0300

    experiment: add checkbox-group base styles and visual tests

---
+++ packages/field-base/src/styles/group-base-styles.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright (c) 2018 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import '@vaadin/component-base/src/style-props.js';
+import { css, unsafeCSS } from 'lit';
+
+export const group = (name = 'checkbox') => css`
+  @layer base {
+    :host {
+      width: fit-content;
+    }
+
+    .vaadin-group-field-container {
+      display: contents;
+    }
+
+    :host,
+    [part='group-field'] {
+      display: flex;
+      flex-direction: column;
+      gap: var(--vaadin-${unsafeCSS(name)}-group-gap, var(--_vaadin-gap-container-block));
+    }
+
+    [part='group-field'] {
+      gap: 0.5lh 1.5em;
+    }
+
+    :host([theme~='horizontal']) [part='group-field'] {
+      flex-flow: row wrap;
+    }
+  }
+`;
