662a7afe701de2c388b059870f728917479cd946
Sergey Vinogradov <mr.vursen@gmail.com>
2025-05-30 17:41:52 +0400

    add missing license header

--- packages/vaadin-themable-mixin/src/css-property-observer.js
+++ packages/vaadin-themable-mixin/src/css-property-observer.js
@@ -1,4 +1,10 @@
 /**
+ * @license
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
  * WARNING: For internal use only. Do not use this class in custom components.
  *
  * @private
