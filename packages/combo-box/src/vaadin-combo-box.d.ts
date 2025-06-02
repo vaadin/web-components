26205f4bf9eda1b60837953b22ed0d0ae9369b39
Serhii Kulykov <iamkulykov@gmail.com>
2025-06-02 16:55:03 +0300

    refactor: split some combo-box mixin logic into base mixin (#9344)

--- packages/combo-box/src/vaadin-combo-box.d.ts
+++ packages/combo-box/src/vaadin-combo-box.d.ts
@@ -20,6 +20,7 @@ import type { PatternMixinClass } from '@vaadin/field-base/src/pattern-mixin.js'
 import type { ValidateMixinClass } from '@vaadin/field-base/src/validate-mixin.js';
 import type { ThemableMixinClass } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import type { ThemePropertyMixinClass } from '@vaadin/vaadin-themable-mixin/vaadin-theme-property-mixin.js';
+import type { ComboBoxBaseMixinClass } from './vaadin-combo-box-base-mixin.js';
 import type { ComboBoxDataProviderMixinClass } from './vaadin-combo-box-data-provider-mixin.js';
 import type { ComboBoxMixinClass } from './vaadin-combo-box-mixin.js';
 import type { ComboBoxDefaultItem } from './vaadin-combo-box-mixin.js';
@@ -230,6 +231,7 @@ declare class ComboBox<TItem = ComboBoxDefaultItem> extends HTMLElement {
 interface ComboBox<TItem = ComboBoxDefaultItem>
   extends ComboBoxDataProviderMixinClass<TItem>,
     ComboBoxMixinClass<TItem>,
+    ComboBoxBaseMixinClass,
     ValidateMixinClass,
     PatternMixinClass,
     LabelMixinClass,
