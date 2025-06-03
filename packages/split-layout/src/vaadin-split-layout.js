e6236a7bb2c32ebc5b259f2451fcb9ae083897cc
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-03 14:54:11 +0400

    feat: port split-layout Lumo styles to CSS files

--- packages/split-layout/src/vaadin-split-layout.js
+++ packages/split-layout/src/vaadin-split-layout.js
@@ -7,6 +7,7 @@ import { html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
+import { CSSInjectionMixin } from '@vaadin/vaadin-themable-mixin/css-injection-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { SplitLayoutMixin } from './vaadin-split-layout-mixin.js';
 import { splitLayoutStyles } from './vaadin-split-layout-styles.js';
@@ -155,7 +156,7 @@ import { splitLayoutStyles } from './vaadin-split-layout-styles.js';
  * @mixes SplitLayoutMixin
  * @mixes ThemableMixin
  */
-class SplitLayout extends SplitLayoutMixin(ElementMixin(ThemableMixin(PolylitMixin(LitElement)))) {
+class SplitLayout extends SplitLayoutMixin(ElementMixin(ThemableMixin(CSSInjectionMixin(PolylitMixin(LitElement))))) {
   static get is() {
     return 'vaadin-split-layout';
   }
