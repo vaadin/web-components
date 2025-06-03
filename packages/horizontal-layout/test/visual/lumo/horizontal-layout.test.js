64dbcabf700009aa8119a3a67dd982b5cf1e5d96
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-03 14:45:27 +0400

    feat: port horizontal-layout Lumo styles to CSS files

--- packages/horizontal-layout/test/visual/lumo/horizontal-layout.test.js
+++ packages/horizontal-layout/test/visual/lumo/horizontal-layout.test.js
@@ -1,6 +1,8 @@
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
 import { visualDiff } from '@web/test-runner-visual-regression';
-import '../../../theme/lumo/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-lumo-styles/props.css';
+import '@vaadin/vaadin-lumo-styles/components/horizontal-layout.css';
+import '../../../vaadin-horizontal-layout.js';
 
 describe('horizontal-layout', () => {
   let div, element;
