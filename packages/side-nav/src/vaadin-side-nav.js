6443e5a66f73a2e7d40a3291a9cf40cccdb178f7
Jouni Koivuviita <jouni@vaadin.com>
2025-06-04 13:44:31 +0300

    import side-nav-item together with side-nav

--- packages/side-nav/src/vaadin-side-nav.js
+++ packages/side-nav/src/vaadin-side-nav.js
@@ -3,6 +3,7 @@
  * Copyright (c) 2023 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import './vaadin-side-nav-item.js';
 import { html, LitElement } from 'lit';
 import { FocusMixin } from '@vaadin/a11y-base/src/focus-mixin.js';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
