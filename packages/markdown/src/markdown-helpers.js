5c9fe697968c916bf6d0c46f9cd41e2f887d7eef
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-05 14:07:30 +0400

    fix linter errors

--- packages/markdown/src/markdown-helpers.js
+++ packages/markdown/src/markdown-helpers.js
@@ -1,3 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
 import DOMPurify from 'dompurify';
 import { marked } from 'marked';
 
