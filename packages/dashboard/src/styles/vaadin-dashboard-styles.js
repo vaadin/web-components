0860c34a91532100cd3c7957905130066ef34dbe
Serhii Kulykov <iamkulykov@gmail.com>
2025-06-09 12:16:03 +0300

    refactor: extract dashboard core CSS to styles folder (#9429)

---
+++ packages/dashboard/src/styles/vaadin-dashboard-styles.js
@@ -0,0 +1,25 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2025 Vaadin Ltd.
+ *
+ * This program is available under Vaadin Commercial License and Service Terms.
+ *
+ *
+ * See https://vaadin.com/commercial-license-and-service-terms for the full
+ * license.
+ */
+import { css } from 'lit';
+import { dashboardLayoutStyles } from './vaadin-dashboard-layout-styles.js';
+
+const dashboard = css`
+  #grid[item-resizing] {
+    -webkit-user-select: none;
+    user-select: none;
+  }
+
+  ::slotted(vaadin-dashboard-widget-wrapper) {
+    display: contents;
+  }
+`;
+
+export const dashboardStyles = [dashboardLayoutStyles, dashboard];
