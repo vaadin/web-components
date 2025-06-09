6e4380cd397b8fe40c60aa33c0750ce9a9a99fc0
Serhii Kulykov <iamkulykov@gmail.com>
2025-06-09 16:53:38 +0300

    Apply suggestions from code review

--- packages/menu-bar/src/styles/vaadin-menu-bar-overlay-styles.js
+++ packages/menu-bar/src/styles/vaadin-menu-bar-overlay-styles.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2000 - 2025 Vaadin Ltd.
+ * Copyright (c) 2019 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { menuOverlayStyles } from '@vaadin/context-menu/src/styles/vaadin-menu-overlay-styles.js';
