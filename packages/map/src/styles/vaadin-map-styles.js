14d13c7d6815bbba48b9a5f51802be551cb66542
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-09 17:17:07 +0400

    refactor: extract map core CSS to styles folder

--- packages/map/src/styles/vaadin-map-styles.js
+++ packages/map/src/styles/vaadin-map-styles.js
@@ -8,7 +8,7 @@
  * See https://vaadin.com/commercial-license-and-service-terms for the full
  * license.
  */
-import { css } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { css } from 'lit';
 
 export const mapStyles = css`
   :host {
