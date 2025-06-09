2e44baf956dab317ba4fee231da79e0faebbf102
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-09 17:44:59 +0400

    remove extra blank line

--- packages/menu-bar/src/styles/vaadin-menu-bar-overlay-styles.js
+++ packages/menu-bar/src/styles/vaadin-menu-bar-overlay-styles.js
@@ -3,7 +3,6 @@
  * Copyright (c) 2000 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-
 import { menuOverlayStyles } from '@vaadin/context-menu/src/styles/vaadin-menu-overlay-styles.js';
 import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
 
