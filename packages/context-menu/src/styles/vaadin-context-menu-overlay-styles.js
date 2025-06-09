ca94eb1c379633c7620de22526c2767c3f5d2747
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-09 13:42:16 +0400

    refactor: extract context-menu core CSS to styles folder

---
+++ packages/context-menu/src/styles/vaadin-context-menu-overlay-styles.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2000 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
+import { styles } from './vaadin-menu-overlay-styles.js';
+export const contextMenuOverlayStyles = [overlayStyles, styles];
