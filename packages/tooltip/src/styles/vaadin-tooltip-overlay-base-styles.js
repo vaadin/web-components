717a79953658b545ef1489d840b8a9f27efe20a9
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-09 11:34:52 +0400

    refactor: extract tooltip core CSS to styles folder

---
+++ packages/tooltip/src/styles/vaadin-tooltip-overlay-base-styles.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2022 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+
+export const tooltipOverlayStyles = css``;
