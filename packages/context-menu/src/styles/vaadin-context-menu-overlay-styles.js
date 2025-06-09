f423d5fcbf4456710f16e5ac27a65ef6491d2797
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-09 13:44:26 +0400

    rename styles to menuOverlayStyles

--- packages/context-menu/src/styles/vaadin-context-menu-overlay-styles.js
+++ packages/context-menu/src/styles/vaadin-context-menu-overlay-styles.js
@@ -4,5 +4,6 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
-import { styles } from './vaadin-menu-overlay-styles.js';
-export const contextMenuOverlayStyles = [overlayStyles, styles];
+import { menuOverlayStyles } from './vaadin-menu-overlay-styles.js';
+
+export const contextMenuOverlayStyles = [overlayStyles, menuOverlayStyles];
