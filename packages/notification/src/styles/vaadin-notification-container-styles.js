223b72a4c8e153bc1e6ff143b0426fd26f1d7c51
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-16 11:55:39 +0400

    refactor: extract notification core CSS to styles folder

--- packages/notification/src/styles/vaadin-notification-container-styles.js
+++ packages/notification/src/styles/vaadin-notification-container-styles.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2016 - 2025 Vaadin Ltd.
+ * Copyright (c) 2000 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { css } from 'lit';
@@ -50,22 +50,6 @@ export const notificationContainerStyles = css`
 
     [region-group] > [region] {
       flex: initial;
-    }
-  }
-`;
-
-export const notificationCardStyles = css`
-  :host {
-    display: block;
-  }
-
-  [part='overlay'] {
-    pointer-events: auto;
-  }
-
-  @media (forced-colors: active) {
-    [part='overlay'] {
-      outline: 3px solid;
     }
   }
 `;
