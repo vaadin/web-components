2241a292d9fd4db65b9e8630647cf0c5d6ac828f
Jouni Koivuviita <jouni@vaadin.com>
2025-06-17 09:32:37 +0300

    experiment: confirm-dialog base styles
    - initialize core and base styles
    - Update confirm-dialog.html
    - fix: hide reject button part when not used. This avoids an unnecessary layout gap.
    - base styles

---
+++ packages/confirm-dialog/src/styles/vaadin-confirm-dialog-overlay-base-styles.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright (c) 2025 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+import { dialogOverlayBase } from '@vaadin/dialog/src/styles/vaadin-dialog-overlay-core-styles.js';
+import { overlayStyles } from '@vaadin/overlay/src/vaadin-overlay-styles.js';
+
+const confirmDialogOverlay = css`
+  @layer base {
+    :host {
+      --vaadin-dialog-min-width: var(--vaadin-confirm-dialog-min-width, 15em);
+      --vaadin-dialog-max-width: var(--vaadin-confirm-dialog-max-width, 25em);
+    }
+
+    ::slotted([slot='header']) {
+      display: contents;
+      font: inherit !important;
+      color: inherit !important;
+    }
+
+    [part='header'] {
+      color: var(--vaadin-dialog-title-color, var(--_vaadin-color-strong));
+      font-weight: var(--vaadin-dialog-title-font-weight, 600);
+      font-size: var(--vaadin-dialog-title-font-size, 1em);
+      line-height: var(--vaadin-dialog-title-line-height, inherit);
+    }
+  }
+`;
+
+export const confirmDialogOverlayStyles = [overlayStyles, dialogOverlayBase, confirmDialogOverlay];
