09e8af5093f265bdf95b1a747b85946592ffb216
Diego Cardoso <diego@vaadin.com>
2025-06-17 12:23:24 +0200

    test: update references / add material test

--- packages/form-layout/test/visual/lumo/form-layout.test.js
+++ packages/form-layout/test/visual/lumo/form-layout.test.js
@@ -1,6 +1,7 @@
 import { nextFrame } from '@vaadin/testing-helpers';
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/text-field';
 import '../../../theme/lumo/vaadin-form-layout.js';
 import '../../../theme/lumo/vaadin-form-item.js';
 
