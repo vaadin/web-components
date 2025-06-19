22746dab06b7e2cda9eff9efed4a70b02ebfd593
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-19 14:59:05 +0400

    feat: port field-highlighter Lumo styles to CSS files

--- packages/field-highlighter/test/visual/common.js
+++ packages/field-highlighter/test/visual/common.js
@@ -2,19 +2,24 @@ import { css, registerStyles } from '@vaadin/vaadin-themable-mixin/vaadin-themab
 
 /* Hide caret */
 registerStyles(
-  'vaadin-combo-box vaadin-date-picker vaadin-text-field',
+  'vaadin-combo-box vaadin-date-picker vaadin-text-field vaadin-text-area',
   css`
-    :host([focus-ring]) ::slotted(input) {
-      caret-color: transparent;
-    }
-  `,
-);
-
-registerStyles(
-  'vaadin-text-area',
-  css`
+    :host([focus-ring]) ::slotted(input),
     :host([focus-ring]) ::slotted(textarea) {
       caret-color: transparent;
+    }
+
+    /* Show error message immediately */
+    [part='label'],
+    [part='helper-text'],
+    [part='error-message'],
+    [part='required-indicator'] {
+      &,
+      &::before,
+      &::after {
+        animation: none !important;
+        transition: none !important;
+      }
     }
   `,
 );
