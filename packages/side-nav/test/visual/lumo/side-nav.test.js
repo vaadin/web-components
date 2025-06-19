30fce4a953d84ec131c704cafe1a99f13e3e2b29
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-19 15:12:43 +0400

    feat: port side-nav Lumo styles to CSS files

--- packages/side-nav/test/visual/lumo/side-nav.test.js
+++ packages/side-nav/test/visual/lumo/side-nav.test.js
@@ -1,8 +1,11 @@
 import { sendKeys } from '@vaadin/test-runner-commands';
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/vaadin-lumo-styles/props.css';
+import '@vaadin/vaadin-lumo-styles/components/side-nav.css';
+import '@vaadin/vaadin-lumo-styles/components/icon.css';
 import '../not-animated-styles.js';
-import '../../../theme/lumo/vaadin-side-nav.js';
+import '../../../vaadin-side-nav.js';
 import '@vaadin/icon';
 import '@vaadin/icons';
 
