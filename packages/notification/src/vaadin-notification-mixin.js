1b84b82f560d0f9f206735eeb1ad342a323ebf6d
Jouni Koivuviita <jouni@vaadin.com>
2025-06-23 19:18:03 +0300

    feat: notification order
    Update multiple.test.js

--- packages/notification/src/vaadin-notification-mixin.js
+++ packages/notification/src/vaadin-notification-mixin.js
@@ -382,11 +382,13 @@ export const NotificationMixin = (superClass) =>
       }
 
       this._card.slot = this.position;
-      if (this._container.firstElementChild && /top/u.test(this.position)) {
+      if (this._container.firstElementChild && this.position.startsWith('top')) {
         this._container.insertBefore(this._card, this._container.firstElementChild);
       } else {
         this._container.appendChild(this._card);
       }
+
+      this._updateCardOrder(this.position);
     }
 
     /** @private */
@@ -397,6 +399,7 @@ export const NotificationMixin = (superClass) =>
 
       if (this._card.parentNode) {
         this._card.parentNode.removeChild(this._card);
+        this._updateCardOrder(this._card.slot);
       }
       this._card.removeAttribute('closing');
       this._container.opened = Boolean(this._container.firstElementChild);
@@ -443,6 +446,13 @@ export const NotificationMixin = (superClass) =>
           this._durationTimeoutId = setTimeout(() => this.close(), duration);
         }
       }
+    }
+
+    /** @private */
+    _updateCardOrder(position) {
+      this._container.querySelectorAll(`[slot="${position}"]`).forEach((card, i, cards) => {
+        card.style.setProperty('--order', this.position.startsWith('top') ? i + 1 : cards.length - i);
+      });
     }
 
     /**
