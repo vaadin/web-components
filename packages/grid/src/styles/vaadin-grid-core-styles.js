d7473d5a7c30d2159e79bfbcf3804f723d62e72a
Sergey Vinogradov <mr.vursen@gmail.com>
2025-06-25 14:38:00 +0400

    refactor: drop legacy -webkit-sticky fallback (#9545)

--- packages/grid/src/styles/vaadin-grid-core-styles.js
+++ packages/grid/src/styles/vaadin-grid-core-styles.js
@@ -76,7 +76,6 @@ export const gridStyles = css`
   #header,
   #footer {
     display: block;
-    position: -webkit-sticky;
     position: sticky;
     left: 0;
     overflow: visible;
@@ -105,7 +104,6 @@ export const gridStyles = css`
     flex-grow: 1;
     flex-shrink: 0;
     display: block;
-    position: -webkit-sticky;
     position: sticky;
     width: 100%;
     left: 0;
