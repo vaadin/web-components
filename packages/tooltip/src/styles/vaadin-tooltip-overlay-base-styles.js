2c692026a62f313bfbb5aa2a27e6b138ed43cc47
web-padawan <iamkulykov@gmail.com>
2025-06-25 13:10:28 +0300

    experiment: add tooltip base styles and visual tests

--- packages/tooltip/src/styles/vaadin-tooltip-overlay-base-styles.js
+++ packages/tooltip/src/styles/vaadin-tooltip-overlay-base-styles.js
@@ -5,4 +5,41 @@
  */
 import { css } from 'lit';
 
-export const tooltipOverlayStyles = css``;
+export const tooltipOverlayStyles = css`
+  @layer base {
+    [part='overlay'] {
+      max-width: 40ch;
+    }
+
+    [part='content'] {
+      padding: 0.1875rem 0.25rem;
+      white-space: pre-wrap;
+    }
+
+    :host([position^='top'][top-aligned]) [part='overlay'],
+    :host([position^='bottom'][top-aligned]) [part='overlay'] {
+      margin-top: var(--vaadin-tooltip-offset-top, 0.25rem);
+    }
+
+    :host([position^='top'][bottom-aligned]) [part='overlay'],
+    :host([position^='bottom'][bottom-aligned]) [part='overlay'] {
+      margin-bottom: var(--vaadin-tooltip-offset-bottom, 0.25rem);
+    }
+
+    :host([position^='start'][start-aligned]) [part='overlay'],
+    :host([position^='end'][start-aligned]) [part='overlay'] {
+      margin-inline-start: var(--vaadin-tooltip-offset-start, 0.25rem);
+    }
+
+    :host([position^='start'][end-aligned]) [part='overlay'],
+    :host([position^='end'][end-aligned]) [part='overlay'] {
+      margin-inline-end: var(--vaadin-tooltip-offset-end, 0.25rem);
+    }
+
+    @media (forced-colors: active) {
+      [part='overlay'] {
+        border: 1px dashed;
+      }
+    }
+  }
+`;
