955049e9874e9440441186c65438a913fc64ccb3
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-06-26 14:15:33 +0200

    restore CSS transform for visual tests

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -44,10 +44,14 @@ export function enforceThemePlugin(theme) {
       let { body } = context;
 
       if (theme === 'legacy-lumo' && context.response.is('html', 'js')) {
+        // For dev pages: replace link to CSS stylesheet with JS autoload script
         body = body.replace(
           '<link rel="stylesheet" href="/packages/vaadin-lumo-styles/lumo.css">',
           '<script type="module" src="/packages/vaadin-lumo-styles/test/autoload.js"></script>',
         );
+
+        // For visual tests: replace import of CSS file with JS autoload script
+        body = body.replace('vaadin-lumo-styles/global.css', 'vaadin-lumo-styles/test/autoload.js');
       }
 
       if (['base', 'legacy-lumo'].includes(theme) && context.response.is('html', 'js')) {
