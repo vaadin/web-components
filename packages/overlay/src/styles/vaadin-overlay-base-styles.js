7dfebe09b3fcaad88fd372ebbe85f996bd28572f
web-padawan <iamkulykov@gmail.com>
2025-06-28 13:38:24 +0300

    feat: add base styles to reset native popover browser defaults

--- packages/overlay/src/styles/vaadin-overlay-base-styles.js
+++ packages/overlay/src/styles/vaadin-overlay-base-styles.js
@@ -77,3 +77,24 @@ export const overlayStyles = css`
     }
   }
 `;
+
+/**
+ * Styles used to override certain browser defaults for native `[popover]`.
+ * - https://github.com/chromium/chromium/blob/e0b7d15/third_party/blink/renderer/core/html/resources/html.css#L1611
+ * - https://searchfox.org/mozilla-central/rev/f41e8cacb8e8ce254204e2526e98d49d024f1674/layout/style/res/html.css#891
+ */
+export const popoverOverlayStyles = css`
+  @layer base {
+    :host {
+      /* Needed for proper positioning */
+      width: auto;
+      height: auto;
+      /* Reset visuals */
+      border: none;
+      padding: 0;
+      background-color: transparent;
+      /* Do not clip shadows */
+      overflow: visible;
+    }
+  }
+`;
