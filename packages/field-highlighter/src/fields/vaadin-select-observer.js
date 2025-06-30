9fcd49b57cecd1dfc208b38317fae9cf6bd6a6f3
Serhii Kulykov <iamkulykov@gmail.com>
2025-06-30 13:25:32 +0300

    refactor: simplify select field-highlighter logic and tests (#9581)

--- packages/field-highlighter/src/fields/vaadin-select-observer.js
+++ packages/field-highlighter/src/fields/vaadin-select-observer.js
@@ -9,29 +9,12 @@ export class SelectObserver extends FieldObserver {
   constructor(select) {
     super(select);
 
-    this.blurWhileOpened = false;
     this.overlay = select._overlayElement;
-  }
-
-  addListeners(select) {
-    super.addListeners(select);
-
-    select.addEventListener('opened-changed', (event) => {
-      // When in phone mode, focus is lost when closing.
-      if (select._phone && event.detail.value === false) {
-        this.hideOutline(select);
-      }
-    });
   }
 
   onFocusIn(event) {
     if (this.overlay.contains(event.relatedTarget)) {
       // Focus returns on item select, do nothing.
-      return;
-    }
-
-    if (!this.component._phone && this.overlay.hasAttribute('closing')) {
-      // Focus returns on outside click, do nothing.
       return;
     }
 
