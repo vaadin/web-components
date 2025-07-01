179354b60d0d5f6e0b4867947d983f23ce698832
Sergey Vinogradov <mr.vursen@gmail.com>
2025-07-01 17:21:07 +0400

    rename CSSInjector to LumoInjector

--- packages/master-detail-layout/src/vaadin-master-detail-layout.js
+++ packages/master-detail-layout/src/vaadin-master-detail-layout.js
@@ -10,7 +10,7 @@ import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
 import { ResizeMixin } from '@vaadin/component-base/src/resize-mixin.js';
 import { SlotStylesMixin } from '@vaadin/component-base/src/slot-styles-mixin.js';
-import { CSSInjectionMixin } from '@vaadin/vaadin-themable-mixin/css-injection-mixin.js';
+import { LumoInjectionMixin } from '@vaadin/vaadin-themable-mixin/lumo-injection-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
 import { masterDetailLayoutStyles } from './styles/vaadin-master-detail-layout-core-styles.js';
 import { masterDetailLayoutTransitionStyles } from './styles/vaadin-master-detail-layout-transition-core-styles.js';
@@ -60,7 +60,7 @@ import { masterDetailLayoutTransitionStyles } from './styles/vaadin-master-detai
  * @mixes SlotStylesMixin
  */
 class MasterDetailLayout extends SlotStylesMixin(
-  ResizeMixin(ElementMixin(ThemableMixin(CSSInjectionMixin(PolylitMixin(LitElement))))),
+  ResizeMixin(ElementMixin(ThemableMixin(LumoInjectionMixin(PolylitMixin(LitElement))))),
 ) {
   static get is() {
     return 'vaadin-master-detail-layout';
