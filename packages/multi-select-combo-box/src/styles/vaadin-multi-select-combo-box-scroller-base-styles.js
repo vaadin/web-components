84ff46c9cc581434e042731213348f88113e7916
web-padawan <iamkulykov@gmail.com>
2025-07-02 16:10:15 +0300

    chore: align copyright years

--- packages/multi-select-combo-box/src/styles/vaadin-multi-select-combo-box-scroller-base-styles.js
+++ packages/multi-select-combo-box/src/styles/vaadin-multi-select-combo-box-scroller-base-styles.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2025 - 2025 Vaadin Ltd.
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { comboBoxScrollerStyles } from '@vaadin/combo-box/src/styles/vaadin-combo-box-scroller-base-styles.js';
