c6933572b15921874af28a821917d27e5a943388
Sergey Vinogradov <mr.vursen@gmail.com>
2025-07-02 09:15:57 +0400

    test: import whole Lumo autoload.js in MDL tests

--- packages/master-detail-layout/test/visual/lumo/master-detail-layout.test.js
+++ packages/master-detail-layout/test/visual/lumo/master-detail-layout.test.js
@@ -1,6 +1,6 @@
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
-import '@vaadin/vaadin-lumo-styles/color-global.js';
+import '@vaadin/vaadin-lumo-styles/test/autoload.js';
 import '../../../theme/lumo/vaadin-master-detail-layout.js';
 
 window.Vaadin ||= {};
