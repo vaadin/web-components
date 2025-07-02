1dbb7a2ce06e61268757d40c9b1b7668934faf14
web-padawan <iamkulykov@gmail.com>
2025-07-02 11:04:42 +0300

    test: disable caret in focus visual test

--- packages/date-time-picker/test/visual/base/date-time-picker.test.js
+++ packages/date-time-picker/test/visual/base/date-time-picker.test.js
@@ -1,5 +1,6 @@
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import '../common.js';
 import '../../../src/vaadin-date-time-picker.js';
 
 describe('date-time-picker', () => {
