6a36817eea002fc2b04184cd623e225944be9e0e
Joacim Päivärinne <joacim@vaadin.com>
2025-07-03 16:14:01 +0300

    Base menu bar styles

---
+++ packages/menu-bar/src/styles/vaadin-menu-bar-button-base-styles.js
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright (c) 2019 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const menuBarButtonStyles = css`
+  :host {
+    flex-shrink: 0;
+  }
+
+  :host([slot='overflow']) {
+    margin-inline-end: 0;
+  }
+
+  :host([theme~='dropdown-indicators']:not([slot='overflow']):not([theme~='icon'])[aria-haspopup])
+    [part='suffix']::after {
+    background: currentColor;
+    content: '';
+    display: block;
+    height: var(--vaadin-icon-size, 1lh);
+    mask-image: var(--_vaadin-icon-chevron-down);
+    width: var(--vaadin-icon-size, 1lh);
+  }
+`;
