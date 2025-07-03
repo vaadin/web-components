82b9e3220a485d5ed7f83401a900cd45b081438d
web-padawan <iamkulykov@gmail.com>
2025-07-03 15:40:23 +0300

    experiment: add popover base styles and visual tests

---
+++ packages/popover/src/styles/vaadin-popover-overlay-base-styles.js
@@ -0,0 +1,174 @@
+/**
+ * @license
+ * Copyright (c) 2024 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+import { overlayStyles } from '@vaadin/overlay/src/styles/vaadin-overlay-base-styles.js';
+
+const popoverOverlay = css`
+  @layer base {
+    :host {
+      --_arrow-size: var(--vaadin-popover-arrow-size, 0.5rem);
+      --_default-offset: 0.25rem;
+      --_rtl-multiplier: 1;
+    }
+
+    :host([dir='rtl']) {
+      --_rtl-multiplier: -1;
+    }
+
+    :host([modeless][with-backdrop]) [part='backdrop'] {
+      pointer-events: none;
+    }
+
+    :host([position^='top'][top-aligned]) [part='overlay'],
+    :host([position^='bottom'][top-aligned]) [part='overlay'] {
+      margin-top: var(--vaadin-popover-offset-top, var(--_default-offset));
+    }
+
+    [part='overlay'] {
+      position: relative;
+      overflow: visible;
+      max-height: 100%;
+    }
+
+    [part='content'] {
+      overflow: auto;
+      box-sizing: border-box;
+      max-height: 100%;
+      padding: var(--vaadin-popover-padding, var(--vaadin-padding));
+      position: relative;
+      z-index: 1; /* to show above arrow */
+    }
+
+    :host([theme~='no-padding']) [part='content'] {
+      padding: 0;
+    }
+
+    /* Increase the area of the popover so the pointer can go from the target directly to it. */
+    [part='overlay']::before {
+      position: absolute;
+      content: '';
+      inset-block: calc(var(--vaadin-popover-offset-top, var(--_default-offset)) * -1)
+        calc(var(--vaadin-popover-offset-bottom, var(--_default-offset)) * -1);
+      inset-inline: calc(var(--vaadin-popover-offset-start, var(--_default-offset)) * -1)
+        calc(var(--vaadin-popover-offset-end, var(--_default-offset)) * -1);
+      z-index: -1;
+      pointer-events: auto;
+    }
+
+    :host([position^='top'][bottom-aligned]) [part='overlay'],
+    :host([position^='bottom'][bottom-aligned]) [part='overlay'] {
+      margin-bottom: var(--vaadin-popover-offset-bottom, var(--_default-offset));
+    }
+
+    :host([position^='start'][start-aligned]) [part='overlay'],
+    :host([position^='end'][start-aligned]) [part='overlay'] {
+      margin-inline-start: var(--vaadin-popover-offset-start, var(--_default-offset));
+    }
+
+    :host([position^='start'][end-aligned]) [part='overlay'],
+    :host([position^='end'][end-aligned]) [part='overlay'] {
+      margin-inline-end: var(--vaadin-popover-offset-end, var(--_default-offset));
+    }
+
+    [part='arrow'] {
+      display: none;
+    }
+
+    :host([theme~='arrow']) {
+      --_default-offset: calc(0.25rem + var(--_arrow-size) / 2);
+    }
+
+    :host([theme~='arrow']) [part='arrow'] {
+      display: block;
+      position: absolute;
+      background: var(--vaadin-overlay-background, var(--vaadin-background-color));
+      border: var(--vaadin-overlay-border, 1px solid var(--vaadin-border-color));
+      width: var(--_arrow-size);
+      height: var(--_arrow-size);
+      rotate: 45deg;
+    }
+
+    /* bottom / top */
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[start-aligned]) [part='arrow'] {
+      inset-inline-start: calc(var(--_arrow-size) * 2);
+    }
+
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[end-aligned]) [part='arrow'] {
+      inset-inline-end: calc(var(--_arrow-size) * 2);
+    }
+
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[arrow-centered]) [part='arrow'] {
+      inset-inline-start: 50%;
+    }
+
+    /* bottom */
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[top-aligned]) [part='arrow'] {
+      border-bottom-width: 0;
+      border-right-width: 0;
+      top: 0;
+      translate: calc(-50% * var(--_rtl-multiplier)) calc(-50% - 1px);
+    }
+
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[end-aligned][top-aligned]) [part='arrow'] {
+      translate: calc(50% * var(--_rtl-multiplier)) calc(-50% - 1px);
+    }
+
+    /* top */
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[bottom-aligned]) [part='arrow'] {
+      border-top-width: 0;
+      border-left-width: 0;
+      bottom: 0;
+      translate: calc(-50% * var(--_rtl-multiplier)) calc(50% + 1px);
+    }
+
+    :host([theme~='arrow']:is([position^='bottom'], [position^='top'])[end-aligned][bottom-aligned]) [part='arrow'] {
+      translate: calc(50% * var(--_rtl-multiplier)) calc(50% + 1px);
+    }
+
+    /* start / end */
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[top-aligned]) [part='arrow'] {
+      top: calc(var(--_arrow-size) * 2);
+    }
+
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[bottom-aligned]) [part='arrow'] {
+      bottom: calc(var(--_arrow-size) * 2);
+    }
+
+    :host([theme~='arrow']:is([position='start'], [position='end'])[top-aligned]) [part='arrow'] {
+      top: 50%;
+    }
+
+    :host([dir='rtl'][theme~='arrow']:is([position^='start'], [position^='end'])) [part='arrow'] {
+      scale: -1;
+    }
+
+    /* end */
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[start-aligned]) [part='arrow'] {
+      border-top-width: 0;
+      border-right-width: 0;
+      inset-inline-start: 0;
+      translate: calc((-50% - 1px) * var(--_rtl-multiplier)) -50%;
+    }
+
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[start-aligned][bottom-aligned]) [part='arrow'] {
+      translate: calc((-50% - 1px) * var(--_rtl-multiplier)) 50%;
+    }
+
+    /* start */
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[end-aligned]) [part='arrow'] {
+      border-bottom-width: 0;
+      border-left-width: 0;
+      inset-inline-end: 0;
+      translate: calc((50% + 1px) * var(--_rtl-multiplier)) -50%;
+    }
+
+    :host([theme~='arrow']:is([position^='start'], [position^='end'])[end-aligned][bottom-aligned]) [part='arrow'] {
+      translate: calc((50% + 1px) * var(--_rtl-multiplier)) 50%;
+    }
+  }
+`;
+
+export const popoverOverlayStyles = [overlayStyles, popoverOverlay];
---
+++ packages/popover/test/visual/base/popover.test.js
@@ -0,0 +1,77 @@
+import { fixtureSync, nextRender, nextUpdate } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../not-animated-styles.js';
+import '../../../src/vaadin-popover.js';
+
+describe('popover', () => {
+  let div, target, element;
+
+  beforeEach(async () => {
+    element = fixtureSync('<vaadin-popover></vaadin-popover>');
+    element.renderer = (root) => {
+      root.innerHTML = `
+        <div>This is the popover content</div>
+        <div>It contains multiple lines</div>
+      `;
+    };
+    div = fixtureSync(`
+      <div style="display: flex; width: 600px; height: 600px; justify-content: center; align-items: center">
+        <div style="width: 100px; height: 100px; outline: 1px solid red;"></div>
+      </div>
+    `);
+    target = div.firstElementChild;
+    element.target = target;
+    await nextRender();
+  });
+
+  ['ltr', 'rtl'].forEach((dir) => {
+    describe(dir, () => {
+      before(() => {
+        document.documentElement.setAttribute('dir', dir);
+      });
+
+      after(() => {
+        document.documentElement.removeAttribute('dir');
+      });
+
+      [
+        'top-start',
+        'top',
+        'top-end',
+        'bottom-start',
+        'bottom',
+        'bottom-end',
+        'start-top',
+        'start',
+        'start-bottom',
+        'end-top',
+        'end',
+        'end-bottom',
+      ].forEach((position) => {
+        it(position, async () => {
+          element.position = position;
+          await nextUpdate(element);
+          target.click();
+          await nextRender();
+          await visualDiff(div, `${dir}-${position}`);
+        });
+
+        it(`${position} arrow`, async () => {
+          element.setAttribute('theme', 'arrow');
+          element.position = position;
+          await nextUpdate(element);
+          target.click();
+          await nextRender();
+          await visualDiff(div, `${dir}-${position}-arrow`);
+        });
+      });
+    });
+  });
+
+  it('no-padding', async () => {
+    element.setAttribute('theme', 'no-padding');
+    target.click();
+    await nextRender();
+    await visualDiff(div, 'no-padding');
+  });
+});
