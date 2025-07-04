dab81fa65ff37ab260f00d8c890aaae0f6f4fdec
Joacim Päivärinne <joacim@vaadin.com>
2025-07-04 11:10:47 +0300

    Context menu: base styles update

---
+++ packages/context-menu/src/styles/vaadin-context-menu-item-base-styles.js
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+import { itemStyles } from '@vaadin/item/src/styles/vaadin-item-base-styles.js';
+
+const menuItemStyles = css`
+  @layer base {
+    :host([aria-haspopup='true'])::after {
+      background: currentColor;
+      content: '';
+      display: block;
+      height: var(--vaadin-icon-size, 1lh);
+      mask-image: var(--_vaadin-icon-chevron-down);
+      rotate: -90deg;
+      width: var(--vaadin-icon-size, 1lh);
+    }
+  }
+`;
+
+export const contextMenuItemStyles = [itemStyles, menuItemStyles];
