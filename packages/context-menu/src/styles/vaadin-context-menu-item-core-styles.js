9dfdd38ab1c5f2be1d445ea11cd70c1b36b962b7
web-padawan <iamkulykov@gmail.com>
2025-07-04 15:21:32 +0300

    fix: add context-menu-item core styles

---
+++ packages/context-menu/src/styles/vaadin-context-menu-item-core-styles.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { itemStyles } from '@vaadin/item/src/styles/vaadin-item-core-styles.js';
+
+export const contextMenuItemStyles = [itemStyles];
