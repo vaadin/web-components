505b454ab37037c3598aea24eaca8dd557531e6f
Joacim Päivärinne <joacim@vaadin.com>
2025-07-04 11:25:50 +0300

    MenuBar: base styles, layer base added

--- packages/menu-bar/src/styles/vaadin-menu-bar-button-base-styles.js
+++ packages/menu-bar/src/styles/vaadin-menu-bar-button-base-styles.js
@@ -6,29 +6,31 @@
 import { css } from 'lit';
 
 export const menuBarButtonStyles = css`
-  :host {
-    flex-shrink: 0;
-  }
+  @layer base {
+    :host {
+      flex-shrink: 0;
+    }
 
-  :host([focused]) {
-    z-index: 1;
-  }
+    :host([focused]) {
+      z-index: 1;
+    }
 
-  :host([slot='overflow']) {
-    margin-inline-end: 0;
-  }
+    :host([slot='overflow']) {
+      margin-inline-end: 0;
+    }
 
-  :host([theme~='dropdown-indicators']:not([slot='overflow']):not([theme~='icon'])[aria-haspopup])
-    [part='suffix']::after {
-    background: currentColor;
-    content: '';
-    display: block;
-    height: var(--vaadin-icon-size, 1lh);
-    mask-image: var(--_vaadin-icon-chevron-down);
-    width: var(--vaadin-icon-size, 1lh);
-  }
+    :host([theme~='dropdown-indicators']:not([slot='overflow']):not([theme~='icon'])[aria-haspopup])
+      [part='suffix']::after {
+      background: currentColor;
+      content: '';
+      display: block;
+      height: var(--vaadin-icon-size, 1lh);
+      mask-image: var(--_vaadin-icon-chevron-down);
+      width: var(--vaadin-icon-size, 1lh);
+    }
 
-  :host([theme~='end-aligned'][first-visible]) {
-    margin-inline-start: auto;
+    :host([theme~='end-aligned'][first-visible]) {
+      margin-inline-start: auto;
+    }
   }
 `;
