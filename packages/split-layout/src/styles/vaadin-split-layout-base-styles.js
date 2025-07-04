25879604ef329d58a19f603b4cfadf4aa88bc6dd
Joacim Päivärinne <joacim@vaadin.com>
2025-07-04 15:44:27 +0300

    SplitLayout: base theme

---
+++ packages/split-layout/src/styles/vaadin-split-layout-base-styles.js
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * Copyright (c) 2016 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import '@vaadin/component-base/src/style-props.js';
+import { css } from 'lit';
+
+export const splitLayoutStyles = css`
+  @layer base {
+    :host {
+      display: flex;
+      overflow: hidden !important;
+      transform: translateZ(0);
+    }
+
+    :host([hidden]) {
+      display: none !important;
+    }
+
+    :host([orientation='vertical']) {
+      flex-direction: column;
+    }
+
+    :host ::slotted(*) {
+      flex: 1 1 auto;
+      overflow: auto;
+    }
+
+    [part='splitter'] {
+      background-color: var(--vaadin-split-layout-background, var(--vaadin-background-container));
+      flex: none;
+      min-height: var(--vaadin-split-layout-size, 0.5rem);
+      min-width: var(--vaadin-split-layout-size, 0.5rem);
+      overflow: visible;
+      position: relative;
+      z-index: 1;
+    }
+
+    :host(:not([orientation='vertical'])) > [part='splitter'] {
+      cursor: ew-resize;
+    }
+
+    :host([orientation='vertical']) > [part='splitter'] {
+      cursor: ns-resize;
+    }
+
+    [part='handle'] {
+      align-items: center;
+      display: flex;
+      height: var(--vaadin-split-layout-handle-target-size, 3rem);
+      justify-content: center;
+      left: 50%;
+      position: absolute;
+      transform: translate3d(-50%, -50%, 0);
+      top: 50%;
+      width: var(--vaadin-split-layout-handle-target-size, 3rem);
+    }
+
+    [part='handle']::after {
+      background-color: var(--vaadin-split-layout-handle-background, var(--vaadin-color-subtle));
+      border-radius: var(--vaadin-radius-m);
+      content: '';
+      display: block;
+      height: 100%;
+      max-height: 100%;
+      max-width: 100%;
+      width: var(--vaadin-split-layout-handle-size, 0.25rem);
+    }
+
+    :host([orientation='vertical']) [part='handle']::after {
+      height: var(--vaadin-split-layout-handle-size, 0.25rem);
+      width: 100%;
+    }
+
+    @media (forced-colors: active) {
+      [part~='splitter'] {
+        outline: 1px solid;
+      }
+
+      [part~='handle']::after {
+        background-color: AccentColor !important;
+        forced-color-adjust: none;
+      }
+    }
+  }
+`;
