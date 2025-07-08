84a02d7045d1591253264af0a68fbecc335f0acd
Jonte Päivärinne <joacim@vaadin.com>
2025-07-08 15:37:25 +0300

    experiment: add menu-bar base styles and visual tests (#9637)
    Co-authored-by: Jouni Koivuviita <jouni@vaadin.com>

---
+++ packages/menu-bar/src/styles/vaadin-menu-bar-item-base-styles.js
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2019 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { contextMenuItemStyles } from '@vaadin/context-menu/src/styles/vaadin-context-menu-item-base-styles.js';
+
+export const menuBarItemStyles = contextMenuItemStyles;
