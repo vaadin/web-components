57e35cdaa435d3da31614de2629e360af737f9df
web-padawan <iamkulykov@gmail.com>
2025-07-08 12:09:09 +0300

    Update imports to use base styles

--- packages/context-menu/src/styles/vaadin-context-menu-overlay-base-styles.js
+++ packages/context-menu/src/styles/vaadin-context-menu-overlay-base-styles.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2016 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { overlayStyles } from '@vaadin/overlay/src/styles/vaadin-overlay-core-styles.js';
-import { menuOverlayStyles } from './vaadin-menu-overlay-core-styles.js';
+import { overlayStyles } from '@vaadin/overlay/src/styles/vaadin-overlay-base-styles.js';
+import { menuOverlayStyles } from './vaadin-menu-overlay-base-styles.js';
 
 export const contextMenuOverlayStyles = [overlayStyles, menuOverlayStyles];
