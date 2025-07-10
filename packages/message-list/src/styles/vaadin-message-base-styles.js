0366892100a49fbee2af7f5fc889105601c571d6
Jouni Koivuviita <jouni@vaadin.com>
2025-07-10 10:31:26 +0300

    init base styles for message

---
+++ packages/message-list/src/styles/vaadin-message-base-styles.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+import { css } from 'lit';
+
+export const messageStyles = css`
+  :host {
+    display: flex;
+    flex-direction: row;
+    outline: none;
+  }
+
+  :host([hidden]) {
+    display: none !important;
+  }
+
+  [part='content'] {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+
+  [part='header'] {
+    align-items: baseline;
+    display: flex;
+    flex-flow: row wrap;
+  }
+
+  [part='name'] {
+    font-weight: 500;
+  }
+
+  [part='message'] {
+    white-space: pre-wrap;
+  }
+
+  ::slotted([slot='avatar']) {
+    --vaadin-avatar-outline-width: 0;
+    flex-shrink: 0;
+  }
+
+  ::slotted(vaadin-markdown) {
+    white-space: normal;
+  }
+`;
