b32d88e255148f5c5862ebb0e4c6822085e8fc30
Jouni Koivuviita <jouni@vaadin.com>
2025-07-10 10:53:01 +0300

    message base styles

--- packages/message-list/src/styles/vaadin-message-base-styles.js
+++ packages/message-list/src/styles/vaadin-message-base-styles.js
@@ -3,42 +3,64 @@
  * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import '@vaadin/component-base/src/style-props.js';
 import { css } from 'lit';
 
 export const messageStyles = css`
   :host {
     display: flex;
     flex-direction: row;
-    outline: none;
+    padding: var(--vaadin-message-padding, var(--vaadin-padding));
+    gap: var(--vaadin-message-gap, var(--vaadin-gap-container-inline) var(--vaadin-gap-container-block));
   }
 
   :host([hidden]) {
     display: none !important;
   }
 
+  :host(:is(:focus-visible, [focus-ring])) {
+    outline: var(--vaadin-focus-ring-width) solid var(--vaadin-focus-ring-color);
+    outline-offset: calc(var(--vaadin-focus-ring-width) * -1);
+  }
+
   [part='content'] {
     display: flex;
     flex-direction: column;
     flex-grow: 1;
+    gap: inherit;
   }
 
   [part='header'] {
     align-items: baseline;
     display: flex;
     flex-flow: row wrap;
+    gap: inherit;
+    row-gap: 0;
+    line-height: var(--vaadin-message-header-line-height, inherit);
   }
 
   [part='name'] {
-    font-weight: 500;
+    font-size: var(--vaadin-message-name-font-size, inherit);
+    font-weight: var(--vaadin-message-name-font-weight, 500);
+    color: var(--vaadin-message-name-color, var(--vaadin-color));
+  }
+
+  [part='time'] {
+    font-size: var(--vaadin-message-time-font-size, 0.75em);
+    font-weight: var(--vaadin-message-time-font-weight, inherit);
+    color: var(--vaadin-message-time-color, var(--vaadin-color-subtle));
   }
 
   [part='message'] {
     white-space: pre-wrap;
+    font-size: var(--vaadin-message-font-size, inherit);
+    font-weight: var(--vaadin-message-font-weight, inherit);
+    line-height: var(--vaadin-message-line-height, inherit);
+    color: var(--vaadin-message-color, var(--vaadin-color));
   }
 
   ::slotted([slot='avatar']) {
-    --vaadin-avatar-outline-width: 0;
-    flex-shrink: 0;
+    flex: none;
   }
 
   ::slotted(vaadin-markdown) {
