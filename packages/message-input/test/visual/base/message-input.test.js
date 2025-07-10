853dead4f58ea0e0e30687c8338391ee4b255ea6
Jouni Koivuviita <jouni@vaadin.com>
2025-07-10 11:44:13 +0300

    test: add visual tests for message-input

---
+++ packages/message-input/test/visual/base/message-input.test.js
@@ -0,0 +1,46 @@
+import { fixtureSync } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '../../../src/vaadin-message-input.js';
+
+describe('message-input', () => {
+  let div, element;
+
+  ['ltr', 'rtl'].forEach((dir) => {
+    describe(dir, () => {
+      before(() => {
+        document.documentElement.setAttribute('dir', dir);
+      });
+
+      after(() => {
+        document.documentElement.removeAttribute('dir');
+      });
+
+      describe('basic', () => {
+        beforeEach(() => {
+          div = document.createElement('div');
+          div.style.padding = '10px';
+          element = fixtureSync('<vaadin-message-input></vaadin-message-input>', div);
+        });
+
+        it('basic', async () => {
+          await visualDiff(div, `${dir}-basic`);
+        });
+
+        it('value', async () => {
+          element.value = 'Hello';
+          await visualDiff(div, `${dir}-value`);
+        });
+
+        it('disabled', async () => {
+          element.disabled = true;
+          await visualDiff(div, `${dir}-disabled`);
+        });
+
+        it('icon-button variant', async () => {
+          element.setAttribute('theme', 'icon-button');
+          await visualDiff(div, `${dir}-icon-button`);
+        });
+      });
+    });
+  });
+});
