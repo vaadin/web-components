c9c499342efa5ed227090de839e240b1c9f28fae
web-padawan <iamkulykov@gmail.com>
2025-07-10 13:25:25 +0300

    experiment: dashboard section border and outline styles

--- packages/dashboard/src/styles/vaadin-dashboard-section-base-styles.js
+++ packages/dashboard/src/styles/vaadin-dashboard-section-base-styles.js
@@ -8,6 +8,7 @@
  * See https://vaadin.com/commercial-license-and-service-terms for the full
  * license.
  */
+import '@vaadin/component-base/src/style-props.js';
 import { css } from 'lit';
 import { dashboardWidgetAndSectionStyles } from './vaadin-dashboard-widget-section-core-styles.js';
 
@@ -23,6 +24,9 @@ const sectionStyles = css`
     --_section-header-height: minmax(0, auto);
     grid-template-rows: var(--_section-header-height) repeat(auto-fill, var(--_row-height));
     grid-auto-rows: var(--_row-height);
+    border-radius: var(--vaadin-radius-m);
+    --_section-outline-offset: calc(min(var(--_gap), var(--_padding)) / 3);
+    --_focus-ring-offset: calc((var(--_section-outline-offset) - var(--_focus-ring-width)));
   }
 
   :host([hidden]) {
@@ -47,6 +51,24 @@ const sectionStyles = css`
 
   ::slotted(vaadin-dashboard-widget-wrapper) {
     display: contents;
+  }
+
+  /* Section states */
+
+  :host([editable]) {
+    outline: 1px solid var(--vaadin-border-color);
+    outline-offset: calc(var(--_section-outline-offset) - 1px);
+  }
+
+  :host([focused])::after {
+    content: '';
+    display: block;
+    position: absolute;
+    inset: 0;
+    border-radius: var(--vaadin-radius-m);
+    z-index: 9;
+    outline: var(--_focus-ring-width) solid var(--_focus-ring-color);
+    outline-offset: var(--_focus-ring-offset);
   }
 `;
 
