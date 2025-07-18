4a54b46b8e36eb02262af5804d6766eebf6f2699
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-07-18 15:52:38 +0200

    update JSDoc

--- packages/confirm-dialog/src/vaadin-confirm-dialog-mixin.js
+++ packages/confirm-dialog/src/vaadin-confirm-dialog-mixin.js
@@ -20,8 +20,8 @@ export const ConfirmDialogMixin = (superClass) =>
         /**
          * Sets the `aria-describedby` attribute of the overlay element.
          *
-         * By default, all elements inside the message area are linked
-         * through the `aria-describedby` attribute. However, there are
+         * By default, the text contents of all elements inside the message area
+         * are combined into the `aria-description` attribute. However, there are
          * cases where this can confuse screen reader users (e.g. the dialog
          * may present a password confirmation form). For these cases,
          * it's better to associate only the elements that will help describe
