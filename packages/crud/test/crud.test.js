988822a37f5dc123a25c669898e5570a44866f13
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-07-22 17:53:55 +0200

    cleanup test suite

--- packages/crud/test/crud.test.js
+++ packages/crud/test/crud.test.js
@@ -582,23 +582,6 @@ describe('crud', () => {
     });
   });
 
-  describe('custom element definition', () => {
-    let tagName;
-
-    beforeEach(() => {
-      crud = fixtureSync('<vaadin-crud style="width: 300px;"></vaadin-crud>');
-      tagName = crud.tagName.toLowerCase();
-    });
-
-    it('should be defined in custom element registry', () => {
-      expect(customElements.get(tagName)).to.be.ok;
-    });
-
-    it('should have a valid static "is" getter', () => {
-      expect(customElements.get(tagName).is).to.equal(tagName);
-    });
-  });
-
   describe('exportparts', () => {
     let dialog;
 
