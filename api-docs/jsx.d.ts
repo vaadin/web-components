0fe5748bf1772ada7b034ea3c4adfc3602fb706a
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-07-25 12:51:44 +0200

    add type declarations for JSX

---
+++ api-docs/jsx.d.ts
@@ -0,0 +1,6 @@
+// Dummy JSX type declaration needed for type-checking @types/mdx, which is used by Astro
+declare namespace JSX {
+  type Element = any;
+  type ElementClass = any;
+  type IntrinsicElements = any;
+}
