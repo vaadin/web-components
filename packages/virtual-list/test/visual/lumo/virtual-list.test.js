b1f22c99d2547a64e246dffb40460250e5ebca55
web-padawan <iamkulykov@gmail.com>
2025-07-28 11:52:22 +0300

    test: add basic Lumo visual test file for virtual-list

---
+++ packages/virtual-list/test/visual/lumo/virtual-list.test.js
@@ -0,0 +1,30 @@
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/vaadin-lumo-styles/props.css';
+import '../../../vaadin-virtual-list.js';
+
+describe('virtual-list', () => {
+  let div, element;
+
+  beforeEach(async () => {
+    div = document.createElement('div');
+    div.style.padding = '10px';
+    element = fixtureSync(
+      `
+        <vaadin-virtual-list style="height: 200px"></vaadin-virtual-list>
+      `,
+      div,
+    );
+    element.items = Array.from({ length: 100000 }).map((_, i) => {
+      return { label: `Item ${i}` };
+    });
+    element.renderer = (root, _, { item }) => {
+      root.innerHTML = `<div>${item.label}</div>`;
+    };
+    await nextRender();
+  });
+
+  it('basic', async () => {
+    await visualDiff(div, 'basic');
+  });
+});
