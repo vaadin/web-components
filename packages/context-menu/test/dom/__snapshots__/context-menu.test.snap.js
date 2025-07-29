63dafb4c309126ff9a9f74e458e4f2950229777b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-07-29 21:03:32 +0200

    update snapshots

--- packages/context-menu/test/dom/__snapshots__/context-menu.test.snap.js
+++ packages/context-menu/test/dom/__snapshots__/context-menu.test.snap.js
@@ -1,211 +1,192 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["context-menu items"] = 
-`<vaadin-context-menu-overlay opened="">
-  <vaadin-context-menu-list-box
-    aria-orientation="vertical"
-    role="menu"
-  >
-    <vaadin-context-menu-item
-      aria-haspopup="false"
-      aria-selected="false"
-      class="first"
-      focus-ring=""
-      focused=""
-      role="menuitem"
-      tabindex="0"
+snapshots["context-menu items"] =
+`<vaadin-context-menu>
+  <div>
+    Target
+  </div>
+  <div slot="overlay">
+    <vaadin-context-menu-list-box
+      aria-orientation="vertical"
+      role="menu"
     >
-      Menu Item 1
-    </vaadin-context-menu-item>
-    <hr role="separator">
-    <vaadin-context-menu-item
-      aria-expanded="false"
-      aria-haspopup="true"
-      aria-selected="false"
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 2
-    </vaadin-context-menu-item>
-    <vaadin-context-menu-item
-      aria-disabled="true"
-      aria-haspopup="false"
-      aria-selected="false"
-      disabled=""
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 3
-    </vaadin-context-menu-item>
-    <div
-      aria-haspopup="false"
-      class="custom"
-    >
-      Menu Item 4
-    </div>
-    <div
-      aria-haspopup="false"
-      class="last"
-    >
-      Menu Item 5
-    </div>
-  </vaadin-context-menu-list-box>
+      <vaadin-context-menu-item
+        aria-haspopup="false"
+        aria-selected="false"
+        class="first"
+        focus-ring=""
+        focused=""
+        role="menuitem"
+        tabindex="0"
+      >
+        Menu Item 1
+      </vaadin-context-menu-item>
+      <hr role="separator">
+      <vaadin-context-menu-item
+        aria-expanded="false"
+        aria-haspopup="true"
+        aria-selected="false"
+        role="menuitem"
+        tabindex="-1"
+      >
+        Menu Item 2
+      </vaadin-context-menu-item>
+      <vaadin-context-menu-item
+        aria-disabled="true"
+        aria-haspopup="false"
+        aria-selected="false"
+        disabled=""
+        role="menuitem"
+        tabindex="-1"
+      >
+        Menu Item 3
+      </vaadin-context-menu-item>
+      <div
+        aria-haspopup="false"
+        class="custom"
+      >
+        Menu Item 4
+      </div>
+      <div
+        aria-haspopup="false"
+        class="last"
+      >
+        Menu Item 5
+      </div>
+    </vaadin-context-menu-list-box>
+  </div>
   <vaadin-context-menu
-    hidden=""
     modeless=""
+    slot="sub-menu"
   >
   </vaadin-context-menu>
-</vaadin-context-menu-overlay>
+</vaadin-context-menu>
 `;
 /* end snapshot context-menu items */
 
-snapshots["context-menu items nested"] = 
-`<vaadin-context-menu-overlay
+snapshots["context-menu items nested"] =
+`<vaadin-context-menu
   modeless=""
-  opened=""
+  slot="sub-menu"
   start-aligned=""
   top-aligned=""
 >
-  <vaadin-context-menu-list-box
-    aria-orientation="vertical"
-    role="menu"
-  >
-    <vaadin-context-menu-item
-      aria-haspopup="false"
-      aria-selected="false"
-      class="first"
-      role="menuitem"
-      tabindex="0"
+  <div slot="overlay">
+    <vaadin-context-menu-list-box
+      aria-orientation="vertical"
+      role="menu"
     >
-      Menu Item 2-1
-    </vaadin-context-menu-item>
-    <vaadin-context-menu-item
-      aria-expanded="true"
-      aria-haspopup="true"
-      aria-selected="false"
-      class="last"
-      expanded=""
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 2-2
-    </vaadin-context-menu-item>
-  </vaadin-context-menu-list-box>
+      <vaadin-context-menu-item
+        aria-haspopup="false"
+        aria-selected="false"
+        class="first"
+        role="menuitem"
+        tabindex="0"
+      >
+        Menu Item 2-1
+      </vaadin-context-menu-item>
+      <vaadin-context-menu-item
+        aria-expanded="true"
+        aria-haspopup="true"
+        aria-selected="false"
+        class="last"
+        expanded=""
+        role="menuitem"
+        tabindex="-1"
+      >
+        Menu Item 2-2
+      </vaadin-context-menu-item>
+    </vaadin-context-menu-list-box>
+  </div>
   <vaadin-context-menu
-    hidden=""
     modeless=""
+    slot="sub-menu"
     start-aligned=""
     top-aligned=""
   >
+    <div slot="overlay">
+      <vaadin-context-menu-list-box
+        aria-orientation="vertical"
+        role="menu"
+      >
+        <vaadin-context-menu-item
+          aria-haspopup="false"
+          aria-selected="false"
+          class="first"
+          menu-item-checked=""
+          role="menuitem"
+          tabindex="0"
+        >
+          Menu Item 2-2-1
+        </vaadin-context-menu-item>
+        <vaadin-context-menu-item
+          aria-disabled="true"
+          aria-haspopup="false"
+          aria-selected="false"
+          disabled=""
+          role="menuitem"
+          tabindex="-1"
+        >
+          Menu Item 2-2-2
+        </vaadin-context-menu-item>
+        <hr role="separator">
+        <vaadin-context-menu-item
+          aria-haspopup="false"
+          aria-selected="false"
+          class="last"
+          role="menuitem"
+          tabindex="-1"
+        >
+          Menu Item 2-2-3
+        </vaadin-context-menu-item>
+      </vaadin-context-menu-list-box>
+    </div>
+    <vaadin-context-menu
+      modeless=""
+      slot="sub-menu"
+    >
+    </vaadin-context-menu>
   </vaadin-context-menu>
-</vaadin-context-menu-overlay>
+</vaadin-context-menu>
 `;
 /* end snapshot context-menu items nested */
 
-snapshots["context-menu items overlay class"] = 
+snapshots["context-menu overlay class"] =
 `<vaadin-context-menu-overlay
   class="context-menu-overlay custom"
+  exportparts="backdrop, overlay, content"
   opened=""
+  popover="manual"
 >
-  <vaadin-context-menu-list-box
-    aria-orientation="vertical"
-    role="menu"
+  <slot name="overlay">
+  </slot>
+  <slot
+    name="sub-menu"
+    slot="sub-menu"
   >
-    <vaadin-context-menu-item
-      aria-haspopup="false"
-      aria-selected="false"
-      class="first"
-      focus-ring=""
-      focused=""
-      role="menuitem"
-      tabindex="0"
-    >
-      Menu Item 1
-    </vaadin-context-menu-item>
-    <hr role="separator">
-    <vaadin-context-menu-item
-      aria-expanded="false"
-      aria-haspopup="true"
-      aria-selected="false"
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 2
-    </vaadin-context-menu-item>
-    <vaadin-context-menu-item
-      aria-disabled="true"
-      aria-haspopup="false"
-      aria-selected="false"
-      disabled=""
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 3
-    </vaadin-context-menu-item>
-    <div
-      aria-haspopup="false"
-      class="custom"
-    >
-      Menu Item 4
-    </div>
-    <div
-      aria-haspopup="false"
-      class="last"
-    >
-      Menu Item 5
-    </div>
-  </vaadin-context-menu-list-box>
-  <vaadin-context-menu
-    hidden=""
-    modeless=""
-  >
-  </vaadin-context-menu>
+  </slot>
 </vaadin-context-menu-overlay>
 `;
-/* end snapshot context-menu items overlay class */
+/* end snapshot context-menu overlay class */
 
-snapshots["context-menu items overlay class nested"] = 
+snapshots["context-menu overlay class nested"] =
 `<vaadin-context-menu-overlay
   class="context-menu-overlay custom"
+  exportparts="backdrop, overlay, content"
   modeless=""
   opened=""
+  popover="manual"
   start-aligned=""
   top-aligned=""
 >
-  <vaadin-context-menu-list-box
-    aria-orientation="vertical"
-    role="menu"
+  <slot name="overlay">
+  </slot>
+  <slot
+    name="sub-menu"
+    slot="sub-menu"
   >
-    <vaadin-context-menu-item
-      aria-haspopup="false"
-      aria-selected="false"
-      class="first"
-      role="menuitem"
-      tabindex="0"
-    >
-      Menu Item 2-1
-    </vaadin-context-menu-item>
-    <vaadin-context-menu-item
-      aria-expanded="true"
-      aria-haspopup="true"
-      aria-selected="false"
-      class="last"
-      expanded=""
-      role="menuitem"
-      tabindex="-1"
-    >
-      Menu Item 2-2
-    </vaadin-context-menu-item>
-  </vaadin-context-menu-list-box>
-  <vaadin-context-menu
-    hidden=""
-    modeless=""
-    start-aligned=""
-    top-aligned=""
-  >
-  </vaadin-context-menu>
+  </slot>
 </vaadin-context-menu-overlay>
 `;
-/* end snapshot context-menu items overlay class nested */
-
+/* end snapshot context-menu overlay class nested */
