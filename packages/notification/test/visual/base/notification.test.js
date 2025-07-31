338f7fea2a36bcfcdbeaaa363bc26ae56c0febcb
Jouni Koivuviita <jouni@vaadin.com>
2025-07-31 11:58:55 +0300

    test: add base style visual tests

---
+++ packages/notification/test/visual/base/notification.test.js
@@ -0,0 +1,39 @@
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/button';
+import '../../not-animated-styles.js';
+import '../../../src/vaadin-notification.js';
+
+describe('notification', () => {
+  let element;
+
+  beforeEach(() => {
+    element = fixtureSync('<vaadin-notification duration="0"></vaadin-notification>');
+    element.renderer = (root) => {
+      root.textContent = element.position;
+    };
+  });
+
+  afterEach(() => {
+    element.opened = false;
+  });
+
+  [
+    'top-stretch',
+    'top-start',
+    'top-center',
+    'top-end',
+    'middle',
+    'bottom-start',
+    'bottom-center',
+    'bottom-end',
+    'bottom-stretch',
+  ].forEach((position) => {
+    it(position, async () => {
+      element.position = position;
+      element.opened = true;
+      await nextRender();
+      await visualDiff(document.body, `${position}`);
+    });
+  });
+});
