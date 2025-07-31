86cf69185698ea54a578dd07b5be3f9b43980988
Jouni Koivuviita <jouni@vaadin.com>
2025-07-31 11:25:15 +0300

    remove animations and transitions

--- packages/notification/src/styles/vaadin-notification-card-base-styles.js
+++ packages/notification/src/styles/vaadin-notification-card-base-styles.js
@@ -7,27 +7,6 @@ import '@vaadin/component-base/src/style-props.js';
 import { css } from 'lit';
 
 export const notificationCardStyles = css`
-  @keyframes enter {
-    0% {
-      translate: var(--_enter-translate, 0 100%);
-      margin-block: 0;
-      height: var(--_enter-height, auto);
-      max-height: var(--_enter-max-height, none);
-    }
-  }
-
-  @keyframes enter-overlay {
-    0% {
-      opacity: 0;
-    }
-  }
-
-  @keyframes exit-overlay {
-    100% {
-      opacity: 0;
-    }
-  }
-
   :host {
     display: block;
   }
@@ -35,30 +14,15 @@ export const notificationCardStyles = css`
   [part='overlay'] {
     pointer-events: auto;
     box-sizing: border-box;
-    width: 100%;
+    width: var(--vaadin-notification-width, 40ch);
+    max-width: 100%;
     padding: var(--vaadin-notification-padding, var(--vaadin-padding));
     background: var(--vaadin-notification-background, var(--vaadin-background-container));
     border: var(--vaadin-notification-border-width, 1px) solid
       var(--vaadin-notification-border-color, var(--vaadin-border-color));
-    box-shadow: var(--vaadin-notification-box-shadow, 0 8px 24px -4px rgba(0, 0, 0, 0.3));
+    box-shadow: var(--vaadin-notification-shadow, 0 8px 24px -4px rgba(0, 0, 0, 0.3));
     border-radius: var(--vaadin-notification-border-radius, var(--vaadin-radius-l));
     cursor: default;
-    animation: enter-overlay 400ms var(--vaadin-ease-fluid);
-    clip-path: var(--_clip-path);
-    opacity: var(--_opacity);
-    transition: 400ms var(--vaadin-ease-fluid);
-    transition-property: clip-path, opacity, box-shadow;
-    transition-duration: var(--_clip-path-duration, 400ms), 400ms, 400ms;
-  }
-
-  :host([closing]) [part='overlay'] {
-    animation: exit-overlay 400ms var(--vaadin-ease-fluid);
-  }
-
-  :host([slot^='top']) {
-    display: flex;
-    align-items: end;
-    --_enter-translate: 0 -100%;
   }
 
   @media (forced-colors: active) {
--- packages/notification/src/styles/vaadin-notification-container-base-styles.js
+++ packages/notification/src/styles/vaadin-notification-container-base-styles.js
@@ -8,23 +8,24 @@ import { css } from 'lit';
 
 export const notificationContainerStyles = css`
   :host {
-    /* How much space to reserve for overlay box shadow */
+    /* How much space to reserve for overlay box shadow, to prevent clipping it with overflow:auto */
     --_paint-area: 2em;
     /* Space between notifications and the viewport */
     --_padding: var(--vaadin-notification-viewport-inset, var(--vaadin-padding));
-    /* Space between notifications in a stack and how much collapsed cards show below the top most */
+    /* Space between notifications */
     --_gap: var(--vaadin-notification-gap, var(--vaadin-gap-container-block));
-    position: fixed;
-    z-index: 1000;
-    inset: 0;
-    box-sizing: border-box;
     display: grid;
     /* top-stretch, top and bottom regions, bottom-stretch */
     grid-template-rows: auto 1fr auto;
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    padding: var(--_padding);
+    border: 0;
+    background: transparent;
     pointer-events: none;
     interpolate-size: allow-keywords;
-    padding: var(--_padding);
-    overflow: hidden;
   }
 
   :host > * {
@@ -37,47 +38,44 @@ export const notificationContainerStyles = css`
   }
 
   [region] {
-    z-index: 1;
     max-width: 100%;
     max-height: 100%;
     pointer-events: auto;
     scrollbar-width: none;
   }
 
+  /* scrollbar-width is supported since Safari 18.2, use the following for earlier */
+  [region]::-webkit-scrollbar {
+    display: none;
+  }
+
   [region='top-stretch'] {
     grid-row: 1;
-    z-index: 3;
+    --vaadin-notification-width: 100%;
   }
 
   [region='bottom-stretch'] {
     grid-row: 3;
-    z-index: 3;
+    --vaadin-notification-width: 100%;
   }
 
   [region='middle'],
   [region-group] > [region] {
     position: absolute;
-    width: var(--vaadin-notification-width, 40ch);
   }
 
   [region='middle'] {
     position: fixed;
-    z-index: 4;
     top: 50%;
     left: 50%;
     translate: -50% -50%;
     max-width: calc(100% - var(--_padding) * 2);
   }
 
-  /* scrollbar-width is supported in Safari 18.2, use the following for earlier */
-  [region]::-webkit-scrollbar {
-    display: none;
-  }
-
   [region]:where(:hover, :focus-within) {
+    z-index: 1;
     overflow: auto;
     overscroll-behavior: contain;
-    z-index: 2;
     padding: var(--_paint-area);
   }
 
@@ -109,83 +107,10 @@ export const notificationContainerStyles = css`
 
   ::slotted(*) {
     margin-bottom: var(--_gap);
-    transition: 400ms var(--vaadin-ease-fluid);
-    position: relative;
-    z-index: calc(var(--order) * -1);
-    --_inset: calc(var(--_paint-area) * -1);
-    --_clip-path: inset(var(--_inset));
   }
 
   :is([region^='bottom'], [region='middle']) ::slotted(*) {
     margin-top: var(--_gap);
     margin-bottom: 0;
-  }
-
-  @keyframes enter {
-    0% {
-      translate: var(--_enter-translate, 0 100%);
-      margin-block: 0;
-      height: var(--_enter-height, auto);
-      max-height: var(--_enter-max-height, none);
-    }
-  }
-
-  ::slotted(:is([opening], [closing])) {
-    animation: enter 400ms var(--vaadin-ease-fluid);
-    z-index: 9999;
-  }
-
-  ::slotted([closing]) {
-    animation-direction: reverse;
-  }
-
-  [region]:not(:hover, :focus-within) ::slotted(:not([style*='order: 1;'])) {
-    --_inset-exclude: calc(100% - var(--_gap) - 2px);
-    --_clip-path: inset(var(--_inset-exclude) var(--_inset) var(--_inset) var(--_inset));
-  }
-
-  :is([region^='bottom'], [region='middle']):not(:hover, :focus-within) ::slotted(:not([style*='order: 1;'])) {
-    --_clip-path: inset(var(--_inset) var(--_inset) var(--_inset-exclude) var(--_inset));
-    --vaadin-notification-box-shadow: none;
-  }
-
-  @supports (interpolate-size: allow-keywords) {
-    ::slotted(*) {
-      --_enter-height: 0;
-    }
-
-    [region]:not(:hover, :focus-within) ::slotted(:not([style*='order: 1;'])) {
-      height: 0;
-      --_clip-path-duration: 450ms;
-    }
-  }
-
-  @supports not (interpolate-size: allow-keywords) {
-    ::slotted(*) {
-      max-height: 25em;
-      --_enter-max-height: 0;
-      --_clip-path-duration: 250ms;
-    }
-
-    [region]:not(:hover, :focus-within) ::slotted(:not([style*='order: 1;'])) {
-      --_clip-path-duration: 500ms;
-      max-height: 0;
-    }
-  }
-
-  [region]:not(:hover, :focus-within) {
-    ::slotted([style*='order: 2;']) {
-      scale: 0.94;
-    }
-
-    ::slotted([style*='order: 3;']) {
-      scale: 0.86;
-    }
-
-    ::slotted(:not([style*='order: 1;'], [style*='order: 2;'], [style*='order: 3;'])) {
-      scale: 0.78;
-      --_opacity: 0;
-      margin-block: 0;
-    }
   }
 `;
