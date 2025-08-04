6c8d631af9ee591bec85775a6c5c9db9a9e40c85
Jouni Koivuviita <jouni@vaadin.com>
2025-08-04 13:30:01 +0300

    field outline base styles

--- packages/field-highlighter/src/styles/vaadin-field-outline-base-styles.js
+++ packages/field-highlighter/src/styles/vaadin-field-outline-base-styles.js
@@ -3,20 +3,20 @@
  * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
+import '@vaadin/component-base/src/user-colors.js';
 import { css } from 'lit';
 
 export const fieldOutlineStyles = css`
   :host {
     display: block;
-    box-sizing: border-box;
     position: absolute;
     inset: 0;
-    width: 100%;
-    height: 100%;
     pointer-events: none;
-    user-select: none;
     opacity: 0;
     --_active-user-color: transparent;
+    outline: 3px solid var(--_active-user-color);
+    /* TODO doesn't inherit correctly from vaadin-input-container for some reason, so we use the internal _radius property */
+    border-radius: var(--_radius, inherit);
   }
 
   :host([has-active-user]) {
