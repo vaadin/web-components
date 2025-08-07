2f40376e2fcf1b14a454daa06a25d874485cf618
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-08-07 10:45:47 +0200

    remove overlay class mixin from login-overlay

--- packages/login/test/dom/__snapshots__/login-overlay.test.snap.js
+++ packages/login/test/dom/__snapshots__/login-overlay.test.snap.js
@@ -1,7 +1,7 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["vaadin-login-overlay host default"] = 
+snapshots["vaadin-login-overlay host default"] =
 `<vaadin-login-overlay-wrapper
   exportparts="backdrop, overlay, content, card, brand, description, form-wrapper"
   focus-trap=""
@@ -61,7 +61,7 @@ snapshots["vaadin-login-overlay host default"] =
 `;
 /* end snapshot vaadin-login-overlay host default */
 
-snapshots["vaadin-login-overlay host i18n"] = 
+snapshots["vaadin-login-overlay host i18n"] =
 `<vaadin-login-overlay-wrapper
   exportparts="backdrop, overlay, content, card, brand, description, form-wrapper"
   focus-trap=""
@@ -121,7 +121,7 @@ snapshots["vaadin-login-overlay host i18n"] =
 `;
 /* end snapshot vaadin-login-overlay host i18n */
 
-snapshots["vaadin-login-overlay host i18n-partial"] = 
+snapshots["vaadin-login-overlay host i18n-partial"] =
 `<vaadin-login-overlay-wrapper
   exportparts="backdrop, overlay, content, card, brand, description, form-wrapper"
   focus-trap=""
@@ -181,68 +181,7 @@ snapshots["vaadin-login-overlay host i18n-partial"] =
 `;
 /* end snapshot vaadin-login-overlay host i18n-partial */
 
-snapshots["vaadin-login-overlay host overlay class"] = 
-`<vaadin-login-overlay-wrapper
-  class="custom login-overlay"
-  exportparts="backdrop, overlay, content, card, brand, description, form-wrapper"
-  focus-trap=""
-  id="overlay"
-  opened=""
-  popover="manual"
-  with-backdrop=""
->
-  <slot
-    name="title"
-    slot="title"
-  >
-  </slot>
-  <vaadin-login-form-wrapper
-    aria-labelledby="title"
-    exportparts="error-message, error-message-title, error-message-description, footer"
-    id="form"
-    part="form"
-    role="region"
-  >
-    <div
-      aria-level="2"
-      id="title"
-      part="form-title"
-      role="heading"
-      slot="form-title"
-    >
-      Log in
-    </div>
-    <slot
-      name="form"
-      slot="form"
-    >
-    </slot>
-    <slot
-      name="custom-form-area"
-      slot="custom-form-area"
-    >
-    </slot>
-    <slot
-      name="submit"
-      slot="submit"
-    >
-    </slot>
-    <slot
-      name="forgot-password"
-      slot="forgot-password"
-    >
-    </slot>
-    <slot
-      name="footer"
-      slot="footer"
-    >
-    </slot>
-  </vaadin-login-form-wrapper>
-</vaadin-login-overlay-wrapper>
-`;
-/* end snapshot vaadin-login-overlay host overlay class */
-
-snapshots["vaadin-login-overlay shadow default"] = 
+snapshots["vaadin-login-overlay shadow default"] =
 `<div
   id="backdrop"
   part="backdrop"
@@ -275,7 +214,7 @@ snapshots["vaadin-login-overlay shadow default"] =
 `;
 /* end snapshot vaadin-login-overlay shadow default */
 
-snapshots["vaadin-login-overlay shadow i18n"] = 
+snapshots["vaadin-login-overlay shadow i18n"] =
 `<div
   id="backdrop"
   part="backdrop"
