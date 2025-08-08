2614ebbff3431edad585b9c8af21ccda51050b0e
web-padawan <iamkulykov@gmail.com>
2025-08-08 16:04:11 +0300

    test: remove no longer needed multi-select-combo-box styles

--- packages/multi-select-combo-box/test/chips.test.js
+++ packages/multi-select-combo-box/test/chips.test.js
@@ -1,7 +1,6 @@
 import { expect } from '@vaadin/chai-plugins';
 import { sendKeys } from '@vaadin/test-runner-commands';
 import { fixtureSync, nextRender, nextResize, nextUpdate } from '@vaadin/testing-helpers';
-import './multi-select-combo-box-test-styles.js';
 import '../src/vaadin-multi-select-combo-box.js';
 
 describe('chips', () => {
@@ -12,7 +11,7 @@ describe('chips', () => {
   const getChipContent = (chip) => chip.shadowRoot.querySelector('[part="label"]').textContent;
 
   beforeEach(async () => {
-    comboBox = fixtureSync(`<vaadin-multi-select-combo-box></vaadin-multi-select-combo-box>`);
+    comboBox = fixtureSync(`<vaadin-multi-select-combo-box style="line-height: 1.25"></vaadin-multi-select-combo-box>`);
     comboBox.items = ['apple', 'banana', 'lemon', 'orange'];
     comboBox.selectedItems = ['orange'];
     await nextRender();
@@ -181,7 +180,7 @@ describe('chips', () => {
       });
 
       it('should update overflow chip on clear button state change', async () => {
-        comboBox.style.width = '370px';
+        comboBox.style.width = '335px';
         await nextResize(comboBox);
 
         comboBox.clearButtonVisible = true;
