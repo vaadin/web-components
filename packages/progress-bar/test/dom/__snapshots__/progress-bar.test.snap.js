51c665cc70b678599bfeab35f3676e295e026413
web-padawan <iamkulykov@gmail.com>
2025-08-08 12:49:37 +0300

    test: add missing snapshot tests to progress-bar

---
+++ packages/progress-bar/test/dom/__snapshots__/progress-bar.test.snap.js
@@ -0,0 +1,22 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-progress-bar host"] = 
+`<vaadin-progress-bar
+  aria-valuemax="1"
+  aria-valuemin="0"
+  role="progressbar"
+  style="--vaadin-progress-value: 0;"
+>
+</vaadin-progress-bar>
+`;
+/* end snapshot vaadin-progress-bar host */
+
+snapshots["vaadin-progress-bar shadow"] = 
+`<div part="bar">
+  <div part="value">
+  </div>
+</div>
+`;
+/* end snapshot vaadin-progress-bar shadow */
+
