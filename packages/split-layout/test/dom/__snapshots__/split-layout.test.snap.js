ebfc656f15293e53c02db4ce6265abe05f723d18
Serhii Kulykov <iamkulykov@gmail.com>
2025-08-08 16:24:38 +0300

    test: add missing snapshot tests to split-layout (#9945)

---
+++ packages/split-layout/test/dom/__snapshots__/split-layout.test.snap.js
@@ -0,0 +1,31 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-split-layout host"] = 
+`<vaadin-split-layout orientation="horizontal">
+</vaadin-split-layout>
+`;
+/* end snapshot vaadin-split-layout host */
+
+snapshots["vaadin-split-layout shadow"] = 
+`<slot
+  id="primary"
+  name="primary"
+>
+</slot>
+<div
+  id="splitter"
+  part="splitter"
+  style="touch-action: none;"
+>
+  <div part="handle">
+  </div>
+</div>
+<slot
+  id="secondary"
+  name="secondary"
+>
+</slot>
+`;
+/* end snapshot vaadin-split-layout shadow */
+
