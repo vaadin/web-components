95b9e5037ac18dccae3b214a5e56914c4bc3edc8
Sascha Ißbrücker <sissbruecker@vaadin.com>
2025-08-15 09:31:56 +0200

    refactor: deprecate renderers in Popover (vaadin/react-components#349)
    * refactor: deprecate renderers in Popover
    
    * fix import

--- packages/react-components/src/Popover.tsx
+++ packages/react-components/src/Popover.tsx
@@ -9,6 +9,7 @@ import {
 import { Popover as _Popover, type PopoverElement, type PopoverProps as _PopoverProps } from './generated/Popover.js';
 import { useSimpleOrChildrenRenderer } from './renderers/useSimpleOrChildrenRenderer.js';
 import type { ReactSimpleRendererProps } from './renderers/useSimpleRenderer.js';
+import { issueWarning } from './utils/warnings.js';
 
 export * from './generated/Popover.js';
 
@@ -22,10 +23,22 @@ type OmittedPopoverHTMLAttributes = Omit<
 export type PopoverProps = Partial<Omit<_PopoverProps, 'children' | 'renderer' | keyof OmittedPopoverHTMLAttributes>> &
   Readonly<{
     children?: ReactNode | ComponentType<PopoverReactRendererProps>;
+    /**
+     * @deprecated Pass children directly to the popover instead
+     */
     renderer?: ComponentType<PopoverReactRendererProps> | null;
   }>;
 
 function Popover({ children, ...props }: PopoverProps, ref: ForwardedRef<PopoverElement>): ReactElement | null {
+  if (props.renderer) {
+    issueWarning('<Popover>: Using the renderer prop is deprecated. Pass children directly to the component instead.');
+  }
+  if (typeof children === 'function') {
+    issueWarning(
+      '<Popover>: Passing a component type as children is deprecated. Pass children directly to the component instead.',
+    );
+  }
+
   const [portals, renderer] = useSimpleOrChildrenRenderer(props.renderer, children);
 
   return (
