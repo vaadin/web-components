86e536a146d22928f29c87f479f35883c733b217
Herberts Markuns <herberts@vaadin.com>
2025-08-15 13:35:37 +0300

    feat: Tooltip support for vaadin-side-nav-item

--- packages/side-nav/test/dom/side-nav.test.js
+++ packages/side-nav/test/dom/side-nav.test.js
@@ -10,8 +10,8 @@ describe('vaadin-side-nav', () => {
       `
         <vaadin-side-nav>
           <span slot="label">Main menu</span>
-          <vaadin-side-nav-item>Item 1</vaadin-side-nav-item>
-          <vaadin-side-nav-item>Item 2</vaadin-side-nav-item>
+          <vaadin-side-nav-item><span>Item 1</span></vaadin-side-nav-item>
+          <vaadin-side-nav-item><span>Item 2</span></vaadin-side-nav-item>
         </vaadin-side-nav>
             `,
     );
