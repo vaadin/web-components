3b07c43dfceb6fd708a65f53aa6e543d766d5a73
Serhii Kulykov <iamkulykov@gmail.com>
2025-08-18 09:35:36 +0300

    chore: fix processing of font assets in dev pages builds (#10007)

--- dev/rollup.config.js
+++ dev/rollup.config.js
@@ -3,6 +3,7 @@ import terser from '@rollup/plugin-terser';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import postcss from 'postcss';
 import atImport from 'postcss-import';
+import url from 'postcss-url';
 import { appendStyles, generateListing } from '../wds-utils.js';
 
 export default {
@@ -11,6 +12,7 @@ export default {
   plugins: [
     nodeResolve(),
     html({
+      bundleAssetsFromCss: true, // Use Aura CSS fonts
       flattenOutput: false, // Preserve "charts" folder
       transformHtml: [
         (html) => appendStyles(html),
@@ -22,9 +24,12 @@ export default {
       transformAsset: [
         async (content, filePath) => {
           if (filePath.endsWith('.css')) {
-            const result = await postcss().use(atImport()).process(content, {
-              from: filePath,
-            });
+            const result = await postcss()
+              .use(atImport())
+              .use(url({ url: 'rebase' }))
+              .process(content, {
+                from: filePath,
+              });
             return result.css;
           }
         },
