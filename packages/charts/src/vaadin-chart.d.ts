fcddd004568e9b7027c5bbf9ae439be363516a93
Serhii Kulykov <iamkulykov@gmail.com>
2025-08-21 21:18:55 +0300

    docs: fix typos in charts axis events JSDoc (#10052)

--- packages/charts/src/vaadin-chart.d.ts
+++ packages/charts/src/vaadin-chart.d.ts
@@ -168,8 +168,8 @@ export * from './vaadin-chart-mixin.js';
  * @fires {CustomEvent} point-drag-start - Fired when starting to drag a point.
  * @fires {CustomEvent} point-drop - Fired when the point is dropped.
  * @fires {CustomEvent} point-drag - Fired while dragging a point.
- * @fires {CustomEvent} xaxes-extremes-set - Fired when when the minimum and maximum is set for the X axis.
- * @fires {CustomEvent} yaxes-extremes-set - Fired when when the minimum and maximum is set for the Y axis.
+ * @fires {CustomEvent} xaxes-extremes-set - Fired when the minimum and maximum is set for the X axis.
+ * @fires {CustomEvent} yaxes-extremes-set - Fired when the minimum and maximum is set for the Y axis.
  */
 declare class Chart extends ChartMixin(ThemableMixin(ElementMixin(HTMLElement))) {
   addEventListener<K extends keyof ChartEventMap>(
