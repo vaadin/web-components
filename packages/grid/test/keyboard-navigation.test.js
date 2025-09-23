f456ec3ecd85430b8f15f9300bb3741c719c56d7
web-padawan <iamkulykov@gmail.com>
2025-09-23 11:27:06 +0300

    test: revert change to keyboard navigation test

--- packages/grid/test/keyboard-navigation.test.js
+++ packages/grid/test/keyboard-navigation.test.js
@@ -1841,7 +1841,7 @@ describe('keyboard navigation on column groups', () => {
     pageDown();
 
     const firstVisibleIndex = getFirstVisibleItem(grid).index;
-    expect(firstVisibleIndex).to.be.above(4);
+    expect(firstVisibleIndex).to.be.above(5);
 
     // Tab to header
     tabToHeader();
