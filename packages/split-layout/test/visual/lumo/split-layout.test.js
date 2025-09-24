2fdf4cb9fd4165d43880fd135e3279656e1bccbc
web-padawan <iamkulykov@gmail.com>
2025-09-24 11:52:11 +0300

    fix: use position: absolute for the split-layout handle

--- packages/split-layout/test/visual/lumo/split-layout.test.js
+++ packages/split-layout/test/visual/lumo/split-layout.test.js
@@ -7,21 +7,43 @@ import '../../../vaadin-split-layout.js';
 describe('split-layout', () => {
   let element;
 
-  beforeEach(() => {
-    element = fixtureSync(`
-      <vaadin-split-layout style="width: 200px; height: 100px">
-        <div></div>
-        <div></div>
-      </vaadin-split-layout>
-    `);
+  describe('basic', () => {
+    beforeEach(() => {
+      element = fixtureSync(`
+        <vaadin-split-layout style="width: 200px; height: 100px">
+          <div></div>
+          <div></div>
+        </vaadin-split-layout>
+      `);
+    });
+
+    it('horizontal', async () => {
+      await visualDiff(element, 'horizontal');
+    });
+
+    it('vertical', async () => {
+      element.orientation = 'vertical';
+      await visualDiff(element, 'vertical');
+    });
   });
 
-  it('horizontal', async () => {
-    await visualDiff(element, 'horizontal');
-  });
+  describe('nested', () => {
+    beforeEach(() => {
+      element = fixtureSync(`
+        <vaadin-split-layout orientation="vertical" style="height: 500px">
+          <vaadin-split-layout slot="primary" orientation="horizontal">
+            <span slot="primary">Foo</span>
+            <span slot="secondary">Bar</span>
+          </vaadin-split-layout>>
+          <div slot="secondary">
+            Baz
+          </div>
+        </vaadin-split-layout>
+      `);
+    });
 
-  it('vertical', async () => {
-    element.orientation = 'vertical';
-    await visualDiff(element, 'vertical');
+    it('horizontal', async () => {
+      await visualDiff(element, 'nested');
+    });
   });
 });
