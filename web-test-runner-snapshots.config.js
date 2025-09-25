7a0d6b1962b4ab4d6c6efaa26f48774c46fff296
web-padawan <iamkulykov@gmail.com>
2025-09-25 10:59:08 +0300

    chore: use Playwright also for snapshot tests

--- web-test-runner-snapshots.config.js
+++ web-test-runner-snapshots.config.js
@@ -1,4 +1,15 @@
 /* eslint-env node */
+const { playwrightLauncher } = require('@web/test-runner-playwright');
 const { createSnapshotTestsConfig } = require('./wtr-utils.js');
 
-module.exports = createSnapshotTestsConfig();
+module.exports = createSnapshotTestsConfig({
+  browsers: [
+    playwrightLauncher({
+      product: 'chromium',
+      launchOptions: {
+        channel: 'chrome',
+        headless: true,
+      },
+    }),
+  ],
+});
