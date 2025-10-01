5cae25d3589407e6f28cdb089d5a231c6482c211
Jouni Koivuviita <jouni@vaadin.com>
2025-10-01 11:04:19 +0300

    refactor: add default border radius to upload-file base styles (#10271)

--- packages/upload/src/styles/vaadin-upload-file-base-styles.js
+++ packages/upload/src/styles/vaadin-upload-file-base-styles.js
@@ -13,6 +13,7 @@ export const uploadFileStyles = css`
     gap: var(--vaadin-upload-file-gap, var(--vaadin-gap-s));
     grid-template-columns: var(--vaadin-icon-size, 1lh) minmax(0, 1fr) auto;
     padding: var(--vaadin-upload-file-padding, var(--vaadin-padding-s));
+    border-radius: var(--vaadin-upload-file-border-radius, var(--vaadin-radius-m));
   }
 
   :host(:focus-visible) {
