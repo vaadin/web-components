d74bcb0cf5238cc99f2f28a477847e2c1ff8c8bb
Jouni Koivuviita <jouni@vaadin.com>
2025-10-02 16:43:28 +0300

    fix: grid cell focus outline
    - hide native focus outline
    - don't show pseudo element focus outline when reordering columns

--- packages/grid/src/styles/vaadin-grid-base-styles.js
+++ packages/grid/src/styles/vaadin-grid-base-styles.js
@@ -431,6 +431,7 @@ export const gridStyles = css`
     z-index: 1;
     -webkit-backdrop-filter: var(--_reorder-curtain-filter);
     backdrop-filter: var(--_reorder-curtain-filter);
+    outline: 0;
   }
 
   :host([reordering]) [part~='cell'][reorder-status='allowed'] {
@@ -555,6 +556,11 @@ export const gridStyles = css`
     transform: translateX(var(--_grid-horizontal-scroll-position));
     inset-inline: 0;
     bottom: 0;
+  }
+
+  [part~='row']:focus-visible,
+  [part~='cell']:focus-visible {
+    outline: 0;
   }
 
   [part~='row']:focus-visible::after,
