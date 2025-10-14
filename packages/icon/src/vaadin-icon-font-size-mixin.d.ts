a58c308c133da5ab49c18def0b73bd140981a464
web-padawan <iamkulykov@gmail.com>
2025-10-14 14:03:34 +0300

    chore: align .d.ts comment

--- packages/icon/src/vaadin-icon-font-size-mixin.d.ts
+++ packages/icon/src/vaadin-icon-font-size-mixin.d.ts
@@ -7,6 +7,9 @@ import type { Constructor } from '@open-wc/dedupe-mixin';
 
 /**
  * Mixin which enables the font icon sizing fallback for browsers that do not support CSS Container Queries.
+ * In older versions of Safari, it didn't support Container Queries units used in pseudo-elements. It has been fixed in
+ * recent versions, but there's an regression in Safari 26, which caused the same issue to happen when the icon is
+ * attached to an element with shadow root.
  * The mixin does nothing if the browser supports CSS Container Query units for pseudo elements.
  */
 export declare function IconFontSizeMixin<T extends Constructor<HTMLElement>>(
