8d0f368217b91d7d9805efd48246cf55249a6e87
web-padawan <iamkulykov@gmail.com>
2025-10-14 14:04:53 +0300

    chore: align copyright year

--- packages/icon/src/vaadin-icon-helpers.js
+++ packages/icon/src/vaadin-icon-helpers.js
@@ -1,9 +1,8 @@
 /**
  * @license
- * Copyright (c) 2016 - 2025 Vaadin Ltd.
+ * Copyright (c) 2021 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-
 import { isSafari } from '@vaadin/component-base/src/browser-utils.js';
 
 /**
