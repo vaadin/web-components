1a10749844496ab9ff4a8a747519941aa1a8b3c8
Ugur Saglam <106508695+ugur-vaadin@users.noreply.github.com>
2025-10-21 11:32:19 +0300

    fix: do not dispatch grid-drop event on dropping row to input (#10351)
    * fix: do not allow drop on inputs in headers and footers
    
    * fix: let text be pasted when there is a drop mode set
    
    * refactor: do not prevent drop on inputs in header
    
    * test: revert unnecessary line

--- packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
+++ packages/grid/src/vaadin-grid-drag-and-drop-mixin.js
@@ -413,7 +413,7 @@ export const DragAndDropMixin = (superClass) =>
 
     /** @private */
     _onDrop(e) {
-      if (this.dropMode) {
+      if (this.dropMode && this._dropLocation) {
         e.stopPropagation();
         e.preventDefault();
 
