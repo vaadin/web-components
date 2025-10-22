ab88f6022d8413b34ca0179f92ecb6da642e4663
Jouni Koivuviita <jouni@vaadin.com>
2025-10-22 14:46:19 +0200

    Update user-colors.js

--- packages/component-base/src/styles/user-colors.js
+++ packages/component-base/src/styles/user-colors.js
@@ -13,42 +13,38 @@ addGlobalThemeStyles(
       :where(html) {
         --_color-count: 10;
         --_hue-step: round(360 / var(--_color-count), 1);
-        --vaadin-user-color-0: var(--vaadin-user-color, #4172d5);
+        --vaadin-user-color-0: var(--vaadin-user-color, oklch(0.52 0.2 240));
         --vaadin-user-color-1: oklch(
-          from var(--vaadin-user-color-0) min(0.8, l - 0.1) c
-            calc(h + var(--_hue-step) * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) calc(0.62 + clamp(-0.15, (0.6201 - l) * 10000, 0.15)) c
+            calc(h - var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-2: oklch(
-          from var(--vaadin-user-color-0) max(0.2, l + 0.1) c
-            calc(h + var(--_hue-step) * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) l c calc(h - var(--_hue-step) * -2 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-3: oklch(
-          from var(--vaadin-user-color-0) min(0.8, l - 0.1) c
-            calc(h - var(--_hue-step) * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) calc(0.62 + clamp(-0.15, (0.6201 - l) * 10000, 0.15)) c
+            calc(h - var(--_hue-step) * 0 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-4: oklch(
-          from var(--vaadin-user-color-0) max(0.2, l + 0.1) c
-            calc(h - var(--_hue-step) * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) l c calc(h - var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-5: oklch(
-          from var(--vaadin-user-color-0) min(0.8, l - 0.1) c
-            calc(h + var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) calc(0.62 + clamp(-0.15, (0.6201 - l) * 10000, 0.15)) c
+            calc(h - var(--_hue-step) * -2 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-6: oklch(
-          from var(--vaadin-user-color-0) max(0.2, l + 0.1) c
-            calc(h + var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) l c calc(h - var(--_hue-step) * -4 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-7: oklch(
-          from var(--vaadin-user-color-0) min(0.8, l - 0.1) c
-            calc(h - var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) calc(0.62 + clamp(-0.15, (0.6201 - l) * 10000, 0.15)) c
+            calc(h - var(--_hue-step) * 4 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-8: oklch(
-          from var(--vaadin-user-color-0) max(0.2, l + 0.1) c
-            calc(h - var(--_hue-step) * 2 * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) l c calc(h - var(--_hue-step) * 4 * var(---_vaadin-safari-17-deg, 1))
         );
         --vaadin-user-color-9: oklch(
-          from var(--vaadin-user-color-0) min(0.8, l - 0.1) c
-            calc(h + var(--_hue-step) * 3 * var(---_vaadin-safari-17-deg, 1))
+          from var(--vaadin-user-color-0) calc(0.62 + clamp(-0.15, (0.6201 - l) * 10000, 0.15)) c
+            calc(h - var(--_hue-step) * 6 * var(---_vaadin-safari-17-deg, 1))
         );
       }
 
