57e9bcb053197b241d55dd8b81d53f6c07df888f
web-padawan <iamkulykov@gmail.com>
2025-11-03 16:43:09 +0200

    refactor!: remove rich-text-editor base styles custom properties

--- packages/rich-text-editor/src/styles/vaadin-rich-text-editor-popup-overlay-base-styles.js
+++ packages/rich-text-editor/src/styles/vaadin-rich-text-editor-popup-overlay-base-styles.js
@@ -27,10 +27,10 @@ export const richTextEditorPopupOverlay = css`
       var(--vaadin-rich-text-editor-overlay-color-option-border-color, transparent);
     border-radius: var(--vaadin-rich-text-editor-overlay-color-option-border-radius, 9999px);
     cursor: var(--vaadin-clickable-cursor);
-    font-size: var(--vaadin-rich-text-editor-overlay-color-option-font-size, inherit);
+    font-size: inherit;
     height: var(--vaadin-rich-text-editor-overlay-color-option-height, 1lh);
-    line-height: var(--vaadin-rich-text-editor-overlay-color-option-line-height, inherit);
-    padding: var(--vaadin-rich-text-editor-overlay-color-option-padding, 0);
+    line-height: inherit;
+    padding: 0;
     width: var(--vaadin-rich-text-editor-overlay-color-option-width, 1lh);
   }
 
