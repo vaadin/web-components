19c6b6ef0ddcc65af7cf2104e785fa2379e9b06e
Jouni Koivuviita <jouni@vaadin.com>
2025-11-06 15:51:57 +0200

    remove empty custom-field style declaration

--- packages/custom-field/src/styles/vaadin-custom-field-base-styles.js
+++ packages/custom-field/src/styles/vaadin-custom-field-base-styles.js
@@ -3,9 +3,6 @@
  * Copyright (c) 2019 - 2025 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { css } from 'lit';
 import { field } from '@vaadin/field-base/src/styles/field-base-styles.js';
 
-const customField = css``;
-
-export const customFieldStyles = [field, customField];
+export const customFieldStyles = [field];
