8c132ded717a32f66627c8b4bce6145f0f1200ec
Sergey Vinogradov <mr.vursen@gmail.com>
2025-11-10 15:09:03 +0400

    test: add more visual tests for grid focus outline

--- packages/grid/test/visual/grid-focus.common.js
+++ packages/grid/test/visual/grid-focus.common.js
@@ -4,11 +4,55 @@ import { visualDiff } from '@web/test-runner-visual-regression';
 import { flushGrid } from '../helpers.js';
 import { users } from './users.js';
 
+export function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 describe('grid focus', () => {
   let element;
 
+  beforeEach(async () => {
+    element = fixtureSync(`
+      <vaadin-grid style="width: 500px; height: 300px;">
+        <vaadin-grid-column-group>
+          <vaadin-grid-column data-name="First" width="200px"></vaadin-grid-column>
+          <vaadin-grid-column data-name="Last" width="200px"></vaadin-grid-column>
+        </vaadin-grid-column-group>
+        <vaadin-grid-column data-name="City" width="200px"></vaadin-grid-column>
+      </vaadin-grid>
+      <style>
+        vaadin-grid {
+          --vaadin-grid-row-border-width: 4px;
+          --vaadin-grid-column-border-width: 4px;
+        }
+      </style>
+    `);
+    element.items = users.slice(0, 10);
+
+    [...element.querySelectorAll('vaadin-grid-column, vaadin-grid-column-group')].forEach((column) => {
+      column.headerRenderer = (root) => {
+        root.textContent = capitalize(column.dataset.name ?? 'Header');
+      };
+      column.renderer = (root, column, { item }) => {
+        root.textContent = {
+          First: item.name.first,
+          Last: item.name.last,
+          City: item.location.city,
+        }[column.dataset.name];
+      };
+      column.footerRenderer = (root) => {
+        root.textContent = capitalize(column.dataset.name ?? 'Footer');
+      };
+    });
+
+    flushGrid(element);
+    await nextRender();
+  });
+
   ['ltr', 'rtl'].forEach((dir) => {
     describe(dir, () => {
+      const isRTL = dir === 'rtl';
+
       before(() => {
         document.documentElement.setAttribute('dir', dir);
       });
@@ -16,45 +60,178 @@ describe('grid focus', () => {
       after(() => {
         document.documentElement.removeAttribute('dir');
       });
+
+      describe('first header row', () => {
+        beforeEach(async () => {
+          // Focus first header row
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: isRTL ? 'ArrowRight' : 'ArrowLeft' });
+        });
+
+        it('default', async () => {
+          await visualDiff(element, `${dir}-first-header-row`);
+        });
+
+        it('first cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await visualDiff(element, `${dir}-first-header-row-first-cell`);
+        });
+
+        it('last cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await sendKeys({ press: 'End' });
+          await visualDiff(element, `${dir}-first-header-row-last-cell`);
+        });
+      });
+
+      describe('last header row', () => {
+        beforeEach(async () => {
+          // Focus last header row
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: isRTL ? 'ArrowRight' : 'ArrowLeft' });
+          await sendKeys({ press: 'ArrowDown' });
+        });
+
+        it('default', async () => {
+          await visualDiff(element, `${dir}-last-header-row`);
+        });
+
+        it('first cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await visualDiff(element, `${dir}-last-header-row-first-cell`);
+        });
+
+        it('last cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await sendKeys({ press: 'End' });
+          await visualDiff(element, `${dir}-last-header-row-last-cell`);
+        });
+      });
+
+      describe('first body row', () => {
+        beforeEach(async () => {
+          // Focus first body row
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: isRTL ? 'ArrowRight' : 'ArrowLeft' });
+        });
+
+        it('default', async () => {
+          await visualDiff(element, `${dir}-first-body-row`);
+        });
+
+        it('first cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await visualDiff(element, `${dir}-first-body-row-first-cell`);
+        });
+
+        it('last cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await sendKeys({ press: 'End' });
+          await visualDiff(element, `${dir}-first-body-row-last-cell`);
+        });
+      });
+
+      describe('last body row', () => {
+        beforeEach(async () => {
+          // Focus last body row
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: 'Tab' });
+          await sendKeys({ press: isRTL ? 'ArrowRight' : 'ArrowLeft' });
+          await sendKeys({ press: 'End' });
+        });
+
+        it('default', async () => {
+          await visualDiff(element, `${dir}-last-body-row`);
+        });
+
+        it('first cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await visualDiff(element, `${dir}-last-body-row-first-cell`);
+        });
+
+        it('last cell', async () => {
+          await sendKeys({ press: isRTL ? 'ArrowLeft' : 'ArrowRight' });
+          await sendKeys({ press: 'End' });
+          await visualDiff(element, `${dir}-last-body-row-last-cell`);
+        });
+      });
     });
 
-    beforeEach(async () => {
-      element = fixtureSync(`
-        <vaadin-grid style="width: 550px">
-          <vaadin-grid-column-group header="Name" frozen>
-            <vaadin-grid-column path="name.first" width="200px" flex-shrink="0"></vaadin-grid-column>
-            <vaadin-grid-column path="name.last" width="200px" flex-shrink="0"></vaadin-grid-column>
-          </vaadin-grid-column-group>
-          <vaadin-grid-column path="location.city" width="200px" flex-shrink="0"></vaadin-grid-column>
-        </vaadin-grid>
-      `);
-      element.items = users;
-      flushGrid(element);
+    describe('without header', () => {
+      beforeEach(async () => {
+        [...element.querySelectorAll('vaadin-grid-column, vaadin-grid-column-group')].forEach((column) => {
+          column.headerRenderer = null;
+        });
+        await nextRender();
+      });
 
-      // Scroll all the way to end
-      element.$.table.scrollLeft = element.__isRTL ? -1000 : 1000;
+      it('first body row', async () => {
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'ArrowLeft' });
+        await visualDiff(element, `without-header-first-body-row`);
+      });
 
-      // Focus a header row
-      await sendKeys({ press: 'Tab' });
-      // Switch to row focus mode
-      await sendKeys({ press: element.__isRTL ? 'ArrowRight' : 'ArrowLeft' });
-      // Tab to body row
-      await sendKeys({ press: 'Tab' });
-
-      await nextRender();
+      it('first body row cell', async () => {
+        await sendKeys({ press: 'Tab' });
+        await visualDiff(element, `without-header-first-body-row-cell`);
+      });
     });
 
-    it('row focus', async () => {
-      await visualDiff(element, `${dir}-row-focus`);
+    describe('without footer', () => {
+      beforeEach(async () => {
+        [...element.querySelectorAll('vaadin-grid-column, vaadin-grid-column-group')].forEach((column) => {
+          column.footerRenderer = null;
+        });
+        await nextRender();
+      });
+
+      it('last body row', async () => {
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'ArrowLeft' });
+        await sendKeys({ press: 'End' });
+        await visualDiff(element, `without-footer-last-body-row`);
+      });
+
+      it('last body row cell', async () => {
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'ArrowLeft' });
+        await sendKeys({ press: 'End' });
+        await sendKeys({ press: 'ArrowRight' });
+        await visualDiff(element, `without-footer-last-body-row-cell`);
+      });
     });
 
-    it('row focus - header', async () => {
-      // Focus a header row
-      element.tabIndex = 0;
-      element.focus();
-      await sendKeys({ press: 'Tab' });
+    describe('scrolling', () => {
+      beforeEach(async () => {
+        // Focus first body row
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'Tab' });
+        await sendKeys({ press: 'ArrowLeft' });
+      });
 
-      await visualDiff(element, `${dir}-row-focus-header`);
+      it('overflow top', async () => {
+        // Scroll to end
+        await sendKeys({ press: 'End' });
+        // Scroll a few pages up to get first visible row focused
+        await sendKeys({ press: 'PageUp' });
+        await sendKeys({ press: 'PageUp' });
+        await visualDiff(element, `overflow-top`);
+      });
+
+      it('overflow bottom', async () => {
+        // Scroll a few pages down to get last visible row focused
+        await sendKeys({ press: 'PageDown' });
+        await sendKeys({ press: 'PageDown' });
+        await visualDiff(element, `overflow-bottom`);
+      });
+
+      it('overflow left', () => {
+        element.$.table.scrollLeft = element.$.table.scrollWidth;
+        return visualDiff(element, `overflow-left`);
+      });
     });
   });
 });
