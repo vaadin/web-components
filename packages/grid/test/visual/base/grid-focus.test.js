b0bb6b468690e1523396ebb9c2b65eaf4c518e18
Sergey Vinogradov <mr.vursen@gmail.com>
2025-11-10 12:46:02 +0400

    test: extract grid focus visual tests into separate file (#10461)

---
+++ packages/grid/test/visual/base/grid-focus.test.js
@@ -0,0 +1,3 @@
+import '../../../src/vaadin-grid.js';
+import '../../../src/vaadin-grid-column-group.js';
+import '../grid-focus.common.js';
---
+++ packages/grid/test/visual/lumo/grid-focus.test.js
@@ -0,0 +1,5 @@
+import '@vaadin/vaadin-lumo-styles/src/props/index.css';
+import '@vaadin/vaadin-lumo-styles/components/grid.css';
+import '../../../vaadin-grid.js';
+import '../../../vaadin-grid-column-group.js';
+import '../grid-focus.common.js';
