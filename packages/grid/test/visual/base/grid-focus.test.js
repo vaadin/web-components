0609567c0bf20e7ef79f5280be00d3ccc2f87471
Sergey Vinogradov <mr.vursen@gmail.com>
2025-11-10 12:29:59 +0400

    test: extract grid focus visual tests into separate file

---
+++ packages/grid/test/visual/base/grid-focus.test.js
@@ -0,0 +1,7 @@
+import '../../../src/vaadin-grid.js';
+import '../../../src/vaadin-grid-column-group.js';
+import '../../../src/vaadin-grid-selection-column.js';
+import '../../../src/vaadin-grid-sorter.js';
+import '../../../src/vaadin-grid-sort-column.js';
+import '../../../src/vaadin-grid-tree-column.js';
+import '../grid-focus.common.js';
