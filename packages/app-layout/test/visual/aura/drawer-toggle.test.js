140721e35be5f1f53bf32818ac5eebd56ebb7a06
web-padawan <iamkulykov@gmail.com>
2025-12-22 14:44:42 +0200

    test: add Aura visual tests for app-layout and drawer-toggle

---
+++ packages/app-layout/test/visual/aura/drawer-toggle.test.js
@@ -0,0 +1,34 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/aura/aura.css';
+import '../../../vaadin-drawer-toggle.js';
+
+describe('drawer-toggle', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.style.padding = '10px';
+    element = fixtureSync('<vaadin-drawer-toggle></vaadin-drawer-toggle>', div);
+  });
+
+  it('basic', async () => {
+    await visualDiff(div, 'basic');
+  });
+
+  it('active', async () => {
+    element.setAttribute('active', '');
+    await visualDiff(div, 'active');
+  });
+
+  it('primary', async () => {
+    element.setAttribute('theme', 'primary');
+    await visualDiff(div, 'primary');
+  });
+
+  it('tertiary', async () => {
+    element.setAttribute('theme', 'tertiary');
+    await visualDiff(div, 'tertiary');
+  });
+});
