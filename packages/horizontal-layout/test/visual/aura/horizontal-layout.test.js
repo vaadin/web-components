9330563f3a444ff81ecc0eb69da310e1ff52ff0c
web-padawan <iamkulykov@gmail.com>
2025-12-23 16:37:09 +0200

    test: add Aura visual tests for horizontal and vertical layout

---
+++ packages/horizontal-layout/test/visual/aura/horizontal-layout.test.js
@@ -0,0 +1,61 @@
+import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/aura/aura.css';
+import '../../../vaadin-horizontal-layout.js';
+
+describe('horizontal-layout', () => {
+  let div, element;
+
+  beforeEach(() => {
+    div = document.createElement('div');
+    div.style.display = 'flex';
+    div.style.padding = '2px';
+    element = fixtureSync(
+      `
+        <vaadin-horizontal-layout style="border: solid 1px var(--vaadin-border-color)">
+          <div class="aura-surface" style="padding: 20px; --aura-surface-level: -1">
+            Item 1
+          </div>
+          <div class="aura-surface-solid" style="padding: 20px; --aura-surface-level: -1">
+            Item 2
+          </div>
+        </vaadin-horizontal-layout>
+      `,
+      div,
+    );
+  });
+
+  it('basic', async () => {
+    await visualDiff(div, 'basic');
+  });
+
+  it('theme-margin', async () => {
+    element.setAttribute('theme', 'margin');
+    await visualDiff(div, 'theme-margin');
+  });
+
+  it('theme-padding', async () => {
+    element.setAttribute('theme', 'padding');
+    await visualDiff(div, 'theme-padding');
+  });
+
+  it('theme-spacing', async () => {
+    element.setAttribute('theme', 'spacing');
+    await visualDiff(div, 'theme-spacing');
+  });
+
+  it('theme-margin-padding', async () => {
+    element.setAttribute('theme', 'margin padding');
+    await visualDiff(div, 'theme-margin-padding');
+  });
+
+  it('theme-margin-spacing', async () => {
+    element.setAttribute('theme', 'margin spacing');
+    await visualDiff(div, 'theme-margin-spacing');
+  });
+
+  it('theme-margin-padding-spacing', async () => {
+    element.setAttribute('theme', 'margin padding spacing');
+    await visualDiff(div, 'theme-margin-padding-spacing');
+  });
+});
