fd5c5f39028306ba5c53c2e3986d1eb93b644acb
web-padawan <iamkulykov@gmail.com>
2026-01-02 10:24:43 +0200

    chore: update copyright year to 2026

--- packages/popover/src/lit/renderer-directives.js
+++ packages/popover/src/lit/renderer-directives.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2024 - 2025 Vaadin Ltd.
+ * Copyright (c) 2024 - 2026 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { directive } from 'lit/directive.js';
