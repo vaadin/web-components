d41da24179f2cb8874127e1f4c8a588d3e2ec16d
web-padawan <iamkulykov@gmail.com>
2026-01-05 17:11:06 +0200

    test: reuse text-field animation reset styles in visual tests

--- packages/password-field/test/visual/lumo/password-field.test.js
+++ packages/password-field/test/visual/lumo/password-field.test.js
@@ -1,9 +1,9 @@
 import { sendKeys } from '@vaadin/test-runner-commands';
 import { fixtureSync } from '@vaadin/testing-helpers/dist/fixture.js';
 import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/text-field/test/visual/common.js';
 import '@vaadin/vaadin-lumo-styles/src/props/index.css';
 import '@vaadin/vaadin-lumo-styles/components/password-field.css';
-import '../common.js';
 import '../../../vaadin-password-field.js';
 
 describe('password-field', () => {
