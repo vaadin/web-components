cf5f11ea542437fdd1d8b7ee96fff897936fb958
Serhii Kulykov <iamkulykov@gmail.com>
2026-01-07 08:13:33 +0200

    test: add Aura visual tests for notification (#10857)

---
+++ packages/notification/test/visual/aura/notification.test.js
@@ -0,0 +1,39 @@
+import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
+import { visualDiff } from '@web/test-runner-visual-regression';
+import '@vaadin/aura/aura.css';
+import '../../not-animated-styles.js';
+import '../../../vaadin-notification.js';
+
+describe('notification', () => {
+  let element;
+
+  beforeEach(() => {
+    element = fixtureSync('<vaadin-notification duration="0"></vaadin-notification>');
+    element.renderer = (root) => {
+      root.textContent = element.position;
+    };
+  });
+
+  afterEach(() => {
+    element.opened = false;
+  });
+
+  [
+    'top-stretch',
+    'top-start',
+    'top-center',
+    'top-end',
+    'middle',
+    'bottom-start',
+    'bottom-center',
+    'bottom-end',
+    'bottom-stretch',
+  ].forEach((position) => {
+    it(position, async () => {
+      element.position = position;
+      element.opened = true;
+      await nextRender();
+      await visualDiff(document.body, position);
+    });
+  });
+});
