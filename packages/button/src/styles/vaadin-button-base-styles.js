24323c3784e5e09701c96470c175246eaa851d8b
Jouni Koivuviita <jouni@vaadin.com>
2026-03-12 11:07:05 +0200

    fix: don't stretch buttons and single-line inputs in row flex container (#11311)

--- packages/button/src/styles/vaadin-button-base-styles.js
+++ packages/button/src/styles/vaadin-button-base-styles.js
@@ -20,7 +20,7 @@ export const buttonStyles = css`
     cursor: var(--vaadin-clickable-cursor);
     box-sizing: border-box;
     flex-shrink: 0;
-    height: var(--vaadin-button-height, auto);
+    height: var(--vaadin-button-height, fit-content);
     margin: var(--vaadin-button-margin, 0);
     padding: var(--vaadin-button-padding, var(--vaadin-padding-block-container) var(--vaadin-padding-inline-container));
     font-family: var(--vaadin-button-font-family, inherit);
