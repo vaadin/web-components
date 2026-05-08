adb5bb76c0b7221c2fc8b9e8573cff42b2aa2425
web-padawan <iamkulykov@gmail.com>
2026-05-08 16:02:23 +0300

    test: replace grid sorter unit tests with snapshots

---
+++ packages/grid/test/dom/__snapshots__/grid-sorter.test.snap.js
@@ -0,0 +1,46 @@
+/* @web/test-runner snapshot v1 */
+export const snapshots = {};
+
+snapshots["vaadin-grid-sorter host default"] = 
+`<vaadin-grid-sorter>
+</vaadin-grid-sorter>
+`;
+/* end snapshot vaadin-grid-sorter host default */
+
+snapshots["vaadin-grid-sorter host direction asc"] = 
+`<vaadin-grid-sorter direction="asc">
+</vaadin-grid-sorter>
+`;
+/* end snapshot vaadin-grid-sorter host direction asc */
+
+snapshots["vaadin-grid-sorter host direction desc"] = 
+`<vaadin-grid-sorter direction="desc">
+</vaadin-grid-sorter>
+`;
+/* end snapshot vaadin-grid-sorter host direction desc */
+
+snapshots["vaadin-grid-sorter shadow default"] = 
+`<div part="content">
+  <slot>
+  </slot>
+</div>
+<div part="indicators">
+  <span part="order">
+  </span>
+</div>
+`;
+/* end snapshot vaadin-grid-sorter shadow default */
+
+snapshots["vaadin-grid-sorter shadow order"] = 
+`<div part="content">
+  <slot>
+  </slot>
+</div>
+<div part="indicators">
+  <span part="order">
+    2
+  </span>
+</div>
+`;
+/* end snapshot vaadin-grid-sorter shadow order */
+
