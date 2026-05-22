a8a445984292c4aa7c7ee7bb9f1794b719733e03
Diego Cardoso <diego@vaadin.com>
2026-05-22 09:28:38 +0200

    fix: use CSS property to set highcharts label text color (#11791)

--- packages/charts/theme/vaadin-chart-base-theme.js
+++ packages/charts/theme/vaadin-chart-base-theme.js
@@ -824,6 +824,10 @@ const chartBaseTheme = css`
     fill: inherit;
   }
 
+  :where([styled-mode]) .highcharts-label text {
+    fill: var(--vaadin-charts-data-label, hsla(214, 42%, 18%, 0.72));
+  }
+
   :where([styled-mode]) .highcharts-candlestick-series .highcharts-point {
     stroke: var(--vaadin-charts-contrast-60pct, hsla(214, 43%, 19%, 0.61));
     stroke-width: 1px;
