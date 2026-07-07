124765fd56412082f3297a2e5979c8d9a128795c
web-padawan <iamkulykov@gmail.com>
2026-07-07 15:18:16 +0300

    test: extend snapshots to cover orientation

--- packages/split-layout/test/dom/__snapshots__/split-layout.test.snap.js
+++ packages/split-layout/test/dom/__snapshots__/split-layout.test.snap.js
@@ -1,13 +1,19 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["vaadin-split-layout host"] = 
+snapshots["vaadin-split-layout host default"] = 
 `<vaadin-split-layout orientation="horizontal">
 </vaadin-split-layout>
 `;
-/* end snapshot vaadin-split-layout host */
+/* end snapshot vaadin-split-layout host default */
 
-snapshots["vaadin-split-layout shadow"] = 
+snapshots["vaadin-split-layout host vertical"] = 
+`<vaadin-split-layout orientation="vertical">
+</vaadin-split-layout>
+`;
+/* end snapshot vaadin-split-layout host vertical */
+
+snapshots["vaadin-split-layout shadow default"] = 
 `<slot
   id="primary"
   name="primary"
@@ -33,9 +39,37 @@ snapshots["vaadin-split-layout shadow"] =
 >
 </slot>
 `;
-/* end snapshot vaadin-split-layout shadow */
+/* end snapshot vaadin-split-layout shadow default */
 
-snapshots["vaadin-split-layout i18n"] = 
+snapshots["vaadin-split-layout shadow vertical"] = 
+`<slot
+  id="primary"
+  name="primary"
+>
+</slot>
+<div
+  aria-label="Resize separator"
+  aria-orientation="vertical"
+  aria-valuemax="100"
+  aria-valuemin="0"
+  id="splitter"
+  part="splitter"
+  role="slider"
+  style="touch-action: none;"
+  tabindex="0"
+>
+  <div part="handle">
+  </div>
+</div>
+<slot
+  id="secondary"
+  name="secondary"
+>
+</slot>
+`;
+/* end snapshot vaadin-split-layout shadow vertical */
+
+snapshots["vaadin-split-layout shadow i18n"] = 
 `<slot
   id="primary"
   name="primary"
@@ -61,5 +95,5 @@ snapshots["vaadin-split-layout i18n"] =
 >
 </slot>
 `;
-/* end snapshot vaadin-split-layout i18n */
+/* end snapshot vaadin-split-layout shadow i18n */
 
