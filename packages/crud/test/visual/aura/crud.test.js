bf0d1afa4684c48a4f644bdef3bf7ed931f6744e
web-padawan <iamkulykov@gmail.com>
2026-07-29 09:06:47 +0300

    fix: use correct selector for crud dialog overlay in Aura

--- packages/crud/test/visual/aura/crud.test.js
+++ packages/crud/test/visual/aura/crud.test.js
@@ -1,6 +1,7 @@
 import { fixtureSync, nextRender } from '@vaadin/testing-helpers';
 import { visualDiff } from '@web/test-runner-visual-regression';
 import '@vaadin/aura/aura.css';
+import '../../not-animated-styles.css';
 import '../../../vaadin-crud.js';
 
 describe('crud', () => {
@@ -9,7 +10,7 @@ describe('crud', () => {
   beforeEach(async () => {
     div = document.createElement('div');
     div.style.height = '100%';
-    element = fixtureSync('<vaadin-crud></vaadin-crud>', div);
+    element = fixtureSync('<vaadin-crud style="height: calc(100vh - 16px)"></vaadin-crud>', div);
     element.items = [{ name: { first: 'John', last: 'Doe' } }, { name: { first: 'Jane', last: 'Doe' } }];
     await nextRender();
   });
@@ -26,5 +27,10 @@ describe('crud', () => {
   it('no-row-borders', async () => {
     element.setAttribute('theme', 'no-row-borders');
     await visualDiff(element, 'no-row-borders');
+  });
+
+  it('editor-position-default', async () => {
+    element.editedItem = {};
+    await visualDiff(div, 'editor-position-default');
   });
 });
