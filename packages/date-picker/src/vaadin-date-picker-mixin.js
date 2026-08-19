16070b168a22404b0906909ffe13694f7345eccc
Serhii Kulykov <iamkulykov@gmail.com>
2026-08-19 11:21:59 +0300

    fix: do not validate fullscreen date-picker on internal blur while opening (#12434)

--- packages/date-picker/src/vaadin-date-picker-mixin.js
+++ packages/date-picker/src/vaadin-date-picker-mixin.js
@@ -401,7 +401,10 @@ export const DatePickerMixin = (subclass) =>
       super._onFocus(event);
 
       if (this._noInput && !isKeyboardActive()) {
+        // Blur to hide the virtual keyboard, but do not validate.
+        this.__ignoreInternalBlur = true;
         event.target.blur();
+        this.__ignoreInternalBlur = false;
       }
     }
 
@@ -411,6 +414,10 @@ export const DatePickerMixin = (subclass) =>
      */
     _onBlur(event) {
       super._onBlur(event);
+
+      if (this.__ignoreInternalBlur) {
+        return;
+      }
 
       if (!this.opened) {
         this.__commitParsedOrFocusedDate();
