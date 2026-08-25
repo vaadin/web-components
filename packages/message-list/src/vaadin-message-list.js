287c1111b1b122ed6ff27651dac66fb8a9457adc
Jouni Koivuviita <jouni@vaadin.com>
2026-08-25 14:43:09 +0300

    feat: add bubble theme variant to message list

--- packages/message-list/src/vaadin-message-list.js
+++ packages/message-list/src/vaadin-message-list.js
@@ -93,6 +93,20 @@ class MessageList extends SlotStylesMixin(MessageListMixin(ElementMixin(Themable
       ${tag} :where(vaadin-markdown > :is(h1, h2, h3, h4, h5, h6, p, ul, ol):last-child) {
         margin-bottom: 0;
       }
+
+      ${tag}[theme~='bubble'] :where(vaadin-message[theme~='self'])::part(name),
+      ${tag}[theme~='bubble'][theme~='one-to-one'] :where(vaadin-message)::part(name) {
+        border: 0 !important;
+        clip: rect(1px, 1px, 1px, 1px) !important;
+        clip-path: inset(50%) !important;
+        height: 1px !important;
+        margin: -1px !important;
+        overflow: hidden !important;
+        padding: 0 !important;
+        position: absolute !important;
+        width: 1px !important;
+        white-space: nowrap !important;
+      }
       `,
     ];
   }
