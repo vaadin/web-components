615480b2bcbae85294b7f0147df7d44f3e1cdfb3
Serhii Kulykov <iamkulykov@gmail.com>
2026-08-27 11:49:22 +0300

    refactor: extract message-list base styles into separate file (#12518)

--- packages/message-list/src/vaadin-message-list.js
+++ packages/message-list/src/vaadin-message-list.js
@@ -4,12 +4,13 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import './vaadin-message.js';
-import { css, html, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';
 import { ElementMixin } from '@vaadin/component-base/src/element-mixin.js';
 import { PolylitMixin } from '@vaadin/component-base/src/polylit-mixin.js';
 import { SlotStylesMixin } from '@vaadin/component-base/src/slot-styles-mixin.js';
 import { ThemableMixin } from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
+import { messageListStyles } from './styles/vaadin-message-list-base-styles.js';
 import { MessageListMixin } from './vaadin-message-list-mixin.js';
 
 /**
@@ -64,36 +65,7 @@ class MessageList extends SlotStylesMixin(MessageListMixin(ElementMixin(Themable
   }
 
   static get styles() {
-    return css`
-      :host {
-        display: block;
-        overflow: auto;
-        box-sizing: border-box;
-        padding: var(--vaadin-message-list-padding, var(--vaadin-padding-xs) 0);
-        scroll-padding: var(--vaadin-message-list-padding, var(--vaadin-padding-xs) 0);
-        scroll-snap-type: y proximity;
-      }
-
-      :host([hidden]) {
-        display: none !important;
-      }
-
-      [part='list'] {
-        display: flex;
-        flex-direction: column;
-        box-sizing: border-box;
-        width: 100%;
-        min-height: 100%;
-        max-width: var(--vaadin-message-list-max-width, none);
-        margin-inline: auto;
-      }
-
-      [part='list']::after {
-        content: '';
-        display: block;
-        scroll-snap-align: var(--_vaadin-message-list-scroll-snap-align, none);
-      }
-    `;
+    return messageListStyles;
   }
 
   /** @protected */
