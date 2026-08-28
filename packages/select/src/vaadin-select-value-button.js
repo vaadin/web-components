076948c05bd4f660247ecbd6d8739cec0aa77504
Diego Cardoso <diego@vaadin.com>
2026-08-28 10:46:34 +0200

    fix: hide button container from accessibility tree (#12525)

--- packages/select/src/vaadin-select-value-button.js
+++ packages/select/src/vaadin-select-value-button.js
@@ -32,7 +32,7 @@ class SelectValueButton extends ButtonMixin(ThemableMixin(PolylitMixin(LumoInjec
   /** @protected */
   render() {
     return html`
-      <div class="vaadin-button-container">
+      <div class="vaadin-button-container" role="presentation">
         <span part="label">
           <slot></slot>
         </span>
