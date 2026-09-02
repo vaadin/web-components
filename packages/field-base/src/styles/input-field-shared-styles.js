6143d2d729601f09ee40bec68fe1d59d3a5d6c28
Sergey Vinogradov <mr.vursen@gmail.com>
2026-09-02 09:43:45 +0400

    wip

--- packages/field-base/src/styles/input-field-shared-styles.js
+++ packages/field-base/src/styles/input-field-shared-styles.js
@@ -4,6 +4,6 @@
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
 import { button } from './button-base-styles.js';
-import { field } from './field-base-styles.js';
+import { field, fieldLabelAside } from './field-base-styles.js';
 
-export const inputFieldShared = [field, button];
+export const inputFieldShared = [field, fieldLabelAside, button];
