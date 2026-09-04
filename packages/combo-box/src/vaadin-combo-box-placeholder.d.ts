3d0432923c05072907d39e9bedab1175e0581c33
Tomi Virkki <tomivirkki@users.noreply.github.com>
2026-09-04 15:50:36 +0300

    fix: add type declaration for combo box placeholder module (#12622)

---
+++ packages/combo-box/src/vaadin-combo-box-placeholder.d.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright (c) 2015 - 2026 Vaadin Ltd.
+ * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+ */
+
+/**
+ * Placeholder object class representing items being loaded.
+ *
+ * @private
+ */
+declare class ComboBoxPlaceholder {
+  // Nominal brand: keeps structurally similar values from being assignable
+  // eslint-disable-next-line @typescript-eslint/no-unused-private-class-members
+  private __comboBoxPlaceholder: true;
+
+  toString(): string;
+}
+
+export { ComboBoxPlaceholder };
