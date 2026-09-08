f5fc0b4e9d87c62d12250a64ab800db6b1de6616
Jouni Koivuviita <jouni@vaadin.com>
2026-09-08 13:46:33 +0300

    feat: add header, prefix and footer slots to message-input (CP: 25.3)

--- packages/message-input/test/dom/__snapshots__/message-input.test.snap.js
+++ packages/message-input/test/dom/__snapshots__/message-input.test.snap.js
@@ -1,7 +1,7 @@
 /* @web/test-runner snapshot v1 */
 export const snapshots = {};
 
-snapshots["vaadin-message-input default"] = 
+snapshots["vaadin-message-input host default"] = 
 `<vaadin-message-input>
   <vaadin-message-input-button
     aria-disabled="true"
@@ -40,9 +40,9 @@ snapshots["vaadin-message-input default"] =
   </vaadin-text-area>
 </vaadin-message-input>
 `;
-/* end snapshot vaadin-message-input default */
+/* end snapshot vaadin-message-input host default */
 
-snapshots["vaadin-message-input value"] = 
+snapshots["vaadin-message-input host value"] = 
 `<vaadin-message-input>
   <vaadin-message-input-button
     role="button"
@@ -80,9 +80,9 @@ snapshots["vaadin-message-input value"] =
   </vaadin-text-area>
 </vaadin-message-input>
 `;
-/* end snapshot vaadin-message-input value */
+/* end snapshot vaadin-message-input host value */
 
-snapshots["vaadin-message-input disabled"] = 
+snapshots["vaadin-message-input host disabled"] = 
 `<vaadin-message-input disabled="">
   <vaadin-message-input-button
     aria-disabled="true"
@@ -125,5 +125,75 @@ snapshots["vaadin-message-input disabled"] =
   </vaadin-text-area>
 </vaadin-message-input>
 `;
-/* end snapshot vaadin-message-input disabled */
+/* end snapshot vaadin-message-input host disabled */
+
+snapshots["vaadin-message-input host with content slots default"] = 
+`<vaadin-message-input
+  has-footer=""
+  has-header=""
+  has-prefix=""
+>
+  <div slot="header">
+    Header
+  </div>
+  <span slot="prefix">
+    Prefix
+  </span>
+  <div slot="footer">
+    Footer
+  </div>
+  <vaadin-message-input-button
+    aria-disabled="true"
+    disabled=""
+    role="button"
+    slot="button"
+    tabindex="-1"
+  >
+    Send
+  </vaadin-message-input-button>
+  <vaadin-text-area
+    placeholder="Message"
+    slot="textarea"
+  >
+    <label
+      for="textarea-vaadin-text-area-3"
+      id="label-vaadin-text-area-0"
+      slot="label"
+    >
+    </label>
+    <div
+      hidden=""
+      id="error-message-vaadin-text-area-2"
+      slot="error-message"
+    >
+    </div>
+    <textarea
+      aria-label="Message"
+      enterkeyhint="send"
+      id="textarea-vaadin-text-area-3"
+      placeholder="Message"
+      rows="1"
+      slot="textarea"
+    >
+    </textarea>
+  </vaadin-text-area>
+</vaadin-message-input>
+`;
+/* end snapshot vaadin-message-input host with content slots default */
+
+snapshots["vaadin-message-input shadow default"] = 
+`<slot name="header">
+</slot>
+<slot name="prefix">
+</slot>
+<slot name="textarea">
+</slot>
+<slot name="button">
+</slot>
+<slot name="footer">
+</slot>
+<slot name="tooltip">
+</slot>
+`;
+/* end snapshot vaadin-message-input shadow default */
 
