4b377c89596041d8d1e8fc9c67698cbd2fff5fc9
Diego Cardoso <diego@vaadin.com>
2026-09-15 13:47:24 +0200

    fix: focus dialog when a drag or resize gesture is a click (CP: 25.3)
    Dragging and resizing call preventDefault() on mousedown, which cancels
    the browser's native focus change. A modeless dialog then stayed unfocused
    and ignored Esc when the click landed on its header or a resize handle.
    
    Focusing happens at the end of the gesture and only when the pointer did
    not move, so dragging the dialog out of the way keeps focus on the field
    the user is editing.
    
    Fixes: https://github.com/vaadin/flow-components/issues/9815

--- packages/dialog/src/vaadin-dialog-resizable-mixin.js
+++ packages/dialog/src/vaadin-dialog-resizable-mixin.js
@@ -3,7 +3,8 @@
  * Copyright (c) 2017 - 2026 Vaadin Ltd.
  * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
  */
-import { eventInWindow, getMouseOrFirstTouchEvent } from './vaadin-dialog-utils.js';
+import { isKeyboardActive } from '@vaadin/a11y-base/src/focus-utils.js';
+import { ClickTracker, eventInWindow, getMouseOrFirstTouchEvent } from './vaadin-dialog-utils.js';
 export const DialogResizableMixin = (superClass) =>
   class VaadinDialogResizableMixin extends superClass {
     static get properties() {
@@ -24,6 +25,7 @@ export const DialogResizableMixin = (superClass) =>
       super.ready();
       this._originalBounds = {};
       this._originalMouseCoords = {};
+      this.__resizeTracker = new ClickTracker();
       this._resizeListeners = { start: {}, resize: {}, stop: {} };
       this._addResizeListeners();
     }
@@ -35,7 +37,7 @@ export const DialogResizableMixin = (superClass) =>
         const resizer = document.createElement('div');
         this._resizeListeners.start[direction] = (e) => this._startResize(e, direction);
         this._resizeListeners.resize[direction] = (e) => this._resize(e, direction);
-        this._resizeListeners.stop[direction] = () => this._stopResize(direction);
+        this._resizeListeners.stop[direction] = (e) => this._stopResize(e, direction);
         if (direction.length === 1) {
           resizer.classList.add('edge');
         }
@@ -60,6 +62,8 @@ export const DialogResizableMixin = (superClass) =>
 
       if (e.button === 0 || e.touches) {
         e.preventDefault();
+        // `preventDefault()` above cancels the native focus change, restored in `_stopResize()`
+        this.__resizeTracker.start(e);
 
         this._originalBounds = this.$.overlay.getBounds();
         const event = getMouseOrFirstTouchEvent(e);
@@ -127,10 +131,16 @@ export const DialogResizableMixin = (superClass) =>
     }
 
     /**
+     * @param {!MouseEvent | !TouchEvent} e
      * @param {!DialogResizableDirection} direction
      * @protected
      */
-    _stopResize(direction) {
+    _stopResize(e, direction) {
+      // Focus on click, not on resize, so that resizing the dialog doesn't interrupt editing
+      if (this.__resizeTracker.isClick(e) && !this.$.overlay.containsFocus()) {
+        this.focus({ preventScroll: true, focusVisible: isKeyboardActive() });
+      }
+
       window.removeEventListener('mousemove', this._resizeListeners.resize[direction]);
       window.removeEventListener('touchmove', this._resizeListeners.resize[direction]);
       window.removeEventListener('mouseup', this._resizeListeners.stop[direction]);
