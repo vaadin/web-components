961c8d65d8c0c813aadd2a3ab1be8783ea39955a
Serhii Kulykov <iamkulykov@gmail.com>
2026-09-21 15:50:48 +0200

    refactor: only recompute select all button label when it can change (#12789)

--- packages/multi-select-combo-box/src/vaadin-multi-select-combo-box-select-all-mixin.js
+++ packages/multi-select-combo-box/src/vaadin-multi-select-combo-box-select-all-mixin.js
@@ -59,13 +59,13 @@ export const MultiSelectComboBoxSelectAllMixin = (superClass) =>
     updated(props) {
       super.updated(props);
 
-      if (
-        props.has('_highlightState') &&
-        props.get('_highlightState')?.type === 'item' &&
-        this._isSelectAllHighlighted
-      ) {
-        // When highlighting the button, revert input value to the filter
-        this._revertInputValue();
+      if (props.has('_highlightState')) {
+        this.#button.focused = this._isSelectAllHighlighted;
+
+        if (props.get('_highlightState')?.type === 'item' && this._isSelectAllHighlighted) {
+          // When highlighting the button, revert input value to the filter
+          this._revertInputValue();
+        }
       }
 
       const buttonProps = [
@@ -76,7 +76,6 @@ export const MultiSelectComboBoxSelectAllMixin = (superClass) =>
         'selectedItems',
         'itemIdPath',
         'filter',
-        '_highlightState',
         '__effectiveI18n',
       ];
       if (buttonProps.some((prop) => props.has(prop))) {
@@ -127,7 +126,6 @@ export const MultiSelectComboBoxSelectAllMixin = (superClass) =>
 
       const button = this.#button;
       button.label = this.#getButtonLabel();
-      button.focused = this._isSelectAllHighlighted;
 
       if (button.parentNode !== this) {
         this.appendChild(button);
