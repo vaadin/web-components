83e8072f841c5d6883a6851aca20d9ce1519d767
Serhii Kulykov <iamkulykov@gmail.com>
2026-09-24 15:33:11 +0300

    test: cover menu-bar overflow state attributes in DOM snapshots (#12832)

--- packages/menu-bar/test/dom/menu-bar.test.js
+++ packages/menu-bar/test/dom/menu-bar.test.js
@@ -11,7 +11,20 @@ describe('menu-bar', () => {
     ignoreAttributes: ['style'],
   };
 
+  // Inline styles on the host come from the test setup and from layout measurements
+  const HOST_CONFIG = {
+    ignoreAttributes: [{ tags: ['vaadin-menu-bar'], attributes: ['style'] }],
+  };
+
   beforeEach(async () => {
+    // Fixed button width so that the collapsed set does not depend on the font
+    fixtureSync(`
+      <style>
+        vaadin-menu-bar-button {
+          width: 60px;
+        }
+      </style>
+    `);
     menu = fixtureSync('<vaadin-menu-bar></vaadin-menu-bar>');
     menu.items = [
       { text: 'Home', className: 'home' },
@@ -34,23 +47,39 @@ describe('menu-bar', () => {
     await nextResize(menu);
   });
 
-  it('basic', async () => {
-    await expect(menu).dom.to.equalSnapshot();
+  describe('host', () => {
+    it('default', async () => {
+      await expect(menu).dom.to.equalSnapshot();
+    });
+
+    it('opened', async () => {
+      menu._buttons[1].click();
+      await nextRender();
+      await expect(menu).dom.to.equalSnapshot();
+    });
+
+    it('overlay', async () => {
+      menu._buttons[1].click();
+      await nextRender();
+      await expect(menu._subMenu._overlayElement).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
+    });
+
+    it('has-overflow', async () => {
+      menu.style.width = '180px';
+      await nextResize(menu);
+      await expect(menu).dom.to.equalSnapshot(HOST_CONFIG);
+    });
+
+    it('has-single-button', async () => {
+      menu.style.width = '100px';
+      await nextResize(menu);
+      await expect(menu).dom.to.equalSnapshot(HOST_CONFIG);
+    });
   });
 
-  it('opened', async () => {
-    menu._buttons[1].click();
-    await nextRender();
-    await expect(menu).dom.to.equalSnapshot();
-  });
-
-  it('overlay', async () => {
-    menu._buttons[1].click();
-    await nextRender();
-    await expect(menu._subMenu._overlayElement).dom.to.equalSnapshot(SNAPSHOT_CONFIG);
-  });
-
-  it('shadow', async () => {
-    await expect(menu).shadowDom.to.equalSnapshot();
+  describe('shadow', () => {
+    it('default', async () => {
+      await expect(menu).shadowDom.to.equalSnapshot();
+    });
   });
 });
